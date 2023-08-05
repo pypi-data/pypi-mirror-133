dd0227bb095841ccbd031a784ce7b87853ce139a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:53 +0000

    Add package tuhls-core/1.1.8/tuhls_core-1.1.8-py3-none-any.whl

---
+++ packages/tuhls-core/tuhls_core-1.1.8-py3-none-any.whl/tuhls/core/manage.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+def build_env():
+    print(  # noqa
+        os.environ["DJANGO_CONFIGURATION"], os.environ["DJANGO_SETTINGS_MODULE"]
+    )
+    import configurations
+
+    configurations.setup()
+
+
+def run():
+    from configurations.management import execute_from_command_line
+
+    build_env()
+    execute_from_command_line(sys.argv)
+
+
+def run_example():
+    from django.core.management import execute_from_command_line
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+    execute_from_command_line(sys.argv)
+
+
+def wsgi():
+    from configurations.wsgi import get_wsgi_application
+
+    build_env()
+    return get_wsgi_application()
