f7a056ac8bf090d94de02e908e39c459523bf575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package proxy.py/2.4.0rc5/proxy.py-2.4.0rc5-py3-none-any.whl

---
+++ packages/proxy.py/proxy.py-2.4.0rc5-py3-none-any.whl/proxy/dashboard/dashboard.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+import os
+import json
+import logging
+from typing import List, Tuple, Any, Dict
+
+from .plugin import ProxyDashboardWebsocketPlugin
+
+from ..common.utils import bytes_
+
+from ..http.responses import permanentRedirectResponse
+from ..http.parser import HttpParser
+from ..http.websocket import WebsocketFrame
+from ..http.server import HttpWebServerPlugin, HttpWebServerBasePlugin, httpProtocolTypes
+
+logger = logging.getLogger(__name__)
+
+
+class ProxyDashboard(HttpWebServerBasePlugin):
+    """Proxy Dashboard."""
+
+    # Redirects to /dashboard/
+    REDIRECT_ROUTES = [
+        (httpProtocolTypes.HTTP, r'/dashboard$'),
+        (httpProtocolTypes.HTTPS, r'/dashboard$'),
+        (httpProtocolTypes.HTTP, r'/dashboard/proxy.html$'),
+        (httpProtocolTypes.HTTPS, r'/dashboard/proxy.html$'),
+    ]
+
+    # Index html route
+    INDEX_ROUTES = [
+        (httpProtocolTypes.HTTP, r'/dashboard/$'),
+        (httpProtocolTypes.HTTPS, r'/dashboard/$'),
+    ]
+
+    # Handles WebsocketAPI requests for dashboard
+    WS_ROUTES = [
+        (httpProtocolTypes.WEBSOCKET, r'/dashboard$'),
+    ]
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.plugins: Dict[str, ProxyDashboardWebsocketPlugin] = {}
+        if b'ProxyDashboardWebsocketPlugin' in self.flags.plugins:
+            for klass in self.flags.plugins[b'ProxyDashboardWebsocketPlugin']:
+                p = klass(self.flags, self.client, self.event_queue)
+                for method in p.methods():
+                    self.plugins[method] = p
+
+    def routes(self) -> List[Tuple[int, str]]:
+        return ProxyDashboard.REDIRECT_ROUTES + \
+            ProxyDashboard.INDEX_ROUTES + \
+            ProxyDashboard.WS_ROUTES
+
+    def handle_request(self, request: HttpParser) -> None:
+        if request.path == b'/dashboard/':
+            self.client.queue(
+                HttpWebServerPlugin.read_and_build_static_file_response(
+                    os.path.join(
+                        self.flags.static_server_dir,
+                        'dashboard', 'proxy.html',
+                    ),
+                ),
+            )
+        elif request.path in (
+                b'/dashboard',
+                b'/dashboard/proxy.html',
+        ):
+            self.client.queue(permanentRedirectResponse(b'/dashboard/'))
+
+    def on_websocket_open(self) -> None:
+        logger.info('app ws opened')
+
+    def on_websocket_message(self, frame: WebsocketFrame) -> None:
+        try:
+            assert frame.data
+            message = json.loads(frame.data)
+        except UnicodeDecodeError:
+            logger.error(frame.data)
+            logger.info(frame.opcode)
+            return
+
+        method = message['method']
+        if method == 'ping':
+            self.reply({'id': message['id'], 'response': 'pong'})
+        elif method in self.plugins:
+            self.plugins[method].handle_message(message)
+        else:
+            logger.info(frame.data)
+            logger.info(frame.opcode)
+            self.reply({'id': message['id'], 'response': 'not_implemented'})
+
+    def on_client_connection_close(self) -> None:
+        logger.info('app ws closed')
+        # TODO(abhinavsingh): unsubscribe
+
+    def reply(self, data: Dict[str, Any]) -> None:
+        self.client.queue(
+            memoryview(
+                WebsocketFrame.text(
+                    bytes_(
+                        json.dumps(data),
+                    ),
+                ),
+            ),
+        )
---
+++ packages/proxy.py/proxy.py-2.4.0rc5-py3-none-any.whl/proxy/http/plugin.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+import socket
+import argparse
+
+from abc import ABC, abstractmethod
+from typing import Tuple, List, Union, Optional, TYPE_CHECKING
+
+from ..common.types import Readables, Writables
+from ..core.event import EventQueue
+from ..core.connection import TcpClientConnection
+
+from .parser import HttpParser
+
+if TYPE_CHECKING:
+    from ..core.connection import UpstreamConnectionPool
+
+
+class HttpProtocolHandlerPlugin(ABC):
+    """Base HttpProtocolHandler Plugin class.
+
+    NOTE: This is an internal plugin and in most cases only useful for core contributors.
+    If you are looking for proxy server plugins see `<proxy.HttpProxyBasePlugin>`.
+
+    Implements various lifecycle events for an accepted client connection.
+    Following events are of interest:
+
+    1. Client Connection Accepted
+       A new plugin instance is created per accepted client connection.
+       Add your logic within __init__ constructor for any per connection setup.
+    2. Client Request Chunk Received
+       on_client_data is called for every chunk of data sent by the client.
+    3. Client Request Complete
+       on_request_complete is called once client request has completed.
+    4. Server Response Chunk Received
+       on_response_chunk is called for every chunk received from the server.
+    5. Client Connection Closed
+       Add your logic within `on_client_connection_close` for any per connection tear-down.
+    """
+
+    def __init__(
+            self,
+            uid: str,
+            flags: argparse.Namespace,
+            client: TcpClientConnection,
+            request: HttpParser,
+            event_queue: Optional[EventQueue],
+            upstream_conn_pool: Optional['UpstreamConnectionPool'] = None,
+    ):
+        self.uid: str = uid
+        self.flags: argparse.Namespace = flags
+        self.client: TcpClientConnection = client
+        self.request: HttpParser = request
+        self.event_queue = event_queue
+        self.upstream_conn_pool = upstream_conn_pool
+        super().__init__()
+
+    @staticmethod
+    @abstractmethod
+    def protocols() -> List[int]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_descriptors(self) -> Tuple[List[int], List[int]]:
+        """Implementations must return a list of descriptions that they wish to
+        read from and write into."""
+        return [], []  # pragma: no cover
+
+    @abstractmethod
+    async def write_to_descriptors(self, w: Writables) -> bool:
+        """Implementations must now write/flush data over the socket.
+
+        Note that buffer management is in-build into the connection classes.
+        Hence implementations MUST call
+        :meth:`~proxy.core.connection.TcpConnection.flush` here, to send
+        any buffered data over the socket.
+        """
+        return False  # pragma: no cover
+
+    @abstractmethod
+    async def read_from_descriptors(self, r: Readables) -> bool:
+        """Implementations must now read data over the socket."""
+        return False  # pragma: no cover
+
+    @abstractmethod
+    def on_client_data(self, raw: memoryview) -> Optional[memoryview]:
+        """Called only after original request has been completely received."""
+        return raw  # pragma: no cover
+
+    @abstractmethod
+    def on_request_complete(self) -> Union[socket.socket, bool]:
+        """Called right after client request parser has reached COMPLETE state."""
+        return False  # pragma: no cover
+
+    @abstractmethod
+    def on_response_chunk(self, chunk: List[memoryview]) -> List[memoryview]:
+        """Handle data chunks as received from the server.
+
+        Return optionally modified chunk to return back to client."""
+        return chunk  # pragma: no cover
+
+    @abstractmethod
+    def on_client_connection_close(self) -> None:
+        """Client connection shutdown has been received, flush has been called,
+        perform any cleanup work here.
+        """
+        pass  # pragma: no cover
