a0f31a5a0c3654398c7fcc5b05c13f7508329dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:50 +0000

    Add package round-using-error/1.0.0rc0/round_using_error-1.0.0rc0.tar.gz

---
+++ packages/round-using-error/round_using_error-1.0.0rc0.tar.gz/round_using_error-1.0.0rc0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="round_using_error",
+    version="1.0.0rc0",
+    description="Output numbers +/- error with appropriate rounding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gutow/round_using_error",
+    author="Jonathan Gutow",
+    author_email="gutow@uwosh.edu",
+    license="GPL-3.0+",
+    packages=setuptools.find_packages(),
+    data_files=[
+        ('image', ['rndwitherr_Jupyter_display.png'])
+    ],
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ]
+)
