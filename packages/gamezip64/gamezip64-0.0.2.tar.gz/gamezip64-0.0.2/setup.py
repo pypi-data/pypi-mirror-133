137b897900048bf60dde6423b44c438c23950708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package gamezip64/0.0.2/gamezip64-0.0.2.tar.gz

---
+++ packages/gamezip64/gamezip64-0.0.2.tar.gz/gamezip64-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        version='0.0.2',
+        name='gamezip64',
+        author='Adam Jenca',
+        author_email='jenca.a@gjh.sk',
+        description='microbit Kitronik Game ZIP 64 plugin',
+        py_modules=['gamezip64'],
+        entry_points={'console_scripts':['gz64=gamezip64:main']},
+        )
