f250af2945a29de43c32ca41ceb623e11d84614b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package cashocs/1.5.1/cashocs-1.5.1-py3-none-any.whl

---
+++ packages/cashocs/cashocs-1.5.1-py3-none-any.whl/cashocs/_optimal_control/methods/ncg.py
@@ -0,0 +1,279 @@
+# Copyright (C) 2020-2022 Sebastian Blauth
+#
+# This file is part of CASHOCS.
+#
+# CASHOCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CASHOCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CASHOCS.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Nonlinear conjugate gradient methods.
+
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List
+
+import fenics
+import numpy as np
+
+from ..._optimal_control import ArmijoLineSearch, ControlOptimizationAlgorithm
+
+
+if TYPE_CHECKING:
+    from ..optimal_control_problem import OptimalControlProblem
+
+
+class NCG(ControlOptimizationAlgorithm):
+    """Nonlinear conjugate gradient method."""
+
+    def __init__(self, optimization_problem: OptimalControlProblem):
+        """
+        Parameters
+        ----------
+        optimization_problem : OptimalControlProblem
+            The OptimalControlProblem object
+        """
+
+        super().__init__(optimization_problem)
+
+        self.line_search = ArmijoLineSearch(self)
+
+        self.gradient_prev = [
+            fenics.Function(V) for V in optimization_problem.control_spaces
+        ]
+        self.differences = [
+            fenics.Function(V) for V in optimization_problem.control_spaces
+        ]
+        self.temp_HZ = [fenics.Function(V) for V in optimization_problem.control_spaces]
+        self.control_constraints = optimization_problem.control_constraints
+
+        self.cg_method = self.config.get("AlgoCG", "cg_method", fallback="FR")
+        self.cg_periodic_restart = self.config.getboolean(
+            "AlgoCG", "cg_periodic_restart", fallback=False
+        )
+        self.cg_periodic_its = self.config.getint(
+            "AlgoCG", "cg_periodic_its", fallback=10
+        )
+        self.cg_relative_restart = self.config.getboolean(
+            "AlgoCG", "cg_relative_restart", fallback=False
+        )
+        self.cg_restart_tol = self.config.getfloat(
+            "AlgoCG", "cg_restart_tol", fallback=0.25
+        )
+
+    def project_direction(self, a: List[fenics.Function]) -> None:
+        """Restricts the search direction to the inactive set.
+
+        Parameters
+        ----------
+        a : list[fenics.Function]
+            A function that shall be projected / restricted (will be overwritten)
+
+        Returns
+        -------
+        None
+        """
+
+        for j in range(self.form_handler.control_dim):
+            idx = np.asarray(
+                np.logical_or(
+                    np.logical_and(
+                        self.controls[j].vector()[:]
+                        <= self.control_constraints[j][0].vector()[:],
+                        a[j].vector()[:] < 0.0,
+                    ),
+                    np.logical_and(
+                        self.controls[j].vector()[:]
+                        >= self.control_constraints[j][1].vector()[:],
+                        a[j].vector()[:] > 0.0,
+                    ),
+                )
+            ).nonzero()[0]
+
+            a[j].vector()[idx] = 0.0
+
+    def run(self) -> None:
+        """Performs the optimization via the nonlinear cg method
+
+        Returns
+        -------
+        None
+        """
+
+        self.iteration = 0
+        self.memory = 0
+        self.relative_norm = 1.0
+        self.state_problem.has_solution = False
+        for i in range(len(self.gradient)):
+            self.gradient[i].vector().vec().set(1.0)
+
+        while True:
+
+            for i in range(self.form_handler.control_dim):
+                self.gradient_prev[i].vector().vec().aypx(
+                    0.0, self.gradient[i].vector().vec()
+                )
+
+            self.adjoint_problem.has_solution = False
+            self.gradient_problem.has_solution = False
+            self.gradient_problem.solve()
+
+            self.gradient_norm = np.sqrt(self._stationary_measure_squared())
+            if self.iteration > 0:
+                if self.cg_method == "FR":
+                    self.beta_numerator = self.form_handler.scalar_product(
+                        self.gradient, self.gradient
+                    )
+                    self.beta_denominator = self.form_handler.scalar_product(
+                        self.gradient_prev, self.gradient_prev
+                    )
+                    self.beta = self.beta_numerator / self.beta_denominator
+
+                elif self.cg_method == "PR":
+                    for i in range(len(self.gradient)):
+                        self.differences[i].vector().vec().aypx(
+                            0.0,
+                            self.gradient[i].vector().vec()
+                            - self.gradient_prev[i].vector().vec(),
+                        )
+
+                    self.beta_numerator = self.form_handler.scalar_product(
+                        self.gradient, self.differences
+                    )
+                    self.beta_denominator = self.form_handler.scalar_product(
+                        self.gradient_prev, self.gradient_prev
+                    )
+                    self.beta = self.beta_numerator / self.beta_denominator
+
+                elif self.cg_method == "HS":
+                    for i in range(len(self.gradient)):
+                        self.differences[i].vector().vec().aypx(
+                            0.0,
+                            self.gradient[i].vector().vec()
+                            - self.gradient_prev[i].vector().vec(),
+                        )
+
+                    self.beta_numerator = self.form_handler.scalar_product(
+                        self.gradient, self.differences
+                    )
+                    self.beta_denominator = self.form_handler.scalar_product(
+                        self.differences, self.search_direction
+                    )
+                    self.beta = self.beta_numerator / self.beta_denominator
+
+                elif self.cg_method == "DY":
+                    for i in range(len(self.gradient)):
+                        self.differences[i].vector().vec().aypx(
+                            0.0,
+                            self.gradient[i].vector().vec()
+                            - self.gradient_prev[i].vector().vec(),
+                        )
+
+                    self.beta_numerator = self.form_handler.scalar_product(
+                        self.gradient, self.gradient
+                    )
+                    self.beta_denominator = self.form_handler.scalar_product(
+                        self.search_direction, self.differences
+                    )
+                    self.beta = self.beta_numerator / self.beta_denominator
+
+                elif self.cg_method == "HZ":
+                    for i in range(len(self.gradient)):
+                        self.differences[i].vector().vec().aypx(
+                            0.0,
+                            self.gradient[i].vector().vec()
+                            - self.gradient_prev[i].vector().vec(),
+                        )
+
+                    dy = self.form_handler.scalar_product(
+                        self.search_direction, self.differences
+                    )
+                    y2 = self.form_handler.scalar_product(
+                        self.differences, self.differences
+                    )
+
+                    for i in range(len(self.gradient)):
+                        self.differences[i].vector().vec().axpy(
+                            -2 * y2 / dy, self.search_direction[i].vector().vec()
+                        )
+
+                    self.beta = (
+                        self.form_handler.scalar_product(
+                            self.differences, self.gradient
+                        )
+                        / dy
+                    )
+
+            if self.iteration == 0:
+                self.gradient_norm_initial = self.gradient_norm
+                if self.gradient_norm_initial == 0:
+                    self.objective_value = self.cost_functional.evaluate()
+                    self.converged = True
+                    break
+                self.beta = 0.0
+
+            self.relative_norm = self.gradient_norm / self.gradient_norm_initial
+            if self.gradient_norm <= self.atol + self.rtol * self.gradient_norm_initial:
+                if self.iteration == 0:
+                    self.objective_value = self.cost_functional.evaluate()
+                self.converged = True
+                break
+
+            for i in range(self.form_handler.control_dim):
+                self.search_direction[i].vector().vec().aypx(
+                    self.beta, -self.gradient[i].vector().vec()
+                )
+
+            if self.cg_periodic_restart:
+                if self.memory < self.cg_periodic_its:
+                    self.memory += 1
+                else:
+                    for i in range(len(self.gradient)):
+                        self.search_direction[i].vector().vec().aypx(
+                            0.0, -self.gradient[i].vector().vec()
+                        )
+                    self.memory = 0
+            if self.cg_relative_restart:
+                if (
+                    abs(
+                        self.form_handler.scalar_product(
+                            self.gradient, self.gradient_prev
+                        )
+                    )
+                    / pow(self.gradient_norm, 2)
+                    >= self.cg_restart_tol
+                ):
+                    for i in range(len(self.gradient)):
+                        self.search_direction[i].vector().vec().aypx(
+                            0.0, -self.gradient[i].vector().vec()
+                        )
+
+            self.project_direction(self.search_direction)
+            self.directional_derivative = self.form_handler.scalar_product(
+                self.gradient, self.search_direction
+            )
+
+            if self.directional_derivative >= 0:
+                for i in range(len(self.gradient)):
+                    self.search_direction[i].vector().vec().aypx(
+                        0.0, -self.gradient[i].vector().vec()
+                    )
+
+            self.objective_value = self.cost_functional.evaluate()
+            self.output()
+
+            self.line_search.search(self.search_direction, self.has_curvature_info)
+
+            self.iteration += 1
+            if self.nonconvergence():
+                break
---
+++ packages/cashocs/cashocs-1.5.1-py3-none-any.whl/cashocs/_pde_problems/adjoint_problem.py
@@ -0,0 +1,188 @@
+# Copyright (C) 2020-2022 Sebastian Blauth
+#
+# This file is part of CASHOCS.
+#
+# CASHOCS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CASHOCS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CASHOCS.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Abstract implementation of an adjoint problem.
+
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, List
+
+import fenics
+import numpy as np
+from petsc4py import PETSc
+
+from .._exceptions import NotConvergedError
+from .._interfaces import PDEProblem
+from ..utils import _assemble_petsc_system, _setup_petsc_options, _solve_linear_problem
+
+
+if TYPE_CHECKING:
+    from .._forms import FormHandler
+    from .state_problem import StateProblem
+
+
+class AdjointProblem(PDEProblem):
+    """The adjoint problem.
+
+    This class implements the adjoint problem as well as its solver.
+    """
+
+    def __init__(
+        self,
+        form_handler: FormHandler,
+        state_problem: StateProblem,
+        temp_dict: Dict = None,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        form_handler : FormHandler
+            The FormHandler object for the optimization problem.
+        state_problem : StateProblem
+            The StateProblem object used to get the point where we linearize the problem.
+        temp_dict : dict
+            A dictionary used for reinitializations when remeshing is performed.
+        """
+
+        super().__init__(form_handler)
+
+        self.state_problem = state_problem
+        self.temp_dict = temp_dict
+
+        self.adjoints = self.form_handler.adjoints
+        self.bcs_list_ad = self.form_handler.bcs_list_ad
+
+        self.rtol = self.config.getfloat("StateSystem", "picard_rtol", fallback=1e-10)
+        self.atol = self.config.getfloat("StateSystem", "picard_atol", fallback=1e-12)
+        self.maxiter = self.config.getint("StateSystem", "picard_iter", fallback=50)
+        self.picard_verbose = self.config.getboolean(
+            "StateSystem", "picard_verbose", fallback=False
+        )
+
+        self.ksps = [PETSc.KSP().create() for i in range(self.form_handler.state_dim)]
+        _setup_petsc_options(self.ksps, self.form_handler.adjoint_ksp_options)
+
+        self.A_tensors = [
+            fenics.PETScMatrix() for i in range(self.form_handler.state_dim)
+        ]
+        self.b_tensors = [
+            fenics.PETScVector() for i in range(self.form_handler.state_dim)
+        ]
+
+        self.res_j_tensors = [
+            fenics.PETScVector() for i in range(self.form_handler.state_dim)
+        ]
+
+        try:
+            self.number_of_solves = self.temp_dict["output_dict"].get(
+                "adjoint_solves", 0
+            )
+        except TypeError:
+            self.number_of_solves = 0
+
+    def solve(self) -> List[fenics.Function]:
+        """Solves the adjoint system.
+
+        Returns
+        -------
+        list[fenics.Function]
+            The list of adjoint variables.
+        """
+
+        self.state_problem.solve()
+
+        if not self.has_solution:
+            if (
+                not self.form_handler.state_is_picard
+                or self.form_handler.state_dim == 1
+            ):
+                for i in range(self.form_handler.state_dim):
+                    _assemble_petsc_system(
+                        self.form_handler.adjoint_eq_lhs[-1 - i],
+                        self.form_handler.adjoint_eq_rhs[-1 - i],
+                        self.bcs_list_ad[-1 - i],
+                        A_tensor=self.A_tensors[-1 - i],
+                        b_tensor=self.b_tensors[-1 - i],
+                    )
+                    _solve_linear_problem(
+                        self.ksps[-1 - i],
+                        self.A_tensors[-1 - i].mat(),
+                        self.b_tensors[-1 - i].vec(),
+                        self.adjoints[-1 - i].vector().vec(),
+                        self.form_handler.adjoint_ksp_options[-1 - i],
+                    )
+                    self.adjoints[-1 - i].vector().apply("")
+
+            else:
+                for i in range(self.maxiter + 1):
+                    res = 0.0
+                    for j in range(self.form_handler.state_dim):
+                        fenics.assemble(
+                            self.form_handler.adjoint_picard_forms[j],
+                            tensor=self.res_j_tensors[j],
+                        )
+                        [
+                            bc.apply(self.res_j_tensors[j])
+                            for bc in self.form_handler.bcs_list_ad[j]
+                        ]
+                        res += pow(self.res_j_tensors[j].norm("l2"), 2)
+
+                    if res == 0:
+                        break
+
+                    res = np.sqrt(res)
+
+                    if i == 0:
+                        res_0 = res
+
+                    if self.picard_verbose:
+                        print(
+                            f"Iteration {i:d}: ||res|| (abs) = {res:.3e}   ||res|| (rel) = {res/res_0:.3e}"
+                        )
+
+                    if res / res_0 < self.rtol or res < self.atol:
+                        break
+
+                    if i == self.maxiter:
+                        raise NotConvergedError(
+                            "Picard iteration for the adjoint system"
+                        )
+
+                    for j in range(self.form_handler.state_dim):
+                        _assemble_petsc_system(
+                            self.form_handler.adjoint_eq_lhs[-1 - j],
+                            self.form_handler.adjoint_eq_rhs[-1 - j],
+                            self.bcs_list_ad[-1 - j],
+                            A_tensor=self.A_tensors[-1 - j],
+                            b_tensor=self.b_tensors[-1 - j],
+                        )
+                        _solve_linear_problem(
+                            self.ksps[-1 - j],
+                            self.A_tensors[-1 - j].mat(),
+                            self.b_tensors[-1 - j].vec(),
+                            self.adjoints[-1 - j].vector().vec(),
+                            self.form_handler.adjoint_ksp_options[-1 - j],
+                        )
+                        self.adjoints[-1 - j].vector().apply("")
+
+            if self.picard_verbose and self.form_handler.state_is_picard:
+                print("")
+            self.has_solution = True
+            self.number_of_solves += 1
+
+        return self.adjoints
