80bbd45cfc923b4bc9a15031b8508e6b1412ffda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:15 +0000

    Add package crawlMpGui/0.0.3/crawlMpGui-0.0.3.tar.gz

---
+++ packages/crawlMpGui/crawlMpGui-0.0.3.tar.gz/crawlMpGui-0.0.3/README.md
@@ -0,0 +1,13 @@
+# crawlMp-gui
+
+![GitHub](https://img.shields.io/github/license/domarm-comat/crawlMpGui)
+
+Graphical interface for crawlMp.  
+Results are paged into page_sized chunks.  
+User can sort hits by column and apply multiple filter based on column type.  
+
+# Icons attribution
+
+Icon made by [Roundicons](https://www.flaticon.com/authors/roundicons "Roundicons") from [www.flaticon.com](www.flaticon.com)  
+Icon made by [Smauj](https://www.flaticon.com/authors/Smauj "Smauj") from [www.flaticon.com](www.flaticon.com)  
+Icon made by [deemakdaksina](https://www.flaticon.com/authors/deemakdaksina "deemakdaksina") from [www.flaticon.com](www.flaticon.com)  
---
+++ packages/crawlMpGui/crawlMpGui-0.0.3.tar.gz/crawlMpGui-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crawlMpGui",
+    version="0.0.3",
+    license='MIT',
+    author="Martin Domaracký",
+    author_email="domarm@comat.sk",
+    description="GUI for CrawlMp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/domarm-comat/crawlMp",
+    packages=setuptools.find_packages(),
+    #package_data={'crawlMp.tests': ['fs_files.txt', 'fs_win_files.txt']},
+    scripts=['crawlMpGui/scripts/search_fs_mp_gui'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+    ],
+    install_requires=[
+        "crawlMp>=0.3.7",
+        "PyQt6>=6.2.2"
+    ],
+    extras_requires={
+    },
+    python_requires='>=3.6',
+)
