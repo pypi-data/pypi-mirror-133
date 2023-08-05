ac263afc306e96277af6e3a05a1bdc54963ac586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:33 +0000

    Add package calcar/0.1/calcar-0.1-py3-none-any.whl

---
+++ packages/calcar/calcar-0.1-py3-none-any.whl/calctp/__init__.py
@@ -0,0 +1,20 @@
+__all__ = ['']
+
+
+def add_numbers(num1, num2):
+    print(num1 + num2) 
+
+def substract_numbers(num1, num2):
+    print(num1 - num2)
+
+def multiply_numbers(num1, num2):
+    print(num1 * num2)
+
+def divide_numbers(num1, num2):
+    print(num1 / num2)
+
+def print_number(num="Error: No number was given"):
+    print(num)
+
+def average_numbers(num1, num2):
+    print( (num1 + num2) / 2)
