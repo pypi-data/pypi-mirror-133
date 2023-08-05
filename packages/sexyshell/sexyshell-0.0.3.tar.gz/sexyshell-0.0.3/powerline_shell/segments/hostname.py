122b37aea4bcd74cc410965be38b78d3f7ebb97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:21 +0000

    Add package sexyshell/0.0.3/sexyshell-0.0.3.tar.gz

---
+++ packages/sexyshell/sexyshell-0.0.3.tar.gz/sexyshell-0.0.3/powerline_shell/segments/hostname.py
@@ -0,0 +1,27 @@
+from ..utils import BasicSegment
+from ..color_compliment import stringToHashToColorAndOpposite
+from ..colortrans import rgb2short
+from socket import gethostname
+from sys import platform
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        powerline = self.powerline
+        if powerline.segment_conf("hostname", "colorize"):
+            hostname = gethostname()
+            FG, BG = stringToHashToColorAndOpposite(hostname)
+            FG, BG = (rgb2short(*color) for color in [FG, BG])
+            system_emoji = '' if platform == 'darwin' else '🖥'
+            host_prompt = " %s %s " % (system_emoji, hostname.split('.')[0])
+            powerline.append(host_prompt, FG, BG)
+        else:
+            if powerline.args.shell == "bash":
+                host_prompt = r" \h "
+            elif powerline.args.shell == "zsh":
+                host_prompt = " %m "
+            else:
+                host_prompt = " %s " % gethostname().split(".")[0]
+            powerline.append(host_prompt,
+                             powerline.theme.HOSTNAME_FG,
+                             powerline.theme.HOSTNAME_BG)
