bb09be0a7ea8753337f685c6464e341ba316c0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:13 +0000

    Add package freshpy/1.0.0/freshpy-1.0.0.tar.gz

---
+++ packages/freshpy/freshpy-1.0.0.tar.gz/freshpy-1.0.0/src/freshpy/utils/version.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            freshpy.utils.version
+:Synopsis:          This simple script contains the package version
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     02 Jan 2022
+"""
+
+from . import log_utils
+
+# Initialize logging
+logger = log_utils.initialize_logging(__name__)
+
+# Define special and global variables
+__version__ = "1.0.0"
+
+
+def get_full_version():
+    """This function returns the current full version of the freshpy package.
+
+    .. versionadded:: 1.0.0
+    """
+    return __version__
