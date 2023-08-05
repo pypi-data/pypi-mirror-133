b9e0039975b2dcb1fec85e362d49b0c97346de60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:40 +0000

    Add package randomnumgen/1.1.2/randomnumgen-1.1.2.tar.gz

---
+++ packages/randomnumgen/randomnumgen-1.1.2.tar.gz/randomnumgen-1.1.2/README.md
@@ -0,0 +1,28 @@
+# randomnumgen
+
+![PyPI version](https://badge.fury.io/py/randomnumgen.svg) ![Tests](https://github.com/SriMethan/randomnumgen/actions/workflows/tests.yml/badge.svg)
+
+**Randomnumgen is a Python library that generates random numbers.**
+
+Installing
+----------
+
+Download and install the latest release:
+
+    pip install randomnumgen
+
+
+Example usuage
+----------
+
+```python
+import randomnumgen
+
+num1 = 1
+num2 = 100
+
+print(randomnum)
+```
+
+## License
+**randomnumgen is licensed under the MIT Check out LICENSE.txt for the full text.**
---
+++ packages/randomnumgen/randomnumgen-1.1.2.tar.gz/randomnumgen-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="randomnumgen",
+    version="1.1.2",
+    author="SriMethan",
+    description="A Python3 library that generates random numbers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SriMethan/randomnumgen",
+    project_urls={
+        "Bug Tracker": "https://github.com/SriMethan/randomnumgen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
