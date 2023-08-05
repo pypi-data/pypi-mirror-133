ff53b7678730dccd8f00f029833281f6453e10f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package myprogressbar/0.0.1/myprogressbar-0.0.1.tar.gz

---
+++ packages/myprogressbar/myprogressbar-0.0.1.tar.gz/myprogressbar-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='myprogressbar',
+      version='0.0.1',
+      description='Progress bar as a Service',
+      author='Kota Uenishi',
+      author_email='kuenishi@gmail.com',
+      url='https://example.com/',
+      packages=find_packages(),
+     )
