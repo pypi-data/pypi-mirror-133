bea96f612014b5e93eedf10a1198d105ee6cb656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:18 +0000

    Add package selsoup/0.2.10/selsoup-0.2.10.tar.gz

---
+++ packages/selsoup/selsoup-0.2.10.tar.gz/selsoup-0.2.10/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['selsoup']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.1.2,<3.0.0', 'selenium>=4.1.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['selsoup = selsoup.selsoup:main']}
+
+setup_kwargs = {
+    'name': 'selsoup',
+    'version': '0.2.10',
+    'description': '',
+    'long_description': None,
+    'author': 'lemerchand',
+    'author_email': 'phoenix.scooter@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
