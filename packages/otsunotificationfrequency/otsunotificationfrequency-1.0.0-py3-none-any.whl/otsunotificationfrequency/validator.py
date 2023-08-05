3bfefc217399fdba96511279854799914ac9164d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:30 +0000

    Add package otsunotificationfrequency/1.0.0/otsunotificationfrequency-1.0.0-py3-none-any.whl

---
+++ packages/otsunotificationfrequency/otsunotificationfrequency-1.0.0-py3-none-any.whl/otsunotificationfrequency/validator.py
@@ -0,0 +1,38 @@
+__all__ = (
+    'CNotificationFrequency',
+    'VNotificationFrequency',
+)
+try:
+    from typing import Any
+
+    from otsuvalidator.bases import Converter, Validator
+
+    from . import NotificationFrequency
+
+except ImportError as e:
+    msg = 'このモジュールは"otsuvalidator"をインストールしている場合のみ使用できます。'
+    raise ImportError(msg)
+
+
+class VNotificationFrequency(Validator):
+
+    def validate(self, value: Any) -> NotificationFrequency:
+        if type(value) is not NotificationFrequency:
+            msg = self.ERRMSG('NotificationFrequency型である必要があります', value)
+            raise TypeError(msg)
+        return value
+
+
+class CNotificationFrequency(VNotificationFrequency, Converter):
+
+    def validate(self, value: Any) -> NotificationFrequency:
+        if type(value) is not NotificationFrequency:
+            try:
+                value = NotificationFrequency(value)
+            except:
+                msg = self.ERRMSG('NotificationFrequency型として扱える必要があります', value)
+                raise TypeError(msg)
+        return super().validate(value)
+
+    def super_validate(self, value: Any) -> NotificationFrequency:
+        return super().validate(value)
