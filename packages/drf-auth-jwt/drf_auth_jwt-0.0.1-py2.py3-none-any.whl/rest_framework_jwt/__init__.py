03b3a16b63d4add4b34055754ec28e326a6c9aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:11 +0000

    Add package drf-auth-jwt/0.0.1/drf_auth_jwt-0.0.1-py2.py3-none-any.whl

---
+++ packages/drf-auth-jwt/drf_auth_jwt-0.0.1-py2.py3-none-any.whl/rest_framework_jwt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "drf-auth-jwt"
+__version__ = "0.0.1"
+__author__ = "Sandile Ximba"
+__license__ = "MIT"
+__copyright__ = "Copyright 2014-2021 JovianOrigin CC"
+
+# Version synonym
+VERSION = __version__
