301f1609c3a61fd020164b6d5abbbaae960f84a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:51 +0000

    Add package artificium/0.0.20211231/artificium-0.0.20211231.tar.gz

---
+++ packages/artificium/artificium-0.0.20211231.tar.gz/artificium-0.0.20211231/artificium/__init__.py
@@ -0,0 +1,7 @@
+def mantra():
+    _mantra = 'Form is emptiness. Emptiness is form.'
+    return _mantra
+
+
+if __name__ == '__main__':
+    print(mantra())
---
+++ packages/artificium/artificium-0.0.20211231.tar.gz/artificium-0.0.20211231/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='artificium',
+      version='0.0.20211231',
+      description='',
+      url='http://artificium.ai',
+      author='Victor Safronov',
+      author_email='victor@aithea.com',
+      license='',
+      packages=['artificium'],
+      zip_safe=False)
