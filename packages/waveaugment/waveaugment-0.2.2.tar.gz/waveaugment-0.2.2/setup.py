aa88248d0e98a35aece6a656d444bcb65a80b32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package waveaugment/0.2.2/waveaugment-0.2.2.tar.gz

---
+++ packages/waveaugment/waveaugment-0.2.2.tar.gz/waveaugment-0.2.2/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree
+
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+
+# Creating the version file
+cwd = os.path.dirname(os.path.abspath(__file__))
+version = '0.2.2'
+sha = 'Unknown'
+
+try:
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).decode('ascii').strip()
+except Exception:
+    pass
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="waveaugment",
+    version=version,
+    url="https://github.com/facebookresearch/WavAugment",
+    # Exclude the build files.
+    packages=find_packages(exclude=["build"]),
+    install_requires=['torch==1.7.1', 'torchaudio']
+)
