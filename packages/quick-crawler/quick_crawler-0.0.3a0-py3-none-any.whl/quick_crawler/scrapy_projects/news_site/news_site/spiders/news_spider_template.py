000bc142e224aca0e9912cf997983652fc66742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:09 +0000

    Add package quick-crawler/0.0.3a0/quick_crawler-0.0.3a0-py3-none-any.whl

---
+++ packages/quick-crawler/quick_crawler-0.0.3a0-py3-none-any.whl/quick_crawler/scrapy_projects/news_site/news_site/spiders/news_spider_template.py
@@ -0,0 +1,70 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+from scrapy.exceptions import CloseSpider
+from quick_crawler import page
+import os
+MAX_NUM_URLS='$NAX_NUM_URLS$'
+url_count=0
+
+def get_page_meta(html_str):
+    soup = BeautifulSoup(html_str, features="lxml")
+
+    keywords=""
+    description=""
+
+    title = soup.title.string
+    print('title = ', title)
+
+    # print(soup.attrs)
+    html=soup.find("html")
+    if "lang" in html.attrs.keys():
+        lang = html["lang"]
+    else:
+        lang = ""
+    print("lang = ",lang)
+
+    meta = soup.find_all('meta')
+    # print(html_str)
+    for tag in meta:
+        if 'name' in tag.attrs.keys():
+            name=tag.attrs['name'].strip().lower()
+            if name=="description":
+                description=tag.attrs['content']
+            if name=="keywords":
+                keywords=tag.attrs['content']
+    model = {
+        "title":title.replace("\n",""),
+        "lang":lang,
+        "keywords":keywords.replace("\n",""),
+        "description":description.replace("\n","")
+    }
+    return model
+
+class NewsSpider(CrawlSpider):
+    name = '$NAME$'
+    allowed_domains = ['$DOMAIN$']
+    start_urls = ['$START_URL$']
+
+    rules = (
+        Rule(LinkExtractor(), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        global url_count
+        global list_model
+        url_count+=1
+        print(self.name + "'s URL: ", response.url)
+        html_str=response.text
+        meta_model=get_page_meta(html_str)
+        meta_model["url"]=page.quick_remove_unicode(response.url)
+        meta_model["id"]=page.quick_remove_unicode(self.name)
+        print(meta_model)
+        #list_model.append(meta_model)
+        if url_count>=MAX_NUM_URLS:
+
+            raise CloseSpider('termination condition met')
+        return meta_model
+
+
