46fddc4bc97cae91450ed3973e0a0a1a117f1ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:10 +0000

    Add package gosu/1.1.22/gosu-1.1.22.tar.gz

---
+++ packages/gosu/gosu-1.1.22.tar.gz/gosu-1.1.22/tests/test_basic.py
@@ -0,0 +1,44 @@
+import os
+
+import plumbum.commands.processes
+import pytest
+
+from gosu.cmds.ci import get_version, git_num_changes
+from gosu.cmds.python import (
+    _get_editable_installs,
+    _get_pkg_pyfiles,
+    _manage_env_vars,
+    _run,
+)
+
+
+def test_run_env():
+    _run("python", "-v")
+
+
+def test_run_env_missing_command():
+    with pytest.raises(plumbum.commands.processes.CommandNotFound):
+        _run("missing")
+
+
+def test_get_env_vars():
+    os.chdir("tests")
+    e = _manage_env_vars()
+    assert e["test"] == "1"
+
+
+def test_get_pkg_pyfiles():
+    e = _get_pkg_pyfiles()
+    assert len(e) > 0
+
+
+def test_get_version():
+    assert get_version() != ""
+
+
+def test_git_num_changes():
+    assert git_num_changes() != ""
+
+
+def test_get_editable_installs():
+    assert _get_editable_installs()["something"] == "../something"
