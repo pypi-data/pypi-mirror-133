5a02e4f2e3c7a7acac7f782215d178c72f90cfcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:29 +0000

    Add package purgatory-circuitbreaker/0.5.1/purgatory-circuitbreaker-0.5.1.tar.gz

---
+++ packages/purgatory-circuitbreaker/purgatory-circuitbreaker-0.5.1.tar.gz/purgatory-circuitbreaker-0.5.1/src/purgatory/typing.py
@@ -0,0 +1,34 @@
+from typing import Callable
+
+from purgatory.domain.messages.base import Event
+
+
+CircuitName = str
+TTL = float
+Threshold = int
+
+
+try:
+    from typing import Literal
+
+    StateName = Literal["opened", "closed", "half-opened"]
+    Hook = Callable[
+        [
+            CircuitName,
+            Literal["circuit_breaker_created", "state_changed", "failed", "recovered"],
+            Event,
+        ],
+        None,
+    ]
+
+except ImportError:
+    StateName = str
+    EventType = str
+    Hook = Callable[
+        [
+            CircuitName,
+            str,
+            Event,
+        ],
+        None,
+    ]
