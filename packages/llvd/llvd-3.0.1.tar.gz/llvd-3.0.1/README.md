c2ed7246b175e09a68f1c34348329b56dbe160ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:19 +0000

    Add package llvd/3.0.1/llvd-3.0.1.tar.gz

---
+++ packages/llvd/llvd-3.0.1.tar.gz/llvd-3.0.1/README.md
@@ -0,0 +1,95 @@
+# LLVD (Linkedin Learning Video Downloader)
+
+[![Downloads](https://pepy.tech/badge/llvd)](https://pepy.tech/project/llvd)
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=102)](https://github.com/ellerbrock/open-source-badge/)
+[![Open Source Love](https://badges.frapsoft.com/os/mit/mit.svg?v=102)](https://github.com/ellerbrock/open-source-badge/)
+
+<p>
+    <img src="https://raw.githubusercontent.com/knowbee/hosting/master/assets/progress_llvd.png" width="auto" height="auto"/>
+</p>
+
+## Features
+
+| Status | Feature                                 |
+| :----- | :-------------------------------------- |
+| ✅     | cookie-based authentication             |
+| ✅     | download by learning path               |
+| ✅     | download a course and all its exercises |
+| ✅     | group videos by chapters                |
+| ✅     | subtitles                               |
+| ✅     | nice progress bar                       |
+| ✅     | resume failed downloads                 |
+| ✅     | skip already downloaded videos          |
+| ✅     | set video format (360p, 540p, 720p)     |
+| ✅     | all platforms                           |
+
+## Prerequisites
+
+- [Python 3](https://www.python.org/downloads/)
+
+## Installation
+
+    $ pip --no-cache-dir install llvd
+
+If you have multiple versions of python installed in your system, use **pip3** instead.
+
+## Example
+
+```cli
+
+    $ llvd --help
+    $ llvd -c "course-slug" -r 720
+
+```
+
+## Windows Users
+
+When you get `'llvd' is not recognized as an internal or external command` after successfully installing `llvd`, run `python3 -m llvd <flags>`
+
+## How to find the course slug ?
+
+- Click on the course
+- Copy the slug next to learning in the URL `https://www.linkedin.com/learning/l-essentiel-d-asp-dot-net-core-pour-dot-net-5`, in this example the course slug is `l-essentiel-d-asp-dot-net-core-pour-dot-net-5`
+
+## Cookie-based authentication
+
+- 1.  Click on the options in the google chrome (top right with 3 vertical dots).
+- 2.  After this, click on more tools followed by Developer Tools (you can also reach here by using the keyboard combination — ctrl+shift+I).
+
+- 3.  Now once you’ve gained access to the developer tools, navigate to the Application tab, and copy the value of two cookies from there named li_at and JSESSIONID respectively.
+
+- 3.  create a cookies.txt file to a place you want to download your courses then paste in the values of li_at and JSESSIONID as shown below.
+
+#### Example (`cookies.txt`)
+
+```sh
+li_at=xxxxx
+JSESSIONID="ajax:xxxxxx"
+```
+
+```cli
+
+    $ llvd --help
+    $ llvd -c "course-slug" -r 720 --cookies
+
+```
+
+### Download the course with subtitles
+
+Use the `--caption` flag to download videos with subtitles
+
+```cli
+
+    $ llvd -c "course-slug" -r 720 --caption
+```
+
+or
+
+```cli
+
+    $ llvd -c "course-slug" -r 720 -ca
+```
+
+## Author
+
+Igwaneza Bruce
---
+++ packages/llvd/llvd-3.0.1.tar.gz/llvd-3.0.1/llvd/downloader.py
@@ -0,0 +1,96 @@
+
+from tqdm import tqdm
+from itertools import starmap
+import requests
+import time
+import click
+import re
+import os
+from llvd.utils import clean_name, subtitles_time_format, throttle
+
+
+def download_video(url, index, filename, path, delay=None):
+    """
+        Downloads a video and saves it by its name plus index for easy sorting
+    """
+    if delay:
+        throttle( delay );
+    maximum_retries = 5
+    with open(f'{path}/{index:0=2d}-{clean_name(filename)}.mp4', 'wb') as f:
+        download_size = 0
+        while maximum_retries > 0:
+            requests.adapters.HTTPAdapter(max_retries=maximum_retries)
+            response = requests.get(
+                url, stream=True, headers={'Accept-Encoding': None, 'Content-Encoding': 'gzip'})
+            download_size = response.headers.get('content-length')
+            if download_size is None and maximum_retries > 0:
+                maximum_retries -= 1
+            else:
+                break
+        pbar = tqdm(
+            total=int(download_size),
+            initial=0,
+            unit='B',
+            unit_scale=True,
+            position=0,
+            leave=True)
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                pbar.set_description("Downloading video... ")
+                pbar.update(len(chunk))
+        pbar.close()
+
+def download_subtitles(index, subs, video_name, path, video_duration):
+    """
+    Writes to a file(subtitle file) caption matching the right time
+    """
+    def subs_to_lines(idx, sub):
+        starts_at = sub['transcriptStartAt']
+        ends_at = subs[idx]['transcriptStartAt'] if idx < len(
+            subs) else video_duration
+        caption = sub['caption']
+        return f"{idx}\n" \
+            f"{subtitles_time_format(starts_at)} --> {subtitles_time_format(ends_at)}\n" \
+            f"{caption}\n\n"
+
+    with open(f"{path}/{index:0=2d}-{clean_name(video_name).strip()}.srt", 'wb') as f:
+        click.echo(f"Downloading subtitles..")
+        for line in starmap(subs_to_lines, enumerate(subs, start=1)):
+            f.write(line.encode('utf8'))
+
+
+def download_exercises(links, path):
+    """
+        Downloads exercises
+    """
+
+    for link in links:
+        maximum_retries = 3
+        filename = re.split("exercises/(.+).zip", link)[1]
+
+        with open(f"{path}/{clean_name(filename)}.zip", 'wb') as f:
+            download_size = 0
+            while maximum_retries > 0:
+                requests.adapters.HTTPAdapter(max_retries=maximum_retries)
+                response = requests.get(
+                    link, stream=True, headers={'Accept-Encoding': None, 'Content-Encoding': 'gzip'})
+                download_size = response.headers.get('content-length')
+                if download_size is None and maximum_retries > 0:
+                    maximum_retries -= 1
+                else:
+                    break
+            pbar = tqdm(
+                total=int(download_size),
+                initial=0,
+                unit='B',
+                unit_scale=True,
+                position=0,
+                leave=True)
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+                    pbar.set_description("Downloading exercise files... ")
+                    pbar.update(len(chunk))
+            pbar.close()
+            print("\n")
