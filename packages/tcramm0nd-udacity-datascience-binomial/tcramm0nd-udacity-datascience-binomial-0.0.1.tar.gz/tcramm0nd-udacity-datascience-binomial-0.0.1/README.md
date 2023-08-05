91571fccbacf9f9fbb7309073a6ff71cac27bd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package tcramm0nd-udacity-datascience-binomial/0.0.1/tcramm0nd-udacity-datascience-binomial-0.0.1.tar.gz

---
+++ packages/tcramm0nd-udacity-datascience-binomial/tcramm0nd-udacity-datascience-binomial-0.0.1.tar.gz/tcramm0nd-udacity-datascience-binomial-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Example Package for Udacity Data Science Nandegree
+
+example upload to PyPI for the Udacity Data Science Nanodegree
---
+++ packages/tcramm0nd-udacity-datascience-binomial/tcramm0nd-udacity-datascience-binomial-0.0.1.tar.gz/tcramm0nd-udacity-datascience-binomial-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tcramm0nd-udacity-datascience-binomial',
+      version='0.0.1',
+      description='Gaussian distributions for Udacity Data Science Nandegree',
+      packages=['distributions'],
+      zip_safe=False)
