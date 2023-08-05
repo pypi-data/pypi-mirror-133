50b975b544ed58e99376300ffd1deaed18107c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:52 +0000

    Add package pyofs/0.2/pyofs-0.2.tar.gz

---
+++ packages/pyofs/pyofs-0.2.tar.gz/pyofs-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyofs',
+      version='0.2',
+      description='uses <open>',
+      packages=['pyofs'],
+      author_email='ivan.selyakovich@gmail.com',
+      zip_safe=False)
