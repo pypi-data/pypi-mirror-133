59d14ace627d1eae0f5d99899d9711b79fdf3a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:54 +0000

    Add package chirper-py/1.1.3/chirper-py-1.1.3.tar.gz

---
+++ packages/chirper-py/chirper-py-1.1.3.tar.gz/chirper-py-1.1.3/src/chirper/api/data_process.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import numpy as np
+
+from ..sgn import Signal1
+if TYPE_CHECKING:
+    from . import GuiInterface
+    from .chirp import Chirp
+
+
+class DataProcess:
+    def __init__(self, api: GuiInterface) -> None:
+        self.api = api
+
+    def process(self, data, request: Chirp, **kwargs):
+        return request.request_type.get_processed(self, data, **kwargs)
+
+    def process_spectrogram(self, data, **kwargs):
+        samp_freq = self.api.samplerate
+        values = data.mean(axis=1)
+        return Signal1.from_freq(values, samp_freq)
