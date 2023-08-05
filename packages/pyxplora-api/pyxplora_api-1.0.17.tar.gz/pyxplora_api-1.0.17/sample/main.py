584e0be3c0055cb95b1c990d9a0749b4d686487e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:56 +0000

    Add package pyxplora-api/1.0.17/pyxplora_api-1.0.17.tar.gz

---
+++ packages/pyxplora-api/pyxplora_api-1.0.17.tar.gz/pyxplora_api-1.0.17/sample/main.py
@@ -0,0 +1,14 @@
+from pyxplora_api import pyxplora_api as PXA
+
+def main():
+    countryCode = "+49"
+    phoneNummer = "123456789"
+    password = "ACCOUNT_PASSWORT"
+    local = "de-DE"
+    timeZone = "Europe/Berlin"
+
+    xplora = PXA.PyXploraApi(countryCode, phoneNummer, password, local, timeZone)
+    print(xplora.getUserName())
+
+if __name__ == '__main__':
+    main()
