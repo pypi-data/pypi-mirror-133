32117f45cdb7b7c4a7951e5957bbfb3fd4764d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:15 +0000

    Add package async-retriever/0.3.1/async_retriever-0.3.1-py3-none-any.whl

---
+++ packages/async-retriever/async_retriever-0.3.1-py3-none-any.whl/async_retriever/__init__.py
@@ -0,0 +1,38 @@
+"""Top-level package."""
+import asyncio
+import sys
+
+from .async_retriever import (
+    delete_url_cache,
+    retrieve,
+    retrieve_binary,
+    retrieve_json,
+    retrieve_text,
+)
+from .exceptions import InvalidInputType, InvalidInputValue, ServiceError
+from .print_versions import show_versions
+
+try:
+    import importlib.metadata as metadata
+except ImportError:
+    import importlib_metadata as metadata  # type: ignore[no-redef]
+
+try:
+    __version__ = metadata.version("async_retriever")
+except Exception:
+    __version__ = "999"
+
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+__all__ = [
+    "retrieve",
+    "retrieve_text",
+    "retrieve_json",
+    "retrieve_binary",
+    "delete_url_cache",
+    "InvalidInputType",
+    "InvalidInputValue",
+    "ServiceError",
+    "show_versions",
+]
