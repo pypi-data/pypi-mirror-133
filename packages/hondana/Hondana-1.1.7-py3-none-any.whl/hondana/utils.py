cd6e55e382a10a050f9e16a2b9a6aad839136cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:05 +0000

    Add package hondana/1.1.7/Hondana-1.1.7-py3-none-any.whl

---
+++ packages/hondana/Hondana-1.1.7-py3-none-any.whl/hondana/utils.py
@@ -0,0 +1,318 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2021-Present AbstractUmbra
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+from __future__ import annotations
+
+import datetime
+import json
+import pathlib
+import re
+from functools import wraps
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    ClassVar,
+    Iterable,
+    Mapping,
+    Optional,
+    TypeVar,
+    Union,
+)
+from urllib.parse import quote as _uriquote
+
+import aiohttp
+
+from .errors import AuthenticationRequired
+
+
+if TYPE_CHECKING:
+    from typing_extensions import Concatenate, ParamSpec
+
+
+C = TypeVar("C", bound="Any")
+T = TypeVar("T")
+if TYPE_CHECKING:
+    B = ParamSpec("B")
+
+
+__all__ = (
+    "MANGADEX_URL_REGEX",
+    "MANGADEX_TIME_REGEX",
+    "MISSING",
+    "Route",
+    "CustomRoute",
+    "to_json",
+    "json_or_text",
+    "to_iso_format",
+    "php_query_builder",
+    "to_snake_case",
+    "to_camel_case",
+    "get_image_mime_type",
+    "as_chunks",
+    "MANGA_TAGS",
+)
+
+_PROJECT_DIR = pathlib.Path(__file__)
+MANGADEX_URL_REGEX = re.compile(
+    r"(?:http[s]?:\/\/)?mangadex\.org\/(?P<type>title|chapter|author|tag)\/(?P<ID>[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12})\/?(?P<title>.*)"
+)
+MANGADEX_TIME_REGEX = re.compile(
+    r"^(P([1-9]|[1-9][0-9])D)?(P?([1-9])W)?(P?T(([1-9]|1[0-9]|2[0-4])H)?(([1-9]|[1-5][0-9]|60)M)?(([1-9]|[1-5][0-9]|60)S)?)?$"
+)
+"""
+``r"^(P([1-9]|[1-9][0-9])D)?(P?([1-9])W)?(P?T(([1-9]|1[0-9]|2[0-4])H)?(([1-9]|[1-5][0-9]|60)M)?(([1-9]|[1-5][0-9]|60)S)?)?$"``
+
+This `regex pattern <https://docs.python.org/3/library/re.html#re-objects>`_ follows the ISO-8601 standard (MangaDex uses PHP DateInterval, located `here <https://www.php.net/manual/en/dateinterval.construct.php>`_.
+The pattern *is* usable but more meant as a guideline for your formatting.
+
+It matches some things like: ``P1D2W`` (1 day, two weeks), ``P1D2WT3H4M`` (1 day, 2 weeks, 3 hours and 4 minutes)
+"""
+
+
+class Route:
+    """A helper class for instantiating a HTTP method to MangaDex.
+
+    Parameters
+    -----------
+    verb: :class:`str`
+        The HTTP verb you wish to perform, e.g. ``"POST"``
+    path: :class:`str`
+        The prepended path to the API endpoint you with to target.
+        e.g. ``"/manga/{manga_id}"``
+    parameters: Any
+        This is a special cased kwargs. Anything passed to these will substitute it's key to value in the `path`.
+        E.g. if your `path` is ``"/manga/{manga_id}"``, and your parameters are ``manga_id="..."``, then it will expand into the path
+        making ``"manga/..."``
+    """
+
+    BASE: ClassVar[str] = "https://api.mangadex.org"
+
+    def __init__(self, verb: str, path: str, **parameters: Any) -> None:
+        self.verb: str = verb
+        self.path: str = path
+        url = self.BASE + self.path
+        if parameters:
+            url = url.format_map({k: _uriquote(v) if isinstance(v, str) else v for k, v in parameters.items()})
+        self.url: str = url
+
+
+class CustomRoute(Route):
+    """A helper class for instantiating a HTTP method to download from MangaDex.
+
+    Parameters
+    -----------
+    verb: :class:`str`
+        The HTTP verb you wish to perform. E.g. ``"POST"``
+    base: :class:`str`
+        The base URL for the download path.
+    path: :class:`str`
+        The prepended path to the API endpoint you with to target.
+        e.g. ``"/manga/{manga_id}"``
+    parameters: Any
+        This is a special cased kwargs. Anything passed to these will substitute it's key to value in the `path`.
+        E.g. if your `path` is ``"/manga/{manga_id}"``, and your parameters are ``manga_id="..."``, then it will expand into the path
+        making ``"manga/..."``
+    """
+
+    def __init__(self, verb: str, base: str, path: str, **parameters: Any) -> None:
+        self.verb: str = verb
+        self.base: str = base
+        self.path: str = path
+        url = self.base + self.path
+        if parameters:
+            url = url.format_map({k: _uriquote(v) if isinstance(v, str) else v for k, v in parameters.items()})
+        self.url: str = url
+
+
+class MissingSentinel:
+    def __eq__(self, _: Any) -> bool:
+        return False
+
+    def __bool__(self) -> bool:
+        return False
+
+    def __repr__(self) -> str:
+        return "..."
+
+
+MISSING: Any = MissingSentinel()
+
+
+def require_authentication(func: Callable[Concatenate[C, B], T]) -> Callable[Concatenate[C, B], T]:
+    """A decorator to assure the `self` parameter of decorated methods has authentication set."""
+
+    @wraps(func)
+    def wrapper(item: C, *args: B.args, **kwargs: B.kwargs) -> T:
+        if not item._http._authenticated:
+            raise AuthenticationRequired("This method requires you to be authenticated to the API.")
+
+        return func(item, *args, **kwargs)
+
+    return wrapper
+
+
+def to_json(obj: Any) -> str:
+    """A quick object that dumps a Python type to JSON object."""
+    return json.dumps(obj, separators=(",", ":"), ensure_ascii=True)
+
+
+async def json_or_text(response: aiohttp.ClientResponse) -> Union[dict[str, Any], str]:
+    """A quick method to parse an `aiohttp.ClientResponse` and test if it's json or text."""
+    text = await response.text(encoding="utf-8")
+    try:
+        if response.headers["content-type"] == "application/json":
+            try:
+                return json.loads(text)
+            except json.JSONDecodeError:
+                pass
+    except KeyError:
+        pass
+
+    return text
+
+
+def to_iso_format(in_: datetime.datetime) -> str:
+    """Quick function to dump a `datetime.datetime` to acceptable ISO 8601 format."""
+    return f"{in_:%Y-%m-%dT%H:%M:%S}"
+
+
+def php_query_builder(obj: Mapping[str, Optional[Union[str, int, bool, list[str], dict[str, str]]]]) -> str:
+    """
+    {"order": {"publishAt": "desc"}, "translatedLanguages": ["en", "jp"]}
+    ->
+    "order[publishAt]=desc&translatedLanguages[]=en&translatedLanguages[]=jp"
+    """
+    fmt = []
+    for key, value in obj.items():
+        if value is None or value is MISSING:
+            fmt.append(f"{key}=null")
+        if isinstance(value, (str, int, bool)):
+            fmt.append(f"{key}={str(value)}")
+        elif isinstance(value, list):
+            fmt.extend(f"{key}[]={item}" for item in value)
+        elif isinstance(value, dict):
+            fmt.extend(f"{key}[{subkey}]={subvalue}" for subkey, subvalue in value.items())
+
+    return "&".join(fmt)
+
+
+def get_image_mime_type(data: bytes) -> str:
+    """Returns the image type from the first few bytes."""
+    if data.startswith(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"):
+        return "image/png"
+    elif data[0:3] == b"\xff\xd8\xff" or data[6:10] in (b"JFIF", b"Exif"):
+        return "image/jpeg"
+    elif data.startswith((b"\x47\x49\x46\x38\x37\x61", b"\x47\x49\x46\x38\x39\x61")):
+        return "image/gif"
+    elif data.startswith(b"RIFF") and data[8:12] == b"WEBP":
+        return "image/webp"
+    else:
+        raise ValueError("Unsupported image type given")
+
+
+def to_snake_case(string: str) -> str:
+    """Quick function to return snake_case from camelCase."""
+    fmt: list[str] = []
+    for character in string:
+        if character.isupper():
+            fmt.append(f"_{character.lower()}")
+            continue
+        fmt.append(character)
+    return "".join(fmt)
+
+
+def to_camel_case(string: str) -> str:
+    """Quick function to return camelCase from snake_case."""
+    first, *rest = string.split("_")
+    chunks = [first.lower(), *map(str.capitalize, rest)]
+
+    return "".join(chunks)
+
+
+def as_chunks(iterator: Iterable[T], max_size: int) -> Iterable[list[T]]:
+    ret = []
+    n = 0
+    for item in iterator:
+        ret.append(item)
+        n += 1
+        if n == max_size:
+            yield ret
+            ret = []
+            n = 0
+    if ret:
+        yield ret
+
+
+def delta_to_format(delta: datetime.timedelta) -> str:
+    def builder(*, weeks: int = None, days: int = None, hours: int = None, minutes: int = None, seconds: int = None) -> str:
+        fmt = ""
+        if weeks or days:
+            fmt += "P"
+        if days:
+            fmt += f"{days}D"
+        if weeks:
+            fmt += f"{weeks}W"
+        if hours or minutes or seconds:
+            fmt += "T"
+        if hours:
+            fmt += f"{hours}H"
+        if minutes:
+            fmt += f"{minutes}M"
+        if seconds:
+            fmt += f"{seconds}S"
+
+        return fmt
+
+    total_secs = int(delta.total_seconds())
+
+    weeks, remaining = divmod(total_secs, 604800)
+    if weeks > 0:
+        total_secs = remaining
+        if remaining == 0:
+            return builder(weeks=weeks)
+
+    days, remaining = divmod(total_secs, 86400)
+    if days > 0:
+        total_secs = remaining
+        if remaining == 0:
+            return builder(weeks=weeks, days=days)
+
+    hours, remaining = divmod(total_secs, 3600)
+    if hours > 0:
+        total_secs = remaining
+        if remaining == 0:
+            return builder(weeks=weeks, days=days, hours=hours)
+
+    minutes, remaining = divmod(total_secs, 60)
+    if minutes > 0:
+        total_secs = remaining
+        if remaining == 0:
+            return builder(weeks=weeks, days=days, hours=hours, minutes=minutes)
+
+    return builder(weeks=weeks, days=days, hours=hours, minutes=minutes, seconds=total_secs)
+
+
+path: pathlib.Path = _PROJECT_DIR.parent / "extras" / "tags.json"
+with open(path, "r") as _fp:
+    MANGA_TAGS: dict[str, list[str]] = json.load(_fp)
