f29d402d89bfcf8da0dc3a982cd5a1dc9a12e791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:34 +0000

    Add package p2k/0.1.3/p2k-0.1.3.tar.gz

---
+++ packages/p2k/p2k-0.1.3.tar.gz/p2k-0.1.3/p2k/psm.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from . import utils
+
+class Linear:
+    def __init__(self, pobj):
+        self.pobj = pobj
+
+    def calibrate(self):
+        pass
+
+    def forward(self):
+        pass
