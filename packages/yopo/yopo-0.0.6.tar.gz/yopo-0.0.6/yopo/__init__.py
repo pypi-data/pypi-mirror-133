0b64684c14ef99d57341ab705ef900417da3dca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:58 +0000

    Add package yopo/0.0.6/yopo-0.0.6.tar.gz

---
+++ packages/yopo/yopo-0.0.6.tar.gz/yopo-0.0.6/yopo/__init__.py
@@ -0,0 +1,39 @@
+import json
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask import request
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+import string
+import os
+import random
+from jupyter_dash import JupyterDash
+from yopo.dashboard import dashboardApp
+import requests
+
+
+
+def dashboard(input=pd.DataFrame(),mode="inline",port=8050):
+    dash_app = JupyterDash(
+    external_stylesheets=[
+        'https://fonts.googleapis.com/css?family=Lato',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+    ],
+    name='dash-app-1',
+    )
+
+    dash_app.layout = html.Div()
+    dash_app = dashboardApp(input,dash_app)
+    dash_app.run_server(mode=mode,port=port)
+
+def kill_dashboard(host="localhost", port=8050):
+    shutdown_url = "http://{host}:{port}/_shutdown_{token}".format(
+        host=host, port=port, token=JupyterDash._token
+    )
+    try:
+        response = requests.get(shutdown_url)
+    except Exception as e:
+        print(e)
