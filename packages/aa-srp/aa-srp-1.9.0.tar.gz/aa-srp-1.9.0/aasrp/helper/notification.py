d163b3c996b211dc18eba9a46c846811f244115f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:43 +0000

    Add package aa-srp/1.9.0/aa-srp-1.9.0.tar.gz

---
+++ packages/aa-srp/aa-srp-1.9.0.tar.gz/aa-srp-1.9.0/aasrp/helper/notification.py
@@ -0,0 +1,54 @@
+"""
+notifications helper
+"""
+
+# Django
+from django.contrib.auth.models import User
+
+# Alliance Auth
+from allianceauth.notifications import notify
+
+# AA SRP
+from aasrp.app_settings import aa_discordnotify_active, allianceauth_discordbot_active
+
+
+def send_user_notification(user: User, level: str, title: str, message: str) -> None:
+    """
+    send notification to user
+    this creates a notification in Auth and a PM in Discord when either
+    AA-Discordbot or AA Discord Notifications are installed
+    :param user:
+    :param level:
+    :param title:
+    :param message:
+    """
+
+    notify(user=user, title=title, level=level, message=message)
+
+    # send a PM to the user on Discord if allianceauth-discordbot
+    # is active and not aa-discordnotify
+    if allianceauth_discordbot_active() and not aa_discordnotify_active():
+        # Third Party
+        import aadiscordbot.tasks
+
+        aadiscordbot.tasks.send_direct_message_by_user_id.delay(user.pk, message)
+
+
+def send_message_to_discord_channel(
+    channel_id: int, message: str, embed: bool = False
+) -> None:
+    """
+    sending a message to a discord channel
+    if AA-Discordbot is installed
+    :param channel_id:
+    :param message:
+    :param embed:
+    """
+
+    if allianceauth_discordbot_active():
+        # Third Party
+        import aadiscordbot.tasks
+
+        aadiscordbot.tasks.send_channel_message_by_discord_id.delay(
+            channel_id, message, embed
+        )
