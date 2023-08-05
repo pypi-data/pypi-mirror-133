996214599d82bf56a5aa8813641514e4d0c2457e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:34 +0000

    Add package jaxson/0.1.5/jaxson-0.1.5.tar.gz

---
+++ packages/jaxson/jaxson-0.1.5.tar.gz/jaxson-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jaxson']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['jax>=0.2.26,<0.3.0', 'jaxlib>=0.1.75,<0.2.0']
+
+setup_kwargs = {
+    'name': 'jaxson',
+    'version': '0.1.5',
+    'description': 'A generative art libary based on Jax',
+    'long_description': None,
+    'author': 'Sam Corzine',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
