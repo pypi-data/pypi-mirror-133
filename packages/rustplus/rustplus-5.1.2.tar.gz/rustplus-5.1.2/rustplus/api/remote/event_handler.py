6c72e87bf1d6067dbde0fea5c42ffebb8e0420bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:03 +0000

    Add package rustplus/5.1.2/rustplus-5.1.2.tar.gz

---
+++ packages/rustplus/rustplus-5.1.2.tar.gz/rustplus-5.1.2/rustplus/api/remote/event_handler.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from ..structures import EntityEvent, TeamEvent, ChatEvent
+
+class EventHandler:
+
+    def __init__(self) -> None:
+        pass
+
+    def register_event(self, name, data : tuple) -> None:
+
+        if not asyncio.iscoroutinefunction(data[0]):
+            raise TypeError("The event registered must be a coroutine")
+
+        setattr(self, str(name), data)
+
+    def _schedule_event(self, loop, coro, arg) -> None:
+        asyncio.run_coroutine_threadsafe(coro(arg), loop)
+
+    def run_entity_event(self, name, app_message) -> None:
+
+        if hasattr(self, str(name)):
+            coro, loop, type = getattr(self, str(name))
+
+            self._schedule_event(loop, coro, EntityEvent(app_message, type))
+
+    def run_team_event(self, app_message) -> None:
+
+        if hasattr(self, "team_changed"):
+            coro, loop = getattr(self, "team_changed")
+            self._schedule_event(loop, coro, TeamEvent(app_message))
+
+    def run_chat_event(self, app_message) -> None:
+
+        if hasattr(self, "chat_message"):
+            coro, loop = getattr(self, "chat_message")
+            self._schedule_event(loop, coro, ChatEvent(app_message))
