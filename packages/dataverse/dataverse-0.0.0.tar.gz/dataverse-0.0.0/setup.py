ef0c368cbc956eb47ddc883d999ddace529d10b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:55 +0000

    Add package dataverse/0.0.0/dataverse-0.0.0.tar.gz

---
+++ packages/dataverse/dataverse-0.0.0.tar.gz/dataverse-0.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = "0.0.0"
+
+setup(
+    name="dataverse",
+    packages=find_packages(exclude=[]),
+    version=version,
+    description=(
+        "Placeholder"
+    ),
+    long_description="Placeholder",
+    long_description_content_type="text/markdown",
+    author="The Authors",
+    author_email="limsweekiat@gmail.com",
+    url="https://github.com/greentfrapp",
+    license="Apache License 2.0",
+    keywords=[
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
