d5b43a862fb10a8c0f535f58d89a724277e4073c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:01 +0000

    Add package rtmilk/0.0.8/rtmilk-0.0.8-py3-none-any.whl

---
+++ packages/rtmilk/rtmilk-0.0.8-py3-none-any.whl/rtmilk/utils.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timezone
+from typing import Generic, Optional, TypeVar
+
+from pydantic.fields import ModelField
+# from pydantic.main import Model
+
+PydanticField = TypeVar('PydanticField')
+
+# copied from https://github.com/samuelcolvin/pydantic/issues/181
+class EmptyStrToNone(Generic[PydanticField]):
+	@classmethod
+	def __get_validators__(cls):
+		yield cls.validate
+
+	@classmethod
+	def validate(cls, v: PydanticField, field: ModelField) -> Optional[PydanticField]: # pylint: disable=unused-argument
+		if v == '':
+			return None
+		return datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+
+# class MissingToEmptyList(Generic[PydanticField]):
+# 	@classmethod
+# 	def __get_validators__(cls):
+# 		yield cls.validate
+
+# 	@classmethod
+# 	def validate(cls, v: PydanticField, field: ModelField) -> Optional[PydanticField]:
+# 		if v
