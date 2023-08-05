b3e28577fcc113062ac7a303b60831b6d0069a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:09 +0000

    Add package generic-iterative-stemmer/0.1.9/generic-iterative-stemmer-0.1.9.tar.gz

---
+++ packages/generic-iterative-stemmer/generic-iterative-stemmer-0.1.9.tar.gz/generic-iterative-stemmer-0.1.9/generic_iterative_stemmer/models/stemmed_keyed_vectors.py
@@ -0,0 +1,78 @@
+import json
+import logging
+import os
+from typing import Optional
+
+import numpy as np
+from gensim.models import KeyedVectors
+
+from ..errors import StemDictFileNotFoundError
+
+log = logging.getLogger(__name__)
+
+
+def get_model_path(base_folder: str) -> str:
+    return os.path.join(base_folder, "model.kv")
+
+
+def get_stem_dict_path_from_model_path(model_path: str) -> str:
+    return f"{model_path}.stem-dict.json"
+
+
+def get_stem_dict_path_from_iteration_folder(base_folder: str) -> str:
+    model_path = get_model_path(base_folder)
+    return get_stem_dict_path_from_model_path(model_path)
+
+
+def save_stem_dict(stem_dict: dict, model_path: str):
+    stem_dict_path = get_stem_dict_path_from_model_path(model_path)
+    with open(stem_dict_path, "w") as file:
+        serialized = json.dumps(stem_dict, indent=2, ensure_ascii=False)
+        file.write(serialized)
+    log.debug(f"Stem dict saved: {stem_dict_path}.")
+
+
+class StemmedKeyedVectors(KeyedVectors):
+    def __init__(
+        self,
+        stem_dict: dict,
+        vector_size: int,
+        count: Optional[int] = 0,
+        dtype: Optional[type] = np.float32,
+        mapfile_path: Optional[str] = None,
+    ):
+        self.stem_dict = stem_dict
+        # TODO: Validate stem_dict is reduced
+        super().__init__(vector_size=vector_size, count=count, dtype=dtype, mapfile_path=mapfile_path)
+
+    def get_vector(self, key, norm=False):
+        stem = self.stem_dict.get(key, key)
+        return super().get_vector(stem, norm=norm)
+
+    @classmethod
+    def from_keyed_vectors(cls, stem_dict: dict, kv: KeyedVectors) -> "StemmedKeyedVectors":
+        model = cls(stem_dict, vector_size=kv.vector_size)
+        # This is pretty ugly, but it's working.
+        for key, value in kv.__dict__.items():
+            model.__dict__[key] = value
+        log.debug("StemmedKeyedVectors loaded.")
+        return model
+
+    @classmethod
+    def load(cls, fname: str, mmap=None):
+        kv: KeyedVectors = super().load(fname=fname, mmap=mmap)  # type: ignore
+        stem_dict_path = get_stem_dict_path_from_model_path(fname)
+        try:
+            with open(stem_dict_path) as file:
+                stem_dict = json.load(file)
+        except FileNotFoundError as e:
+            raise StemDictFileNotFoundError() from e
+        return StemmedKeyedVectors.from_keyed_vectors(stem_dict=stem_dict, kv=kv)
+
+    def save(self, fname: str, *args, **kwargs):
+        super().save(fname, *args, **kwargs)
+        save_stem_dict(self.stem_dict, fname)
+
+    def similarity_unseen_docs(self, *args, **kwargs):
+        # This is here just so that pycharm won't mark this class as abstract.
+        return super().similarity_unseen_docs(*args, **kwargs)
---
+++ packages/generic-iterative-stemmer/generic-iterative-stemmer-0.1.9.tar.gz/generic-iterative-stemmer-0.1.9/generic_iterative_stemmer/training/stemming/stem_dict_generator.py
@@ -0,0 +1,99 @@
+import logging
+from typing import Dict, Iterable, Optional
+
+import editdistance
+from gensim.models import KeyedVectors
+from tqdm import tqdm
+
+from ...utils import measure_time
+from ...utils.async_task_manager import AsyncTaskManager
+
+log = logging.getLogger(__name__)
+
+StemDict = Dict[str, str]
+
+
+class StemDictGenerator:
+    def __init__(
+        self,
+        model: KeyedVectors,
+        k: Optional[int] = 10,
+        min_cosine_similarity: Optional[float] = 0.75,
+        min_cosine_similarity_for_edit_distance: Optional[float] = 0.8,
+        max_len_diff: Optional[int] = 3,
+        max_edit_distance: Optional[int] = 1,
+    ):
+        self.model = model
+        self.k = k
+        self.min_cosine_similarity = min_cosine_similarity
+        self.min_cosine_similarity_for_edit_distance = min_cosine_similarity_for_edit_distance
+        self.max_len_diff = max_len_diff
+        self.max_edit_distance = max_edit_distance
+
+    def find_word_inflections(self, word: str) -> StemDict:
+        """
+        Find which other words in the vocabulary can be stemmed down to this word.
+        """
+        similarities = self.model.most_similar(word, topn=self.k)
+        stem_dict = {}
+        for candidate, grade in similarities:
+            if grade < self.min_cosine_similarity:
+                continue
+            if len(candidate) < len(word):
+                continue
+            if word not in candidate:
+                if self.max_edit_distance is None:
+                    continue
+                edit_distance = editdistance.eval(word, candidate)
+                if edit_distance > self.max_edit_distance:
+                    continue
+                if grade < self.min_cosine_similarity_for_edit_distance:
+                    continue
+            if self.max_len_diff and abs(len(word) - len(candidate)) > self.max_len_diff:
+                continue
+            w2i = self.model.key_to_index
+            if w2i[candidate] < w2i[word]:
+                continue
+            stem_dict[candidate] = word
+        return stem_dict
+
+    @measure_time
+    def generate_model_stemming(self, vocabulary: Iterable[str] = None):
+        if vocabulary is None:
+            vocabulary = self.model.key_to_index.keys()
+        log.info("Generating stem dict for words...")
+        model_stem_dict = {}
+        task_manager = AsyncTaskManager(workers_amount=5)
+        log.debug("Appending stemming tasks...")
+        for word in vocabulary:
+            task_manager.add_task(self.find_word_inflections, args=(word,))
+        log.debug("Collecting stemming results...")
+        for result in tqdm(task_manager, total=task_manager.total_task_count):
+            model_stem_dict.update(result)
+        log.info(f"Total {len(model_stem_dict)} stems generated")
+        reduced_dict = reduce_stem_dict(stem_dict=model_stem_dict)
+        return reduced_dict
+
+
+def reduce_stem_dict(stem_dict: StemDict) -> StemDict:
+    log.debug(f"Reducing stem dict of size {len(stem_dict)}")
+    reduced_dict: StemDict = {}
+    while len(stem_dict) > 0:
+        word_to_reduce = next(iter(stem_dict))
+        _reduce_iteration(stem_dict=stem_dict, reduced_dict=reduced_dict, word_to_reduce=word_to_reduce)
+    return reduced_dict
+
+
+def _reduce_iteration(stem_dict: StemDict, reduced_dict: StemDict, word_to_reduce: str):
+    representative = stem_dict.pop(word_to_reduce)
+    if representative in stem_dict:
+        # Meaning representative itself can be reduced
+        _reduce_iteration(stem_dict, reduced_dict, word_to_reduce=representative)
+    if representative in reduced_dict:
+        # Meaning representative points to the most reduced
+        reduced = reduced_dict[representative]
+    else:
+        # Meaning representative is the edge of a chain
+        reduced = representative
+    if word_to_reduce != reduced:
+        reduced_dict[word_to_reduce] = reduced
