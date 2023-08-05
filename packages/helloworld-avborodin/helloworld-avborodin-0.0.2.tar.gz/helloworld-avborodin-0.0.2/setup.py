43490ab164bdae56a0e2b3ca1685947b62e92910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package helloworld-avborodin/0.0.2/helloworld-avborodin-0.0.2.tar.gz

---
+++ packages/helloworld-avborodin/helloworld-avborodin-0.0.2.tar.gz/helloworld-avborodin-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='helloworld-avborodin',
+	version='0.0.2',
+	author='Andrey Borodin',
+	author_email='boroddon@gmail.com',
+	description='Say hello',
+	py_modules=['helloworld'],
+	package_dir={'':'src'}
+)
