3d4dbe4a554ebad2fd4d4a4c4ee814933a91ab03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:49 +0000

    Add package Redj-Captcha/0.1.0/Redj%20Captcha-0.1.0.tar.gz

---
+++ packages/Redj-Captcha/Redj%20Captcha-0.1.0.tar.gz/Redj Captcha-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    version='0.1.0',
+    author='redj_ai',
+    name='Redj Captcha',
+    url='https://redj.ai/',
+    description='Redj Captcha',
+    author_email='info@redj.ai',
+    long_description=long_description,
+    install_requires=['Pillow==9.0.0'],
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://redj.ai/",
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires=">=3",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
