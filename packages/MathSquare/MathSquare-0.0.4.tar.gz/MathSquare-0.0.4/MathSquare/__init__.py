4cf882032ed6ae3fe6db7bb2d1501b25377f3f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package MathSquare/0.0.4/MathSquare-0.0.4.tar.gz

---
+++ packages/MathSquare/MathSquare-0.0.4.tar.gz/MathSquare-0.0.4/MathSquare/__init__.py
@@ -0,0 +1,157 @@
+# informartion
+
+__author__ = "Zedikon"
+__maintainer__ = "Zedikon"
+__license__ = "GNU General Public License v3 (GPLv3)"
+__version__ = "0.0.4"
+__copyright__ = "Copyright Zedikon 2022"
+
+# MathSquare
+#Area
+def area_rectangle(a, b):
+    if a and b > 0:
+        result = a * b
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def area_quadrate(a):
+    if a > 0:
+        result = a * a
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def area_trapezoid(a, b, h):
+    if a and b and h > 0:
+        result = (1/2 * h * (a + b))
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def area_triangle(a, h):
+    if a and h > 0:
+        result = (1/2 * (a * h))
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def area_parallellogram(a, h):
+    if a and h > 0:
+        result = a * h
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def area_romb(a, h):
+    if a and h > 0:
+        result = a * h
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+# Perimetr
+def perimetr_rectangle(a, b):
+    if a and b > 0:
+        result = 2 * (a + b)
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def perimetr_quadrate(a):
+    if a > 0:
+        result = 4 * a
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def perimetr_trapezoid(a, b, c, d):
+    if a and b and c and d > 0:
+        result = a + b + c + d
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def perimetr_triangle(a, b, c):
+    if a and b and c > 0:
+        result = a + b + c
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+
+#Pythagorean theorem
+def pythagorean_hypotenuse(cathet, cathet1):
+    if cathet and cathet1 > 0:
+        hypotenuse = cathet**2 + cathet1**2
+        result = hypotenuse ** (0.5)
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def pythagorean_cathet(hypotenuse, cathet):
+    if hypotenuse and cathet > 0:
+        cathet1 = hypotenuse**2 - cathet**2
+        result = cathet1 ** (0.5)
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+#Heron
+def heron_area_triangle(a, b, c):
+    if a and b and c > 0:
+        p = 1/2 * (a + b + c)
+        s = p * (p - a) * (p-b) * (p-c)
+        result = s ** (0.5)
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+# joke
+def lenin_area(l, h):
+    if l and h > 0:
+        result = l * h
+        result = "Площадь вождя: " + str(result)
+        return result
+    else:
+        return "Товарищ! Площадь вождя не может быть отрицательной!"
+
+# sin, cos, tg triangle
+def sin_angle(cathet, hypotenuse):
+    if cathet and hypotenuse > 0:
+        result = cathet / hypotenuse
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def cos_angle(cathet, hypotenuse):
+    if cathet and hypotenuse > 0:
+        result = cathet / hypotenuse
+        return result
+    else:
+         return "MathSquare: Sorry but the length can't have a negative value"
+
+def tg_angle(cathet, cathet1):
+    if cathet and cathet1 > 0:
+        result = cathet / cathet1
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+# circle
+def radius(d):
+    if d > 0:
+        result = d / 2
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+def diameter(r):
+    if r > 0:
+        result = r * 2
+        return result
+    else:
+        return "MathSquare: Sorry but the length can't have a negative value"
+
+# Copyright Zedikon 2022
+# All rights reserved
---
+++ packages/MathSquare/MathSquare-0.0.4.tar.gz/MathSquare-0.0.4/README.md
@@ -0,0 +1,18 @@
+# MathSquare
+MathSquare - python library for work with geometric formulas easy.
+For example, you can solve a problem with the Pythagorean theorem in one line of code using Mathsquare, while on a "naked" python it may take 5-6 lines of code.
+
+# opportunities
+Area calculation
+
+Calculation of the area of a triangle by Heron's theorem
+
+calculation of radius and diameter
+
+calculation of the cathet/hypotenuse according to the Pythagorean theorem
+
+# Future
+Calculation of cosine, sine, tangent
+
+# docs
+GitHub wiki page with docs - https://github.com/Zedikon/MathSquare/wiki/MathSquare-wiki
