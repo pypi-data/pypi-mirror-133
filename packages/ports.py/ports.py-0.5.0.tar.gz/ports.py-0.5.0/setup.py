8f3b911a2dd278ecca5c07a1565cafd32c08fae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:04 +0000

    Add package ports.py/0.5.0/ports.py-0.5.0.tar.gz

---
+++ packages/ports.py/ports.py-0.5.0.tar.gz/ports.py-0.5.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='ports.py',
+      version='0.5.0',
+      description='Modern webserver',
+      url='http://github.com/Ehnryu/ports',
+      author='Ehnryu/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['ports'],
+      #entry_points={
+        #'console_scripts': [
+            #'build = buildproj.cli:launch',
+        #],
+    #},
+    #install_requires=[
+        #'tqdm',
+        #],
+    keywords=['webserver', 'flask',"ports"],
+    classifiers=["Programming Language :: Python :: 3"],
+      zip_safe=False)
