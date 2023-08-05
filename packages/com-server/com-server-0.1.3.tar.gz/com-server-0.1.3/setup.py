b3ed2bfab833fe232bc1cff427981c97ca6cec20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:00 +0000

    Add package com-server/0.1.3/com-server-0.1.3.tar.gz

---
+++ packages/com-server/com-server-0.1.3.tar.gz/com-server-0.1.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    install_requires=[
+        "setuptools>=58.5.3",
+        "wheel>=0.37.0",
+        "pyserial>=3.5",
+        "flask_restful>=0.3.9",
+        "flask-cors>=3.0.10",
+        "docopt>=0.6.2",
+        "waitress>=2.0.0"
+    ]
+)
