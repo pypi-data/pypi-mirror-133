895d63b37b8bded9d48606b07800a034140eca52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package pytorch-adapt/0.0.39/pytorch-adapt-0.0.39.tar.gz

---
+++ packages/pytorch-adapt/pytorch-adapt-0.0.39.tar.gz/pytorch-adapt-0.0.39/src/pytorch_adapt/datasets/utils.py
@@ -0,0 +1,16 @@
+import os
+
+
+def check_img_paths(img_dir, img_paths, domain):
+    for x in img_paths:
+        x = os.path.relpath(x, img_dir)
+        if not x.startswith(domain):
+            raise ValueError(
+                f"img_paths contains a path {x} to an image outside of domain {domain}"
+            )
+
+
+def check_length(cls, correct_length):
+    x = len(cls)
+    if x != correct_length:
+        raise ValueError(f"len(self)={x} but should be {correct_length}")
