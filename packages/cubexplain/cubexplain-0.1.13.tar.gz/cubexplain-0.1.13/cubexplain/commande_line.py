019410369ca89a43bfe7945a5d79ec6f48a330d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:16 +0000

    Add package cubexplain/0.1.13/cubexplain-0.1.13.tar.gz

---
+++ packages/cubexplain/cubexplain-0.1.13.tar.gz/cubexplain-0.1.13/cubexplain/commande_line.py
@@ -0,0 +1,24 @@
+import json
+
+from watchdog.events import FileCreatedEvent, FileSystemEventHandler
+from watchdog.observers.polling import PollingObserver
+
+from . import start_session
+from .atotiwatcher import AtotiWatcher
+
+import sys
+
+
+def main():
+    input_path = "./"
+    print(len(sys.argv))
+    if len(sys.argv) > 2:
+        print("Loading files in ", sys.argv[1])
+        input_path = sys.argv[1]
+    print("input_path", input_path)
+    session = start_session(input_path)
+    print(f"Session running at http://localhost:{session.port}")
+    observer = PollingObserver()
+    observer.schedule(AtotiWatcher(session), input_path)
+    observer.start()
+    session.wait()
