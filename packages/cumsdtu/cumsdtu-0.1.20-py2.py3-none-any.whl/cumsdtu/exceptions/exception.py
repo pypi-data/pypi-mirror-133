06ecb2c2c50cf802b5302868a6f4e2798acd18d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:04 +0000

    Add package cumsdtu/0.1.20/cumsdtu-0.1.20-py2.py3-none-any.whl

---
+++ packages/cumsdtu/cumsdtu-0.1.20-py2.py3-none-any.whl/cumsdtu/exceptions/exception.py
@@ -0,0 +1,11 @@
+class VerifierRequestException(Exception):
+    pass
+
+class BackendError(Exception):
+    pass
+
+class SessionRetrieveException(Exception):
+    pass
+
+class VerificationError(Exception):
+    pass
