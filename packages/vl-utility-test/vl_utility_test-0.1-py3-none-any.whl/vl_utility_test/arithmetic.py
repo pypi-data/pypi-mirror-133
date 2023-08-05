c7daeec3993ca85671193cd84dccae44996b1ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package vl-utility-test/0.1/vl_utility_test-0.1-py3-none-any.whl

---
+++ packages/vl-utility-test/vl_utility_test-0.1-py3-none-any.whl/vl_utility_test/arithmetic.py
@@ -0,0 +1,22 @@
+import math
+
+def addition(x,y):
+    return x+y
+
+def subtraction(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def division(x,y):
+    return x/y
+
+def power(x,y):
+    return math.pow(x,y)
+
+def hello():
+    print('Hello There VL !!!')
+
+
+
