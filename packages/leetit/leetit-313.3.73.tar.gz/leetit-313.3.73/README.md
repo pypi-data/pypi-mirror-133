26e23040009554ae8f894d9a8ba6345aaf11fda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:33 +0000

    Add package leetit/313.3.73/leetit-313.3.73.tar.gz

---
+++ packages/leetit/leetit-313.3.73.tar.gz/leetit-313.3.73/README.md
@@ -0,0 +1,76 @@
+# leetit
+1337 translator lib
+
+# Instalation
+```
+$ pip install leetit
+```
+
+# Usage
+Simple example:  
+```Py
+import leetit
+ 
+print(leetit.leet("xacker"))
+print(leetit.leet("xacker", seed=12345, percent=30))
+```
+
+# Transformations
+In order to get a leetspeak from ordinary English, you need to make three transformations:  
+First you need to replace words and phrases with slang acronyms like "nice one" => "n1" or "owned" => "pwnd". To do this, the leetit library provides the `acronyms` function, which accepts text and an optional seed for the PRNG.
+```Py
+import leetit
+
+print(leetit.acronyms("yeah, easy"))
+print(leetit.acronyms("yeah, easy", seed=12345))
+```
+Secondly, you need to change the morphology of words, for example, replacing the suffixes **-er** and **-or** with **-xor** or **-zor**. To do this, the leetit library provides the `morphology` function.  
+```Py
+import leetit
+
+print(leetit.morphology("xacker"))
+print(leetit.morphology("xacker", seed=12345))
+```
+Thirdly, you need to replace all or some of the characters with others similar to them in various ways, for example, **e** can be replaced with **3** or **&**.  To do this, the `substitution` function is provided. In addition to text, this function can also accept the following parameters:  
+1) The "seed" parameter accepts the seed for the rng.
+2) The "percent" parameter specifies which part of the letters will be changed.
+3) The "alphabet" parameter accepts a dictionary describing the rules for replacing letters (about dictionaries below).
+4) The "chars" parameter accepts a list of letters for which replacement will be performed. By default, all Latin letters are included in this list.
+
+## Alphabets
+Alphabets are dictionaries in which lowercase letters act as the key, and arrays with characters with which this letter can be replaced as the value.  
+The leetit library provides several alphabets out of the box:  
+- leetit.ALPHABET_NUMBERS - contains options for replacing letters with numbers. For example, **e** to **3**.
+- leetit.ALPHABET_ASCII - contains everything that is in leetit.ALPHABET_NUMBERS, and in addition options for replacing letters with other letters and combinations of letters and numbers. For example, **e** to **&**.
+- leetit.ALPHABET_UNICODE_ONLY - contains options for replacing latin letters with special characters and letters of other languages.
+- leetit.ALPHABET_UNICODE - contains a union of leetit.ALPHABET_ASCII and leetit.ALPHABET_UNICODE_ONLY
+
+You can also compose your alphabets.
+
+```Py
+import leetit
+
+print(leetit.substitution("To be, or not to be, that is the question"))
+print(leetit.substitution("To be, or not to be, that is the question", seed=12345))
+print(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_NUMBERS))
+print(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_ASCII))
+print(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_UNICODE_ONLY))
+print(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_UNICODE))
+
+MY_ALPHABET = {
+  "e": ["eeeeeeee"],
+  "o": ["oooooooo"],
+}
+
+print(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=MY_ALPHABET))
+```
+
+And finally, the leetit library provides a `leet` function that performs all three transformations on the text in turn:  
+```Py
+def leet(text: str, seed: int = 1337, percent:int = 50, alphabet = ALPHABET_ASCII, chars = string.ascii_lowercase) -> str
+```
+```Py
+import leetit
+ 
+print(leetit.leet("To be, or not to be, that is the question"))
+```
---
+++ packages/leetit/leetit-313.3.73.tar.gz/leetit-313.3.73/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['leetit', 'leetit.leetit', 'leetit.tests']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'leetit',
+    'version': '313.3.73',
+    'description': '1337 translator lib',
+    'long_description': '# leetit\n1337 translator lib\n\n# Instalation\n```\n$ pip install leetit\n```\n\n# Usage\nSimple example:  \n```Py\nimport leetit\n \nprint(leetit.leet("xacker"))\nprint(leetit.leet("xacker", seed=12345, percent=30))\n```\n\n# Transformations\nIn order to get a leetspeak from ordinary English, you need to make three transformations:  \nFirst you need to replace words and phrases with slang acronyms like "nice one" => "n1" or "owned" => "pwnd". To do this, the leetit library provides the `acronyms` function, which accepts text and an optional seed for the PRNG.\n```Py\nimport leetit\n\nprint(leetit.acronyms("yeah, easy"))\nprint(leetit.acronyms("yeah, easy", seed=12345))\n```\nSecondly, you need to change the morphology of words, for example, replacing the suffixes **-er** and **-or** with **-xor** or **-zor**. To do this, the leetit library provides the `morphology` function.  \n```Py\nimport leetit\n\nprint(leetit.morphology("xacker"))\nprint(leetit.morphology("xacker", seed=12345))\n```\nThirdly, you need to replace all or some of the characters with others similar to them in various ways, for example, **e** can be replaced with **3** or **&**.  To do this, the `substitution` function is provided. In addition to text, this function can also accept the following parameters:  \n1) The "seed" parameter accepts the seed for the rng.\n2) The "percent" parameter specifies which part of the letters will be changed.\n3) The "alphabet" parameter accepts a dictionary describing the rules for replacing letters (about dictionaries below).\n4) The "chars" parameter accepts a list of letters for which replacement will be performed. By default, all Latin letters are included in this list.\n\n## Alphabets\nAlphabets are dictionaries in which lowercase letters act as the key, and arrays with characters with which this letter can be replaced as the value.  \nThe leetit library provides several alphabets out of the box:  \n- leetit.ALPHABET_NUMBERS - contains options for replacing letters with numbers. For example, **e** to **3**.\n- leetit.ALPHABET_ASCII - contains everything that is in leetit.ALPHABET_NUMBERS, and in addition options for replacing letters with other letters and combinations of letters and numbers. For example, **e** to **&**.\n- leetit.ALPHABET_UNICODE_ONLY - contains options for replacing latin letters with special characters and letters of other languages.\n- leetit.ALPHABET_UNICODE - contains a union of leetit.ALPHABET_ASCII and leetit.ALPHABET_UNICODE_ONLY\n\nYou can also compose your alphabets.\n\n```Py\nimport leetit\n\nprint(leetit.substitution("To be, or not to be, that is the question"))\nprint(leetit.substitution("To be, or not to be, that is the question", seed=12345))\nprint(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_NUMBERS))\nprint(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_ASCII))\nprint(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_UNICODE_ONLY))\nprint(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=leetit.ALPHABET_UNICODE))\n\nMY_ALPHABET = {\n  "e": ["eeeeeeee"],\n  "o": ["oooooooo"],\n}\n\nprint(leetit.substitution("To be, or not to be, that is the question", percent=100, alphabet=MY_ALPHABET))\n```\n\nAnd finally, the leetit library provides a `leet` function that performs all three transformations on the text in turn:  \n```Py\ndef leet(text: str, seed: int = 1337, percent:int = 50, alphabet = ALPHABET_ASCII, chars = string.ascii_lowercase) -> str\n```\n```Py\nimport leetit\n \nprint(leetit.leet("To be, or not to be, that is the question"))\n```\n',
+    'author': 'DomesticMoth',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/DomesticMoth/leetit',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
