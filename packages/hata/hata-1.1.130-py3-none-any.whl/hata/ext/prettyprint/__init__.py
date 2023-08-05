1ea1cc3a41794596e50ac7a7d4f78aba13632a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:16 +0000

    Add package hata/1.1.130/hata-1.1.130-py3-none-any.whl

---
+++ packages/hata/hata-1.1.130-py3-none-any.whl/hata/ext/prettyprint/__init__.py
@@ -0,0 +1,7 @@
+from .prettyprint import *
+
+__all__ = prettyprint.__all__
+
+from .. import register_library_extension
+register_library_extension('HuyaneMatsu.prettyprint')
+del register_library_extension
