9223a0d59fabeb7abf206e9501abe25565c0bd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:42 +0000

    Add package order2/0.3.6/order2-0.3.6.tar.gz

---
+++ packages/order2/order2-0.3.6.tar.gz/order2-0.3.6/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='order2',
+    version='0.3.6',
+    py_modules=['order2'],
+    install_requires=['matplotlib', 'ac-electricity'],
+    description='Outils pédagogiques pour l\'étude des systèmes du 2ème ordre',
+    author='Fabrice Sincère',
+    author_email='fabrice.sincere@ac-grenoble.fr',
+    maintainer='Fabrice Sincère',
+    maintainer_email='fabrice.sincere@ac-grenoble.fr',
+    url='https://framagit.org/fsincere/order2/-/tree/master',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: French",
+    ],
+    python_requires='>=3',)
