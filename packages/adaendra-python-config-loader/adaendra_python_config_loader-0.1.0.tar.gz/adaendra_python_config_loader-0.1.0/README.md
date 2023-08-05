31acc1fb3bf231fb41f8efa79e8c5fd4bb2d0290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:10 +0000

    Add package adaendra-python-config-loader/0.1.0/adaendra_python_config_loader-0.1.0.tar.gz

---
+++ packages/adaendra-python-config-loader/adaendra_python_config_loader-0.1.0.tar.gz/adaendra_python_config_loader-0.1.0/README.md
@@ -0,0 +1,28 @@
+# Python Config Loader
+
+![badge](https://img.shields.io/badge/version-0.1.0-blue)
+
+The objective of this library is to easily load external configs for a Python project and use it for anywhere 
+in your project.
+
+---
+
+## How to use it
+```python
+from AdaendraConfigs import AdaendraConfigs
+
+print(AdaendraConfigs.configs.abc)
+```
+
+## Environment variables
+|Name|Description|Default value|
+|---|---|---|
+|CONFIG_ENVIRONMENT|Environment to load|*None*|
+|CONFIG_FOLDER|Path to the config files|'/app/resources'|
+|CONFIG_FILE_EXTENSION|File extensions of your config file. Allow : '.yml'/'.yaml'/'.json'|'.yaml'|
+|CONFIG_PROJECT_NAME|Name of your project *(which is generally the name of the config files)*|'application'|
+
+---
+
+## Documentation
+- [PyPi](./documentation/pypi.md)
---
+++ packages/adaendra-python-config-loader/adaendra_python_config_loader-0.1.0.tar.gz/adaendra_python_config_loader-0.1.0/src/adaendra-python-config-loader/AdaendraConfigs.py
@@ -0,0 +1,55 @@
+from adaendra_immutable_dict.AdaendraImmutableDict import AdaendraImmutableDict
+
+
+class AdaendraConfigs(object):
+    """
+    The config loader.
+    """
+
+    # --- PARAMETERS
+    configs = None
+
+    # --- METHODS
+    def __new__(cls, config_dict: dict):
+        if not AdaendraConfigs.configs:
+            AdaendraConfigs.configs = AdaendraConfigs.__AdaendraConfigs(config_dict)
+        return AdaendraConfigs.configs
+
+    def __getattr__(self, name):
+        if not AdaendraConfigs.configs:
+            raise AttributeError(
+                f"No configs were loaded on start."
+            )
+        return getattr(self.configs, name)
+
+    def __getattribute__(self, name):
+        if not AdaendraConfigs.configs:
+            raise AttributeError(
+                f"No configs were loaded on start."
+            )
+        return getattr(self.configs, name)
+
+    def __setattr__(self, name):
+        """
+        Unsupported method, will raise a TypeError.
+        :raise: TypeError
+        """
+        raise TypeError(
+            f"The configs can't be changed."
+        )
+
+    # --- INTERNAL CLASSES
+    class __AdaendraConfigs:
+        """
+        The internal class to generate the singleton.
+        """
+
+        # --- METHODS
+        def __init__(self, configs_dict: dict):
+            self.configs_dict = AdaendraImmutableDict(configs_dict)
+
+        def __str__(self):
+            return "Configs : " + self.configs_dict.__repr__()
+
+        def __getattr__(self, name):
+            return self.configs_dict[name]
