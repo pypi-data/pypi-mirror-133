27de2b8c158d047c2e5aa7cddee719c6aae3366c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package distributions-from-yi/0.1/distributions_from_yi-0.1.tar.gz

---
+++ packages/distributions-from-yi/distributions_from_yi-0.1.tar.gz/distributions_from_yi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_from_yi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_from_yi'],
+      zip_safe=False)
