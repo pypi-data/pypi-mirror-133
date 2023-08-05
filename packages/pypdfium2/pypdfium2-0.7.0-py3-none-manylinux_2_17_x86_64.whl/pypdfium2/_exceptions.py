8c494c897836ce641f98ca76c09699002e3caf4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:18 +0000

    Add package pypdfium2/0.7.0/pypdfium2-0.7.0-py3-none-manylinux_2_17_x86_64.whl

---
+++ packages/pypdfium2/pypdfium2-0.7.0-py3-none-manylinux_2_17_x86_64.whl/pypdfium2/_exceptions.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+
+class PdfiumError (RuntimeError):
+    """
+    An exception from the PDFium library.
+    """
+    pass
+
+
+class LoadPdfError (PdfiumError):
+    """
+    Raised if ``FPDF_GetPageCount()`` returns a value less than 1.
+    """
+    pass
+
+
+class LoadPageError (PdfiumError):
+    """ Raised if PDFium fails to load a page. """
+    pass
+
+
+class PageIndexError (IndexError):
+    """ Raised on the attempt to load an out-of-bounds page number. """
+    pass
+
+
+class RecusionLimitError (RuntimeError):
+    """ Raised if a recursion depth limit is exceeded. """
+    pass
