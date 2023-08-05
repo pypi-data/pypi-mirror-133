521c1c4495a1d30280e7066408e8d6d85abe1043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package xmath/0.1/xmath-0.1-py3-none-any.whl

---
+++ packages/xmath/xmath-0.1-py3-none-any.whl/xmath/__init__.py
@@ -0,0 +1,28 @@
+import math
+
+# xmath module
+
+def sum(num_1,  num_2):
+    return num_1 + num_2
+
+def dif(num_1, num_2):
+    return num_1 - num_2
+
+def mul(num_1, num_2):
+    return num_1 * num_2
+
+def div(num_1, num_2):
+    return num_1 / num_2
+
+def floor(num_1, num_2):
+    return num_1 // num_2
+
+def sq(num):
+    return num * num
+
+def sqrt(num):
+    sqrt = math.sqrt(num)
+    return print(sqrt)
+
+def cube(num):
+    return num * num * num
