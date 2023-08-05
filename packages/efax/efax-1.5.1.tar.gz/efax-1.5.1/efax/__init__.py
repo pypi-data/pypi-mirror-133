0216815297f279b0b550b7e3b82fa180d6db79d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:08 +0000

    Add package efax/1.5.1/efax-1.5.1.tar.gz

---
+++ packages/efax/efax-1.5.1.tar.gz/efax-1.5.1/efax/__init__.py
@@ -0,0 +1,20 @@
+from ._src import (ExpectationParametrization, HasConjugatePrior, NaturalParametrization, Samplable,
+                   ScalarSupport, SquareMatrixSupport, Support, SymmetricMatrixSupport,
+                   VectorSupport)
+from ._src.distributions import (BernoulliEP, BernoulliNP, BetaEP, BetaNP, ChiEP, ChiNP,
+                                 ChiSquareEP, ChiSquareNP, ComplexCircularlySymmetricNormalEP,
+                                 ComplexCircularlySymmetricNormalNP,
+                                 ComplexMultivariateUnitNormalEP, ComplexMultivariateUnitNormalNP,
+                                 ComplexNormalEP, ComplexNormalNP, DirichletEP, DirichletNP,
+                                 ExponentialEP, ExponentialNP, GammaEP, GammaNP, GeometricEP,
+                                 GeometricNP, IsotropicNormalEP, IsotropicNormalNP, LogarithmicEP,
+                                 LogarithmicNP, MultinomialEP, MultinomialNP,
+                                 MultivariateDiagonalNormalEP, MultivariateDiagonalNormalNP,
+                                 MultivariateDiagonalNormalVP, MultivariateNormalEP,
+                                 MultivariateNormalNP, MultivariateNormalVP,
+                                 MultivariateUnitNormalEP, MultivariateUnitNormalNP,
+                                 NegativeBinomialEP, NegativeBinomialNP, NormalEP, NormalNP,
+                                 PoissonEP, PoissonNP, RayleighEP, RayleighNP, VonMisesFisherEP,
+                                 VonMisesFisherNP, WeibullEP, WeibullNP)
+from ._src.scipy_replacement import (ScipyComplexMultivariateNormal, ScipyComplexNormal,
+                                     ScipyDirichlet, ScipyMultivariateNormal, ScipyVonMises)
