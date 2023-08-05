68eb314862bbebd1907502f5917d75c8a1b8b373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package pincer/0.15.0/pincer-0.15.0-py3-none-any.whl

---
+++ packages/pincer/pincer-0.15.0-py3-none-any.whl/pincer/middleware/channel_create.py
@@ -0,0 +1,52 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""Sent when a channel is created/joined on the client."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ..objects.guild.channel import Channel
+from ..utils.conversion import construct_client_dict
+
+if TYPE_CHECKING:
+    from typing import Tuple
+    from ..core.gateway import GatewayDispatch
+    from ..client import Client
+    from ..core.gateway import Gateway
+
+
+async def channel_create_middleware(
+    self: Client,
+    gateway: Gateway,
+    payload: GatewayDispatch
+) -> Tuple[str, Channel]:
+    """|coro|
+
+    Middleware for the ``on_channel_creation`` event.
+
+    Parameters
+    ----------
+    payload : :class:`~pincer.core.gateway.GatewayDispatch`
+        The data received from the ready event.
+    gateway : :class:`~pincer.core.gateway.Gateway`
+        The gateway for the current shard.
+
+    Returns
+    -------
+    Tuple[:class:`str`, List[:class:`~pincer.objects.guild.channel.Channel`]]
+        ``on_channel_creation`` and a channel.
+    """
+
+    channel: Channel = Channel.from_dict(
+        construct_client_dict(self, payload.data)
+    )
+    self.guilds[channel.guild_id].channels.append(channel)
+
+    self.channels[channel.id] = channel
+
+    return "on_channel_creation", channel
+
+
+def export():
+    return channel_create_middleware
---
+++ packages/pincer/pincer-0.15.0-py3-none-any.whl/pincer/objects/guild/features.py
@@ -0,0 +1,78 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+from __future__ import annotations
+
+from enum import Enum, auto
+
+
+class GuildFeature(Enum):
+    """Represents Guild Features strings.
+
+    Attributes
+    ----------
+    ANIMATED_ICON:
+        Guild can have an animated icon.
+    BANNER:
+        Guild can have a banner.
+    COMMERCE:
+        Guild can have commerce.
+    COMMUNITY:
+        Guild is a community.
+    DISCOVERABLE:
+        Guild can be discovered.
+    FEATURABLE:
+        Guild can be featureurable.Guild can have an invite splash.
+    INVITE_SPLASH:
+        Guild can be featureurable.
+    MEMBER_VERIFICATION_GATE_ENABLED:
+        Guild has member verification enabled.
+    NEWS:
+        Guild has a news channel.
+    PARTNERED:
+        Guild is a partner.
+    PREVIEW_ENABLED:
+        Guild has preview enabled.
+    VANITY_URL:
+        Guild has a vanity url.
+    VERIFIED:
+        Guild is verified.
+    VIP_REGIONS:
+        Guild can have VIP regions.
+    WELCOME_SCREEN_ENABLED:
+        Guild has welcome screen enabled.
+    TICKETED_EVENTS_ENABLED:
+        Guild has ticketed events enabled.
+    MONETIZATION_ENABLED:
+        Guild has monetization enabled.
+    MORE_STICKERS:
+        Guild can have more stickers.
+    THREE_DAY_THREAD_ARCHIVE:
+        Guild can have three day archive time for threads.
+    SEVEN_DAY_THREAD_ARCHIVE:
+        Guild can have seven day archive time for threads.
+    PRIVATE_THREADS:
+        Guild can have private threads.
+    """
+    ANIMATED_ICON = auto()
+    BANNER = auto()
+    COMMERCE = auto()
+    COMMUNITY = auto()
+    DISCOVERABLE = auto()
+    FEATURABLE = auto()
+    INVITE_SPLASH = auto()
+    MEMBER_VERIFICATION_GATE_ENABLED = auto()
+    NEWS = auto()
+    PARTNERED = auto()
+    PREVIEW_ENABLED = auto()
+    VANITY_URL = auto()
+    VERIFIED = auto()
+    VIP_REGIONS = auto()
+    WELCOME_SCREEN_ENABLED = auto()
+    TICKETED_EVENTS_ENABLED = auto()
+    MONETIZATION_ENABLED = auto()
+    MORE_STICKERS = auto()
+    THREE_DAY_THREAD_ARCHIVE = auto()
+    SEVEN_DAY_THREAD_ARCHIVE = auto()
+    PRIVATE_THREADS = auto()
+    NEW_THREAD_PERMISSIONS = auto()
