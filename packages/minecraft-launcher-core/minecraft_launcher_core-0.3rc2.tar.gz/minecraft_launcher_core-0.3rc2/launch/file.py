e528a4ff59654677f9f884e7213fa9693fcc8754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:17 +0000

    Add package minecraft-launcher-core/0.3rc2/minecraft_launcher_core-0.3rc2.tar.gz

---
+++ packages/minecraft-launcher-core/minecraft_launcher_core-0.3rc2.tar.gz/minecraft_launcher_core-0.3rc2/launch/file.py
@@ -0,0 +1,6 @@
+import requests
+class File:
+    def __init__(self,url,path):
+        r = requests.get(url,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'})
+        with open(path, 'wb') as f:
+            f.write(r.content)
---
+++ packages/minecraft-launcher-core/minecraft_launcher_core-0.3rc2.tar.gz/minecraft_launcher_core-0.3rc2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name = 'minecraft_launcher_core',     # 包名
+      version = '0.3rc2',  # 版本号
+      description = 'a package for launch minecraft',
+      long_description = '',
+      author = 'jack253.png',
+      author_email = 'guoxiuchen20170402@163.com',
+      url = 'https://github.com',
+      license = 'MIT',
+      install_requires = [],
+      classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+      ],
+      keywords = '',
+      packages = ['launch','minecraft_server'],  # 必填，就是包的代码主目录
+      include_package_data = True,
+)
