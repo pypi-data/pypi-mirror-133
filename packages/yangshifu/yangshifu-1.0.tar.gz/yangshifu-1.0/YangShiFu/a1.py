1a5f898a95afa7d33924e25a77ca377e9d1c26d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:13 +0000

    Add package yangshifu/1.0/yangshifu-1.0.tar.gz

---
+++ packages/yangshifu/yangshifu-1.0.tar.gz/yangshifu-1.0/YangShiFu/a1.py
@@ -0,0 +1,4 @@
+print("a")
+
+def fun_AA():
+    print("in fun a")
---
+++ packages/yangshifu/yangshifu-1.0.tar.gz/yangshifu-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='yangshifu',#对外我们模块的名字
+    version='1.0',#版本号
+    description='模架佬的第一个作品，测试哦', #描述
+    author='模架编程佬',#作者
+    author_email='386124727@qq.com',
+    py_modules=['YangShiFu.a1','YangShiFu.a2']#要发布的模块
+)
