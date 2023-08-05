c3d759ca49143079c64242a6d2fcfec8a82eaf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:58 +0000

    Add package lambda-actor/0.3.8/lambda_actor-0.3.8-py3-none-any.whl

---
+++ packages/lambda-actor/lambda_actor-0.3.8-py3-none-any.whl/lambda_actor/clear_sqs.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+from datetime import datetime
+import time
+from lambda_actor.aws.sqs import *
+import logging
+from lambda_actor.types.type_conf import ActorConf
+from lambda_actor.types.type_actor_message import *
+
+logger = logging.getLogger()
+
+def clear_all_q(bucket: str, prefix: str, conf_filename: str):
+    """
+    """
+    s3_client = boto3.client('s3')
+    actor_conf = ActorConf.get_actor_conf(s3_client=s3_client, bucket=bucket, prefix=prefix, conf_filename=conf_filename)
+
+    trigger_input_path = f"{actor_conf.trigger_file_prefix}/{actor_conf.trigger_file}"
+    tmp_trigger_input_path = f"/tmp/{actor_conf.trigger_file}"
+    logger.info(f"trigger_input_path: {trigger_input_path}")
+    logger.info(f"tmp_trigger_input_path: {tmp_trigger_input_path}")
+    logger.info(f"actor_conf: {actor_conf}")
+
+    sqs = boto3.resource('sqs', region_name='ap-northeast-1')
+    driver_trigger_q = sqs.get_queue_by_name(QueueName=actor_conf.driver_trigger_q)
+    executor_trigger_q = sqs.get_queue_by_name(QueueName=actor_conf.executor_trigger_q)
+    executor_task_q = sqs.get_queue_by_name(QueueName=actor_conf.executor_task_q)
+
+    logger.info(f"clear driver_trigger_q: {receive_all(driver_trigger_q)}")
+    logger.info(f"clear executor_trigger_q: {receive_all(executor_trigger_q)}")
+    logger.info(f"clear executor_task_q: {receive_all(executor_task_q)}")
