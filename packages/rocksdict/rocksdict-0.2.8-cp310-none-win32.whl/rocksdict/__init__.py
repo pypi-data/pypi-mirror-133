821be3f024c1c9fa0c83b9be155b5a544cc4f6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:59 +0000

    Add package rocksdict/0.2.8/rocksdict-0.2.8-cp310-none-win32.whl

---
+++ packages/rocksdict/rocksdict-0.2.8-cp310-none-win32.whl/rocksdict/__init__.py
@@ -0,0 +1,25 @@
+from .rocksdict import *
+
+__doc__ = rocksdict.__doc__
+
+__all__ = ["Rdict",
+           "RdictIter",
+           "Options",
+           "WriteOptions",
+           "ReadOptions",
+           "DBPath",
+           "MemtableFactory",
+           "BlockBasedOptions",
+           "PlainTableFactoryOptions",
+           "CuckooTableOptions",
+           "UniversalCompactOptions",
+           "UniversalCompactionStopStyle",
+           "SliceTransform",
+           "DataBlockIndexType",
+           "BlockBasedIndexType",
+           "Cache",
+           "DBCompactionStyle",
+           "DBCompressionType",
+           "DBRecoveryMode",
+           "Env",
+           "FifoCompactOptions"]
