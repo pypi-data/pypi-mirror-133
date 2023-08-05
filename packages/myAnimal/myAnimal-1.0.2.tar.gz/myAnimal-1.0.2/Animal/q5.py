fe289b8fa0963ecdfde11def9f4c8a410a07a5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:14 +0000

    Add package myAnimal/1.0.2/myAnimal-1.0.2.tar.gz

---
+++ packages/myAnimal/myAnimal-1.0.2.tar.gz/myAnimal-1.0.2/Animal/q5.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+from random import choice
+
+class Animal(ABC):
+    def __init__(self, name, age, color, isWild, energy=0):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.isWild = isWild
+        self.energy = energy
+
+
+    @abstractmethod
+    def move(self,mile):
+        pass
+       
+
+
+    @abstractmethod
+    def eat(self, calory):
+        pass      
+
+
+  
+
+class Lion(Animal):
+    def __init__(self, name, age, color, isWild, energy=0):
+        super().__init__(name, age, color, isWild, energy=0)
+
+    def move(self,mile):
+        if self.energy > mile:
+            self.energy += mile
+
+        else:
+            print("doesnt enough energy please eat sth...") 
+
+
+    def eat(self, calory):
+        self.energy += calory  
+
+    def __add__(father, mother):
+        wildList = [True, False]
+        isWild = choice(wildList)
+
+        return Lion(father.name, 0, mother.color, isWild) 
+
+
+              
+        
+    
+
+
+
---
+++ packages/myAnimal/myAnimal-1.0.2.tar.gz/myAnimal-1.0.2/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+import setuptools
+
+setuptools.setup(name="myAnimal",
+                version= "1.0.2",
+                long_description= "dont install this",
+                packages= setuptools.find_packages(exclude=['env']))
