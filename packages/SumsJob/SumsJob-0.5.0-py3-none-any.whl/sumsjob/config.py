4b383bf27b88a0e81e097c8c6f50229c63b2b9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:19 +0000

    Add package SumsJob/0.5.0/SumsJob-0.5.0-py3-none-any.whl

---
+++ packages/SumsJob/SumsJob-0.5.0-py3-none-any.whl/sumsjob/config.py
@@ -0,0 +1,31 @@
+# Host names of servers
+servers = ["server1", "server2"]
+
+# In some situations, you may need to first log in a local area network (LAN) to access
+# the servers. In this case, provide the host name of LAN. Also, the same home folder
+# should be shared between LAN and all servers.
+# None if LAN is not required.
+LAN = None
+
+# GPUs to be excluded
+# e.g., gpus_exclude = ["Quadro"] to exclude all GPUs whose name contains "Quadro"
+# Emply list if no GPUs to be excluded
+gpus_exclude = []
+
+# Root folder of the files
+# A new folder will be created in this root folder to store your codes.
+# For example, if the jobname is "myjob", then the folder is "~/scratch/myjob".
+path_prefix = "~/scratch"
+
+# Command to run your job file, i.e., the python path
+cmd = "python"
+
+# Options for [rsync](https://linux.die.net/man/1/rsync) to control what files will be
+# copied from your computer to the server, and what files will be transferred back from
+# the server to your computer.
+# Files copied from your computer to the server, i.e., files required to run the job
+# By default, all .py files in the current folder, including .py files in the subfolders.
+files_push = "--include='[^.]*/' --include='*.py' --exclude='*'"
+# Files transferred back from the server to your computer, i.e., new files you need
+# By default, all .dat and .log files
+files_pull = "--include='*.dat' --include='*.log' --exclude='*'"
---
+++ packages/SumsJob/SumsJob-0.5.0-py3-none-any.whl/sumsjob/submit.py
@@ -0,0 +1,201 @@
+__all__ = ["submit"]
+
+import argparse
+import os
+import pickle
+import random
+import subprocess
+import sys
+import time
+
+sys.path.insert(0, os.path.join(os.path.expanduser("~"), ".sumsjob"))
+import config
+from .gpuresource import gpu_available
+from .utils import local_cmdline
+
+
+def get_machine(machine, gpuid, verbose=0):
+    if machine is None:
+        machine, gpuid = gpu_available(first_only=True, verbose=verbose)
+    else:
+        if gpuid == "-1":
+            if verbose > 0:
+                print("Server: %s, CPU" % machine)
+            gpuid = ""
+        else:
+            if verbose > 0:
+                print("Server: %s, gpuid: %s" % (machine, gpuid))
+    return machine, gpuid
+
+
+def push_files(src, machine, dest, options="", verbose=0):
+    if verbose == 2:
+        options = "-avhP " + options
+    else:
+        options = "-a " + options
+    if config.LAN is not None:
+        cmd = "rsync {} {} {}:{}".format(options, src, config.LAN, dest)
+    else:
+        cmd = "rsync {} {} {}:{}".format(options, src, machine, dest)
+    if verbose == 2:
+        print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
+
+def pull_files(machine, runpath, verbose=0):
+    if verbose == 2:
+        options = "-avhP " + config.files_pull
+    else:
+        options = "-a " + config.files_pull
+    if config.LAN is not None:
+        cmd = "rsync {} {}:{}/ .".format(options, config.LAN, runpath)
+    else:
+        cmd = "rsync {} {}:{}/ .".format(options, machine, runpath)
+    if verbose == 2:
+        print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
+
+def submit_one(
+    jobpy,
+    jobname=None,
+    machine=None,
+    gpuid=None,
+    interact=True,
+    inobj=None,
+    infile="in.pickle",
+    verbose=0,
+):
+    machine, gpuid = get_machine(machine, gpuid, verbose=verbose)
+    if machine is None:
+        return
+
+    if jobname is None:
+        jobname = "%d%04x" % (time.time(), random.randint(0, int("ffff", 16)))
+    runpath = os.path.join(config.path_prefix, jobname)
+
+    push_files("./", machine, runpath, options=config.files_push, verbose=verbose)
+
+    if inobj is not None:
+        tmpfile = "/tmp/sumsjob." + jobname
+        with open(tmpfile, "wb") as f:
+            pickle.dump(inobj, f)
+        push_files(tmpfile, machine, os.path.join(runpath, infile), verbose=verbose)
+        subprocess.check_call(["rm", "-f", tmpfile])
+    sys.stdout.flush()
+
+    if interact:
+        cmd = "cd {} && CUDA_VISIBLE_DEVICES={} {} {} 2>&1 | tee {}.log".format(
+            runpath, gpuid, config.cmd, jobpy, jobname
+        )
+        cmd = local_cmdline(machine, cmd, interact=True, verbose=verbose)
+        subprocess.check_call(cmd, shell=True)
+        pull_files(machine, runpath, verbose=verbose)
+    else:
+        # Start a new screen session in the backgroun; run the code;
+        # As soon as the code finishes, the screen session terminates.
+        # - https://serverfault.com/questions/104668/create-screen-and-run-command-without-attaching
+        # - https://askubuntu.com/questions/62562/run-a-program-with-gnu-screen-and-immediately-detach-after
+        cmd = "cd {} && CUDA_VISIBLE_DEVICES={} {} {} 2>&1 | tee {}.log".format(
+            runpath, gpuid, config.cmd, jobpy, jobname
+        )
+        cmd = f'screen -dmS sumsjob-{jobname} bash -c "{cmd}"'
+        cmd = local_cmdline(machine, cmd, verbose=verbose)
+        subprocess.check_call(cmd, shell=True)
+
+        # If we want to keep the screen session, send command to a detached screen session
+        # - https://raymii.org/s/snippets/Sending_commands_or_input_to_a_screen_session.html
+        # - https://askubuntu.com/questions/983063/start-a-screen-session-and-run-a-script-without-attaching-to-it
+        # Start a detached screen
+        # cmd_server = f"screen -dmS {jobname}"
+        # cmd = f"ssh {machine} '{cmd_server}'"
+        # if verbose == 2:
+        #     print(cmd)
+        # subprocess.check_call(cmd, shell=True)
+        # Send command to that session session
+        # cmd_run = "cd {} && CUDA_VISIBLE_DEVICES={} {} {} 2>&1 | tee {}.log".format(
+        #     runpath, gpuid, config.cmd, jobpy, jobname
+        # )
+        # cmd_server = f'screen -S {jobname} -X stuff "{cmd_run}^M"'
+        # cmd = f"ssh {machine} '{cmd_server}'"
+        # if verbose == 2:
+        #     print(cmd)
+        # subprocess.check_call(cmd, shell=True)
+
+        # Show sessions
+        cmd = "screen -list"
+        cmd = local_cmdline(machine, cmd, verbose=verbose)
+        subprocess.check_call(cmd, shell=True)
+        print(f"Server: {machine}")
+        print(f"Job: {jobname}")
+    return runpath
+
+
+def submit(
+    jobpy,
+    jobname=None,
+    machine=None,
+    gpuid=None,
+    interact=True,
+    inobj=None,
+    infile="in.pickle",
+    retry_num=1,
+    retry_period=60,
+    verbose=0,
+):
+    for _ in range(retry_num - 1):
+        try:
+            return submit_one(
+                jobpy,
+                jobname=jobname,
+                machine=machine,
+                gpuid=gpuid,
+                interact=interact,
+                inobj=inobj,
+                infile=infile,
+                verbose=verbose,
+            )
+        except subprocess.CalledProcessError:
+            if verbose > 0:
+                print("Failed to submit. Wait for %d s..." % retry_period)
+                sys.stdout.flush()
+            time.sleep(retry_period)
+    return submit_one(
+        jobpy,
+        jobname=jobname,
+        machine=machine,
+        gpuid=gpuid,
+        interact=interact,
+        inobj=inobj,
+        infile=infile,
+        verbose=verbose,
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Submit a job to (GPU) servers.")
+    parser.add_argument("jobfile", help="File to be run")
+    parser.add_argument(
+        "jobname",
+        nargs="?",
+        help="Job name, and also the folder name of the job. If not provided, a random number will be used.",
+    )
+    parser.add_argument(
+        "-i", "--interact", action="store_true", help="Submit as an interactive job"
+    )
+    parser.add_argument("-s", "--server", help="Server host name")
+    parser.add_argument("--gpuid", help="GPU ID to be used; -1 to use CPU only")
+    args = parser.parse_args()
+
+    submit(
+        args.jobfile,
+        jobname=args.jobname,
+        machine=args.server,
+        gpuid=args.gpuid,
+        interact=args.interact,
+        verbose=2,
+    )
+
+
+if __name__ == "__main__":
+    main()
