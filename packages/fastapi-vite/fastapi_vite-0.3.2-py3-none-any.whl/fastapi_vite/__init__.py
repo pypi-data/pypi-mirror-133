a755f65d0dc9f0e18762828cb2cb52c21654d8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:52 +0000

    Add package fastapi-vite/0.3.2/fastapi_vite-0.3.2-py3-none-any.whl

---
+++ packages/fastapi-vite/fastapi_vite-0.3.2-py3-none-any.whl/fastapi_vite/__init__.py
@@ -0,0 +1,6 @@
+from fastapi_vite.config import settings
+from fastapi_vite.loader import vite_asset, vite_asset_url, vite_hmr_client
+
+__version__ = "0.3.2"
+
+__all__ = ["vite_asset_url", "vite_hmr_client", "vite_asset", "settings"]
