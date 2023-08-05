bec72b280d191c1fc0f7f5cbdbbd0df78803cc9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:16 +0000

    Add package simpleworkspace/1.1.6/SimpleWorkspace-1.1.6-py3-none-any.whl

---
+++ packages/simpleworkspace/SimpleWorkspace-1.1.6-py3-none-any.whl/SimpleWorkspace/CurrentApp/App.py
@@ -0,0 +1,24 @@
+import os as _os
+import SimpleWorkspace as _sw
+
+
+class App:
+    appName = None
+    appCompany = None
+    appTitle = None
+    appHash = None
+    path_currentAppData = ""
+    path_currentAppData_storage = None
+
+    @staticmethod
+    def Setup(appName, appCompany=None):
+        App.appName = appName
+        App.appCompany = appCompany
+        App.appTitle = appName
+        if appCompany != None:
+            App.appTitle += " - " + appCompany
+        App.path_currentAppData = _sw.Path.GetAppdataPath(appName, appCompany)
+        App.path_currentAppData_storage = _os.path.join(App.path_currentAppData, "storage")
+        _sw.Directory.Create(App.path_currentAppData_storage)
+        _sw.SettingsManager._settingsPath = _os.path.join(App.path_currentAppData, "config.json")
+        _sw.SettingsManager.LoadSettings()
---
+++ packages/simpleworkspace/SimpleWorkspace-1.1.6-py3-none-any.whl/SimpleWorkspace/__init__.py
@@ -0,0 +1,10 @@
+from SimpleWorkspace import ConsoleHelper
+from SimpleWorkspace import Conversion
+from SimpleWorkspace import File
+from SimpleWorkspace import Path
+from SimpleWorkspace import Directory
+from SimpleWorkspace import Utility
+from SimpleWorkspace.CurrentApp.App import App
+from SimpleWorkspace.CurrentApp.Log import Log
+from SimpleWorkspace.CurrentApp.Settings import SettingsManager
+
