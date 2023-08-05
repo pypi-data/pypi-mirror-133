5d3d14283a8b5dcac2e503f857ba9f810d22c1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:41 +0000

    Add package dpys/5.0.0/dpys-5.0.0.tar.gz

---
+++ packages/dpys/dpys-5.0.0.tar.gz/dpys-5.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with open("README.md", encoding="utf-8") as file:
+        return file.read()
+
+
+REQUIREMENTS = [
+    "aiosqlite",
+    "aiohttp",
+    "fastapi",
+    "uvicorn",
+    "aiofiles",
+    "pydantic",
+    "python-multipart",
+    "disnake"
+]
+DOCS = "https://jgltechnologies.com/dpys"
+VERSION = "5.0.0"
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+setup(
+    name="dpys",
+    version=VERSION,
+    description="A library to simplify discord.py",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url=DOCS,
+    author="George Luca",
+    author_email="fixingg@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="discord",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+)
