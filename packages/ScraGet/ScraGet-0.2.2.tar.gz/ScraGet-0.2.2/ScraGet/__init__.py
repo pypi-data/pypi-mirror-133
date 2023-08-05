541d4707b485189ab6b74e712fc03a1873002777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:26 +0000

    Add package ScraGet/0.2.2/ScraGet-0.2.2.tar.gz

---
+++ packages/ScraGet/ScraGet-0.2.2.tar.gz/ScraGet-0.2.2/ScraGet/__init__.py
@@ -0,0 +1,14 @@
+from ScraGet.forum import get_post, get_topic
+from ScraGet.project import get_project
+from ScraGet.cloud import get_cloud_data, cloud
+from ScraGet.studios import get_studio
+from ScraGet.user import get_user, get_user_extra
+from ScraGet.frontpage import get_frontpage
+from ScraGet.encoder import encoder
+from ScraGet import ScraGet
+
+
+"""
+ScraGet - A package used to make it easier to get data from the Scratch API.
+Wiki: https://github.com/Quantum-Codes/ScraGet/wiki
+"""
