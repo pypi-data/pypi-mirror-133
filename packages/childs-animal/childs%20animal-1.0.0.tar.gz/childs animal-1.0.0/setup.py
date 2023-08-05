0757dba2bf808d89481f7b7ebeb1f2ce2fcc5f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:55 +0000

    Add package childs-animal/1.0.0/childs%20animal-1.0.0.tar.gz

---
+++ packages/childs-animal/childs%20animal-1.0.0.tar.gz/childs animal-1.0.0/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(name="childs animal",
+                 version="1.0.0",
+                 long_description="childs animal long discriotion",
+                 packages=setuptools.find_packages(exclude=['env']))
+                 
