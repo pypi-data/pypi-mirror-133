44baf33bcf392ba1ab022cabb64c80186bdca923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:43 +0000

    Add package CryptICE/2.0/CryptICE-2.0.tar.gz

---
+++ packages/CryptICE/CryptICE-2.0.tar.gz/CryptICE-2.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+	name = 'CryptICE',
+	packages = ['CryptICE'],
+	version = '2.0',
+	license = 'MIT',
+	description = 'ICE cipher for Python',
+	author = 'Ren',
+	author_email = 'zeze839@gmail.com',
+	url = 'https://github.com/RenardDev/CryptICE',
+	download_url = 'https://github.com/RenardDev/CryptICE/releases/tag/v2.0',
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9'
+	]
+)
