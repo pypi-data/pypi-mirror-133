e3c3419942517cc39cd7842df6491b6d3a692562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:50 +0000

    Add package django-cd/1.1/django-cd-1.1.tar.gz

---
+++ packages/django-cd/django-cd-1.1.tar.gz/django-cd-1.1/django_cd/apps.py
@@ -0,0 +1,34 @@
+""""""
+
+# Standard library modules.
+import sys
+
+# Third party modules.
+from django.apps import AppConfig
+from django.conf import settings
+
+# Local modules.
+from loguru import logger
+
+# Globals and constants variables.
+
+
+class DjangoCdConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_cd"
+
+    def ready(self):
+        if any("manage.py" in arg for arg in sys.argv) and "runserver" not in sys.argv:
+            return
+
+        from .jobs import Job
+
+        self.jobs = {}
+        for filepath in getattr(settings, "JOBFILES", []):
+            job = Job.from_yaml(filepath)
+            if job.name in self.jobs:
+                logger.error(f"Job {job.name} already added")
+                continue
+
+            job.register()
+            self.jobs[job.name] = job
