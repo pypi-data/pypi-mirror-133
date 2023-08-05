8394ba6bde3bdda72c182157a95760e74a75fa99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:29 +0000

    Add package purgatory-circuitbreaker/0.5.1/purgatory_circuitbreaker-0.5.1-py3-none-any.whl

---
+++ packages/purgatory-circuitbreaker/purgatory_circuitbreaker-0.5.1-py3-none-any.whl/purgatory/service/handlers/circuitbreaker.py
@@ -0,0 +1,48 @@
+from purgatory.domain.messages.commands import CreateCircuitBreaker
+from purgatory.domain.messages.events import (
+    CircuitBreakerCreated,
+    CircuitBreakerFailed,
+    CircuitBreakerRecovered,
+    ContextChanged,
+)
+from purgatory.domain.model import Context
+from purgatory.service.unit_of_work import AbstractUnitOfWork
+
+
+async def register_circuit_breaker(
+    cmd: CreateCircuitBreaker, uow: AbstractUnitOfWork
+) -> Context:
+    """
+    Register circuit breaker in the repository when receiving the CreateCircuitBreaker command.
+    """
+    ret = Context(cmd.name, cmd.threshold, cmd.ttl)
+    await uow.contexts.register(ret)
+    uow.contexts.messages.append(
+        CircuitBreakerCreated(cmd.name, cmd.threshold, cmd.ttl)
+    )
+    return ret
+
+
+async def save_circuit_breaker_state(
+    evt: ContextChanged, uow: AbstractUnitOfWork
+) -> None:
+    """
+    Save the circuit breaker state in the repository when receiving the ContextChanged event.
+    """
+    await uow.contexts.update_state(evt.name, evt.state, evt.opened_at)
+
+
+async def inc_circuit_breaker_failure(
+    evt: CircuitBreakerFailed, uow: AbstractUnitOfWork
+) -> None:
+    """
+    Increment the number of failure in the repository when receiving the CircuitBreakerFailed event.
+    """
+    await uow.contexts.inc_failures(evt.name, evt.failure_count)
+
+
+async def reset_failure(evt: CircuitBreakerRecovered, uow: AbstractUnitOfWork) -> None:
+    """
+    Reset the number of failure in the repository when receiving the CircuitBreakerRecovered event.
+    """
+    await uow.contexts.reset_failure(evt.name)
