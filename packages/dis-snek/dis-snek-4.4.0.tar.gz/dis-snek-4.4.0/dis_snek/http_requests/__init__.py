3bbdcc7c6624c4dba89e778c3bde46bba1fa4877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package dis-snek/4.4.0/dis-snek-4.4.0.tar.gz

---
+++ packages/dis-snek/dis-snek-4.4.0.tar.gz/dis-snek-4.4.0/dis_snek/http_requests/__init__.py
@@ -0,0 +1,13 @@
+from dis_snek.http_requests.bot import BotRequests  # noqa
+from dis_snek.http_requests.channels import ChannelRequests  # noqa
+from dis_snek.http_requests.emojis import EmojiRequests  # noqa
+from dis_snek.http_requests.guild import GuildRequests  # noqa
+from dis_snek.http_requests.interactions import InteractionRequests  # noqa
+from dis_snek.http_requests.members import MemberRequests  # noqa
+from dis_snek.http_requests.messages import MessageRequests  # noqa
+from dis_snek.http_requests.reactions import ReactionRequests  # noqa
+from dis_snek.http_requests.stickers import StickerRequests  # noqa
+from dis_snek.http_requests.threads import ThreadRequests  # noqa
+from dis_snek.http_requests.users import UserRequests  # noqa
+from dis_snek.http_requests.webhooks import WebhookRequests  # noqa
+from dis_snek.http_requests.scheduled_events import ScheduledEventsRequests  # noqa
