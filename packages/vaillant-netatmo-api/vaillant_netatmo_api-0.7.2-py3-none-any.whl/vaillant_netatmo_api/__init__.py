b068ca2538b55145489cb8df96d8af3180f824be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package vaillant-netatmo-api/0.7.2/vaillant_netatmo_api-0.7.2-py3-none-any.whl

---
+++ packages/vaillant-netatmo-api/vaillant_netatmo_api-0.7.2-py3-none-any.whl/vaillant_netatmo_api/__init__.py
@@ -0,0 +1,61 @@
+"""Module HTTP communication with the Netatmo API."""
+
+from .auth import AuthClient, auth_client
+from .errors import (
+    ApiException,
+    NonOkResponseException,
+    NetworkException,
+    NetworkTimeoutException,
+    NonRetryableException,
+    RequestBackoffException,
+    RequestClientException,
+    RequestException,
+    RequestServerException,
+    RequestUnauthorizedException,
+    RetryableException,
+    UnsuportedArgumentsException,
+)
+from .thermostat import (
+    Device,
+    Measured,
+    Module,
+    Program,
+    Setpoint,
+    SetpointMode,
+    SystemMode,
+    ThermostatClient,
+    TimeSlot,
+    Zone,
+    thermostat_client,
+)
+from .token import Token, TokenStore
+
+__all__ = [
+    "AuthClient",
+    "ThermostatClient",
+    "ApiException",
+    "NonOkResponseException",
+    "NetworkException",
+    "NetworkTimeoutException",
+    "NonRetryableException",
+    "RequestBackoffException",
+    "RequestClientException",
+    "RequestException",
+    "RequestServerException",
+    "RequestUnauthorizedException",
+    "RetryableException",
+    "UnsuportedArgumentsException",
+    "Device",
+    "Module",
+    "Program",
+    "Zone",
+    "TimeSlot",
+    "Setpoint",
+    "Measured",
+    "SystemMode",
+    "SetpointMode",
+    "Token",
+    "TokenStore",
+    "auth_client",
+    "thermostat_client",
+]
