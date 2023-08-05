4e9726e2e70e632692669dde277816135b1674cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:35 +0000

    Add package nifti-snapshot/0.1.0/nifti_snapshot-0.1.0.tar.gz

---
+++ packages/nifti-snapshot/nifti_snapshot-0.1.0.tar.gz/nifti_snapshot-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import setuptools
+from os.path import join
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = open('requirements.txt').read().split()
+
+setup(
+    name='nifti_snapshot',
+    version='v0.1.0',
+    description='First release to test pypi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='kcho',
+    author_email='kevincho@bwh.harvard.edu',
+    url='https://github.com/pnlbwh/nifti-snapshot',
+    download_url='https://github.com/pnlbwh/nifti-snapshot/archive/refs/tags/nifti-snapshot.zip',
+    keywords=['nifti', 'snapshot'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+    install_requires=requirements,
+    scripts=['bin/nifti_snapshot']
+)
