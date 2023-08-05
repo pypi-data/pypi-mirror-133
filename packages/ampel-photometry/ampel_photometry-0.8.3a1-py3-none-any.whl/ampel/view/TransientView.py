fd1c696b83320b6a428c4d7fa683ae26d88ab5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package ampel-photometry/0.8.3a1/ampel_photometry-0.8.3a1-py3-none-any.whl

---
+++ packages/ampel-photometry/ampel_photometry-0.8.3a1-py3-none-any.whl/ampel/view/TransientView.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                Ampel-photometry/ampel/view/TransientView.py
+# License:             BSD-3-Clause
+# Author:              valery brinnel <firstname.lastname@gmail.com>
+# Date:                13.01.2018
+# Last Modified Date:  17.06.2021
+# Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
+
+from collections.abc import Sequence
+from ampel.content.DataPoint import DataPoint
+from ampel.view.LightCurve import LightCurve
+from ampel.view.SnapView import SnapView
+
+
+class TransientView(SnapView):
+
+	__slots__ = "lightcurve",
+
+	lightcurve: None | Sequence[LightCurve]
+
+	def __init__(self, *args, **kwargs) -> None:
+		super().__init__(*args, **kwargs)
+
+		if self.t0 and self.t1:
+			lightcurve: None | Sequence[LightCurve] = tuple(
+				LightCurve.build(comp, tuple(el for el in self.t0 if el['id'] in comp['dps']))
+				for comp in self.t1
+			)
+		else:
+			lightcurve = None
+		object.__setattr__(self, "lightcurve", lightcurve)
+
+
+	def get_photopoints(self) -> None | Sequence[DataPoint]:
+
+		if not self.t0:
+			return None
+
+		# By convention photopoints have positive int ids
+		return [dp for dp in self.t0 if dp['id'] > 0]
+
+
+	def get_upperlimits(self) -> None | Sequence[DataPoint]:
+
+		if not self.t0:
+			return None
+
+		# By convention photopoints have negative int ids
+		return [dp for dp in self.t0 if dp['id'] < 0]
+
+
+	def get_lightcurves(self) -> None | Sequence[LightCurve]:
+		return self.lightcurve
