e63b099371f9450e747cf8012b88f302fb0fa5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:38 +0000

    Add package fastpip/0.11.7/fastpip-0.11.7-py3-none-any.whl

---
+++ packages/fastpip/fastpip-0.11.7-py3-none-any.whl/fastpip/versions.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+NAME = "fastpip"
+VERNUM = 0, 11, 7
+AUTHOR = "hrpzcf"
+EMAIL = "hrpzcf@foxmail.com"
+WEBSITE = "https://gitee.com/hrpzcf/fastpip"
+VERSION = ".".join(map(str, VERNUM))
+
+__all__ = ["NAME", "AUTHOR", "EMAIL", "WEBSITE", "VERSION", "VERNUM"]
