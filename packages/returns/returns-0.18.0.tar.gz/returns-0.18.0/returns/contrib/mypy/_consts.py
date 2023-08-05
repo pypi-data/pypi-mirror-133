24cf53d88f1cea43af3aaa958eb07ce537b53981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:04 +0000

    Add package returns/0.18.0/returns-0.18.0.tar.gz

---
+++ packages/returns/returns-0.18.0.tar.gz/returns-0.18.0/returns/contrib/mypy/_consts.py
@@ -0,0 +1,24 @@
+from typing_extensions import Final
+
+# Constant fullnames for typechecking
+# ===================================
+
+#: Used for typed ``partial`` function.
+TYPED_PARTIAL_FUNCTION: Final = 'returns.curry.partial'
+
+#: Used for typed ``curry`` decorator.
+TYPED_CURRY_FUNCTION: Final = 'returns.curry.curry'
+
+#: Used for typed ``flow`` call.
+TYPED_FLOW_FUNCTION: Final = 'returns._internal.pipeline.flow.flow'
+
+#: Used for typed ``pipe`` call.
+TYPED_PIPE_FUNCTION: Final = 'returns._internal.pipeline.pipe.pipe'
+TYPED_PIPE_METHOD: Final = 'returns._internal.pipeline.pipe._Pipe.__call__'
+
+#: Used for HKT emulation.
+TYPED_KINDN: Final = 'returns.primitives.hkt.KindN'
+TYPED_KINDN_ACCESS: Final = '{0}.'.format(TYPED_KINDN)
+TYPED_KIND_DEKIND: Final = 'returns.primitives.hkt.dekind'
+TYPED_KIND_KINDED_CALL: Final = 'returns.primitives.hkt.Kinded.__call__'
+TYPED_KIND_KINDED_GET: Final = 'returns.primitives.hkt.Kinded.__get__'
