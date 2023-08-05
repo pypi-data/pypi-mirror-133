bf80f0a3753c7f1250d6498d9944a4f19ce22344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:18 +0000

    Add package latino/0.1.1/latino-0.1.1.tar.gz

---
+++ packages/latino/latino-0.1.1.tar.gz/latino-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='latino',
+    version='0.1.1',
+    author="Daniele Lin",
+    author_email="lindaniele25@gmail.com",
+    license="MIT",
+    description="Traduttore Latino: lemma, paradigma, grammatica e traduzione da Dizionario Olivetti ©.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lindaniele/latino",
+    packages=setuptools.find_packages(),
+    keywords=['latino', 'dizionario', 'olivetti', 'traduttore', 'latin', 'paradigma', 'traduzione', 'translator', 'translate', 'latinum', 'paradigm', 'grammar', 'api'],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'lxml',
+        'bs4',
+        'requests',
+    ],
+    python_requires=">=3.8",
+)
