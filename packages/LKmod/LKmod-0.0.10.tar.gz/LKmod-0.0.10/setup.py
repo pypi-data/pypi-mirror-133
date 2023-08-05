f7e238e439a73f85e7fc55630109b2f96aa644e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:00 +0000

    Add package LKmod/0.0.10/LKmod-0.0.10.tar.gz

---
+++ packages/LKmod/LKmod-0.0.10.tar.gz/LKmod-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.10'
+DESCRIPTION = 'lock and key'
+
+# Setting up
+setup(
+    name="LKmod",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python']
+)
