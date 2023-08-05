1fab998e39205c3e1f1d60b70d33052242811b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:21 +0000

    Add package isocrypt/0.1.1/isocrypt-0.1.1.tar.gz

---
+++ packages/isocrypt/isocrypt-0.1.1.tar.gz/isocrypt-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['isocrypt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['humanfriendly>=10.0,<11.0', 'typer>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['poetry = isocrypt.main:app']}
+
+setup_kwargs = {
+    'name': 'isocrypt',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Poe Rhiza',
+    'author_email': 'poerhiz@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
