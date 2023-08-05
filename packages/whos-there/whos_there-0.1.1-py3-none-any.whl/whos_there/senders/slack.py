881f50c50fefa99c58eb059d068ec4ad6340bba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:36 +0000

    Add package whos-there/0.1.1/whos_there-0.1.1-py3-none-any.whl

---
+++ packages/whos-there/whos_there-0.1.1-py3-none-any.whl/whos_there/senders/slack.py
@@ -0,0 +1,24 @@
+import json
+from typing import List
+
+import requests
+
+from whos_there.senders.base import Sender
+
+
+class SlackSender(Sender):
+    def __init__(self, webhook_url: str, channel: str, user_mentions: List[str] = []) -> None:
+        super().__init__()
+        self.webhook_url = webhook_url
+        self.channel = channel
+        self.user_mentions = " ".join(user_mentions)
+
+    def send(self, text: str) -> None:
+        data = {
+            "username": "Knock Knock",
+            "channel": self.channel,
+            "link_names": 1,
+            "icon_emoji": ":clapper:",  # ":tada:"
+            "text": f"{text} {self.user_mentions}",
+        }
+        return self._send_json(self.webhook_url, data)
