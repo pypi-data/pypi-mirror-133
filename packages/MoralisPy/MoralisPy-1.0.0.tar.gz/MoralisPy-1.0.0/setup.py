b547fcd9739812c0c8256760ae8d67e5043f6e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package MoralisPy/1.0.0/MoralisPy-1.0.0.tar.gz

---
+++ packages/MoralisPy/MoralisPy-1.0.0.tar.gz/MoralisPy-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MoralisPy',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests*']),
+    license='GNU General Public License v3.0',
+    description='A python wrapper for the Molaris REST API',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    url='https://github.com/bberg/MoralisPy',
+    author='Ben Berg',
+    author_email='benberg00@gmail.com',
+    long_description_content_type='text/markdown'
+)
