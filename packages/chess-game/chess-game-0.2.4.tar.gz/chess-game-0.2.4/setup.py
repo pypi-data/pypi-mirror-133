d3ab8077e262e567d86022487dbe9cc1ff496f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:29 +0000

    Add package chess-game/0.2.4/chess-game-0.2.4.tar.gz

---
+++ packages/chess-game/chess-game-0.2.4.tar.gz/chess-game-0.2.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'chess-game',
+  packages = ['chess_game'],
+  version = '0.2.4',
+  license='GPL-3.0+',
+  description = 'A simple chess library',
+  author = 'Utkrisht Sinha',
+  author_email = '',
+  url = 'https://github.com/Utkrisht-1997/chess-game',
+  download_url = 'https://github.com/Utkrisht-1997/chess-game/archive/refs/tags/v_024.tar.gz',
+  keywords = ['chess', 'game', 'python'],
+  install_requires=['colorama'],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
