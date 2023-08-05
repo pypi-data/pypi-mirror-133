1f9a6ac4aaadc613394b53a1278600c990a8f0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:57 +0000

    Add package IPRA/1.0.16/IPRA-1.0.16-py3-none-any.whl

---
+++ packages/IPRA/IPRA-1.0.16-py3-none-any.whl/ipra/Model/policyCollection.py
@@ -0,0 +1,54 @@
+
+class PolicyCollection:
+        
+    def __init__(self):
+        self.AIA = ['AIA']
+        self.AXA = ['AXA']
+        self.BOCG = ['BOCG']
+        self.CHINALIFE = ['CHINALIFE']
+        self.PRU = ['PRU']
+        self.FWD = ['FWD']
+        self.MANULIFE = ['MANULIFE']
+        self.YFLIFE = ['YFL']
+        self.SUNLIFE = ['SUNLIFE']
+        self.totalInsurranceList = [
+            self.AIA,
+            self.AXA,
+            self.BOCG,
+            self.CHINALIFE,
+            self.PRU,
+            self.FWD,
+            self.MANULIFE,
+            self.YFLIFE,
+            self.SUNLIFE
+        ]
+        self.supportedList = ['AIA','AXA','BOCG','CHINALIFE','PRU','FWD','MANULIFE','YFL','SUNLIFE']
+        pass
+    
+    def cleanAllPolicy(self):
+        self.AIA = ['AIA']
+        self.AXA = ['AXA']
+        self.BOCG = ['BOCG']
+        self.CHINALIFE = ['CHINALIFE']
+        self.PRU = ['PRU']
+        self.FWD = ['FWD']
+        self.MANULIFE = ['MANULIFE']
+        self.YFLIFE = ['YFL']
+        self.SUNLIFE = ['SUNLIFE']
+        self.totalInsurranceList = [
+            self.AIA,
+            self.AXA,
+            self.BOCG,
+            self.CHINALIFE,
+            self.PRU,
+            self.FWD,
+            self.MANULIFE,
+            self.YFLIFE,
+            self.SUNLIFE
+        ]
+    
+    def getTotalList(self):
+        return self.totalInsurranceList
+
+    def getSupportedList(self):
+        return self.supportedList
