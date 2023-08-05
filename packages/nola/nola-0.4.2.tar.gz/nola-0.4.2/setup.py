bb323d0cf01c66cc987db442d6301be412531287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package nola/0.4.2/nola-0.4.2.tar.gz

---
+++ packages/nola/nola-0.4.2.tar.gz/nola-0.4.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nola',
+    version='0.4.2',
+    packages=['stations'],
+    url='',
+    license='0.2',
+    author='kerstinFrailey',
+    author_email='frailey.work@gmail.com',
+    description=''
+)
