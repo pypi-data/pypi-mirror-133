9c6197554bc66d15ff32bf197ccfa4af938fc27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:03 +0000

    Add package vital/1.0.1/vital-1.0.1.tar.gz

---
+++ packages/vital/vital-1.0.1.tar.gz/vital-1.0.1/vital/api/__init__.py
@@ -0,0 +1,23 @@
+from vital.api.activity import Activity
+from vital.api.body import Body
+from vital.api.link import Link
+from vital.api.provider_specific import ProviderSpecific
+from vital.api.sleep import Sleep
+from vital.api.testkits import Testkits
+from vital.api.user import User
+from vital.api.vitals import Vitals
+from vital.api.webhooks import Webhooks
+from vital.api.workouts import Workouts
+
+__all__ = [
+    "Link",
+    "Body",
+    "Activity",
+    "Sleep",
+    "ProviderSpecific",
+    "User",
+    "Workouts",
+    "Webhooks",
+    "Vitals",
+    "Testkits",
+]
