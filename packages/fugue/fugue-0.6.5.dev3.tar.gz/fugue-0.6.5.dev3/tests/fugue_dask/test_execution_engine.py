92997d997cf4412d0afb3c3bd3c2a4e30ed67195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package fugue/0.6.5.dev3/fugue-0.6.5.dev3.tar.gz

---
+++ packages/fugue/fugue-0.6.5.dev3.tar.gz/fugue-0.6.5.dev3/tests/fugue_dask/test_execution_engine.py
@@ -0,0 +1,80 @@
+import dask.dataframe as dd
+import pandas as pd
+from fugue.collections.partition import PartitionSpec
+from fugue.dataframe.pandas_dataframe import PandasDataFrame
+from fugue.dataframe.utils import _df_eq as df_eq
+from fugue.workflow.workflow import FugueWorkflow
+from fugue_test.builtin_suite import BuiltInTests
+from fugue_test.execution_suite import ExecutionEngineTests
+
+from fugue_dask.execution_engine import DaskExecutionEngine
+
+
+class DaskExecutionEngineTests(ExecutionEngineTests.Tests):
+    def make_engine(self):
+        e = DaskExecutionEngine(dict(test=True))
+        return e
+
+    def test__join_outer_pandas_incompatible(self):
+        return
+
+    def test_map_with_dict_col(self):
+        # TODO: add back
+        return
+
+    def test_to_df(self):
+        e = self.engine
+        a = e.to_df([[1, 2], [3, 4]], "a:int,b:int", dict(a=1))
+        df_eq(a, [[1, 2], [3, 4]], "a:int,b:int", dict(a=1), throw=True)
+        a = e.to_df(PandasDataFrame([[1, 2], [3, 4]], "a:int,b:int", dict(a=1)))
+        df_eq(a, [[1, 2], [3, 4]], "a:int,b:int", dict(a=1), throw=True)
+        assert a is e.to_df(a)
+
+    def test_repartition(self):
+        e = self.engine
+        a = e.to_df([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], "a:int,b:int")
+        b = e.repartition(a, PartitionSpec())
+        assert a is b
+        b = e.repartition(a, PartitionSpec(num=3))
+        assert 3 == b.num_partitions
+        b = e.repartition(a, PartitionSpec(num="0"))
+        assert a is b
+        b = e.repartition(a, PartitionSpec(num="ROWCOUNT"))
+        assert 5 == b.num_partitions
+        b = e.repartition(a, PartitionSpec(num="ROWCOUNT/2"))
+        assert 2 == b.num_partitions
+        b = e.repartition(a, PartitionSpec(num="ROWCOUNT-ROWCOUNT"))
+        assert a is b
+        b = e.repartition(a, PartitionSpec(by=["a"], num=3))
+        assert a.num_partitions == b.num_partitions
+
+    def test_sample_n(self):
+        # TODO: dask does not support sample by number of rows
+        pass
+
+
+class DaskExecutionEngineBuiltInTests(BuiltInTests.Tests):
+    def make_engine(self):
+        e = DaskExecutionEngine(dict(test=True))
+        return e
+
+    def test_default_init(self):
+        a = FugueWorkflow().df([[0]], "a:int")
+        df_eq(a.compute(DaskExecutionEngine), [[0]], "a:int")
+
+    def test_annotation(self):
+        def m_c(engine: DaskExecutionEngine) -> dd.DataFrame:
+            return dd.from_pandas(
+                pd.DataFrame([[0]], columns=["a"]), npartitions=2
+            )
+
+        def m_p(engine: DaskExecutionEngine, df: dd.DataFrame) -> dd.DataFrame:
+            return df
+
+        def m_o(engine: DaskExecutionEngine, df: dd.DataFrame) -> None:
+            assert 1 == df.compute().shape[0]
+
+        with self.dag() as dag:
+            df = dag.create(m_c).process(m_p)
+            df.assert_eq(dag.df([[0]], "a:long"))
+            df.output(m_o)
