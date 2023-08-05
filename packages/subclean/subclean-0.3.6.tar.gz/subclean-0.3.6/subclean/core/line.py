967a020bab20cb24682804c544e9ea346c8c7af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:21 +0000

    Add package subclean/0.3.6/subclean-0.3.6.tar.gz

---
+++ packages/subclean/subclean-0.3.6.tar.gz/subclean-0.3.6/subclean/core/line.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import re
+
+
+class Line(str):
+    def __len__(self) -> int:
+        return len(str(self.strip_styles()))
+
+    def strip_styles(self) -> Line:
+        return self.sub(r"<[^>]*>", "").sub(r"{[^}]*}", "")
+
+    def sub(self, regex, replacement: str) -> Line:
+        return Line(re.sub(regex, replacement, self))
+
+    def strip(self, chars=None) -> Line:
+        return Line(super().strip(chars))
+
+    def is_dialog(self) -> bool:
+        return bool(re.search(r"^(<\/?i>)*[-]", self))
+
+    @staticmethod
+    def merge(lines: list[Line]) -> Line:
+        return Line(" ".join(lines))
