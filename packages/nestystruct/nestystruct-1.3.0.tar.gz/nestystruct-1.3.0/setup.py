fa74227028f6c0b676f2fad7f33245ac738135b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:24 +0000

    Add package nestystruct/1.3.0/nestystruct-1.3.0.tar.gz

---
+++ packages/nestystruct/nestystruct-1.3.0.tar.gz/nestystruct-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nestystruct',
+    version='1.3.0',
+    py_modules=['nestystruct'],
+    author='Nelson Machado Junior',
+    author_email='nelsonmjunior@gmail.com',
+    url='http://www.christiansngeeks.blogspot.com',
+    description='A simple printer of nested lists',
+)
