13fa59662a2904e35b8bc6ecdb0d7e6874b73f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:15 +0000

    Add package gurun/0.2.1/gurun-0.2.1.tar.gz

---
+++ packages/gurun/gurun-0.2.1.tar.gz/gurun-0.2.1/gurun/__init__.py
@@ -0,0 +1,14 @@
+"""Framework de autotomação de tarefas"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
