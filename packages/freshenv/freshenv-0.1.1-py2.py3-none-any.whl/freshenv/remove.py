11faf65d424e151a45d08ef70c32a93c331cc0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:48 +0000

    Add package freshenv/0.1.1/freshenv-0.1.1-py2.py3-none-any.whl

---
+++ packages/freshenv/freshenv-0.1.1-py2.py3-none-any.whl/freshenv/remove.py
@@ -0,0 +1,25 @@
+import click
+from docker import APIClient, errors
+from rich import print
+
+
+
+
+@click.command("remove")
+@click.option("--name","-n",required=True,help="Name of your environment to remove.")
+@click.option('--force', '-f', is_flag=True, help="Force remove an environment.")
+def remove(name: str, force: bool) -> None:
+    """Remove a freshenv environment."""
+    try:
+        client = APIClient(base_url="unix://var/run/docker.sock")
+        client.remove_container(container=name, force=force)
+        print(f":boom: {name} environment removed.")
+    except errors.NotFound:
+        print(f":ghost: No freshenv environment called [bold]{name}[/bold] found.")
+    except errors.APIError as e:
+        if e.status_code == 409:
+            print(f":runner: {name} is a [bold green]running[/bold green] environment. Close the session first or use the [bold blue]--force[/bold blue] flag.")
+        else:
+            raise Exception(e)
+    except Exception as e:
+        print("Unknown exception: {}".format(e))
