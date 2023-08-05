9b65f64fee4e15760283781e9664ba47f4f8840e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:54 +0000

    Add package e2L/0.0.3/e2L-0.0.3.tar.gz

---
+++ packages/e2L/e2L-0.0.3.tar.gz/e2L-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="e2L", 
+        version='0.0.3',
+        license='MIT',
+        author="Raphaël Nussbaumer",
+        author_email="<rafnuss@gmail.com>",
+        description='Generate custom PDF checklists with eBird occurrence data.',
+        long_description='eBirdToLaTex Checklist Generator is a Python module which generates a customisable bird checklist based on a specific dataset downloaded from eBird.',
+        url="https://github.com/Zoziologie/ebird2latex",
+        download_url="https://github.com/Zoziologie/ebird2latex/archive/refs/tags/v0.0.3.tar.gz",
+        py_modules=["e2L"],
+        install_requires=['requests'],
+        
+        keywords=['python', 'eBird', 'LaTeX'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
