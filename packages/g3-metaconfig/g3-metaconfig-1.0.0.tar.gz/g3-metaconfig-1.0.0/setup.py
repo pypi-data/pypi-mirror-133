462dc8f798ed8c1bb9d4f27e0e494ccc652197f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:42 +0000

    Add package g3-metaconfig/1.0.0/g3-metaconfig-1.0.0.tar.gz

---
+++ packages/g3-metaconfig/g3-metaconfig-1.0.0.tar.gz/g3-metaconfig-1.0.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    install_reqs = f.read().splitlines()
+
+setuptools.setup(
+    name="g3-metaconfig",
+    version="1.0.0",
+    author="Kirill Potapenko",
+    author_email="ajiadb9@bgd.team",
+    description="Simple metaclass to simplify work with argparse",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://bgd.team",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    install_requires=install_reqs,
+    packages=["g3_metaconfig", "g3_metaconfig.tests"],
+    python_requires=">=3.8",
+)
