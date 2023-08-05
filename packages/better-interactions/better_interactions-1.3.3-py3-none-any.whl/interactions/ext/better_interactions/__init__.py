259cf7ed483c41d050641d2f729515dbcb98afaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:06 +0000

    Add package better-interactions/1.3.3/better_interactions-1.3.3-py3-none-any.whl

---
+++ packages/better-interactions/better_interactions-1.3.3-py3-none-any.whl/interactions/ext/better_interactions/__init__.py
@@ -0,0 +1,6 @@
+from . import *
+
+from .callback import component
+from .subcomand import subcommand
+from .components import ActionRow, Button, SelectMenu, spread_to_rows
+from .setup import setup
