adf372c757221bfef2e171d0fb3d0899a40c492f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:47 +0000

    Add package setux-modules/0.21.52.0/setux_modules-0.21.52.0.tar.gz

---
+++ packages/setux-modules/setux_modules-0.21.52.0.tar.gz/setux_modules-0.21.52.0/setux/modules/prepare.py
@@ -0,0 +1,18 @@
+from setux.core.module import Module
+
+
+class Distro(Module):
+    '''Minimum System Requieremnts
+    '''
+    def do_deploy(self, target, **kw):
+        return self.install(target,
+            pkg = 'tmux vim',
+        )
+
+
+class FreeBSD(Distro):
+    def do_deploy(self, target, **kw):
+        return self.install(target,
+            pkg = 'sudo bash python',
+        )
+
