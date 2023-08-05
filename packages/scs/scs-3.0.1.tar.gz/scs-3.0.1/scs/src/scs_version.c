f0d2844353d98ea39c187acaf7857b9007c63100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:18 +0000

    Add package scs/3.0.1/scs-3.0.1.tar.gz

---
+++ packages/scs/scs-3.0.1.tar.gz/scs-3.0.1/scs/src/scs_version.c
@@ -0,0 +1,19 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "glbopts.h"
+
+const char *SCS(version)(void) {
+  return SCS_VERSION;
+}
+size_t SCS(sizeof_int)(void) {
+  return sizeof(scs_int);
+}
+size_t SCS(sizeof_float)(void) {
+  return sizeof(scs_float);
+}
+
+#ifdef __cplusplus
+}
+#endif
