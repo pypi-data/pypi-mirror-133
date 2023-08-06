8c951b720017a24d04f01d7276f42289d652b811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:22 +0000

    Add package bddl/0.0.5/bddl-0.0.5.tar.gz

---
+++ packages/bddl/bddl-0.0.5.tar.gz/bddl-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# TODO package data
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+print('FIND PACKAGES OUTPUT:', find_packages())
+
+
+setup(
+    name='bddl',
+    version='0.0.5',
+    author='Stanford University',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/StanfordVL/bddl',
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'numpy',
+        'networkx',
+        'jupytext'
+    ],
+    package_data={},
+    include_package_data=True
+)
