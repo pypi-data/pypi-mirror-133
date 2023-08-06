cb169e4e452c6611ef6e841f64f865a125e58f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:50 +0000

    Add package msf/0.1.5.1/msf-0.1.5.1-py3-none-any.whl

---
+++ packages/msf/msf-0.1.5.1-py3-none-any.whl/msf/engine/engine.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+'''
+@create_time: 2021/11/17 14:49:28
+@author: lichunyu
+'''
+import json
+
+from flask import Flask, request
+
+from msf.core.core import Graph
+from msf.engine.utils import response_package, param_parse
+
+
+class Engine(object):
+
+    def __init__(self, graph: Graph=None, resource:dict=None, name=__name__) -> None:
+        super().__init__()
+        self.graph = graph if isinstance(graph, Graph) else Graph(graph)
+        self.resource = resource if resource is not None else {}
+        self.create_app(name)
+
+    def flow_mixin(self, path):
+        param = param_parse(request)
+        result = self.graph[path].walk(_resource=self.resource, _param=param)
+        result = response_package(result)
+        return json.dumps(result, ensure_ascii=False)
+
+    def create_app(self, name):
+        self.app = Flask(name)
+        # self.app.register_error_handler()
+        self.app.route('/<path>', methods=['POST'])(self.flow_mixin)
+        return self.app
+
+    def run(self, *args, **kwds):
+        self.app.run(*args, **kwds)
