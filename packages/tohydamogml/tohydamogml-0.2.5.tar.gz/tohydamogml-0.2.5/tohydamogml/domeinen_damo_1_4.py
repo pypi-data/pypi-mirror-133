3cfdc5bc84c59782b75f8cdbed8c66e98b168d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:01 +0000

    Add package tohydamogml/0.2.5/tohydamogml-0.2.5.tar.gz

---
+++ packages/tohydamogml/tohydamogml-0.2.5.tar.gz/tohydamogml-0.2.5/tohydamogml/domeinen_damo_1_4.py
@@ -0,0 +1,103 @@
+
+TYPEKRUISING = {
+    1:	"aquaduct",
+    2:	"brug",
+    3:	"duiker",
+    4:	"sifon",
+    5:	"hevel",
+    6:	"bypass"
+                }
+
+MATERIAALKUNSTWERK = {
+    1: "aluminium",
+    2: "asbestcement",
+    3: "beton",
+    4: "gegolfd plaatstaal",
+    5: "gewapend beton",
+    6: "gietijzer",
+    7: "glad staal",
+    8: "glas",
+    9: "grasbetontegels",
+    10: "hout",
+    11: "ijzer",
+    12: "koper",
+    13: "kunststof",
+    14: "kunststoffolie",
+    15: "kurk",
+    16: "lood",
+    17: "metselwerk",
+    18: "plaatstaal",
+    19: "puinsteen",
+    20: "PVC",
+    21: "staal",
+    22: "steen",
+    23: "voorgespannen beton",
+    24: "riet en/of biezen",
+    25: "zand",
+    26: "gips",
+    28: "roestvrij staal",
+    27: "gres",
+    29: "veen",
+    30: "klei",
+    31: "lokale bodemsoort"
+}
+
+TYPESTUW = {
+    1: "schotbalkstuw",
+    2: "stuw met schuif",
+    3: "stuw met klep",
+    4: "segmentstuw",
+    5: "cascadestuw",
+    6: "hevelstuw",
+    7: "meetstuw",
+    8: "meetschot",
+    9: "stuw met contra-gewicht",
+    10: "inlaat- en/of aflaatstuw",
+    11: "overlaat",
+    12: "drijverstuw",
+    13: "trommelstuw",
+    20: "gronddamstuw",
+    21: "stuwbak",
+    22: "tuimel- of kantelstuw",
+    23: "balgstuw",
+    24: "brievenbusstuw",
+    25: "knijpstuw",
+    26: "conserveringstuw",
+    99: "onbekend"
+}
+
+TYPEREGELBAARHEID = {
+    1: "niet regelbaar (vast)",
+    2: "regelbaar, niet automatisch",
+    3: "regelbaar, automatisch",
+    4: "handmatig",
+    99: "overig",
+}
+
+VORMKOKER = {
+    1: "Rond",
+    2: "Driehoek",
+    3: "Rechthoekig",
+    4: "Eivormig",
+    5: "Ellipsvormig",
+    6: "Paraboolvormig",
+    7: "Trapeziumvormig",
+    8: "Heulprofiel",
+    9: "Muilprofiel",
+    10: "Langwerpig",
+    11: "Scherp",
+    99: "Onbekend",
+}
+
+SOORTAFSLUITMIDDEL = {
+    1: "deur",
+    2: "schotbalk sponning",
+    3: "zandzakken",
+    4: "schuif",
+    5: "terugslagklep",
+    6: "tolklep",
+    97: "niet afsluitbaar",
+    98: "overig",
+    99: "onbekend"
+}
+
