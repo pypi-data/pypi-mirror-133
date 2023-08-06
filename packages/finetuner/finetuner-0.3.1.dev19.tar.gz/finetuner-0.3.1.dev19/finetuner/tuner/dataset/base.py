30eeb418397e151da82754eb99d0cde40d622b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:47 +0000

    Add package finetuner/0.3.1.dev19/finetuner-0.3.1.dev19.tar.gz

---
+++ packages/finetuner/finetuner-0.3.1.dev19.tar.gz/finetuner-0.3.1.dev19/finetuner/tuner/dataset/base.py
@@ -0,0 +1,46 @@
+import abc
+from typing import Generic, Iterator, List, Sequence, Tuple, TypeVar, Union
+
+import numpy as np
+
+AnyLabel = TypeVar('AnyLabel')
+
+
+class BaseSampler(abc.ABC):
+    def __init__(self, labels: Sequence[AnyLabel], batch_size: int):
+        if batch_size <= 0:
+            raise ValueError('batch_size must be a positive integer')
+
+        self._prepare_batches()
+
+    def __iter__(self) -> Iterator[List[int]]:
+        yield from self._batches
+
+        # After batches are exhausted, recreate
+        self._prepare_batches()
+
+    def __len__(self) -> int:
+        return len(self._batches)
+
+    @abc.abstractmethod
+    def _prepare_batches(self) -> None:
+        ...
+
+
+class BaseDataset(abc.ABC, Generic[AnyLabel]):
+    _labels: List[AnyLabel]
+
+    @abc.abstractmethod
+    def __getitem__(self, ind: int) -> Tuple[Union[np.ndarray, str], AnyLabel]:
+        """
+        Get the (preprocessed) content and label for the item at ``ind`` index in the
+        dataset.
+        """
+
+    @property
+    def labels(self) -> List[AnyLabel]:
+        """Get the list of labels for all items in the dataset."""
+        return self._labels
+
+    def __len__(self) -> int:
+        return len(self._labels)
