e8c9c5fa60ceb05e56436d2e61803626eb7ae7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:16 +0000

    Add package linearcli/1.0.1/linearcli-1.0.1.tar.gz

---
+++ packages/linearcli/linearcli-1.0.1.tar.gz/linearcli-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "linearcli",
+    version = "1.0.1",
+    author = "Mike Lyons",
+    author_email = "mdl0394@gmail.com",
+    license = "MIT",
+    description = "Simple CLI interface for linear task manager (https://linear.app)",
+    long_description = "",
+    url = "https://github.com/frenchie4111/linearcli",
+    py_modules = [
+        "linearcli"
+    ],
+    packages = find_packages(),
+    install_requires = [
+        "requests",
+    ],
+    python_requires=">=3.8",
+    entry_points = """
+        [console_scripts]
+        linearcli=linearcli.linear:main
+    """
+)
