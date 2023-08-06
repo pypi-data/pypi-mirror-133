5598500b7184a2aa8947485057914ee00b421921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:37 +0000

    Add package psycopg-pool/3.0.3/psycopg_pool-3.0.3-py3-none-any.whl

---
+++ packages/psycopg-pool/psycopg_pool-3.0.3-py3-none-any.whl/psycopg_pool/__init__.py
@@ -0,0 +1,18 @@
+"""
+psycopg connection pool package
+"""
+
+# Copyright (C) 2021 The Psycopg Team
+
+from .pool import ConnectionPool
+from .pool_async import AsyncConnectionPool
+from .errors import PoolClosed, PoolTimeout, TooManyRequests
+from .version import __version__ as __version__  # noqa: F401
+
+__all__ = [
+    "AsyncConnectionPool",
+    "ConnectionPool",
+    "PoolClosed",
+    "PoolTimeout",
+    "TooManyRequests",
+]
