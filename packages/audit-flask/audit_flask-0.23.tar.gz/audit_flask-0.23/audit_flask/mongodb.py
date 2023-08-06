6b8d66254528a26295e3090e65c3b12e02d873ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package audit-flask/0.23/audit_flask-0.23.tar.gz

---
+++ packages/audit-flask/audit_flask-0.23.tar.gz/audit_flask-0.23/audit_flask/mongodb.py
@@ -0,0 +1,47 @@
+from mongoengine import signals
+from blinker import Signal
+from typing import Tuple
+from audit_flask.utils import *
+
+singal_send_args =  Signal('Send args')
+
+
+class Audit():
+    @classmethod
+    def __audit(cls, document, **kwargs):
+        changes = None
+        updates, removals = document._delta()
+        if 'created' in kwargs:
+            method = 'create'
+            if kwargs['created'] is False:
+                if(type(updates) == Tuple):
+                    changes = dict((key, value) for key, value in updates)
+                else:
+                    changes = updates
+                method = 'update'
+        else:
+            method = 'delete'
+        args = {
+            'object_pk': document.pk,
+            'content_type': document._collection.name,
+            'object_repr': serializar_dict({**document.to_mongo()}),
+            'action': method,
+            'changes': serializar_dict(changes),
+            'remote_addr':remote_addr_validate(),
+            'user': user_validate()
+        }
+        singal_send_args.send(None, **args)
+        
+    @classmethod
+    def post_save(cls, sender, document, **kwargs):
+        cls.__audit(document, **kwargs)
+    
+    @classmethod
+    def post_delete(cls, sender, document, **kwargs):
+        cls.__audit(document, **kwargs)
+
+    
+
+def register_signals_model(cls):
+    signals.post_save.connect(cls.post_save, sender = cls)
+    signals.post_delete.connect(cls.post_delete, sender = cls)
