d84b5fee8be590f346b80c24b70b8d076c4e1d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:14 +0000

    Add package cry-vs.py/0.0.0.1/cry_vs.py-0.0.0.1-py3-none-any.whl

---
+++ packages/cry-vs.py/cry_vs.py-0.0.0.1-py3-none-any.whl/cry_vs/client.py
@@ -0,0 +1,30 @@
+import json
+
+from .emitter import Emitter
+from .HTTPHelper import http
+
+
+class Client:
+    funcs = []
+
+    def listen(func):
+        Client.funcs.append(func)
+
+    def login(self, key, server="https://cry-vs.herokuapp.com", allowUnsecure = False):
+        server = server
+        if server.lower().startswith("http://") and allowUnsecure == False:
+            print(f"{server} could not be loaded as it is http. please change it to https or set alowUnsecure to True in the function paramaters.")
+            return
+
+        r = http.sendRequest (
+            method=http.methods.POST,
+            url=server + "/login",
+            data=json.dumps({
+                "key": key
+            })
+        )
+
+
+        emitter = Emitter(funcs=self.funcs, expire=r.headers["Expire"])
+        emitter.enqueue(name="on_ready")
+        emitter.queue()
