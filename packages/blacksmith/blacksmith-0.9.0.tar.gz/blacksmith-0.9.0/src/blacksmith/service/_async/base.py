2e26bbcfc47b12decca79c4e224e63e205e400af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:33 +0000

    Add package blacksmith/0.9.0/blacksmith-0.9.0.tar.gz

---
+++ packages/blacksmith/blacksmith-0.9.0.tar.gz/blacksmith-0.9.0/src/blacksmith/service/_async/base.py
@@ -0,0 +1,16 @@
+import abc
+
+from blacksmith.domain.model import HTTPRequest, HTTPResponse, HTTPTimeout
+from blacksmith.typing import HttpMethod, Proxies
+
+
+class AsyncAbstractTransport(abc.ABC):
+    def __init__(self, verify_verificate: bool = True, proxies: Proxies = None):
+        self.verify_verificate = verify_verificate
+        self.proxies = proxies
+
+    @abc.abstractmethod
+    async def request(
+        self, method: HttpMethod, request: HTTPRequest, timeout: HTTPTimeout
+    ) -> HTTPResponse:
+        """Process the HTTP Get request."""
