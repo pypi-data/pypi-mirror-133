332dd4fb474d2e830669a8cfc69204ede46e9a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:29 +0000

    Add package example-pkg-AsifEng/1.0/example-pkg-AsifEng-1.0.tar.gz

---
+++ packages/example-pkg-AsifEng/example-pkg-AsifEng-1.0.tar.gz/example-pkg-AsifEng-1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools, os
+
+setuptools.setup(
+    name="example-pkg-AsifEng",  # Replace with your own PyPi
+    version= "1.0",
+    author="Foo Bar",
+    author_email="lidor.ettinger@example.com",
+    description="A small example package",
+    url="https://github.com/naturalett/getting-started",
+    packages=['calculate'],
+    scripts=['calculate/calc.py'],
+    python_requires='>=3.9',
+)
