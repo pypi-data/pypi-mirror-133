cf135ab26b04ba57ac240397273055814d17d7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:31 +0000

    Add package hlpl-composer/1.0.2/hlpl_composer-1.0.2.tar.gz

---
+++ packages/hlpl-composer/hlpl_composer-1.0.2.tar.gz/hlpl_composer-1.0.2/hlpl_composer/__main__.py
@@ -0,0 +1,22 @@
+﻿
+import configparser
+import sys
+import os
+
+def get_hlpl_composer__version(case):
+    config = configparser.ConfigParser()
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    config_file_path = os.path.join(current_directory, 'setup.cfg')
+    config.read(config_file_path)
+    if case=='version':
+       return config['hlpl_composer']['version']
+    if case=='else':
+       return config['hlpl_composer']['version']+'\n'+config['hlpl_composer']['author']+'\n'+config['hlpl_composer']['email']+'\n'+config['hlpl_composer']['url']+'\n'
+       
+ 
+    
+def main():
+    if 'version' in sys.argv:
+        print('\n'+get_hlpl_composer__version('version'))
+    else:
+        print('\n'+get_hlpl_composer__version('else')+'\n'+'hlpl_composer under developement, it will be released next versions of HLPL')
