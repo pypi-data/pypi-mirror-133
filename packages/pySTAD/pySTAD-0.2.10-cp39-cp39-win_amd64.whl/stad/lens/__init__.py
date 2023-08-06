9ded6350a3a64e0262d7c17847007be1b7985010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:18 +0000

    Add package pySTAD/0.2.10/pySTAD-0.2.10-cp39-cp39-win_amd64.whl

---
+++ packages/pySTAD/pySTAD-0.2.10-cp39-cp39-win_amd64.whl/stad/lens/__init__.py
@@ -0,0 +1,9 @@
+"""
+A module containing to configure how STAD uses a lens / filter.
+
+Classes
+------
+Lens :
+  A class that configures a 1-dimensional lens / filter.
+"""
+from .Lens import Lens
