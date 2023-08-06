b75a2d47c950b24c3d25af01f85ff21ff4ff32c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:31 +0000

    Add package hammuon/0.1.1/hammuon-0.1.1.tar.gz

---
+++ packages/hammuon/hammuon-0.1.1.tar.gz/hammuon-0.1.1/hammuon/__init__.py
@@ -0,0 +1,3 @@
+from .data_structure import quickSort
+
+__version__ = '0.1.1'
---
+++ packages/hammuon/hammuon-0.1.1.tar.gz/hammuon-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hammuon', 'hammuon.data_structure']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'hammuon',
+    'version': '0.1.1',
+    'description': 'Basic and important data structures algorithms.',
+    'long_description': '# Basic Algorithms\n\n\n## 1. Quick Sort\nQuick Sort is a sorting algorithm, which is commonly used in computer science. \nQuick Sort is a divide and conquer algorithm. \n\n### Usage\n```python\nfrom data_structure import quickSort\n\nprint(quickSort([8, 12, 55, -12]))\n```\n\n### Output\n```bash\n[-12, 8, 12, 55]\n```',
+    'author': 'mcvarer',
+    'author_email': 'ktuce.mcanv@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mcvarer/algorithms',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
