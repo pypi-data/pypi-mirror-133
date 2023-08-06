44204b03c842ac9572653c9a55b68861cf7a141e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:49 +0000

    Add package django-reactify/1.3.3/django-reactify-1.3.3.tar.gz

---
+++ packages/django-reactify/django-reactify-1.3.3.tar.gz/django-reactify-1.3.3/reactify/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./react/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/js"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV' : JSON.stringify('development')
+    })
+  ],
+};
