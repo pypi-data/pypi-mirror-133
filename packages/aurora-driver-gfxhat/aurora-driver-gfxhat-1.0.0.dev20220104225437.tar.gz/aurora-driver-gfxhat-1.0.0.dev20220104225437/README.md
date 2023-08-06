bf98fc0dc704a6f13efda222f64bb45983c8e08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:22 +0000

    Add package aurora-driver-gfxhat/1.0.0.dev20220104225437/aurora-driver-gfxhat-1.0.0.dev20220104225437.tar.gz

---
+++ packages/aurora-driver-gfxhat/aurora-driver-gfxhat-1.0.0.dev20220104225437.tar.gz/aurora-driver-gfxhat-1.0.0.dev20220104225437/README.md
@@ -0,0 +1,9 @@
+# Pimoroni GFX HAT Driver for Aurora Display Library
+This is a driver package for the GFX HAT for the Raspberry Pi made by
+Pimoroni. It uses its published `gfxhat` package as a dependency.
+
+> Note: To use this package you will need to provide your own installation
+> of Raspberry Pi OS (or compatible) since the Raspberry Pis GPIO bindings
+> are required and not available through PIP. Therefore also the `gfxhat`
+> library is not listed as a required dependency, however you won't be able
+> to run this code on a regular computer anyway.
---
+++ packages/aurora-driver-gfxhat/aurora-driver-gfxhat-1.0.0.dev20220104225437.tar.gz/aurora-driver-gfxhat-1.0.0.dev20220104225437/setup.py
@@ -0,0 +1,54 @@
+from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+
+
+HERE = abspath(dirname(__file__))
+
+
+def _read_file(path: str) -> str:
+    """ Reads any file and returns its content as a string """
+    with open(path, 'r') as f:
+        return f.read()
+
+
+setup(
+    name='aurora-driver-gfxhat',
+    version='1.0.0-dev20220104225437',
+    description='Renderer implementation for Pimoroni GFX HAT',
+    long_description=_read_file(join(HERE, 'README.md')),
+    long_description_content_type='text/markdown',
+    author='Raphael "rGunti" Guntersweiler',
+    author_email='raphael+pip@gunti.cloud',
+    license='MIT',
+    url='https://gitlab.com/aurora-display-lib/gfx-hat-driver',
+    packages=find_packages(include=['aurora_rgbmatrix', 'aurora_rgbmatrix.*']),
+    python_requires='>=3.5',
+    install_requires=[
+        'aurora-display-lib>=1.0.0.dev20220104173716'
+    ],
+    extras_require={
+        'pi': [
+            'gfxhat>=0.0.1'
+        ],
+        'deploy': [
+            'bump2version'
+        ],
+        'test': [
+            'testfixtures>=1.2.0',
+            'pytest>=3.4.0',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    include_package_data=True,
+    include_dirs=[
+    ],
+    entry_points={},
+    zip_safe=False)
