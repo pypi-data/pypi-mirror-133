722accf907d5f38156d87a0b007a651ba424e251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:27 +0000

    Add package decision-tree-ID3-Algorithm/0.0.2/decision-tree-ID3-Algorithm-0.0.2.tar.gz

---
+++ packages/decision-tree-ID3-Algorithm/decision-tree-ID3-Algorithm-0.0.2.tar.gz/decision-tree-ID3-Algorithm-0.0.2/src/printTree.py
@@ -0,0 +1,8 @@
+def print_tree(node,level): 
+        if node.answer!="":
+            print("  "*level,node.answer)
+            return
+        print("  "*level,node.attribute) 
+        for value,n in node.children:
+            print("  "*(level+1),value)
+            print_tree(n,level+2)
