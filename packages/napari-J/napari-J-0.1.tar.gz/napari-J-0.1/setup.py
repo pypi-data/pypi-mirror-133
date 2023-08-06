eedf2f1b7aa7b6a1bf4b13c0e0597f31a4bd03d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:15 +0000

    Add package napari-J/0.1/napari-J-0.1.tar.gz

---
+++ packages/napari-J/napari-J-0.1.tar.gz/napari-J-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+# https://github.com/pypa/setuptools_scm
+use_scm = {"write_to": "napari_j/_version.py"}
+
+
+setup(
+    use_scm_version=use_scm,
+    entry_points={
+        'napari.plugin': [
+            'naparij = napari_j',
+        ],
+    },
+)
