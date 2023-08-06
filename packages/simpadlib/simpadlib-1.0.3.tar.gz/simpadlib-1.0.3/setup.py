fa4320712211622849b78e7f5f33b626e7a5a5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:45 +0000

    Add package simpadlib/1.0.3/simpadlib-1.0.3.tar.gz

---
+++ packages/simpadlib/simpadlib-1.0.3.tar.gz/simpadlib-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from pathlib import Path
+
+def getDescription():
+    try:
+        return (Path(__file__).parent / "PIP_README.md").read_text()
+    except:
+        return ""
+
+setup(
+    name='simpadlib',
+    author='Ashe Muller',
+    author_email='contact.notashe@gmail.com',
+    version='1.0.3',
+    description='A library that allows users to custom-configure their SimPad real-time',
+    long_description=getDescription(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/malevtuber/simpadlib',
+    package_dir={'': 'src'},
+    python_requires='>=3.10',
+    install_requires=['pywinusb>=0.4.2'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/malevtuber/simpadlib/issues'
+    }
+)
