2901d8e406fcc655b7480acdd13cd9405bc0e37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:56 +0000

    Add package wg-utilities/2.1.1/wg_utilities-2.1.1.tar.gz

---
+++ packages/wg-utilities/wg_utilities-2.1.1.tar.gz/wg_utilities-2.1.1/wg_utilities/functions/_processes.py
@@ -0,0 +1,42 @@
+"""Functions for managing processes"""
+
+from logging import getLogger, DEBUG
+from re import compile as compile_regex
+from subprocess import Popen, PIPE
+from sys import exit as sys_exit
+
+from wg_utilities.loggers import add_stream_handler
+
+LOGGER = getLogger(__name__)
+LOGGER.setLevel(DEBUG)
+add_stream_handler(LOGGER)
+
+COMMAND_PATTERN = compile_regex(r"""((?:[^\s"']|"[^"]*"|'[^']*')+)""")
+
+
+def run_cmd(cmd, exit_on_error=True):
+    """Helper function for running commands on the command line
+
+    Args:
+        cmd (str): the command to run in the user's terminal
+        exit_on_error (bool): flag for if the script should exit if the command errored
+
+    Returns:
+        str: the output of the command
+        str: the error from the command, if it errored
+    """
+
+    LOGGER.debug("Running command `%s`", cmd)
+
+    with Popen(COMMAND_PATTERN.split(cmd)[1::2], stdout=PIPE, stderr=PIPE) as process:
+        output, error = process.communicate()
+
+        error = error.decode("utf-8").strip()
+
+        if process.returncode != 0:
+            if exit_on_error:
+                raise RuntimeError(error)
+
+            LOGGER.error(error)
+
+    return output.decode("utf-8").strip(), error
