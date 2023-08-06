f427a13fa2e9c2f53c7bbfccd46403ab6807bb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:00 +0000

    Add package csv-db-package/0.0.3/csv_db_package-0.0.3.tar.gz

---
+++ packages/csv-db-package/csv_db_package-0.0.3.tar.gz/csv_db_package-0.0.3/README.md
@@ -0,0 +1,35 @@
+# csv_db_package
+Package having the local server module to upload a csv file and perform CRUD operations on the data shown using different features through database.
+
+## Installation
+pip install csv_db_package
+
+---
+### STEPS TO INSTALL & MODIFY THE PROJECT 
+-Activate your python venv
+-Install the requirements given:
+    python
+    pip install -r requirements.txt
+    
+Mysql is used as a database engine here.
+-Set env variables
+  <br/>`$env:HOST= your hostname for mysql`
+  <br/>`$env:USER= your username`
+  <br/>`$env:PASSWORD= your password`
+
+-Run server inside directory by using command
+python fileserver.py
+
+-example table
+
+| First column | Second column |                                    |
+|--------------|---------------|------------------------------------|
+| Content Cell | Content Cell  | [update](#update "Goto heading-1") |
+| Content Cell | Content Cell  | [update](#update "Goto heading-1") |
+
+
+___
+© 2022 Ankita Liya
+
+This repository is licensed under the MIT license. See LICENSE for details.
+
---
+++ packages/csv-db-package/csv_db_package-0.0.3.tar.gz/csv_db_package-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="csv_db_package",
+    version="0.0.3",
+    author="Ankita Liya",
+    author_email="ankitaliya321@gmail.com",
+    description="This package is found useful for those who wants to modify their CSV file without using database."
+                "It creates a local server that having a functionality of uploading a csv file and "
+                "then perform crud operations through browser itself."
+                ,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ankitaliya/csv_db_package",
+    project_urls={
+        "Bug Tracker": "https://github.com/ankitaliya/csv_db_package/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={'csv_db_package': ['Templates/*.html']},
+    install_requires=['mysql-connector-python', 'Jinja2', 'pandas'],
+    python_requires=">=3.7",
+)
