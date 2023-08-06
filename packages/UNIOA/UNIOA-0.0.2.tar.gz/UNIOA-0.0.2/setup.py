ae37164b9c849ecca45ef017bc49b27f198a4daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:19 +0000

    Add package UNIOA/0.0.2/UNIOA-0.0.2.tar.gz

---
+++ packages/UNIOA/UNIOA-0.0.2.tar.gz/UNIOA-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UNIOA',
+    version='0.0.2',
+    packages=['UNIOA', 'UNIOA.algs'],
+    url='https://github.com/Huilin-Li/UNIOA.git',
+    license='MIT',
+    author='Huilin',
+    author_email='lihuilin023@gmail.com',
+    description='AutoNIOA'
+)
