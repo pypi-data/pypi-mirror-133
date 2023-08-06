fe36159102ec4deffd9f7dd03b90cbed011315af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package dynamics-client/0.2.4/dynamics_client-0.2.4-py3-none-any.whl

---
+++ packages/dynamics-client/dynamics_client-0.2.4-py3-none-any.whl/dynamics/typing.py
@@ -0,0 +1,65 @@
+try:
+    from typing import (
+        TYPE_CHECKING,
+        Any,
+        Callable,
+        Dict,
+        List,
+        Literal,
+        Optional,
+        Sequence,
+        Set,
+        Tuple,
+        Type,
+        TypedDict,
+        Union,
+    )
+except ImportError:
+    from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Sequence, Set, Tuple, Type, Union
+
+    from typing_extensions import Literal, TypedDict
+
+
+__all__ = [
+    "List",
+    "Dict",
+    "Optional",
+    "Any",
+    "Literal",
+    "Union",
+    "Set",
+    "Sequence",
+    "Type",
+    "Tuple",
+    "TypedDict",
+    "TYPE_CHECKING",
+    "MethodType",
+    "OrderbyType",
+    "FilterType",
+    "ExpandType",
+    "ExpandKeys",
+    "ExpandValues",
+    "ExpandValues",
+    "ExpandDict",
+    "Callable",
+]
+
+
+MethodType = Literal["get", "post", "patch", "delete"]
+OrderbyType = Dict[str, Literal["asc", "desc"]]
+FilterType = Union[Set[str], List[str]]
+
+
+class ExpandType(TypedDict):
+    select: List[str]
+    filter: FilterType
+    top: int
+    orderby: OrderbyType
+    expand: Dict[str, "ExpandType"]
+
+
+ExpandKeys = Literal["select", "filter", "top", "orderby", "expand"]
+ExpandValues = Union[List[str], Set[str], int, OrderbyType, Dict[str, ExpandType]]
+ExpandDict = Dict[str, Optional[ExpandType]]
+FieldType = Union[str, int, float, bool, None]
+CompType = Union[str, int, float]
