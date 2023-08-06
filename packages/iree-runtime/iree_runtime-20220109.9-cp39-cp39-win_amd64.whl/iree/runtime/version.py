28331a6718a401d2944f9acf6362f4907611f7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:28 +0000

    Add package iree-runtime/20220109.9/iree_runtime-20220109.9-cp39-cp39-win_amd64.whl

---
+++ packages/iree-runtime/iree_runtime-20220109.9-cp39-cp39-win_amd64.whl/iree/runtime/version.py
@@ -0,0 +1,9 @@
+# Copyright 2020 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+PACKAGE_SUFFIX = ""
+VERSION = "20220109.9"
+REVISION = "8a5816ef35af389e88b03ff4abd79d489718ff35"
