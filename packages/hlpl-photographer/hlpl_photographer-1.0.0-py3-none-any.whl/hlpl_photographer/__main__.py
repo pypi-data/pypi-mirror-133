be975a317688f11f4e37a2a306e6e3134a53c231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:24 +0000

    Add package hlpl-photographer/1.0.0/hlpl_photographer-1.0.0-py3-none-any.whl

---
+++ packages/hlpl-photographer/hlpl_photographer-1.0.0-py3-none-any.whl/hlpl_photographer/__main__.py
@@ -0,0 +1,22 @@
+﻿
+import configparser
+import sys
+import os
+
+def get_hlpl_character_version(case):
+    config = configparser.ConfigParser()
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    config_file_path = os.path.join(current_directory, 'setup.cfg')
+    config.read(config_file_path)
+    if case=='version':
+       return config['hlpl_photographer']['version']
+    if case=='else':
+       return config['hlpl_photographer']['version']+'\n'+config['hlpl_photographer']['author']+'\n'+config['hlpl_photographer']['email']+'\n'+config['hlpl_photographer']['url']+'\n'
+       
+ 
+    
+def main():
+    if 'version' in sys.argv:
+        print('\n'+get_hlpl_character_version('version'))
+    else:
+        print('\n'+get_hlpl_character_version('else')+'\n'+'hlpl_photographer under developement, it will be released next versions of HLPL')
