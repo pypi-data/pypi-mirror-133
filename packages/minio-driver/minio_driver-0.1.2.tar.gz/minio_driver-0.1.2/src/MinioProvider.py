ff2a95877b9e9464bf4fdeec75b47cf5b50ea050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package minio-driver/0.1.2/minio_driver-0.1.2.tar.gz

---
+++ packages/minio-driver/minio_driver-0.1.2.tar.gz/minio_driver-0.1.2/src/MinioProvider.py
@@ -0,0 +1,15 @@
+"""A MinioProvider Service Provider."""
+from .MinioDriver import MinioDriver
+from masonite.providers import Provider
+
+
+class MinioProvider(Provider):
+
+    def __init__(self, application):
+        self.application = application
+
+    def register(self):
+        self.application.make("storage").add_driver("minio", MinioDriver(self.application))
+
+    def boot(self):
+        pass
