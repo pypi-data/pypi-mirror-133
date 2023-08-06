a43ae82d422812fa3521856f05fe7d7747b8e659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:39 +0000

    Add package ev3msg/0.0.3.5/ev3msg-0.0.3.5.tar.gz

---
+++ packages/ev3msg/ev3msg-0.0.3.5.tar.gz/ev3msg-0.0.3.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='ev3msg',
+    version='0.0.3.5',
+    description='A EV3 Messaging Library for Python',
+    author='JJTV',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read() + '\n\n\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='',
+    license='OSI Approved :: GNU General Public License v3 (GPLv3)',
+    classifiers=classifiers,
+    keywords=["ev3", "messaging", "robotics", "communication", "protocol",
+              "mailbox", "py", "python"],
+    packages=["ev3msg"],
+    install_requires=['pybluez2'],
+    include_package_data=True
+)
