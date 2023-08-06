ec810392026c6572061930ac31999644a206cda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:10 +0000

    Add package aids/0.1.5.5/aids-0.1.5.5-py3-none-any.whl

---
+++ packages/aids/aids-0.1.5.5-py3-none-any.whl/aids/app/settings.py
@@ -0,0 +1,66 @@
+import os
+from pathlib import Path
+import json
+import warnings
+
+class ImproperlyConfigured(Exception):
+    pass
+
+DEACTIVATE_LOG = 0
+
+# notice that '' is a catch-all. This does not 
+# include Untitled scenarios, though.
+DEFAULT_TITLE = ''
+DEFAULT_MIN_ACT = 10
+
+# base path
+# could be changed to cwd() to use this module from console after
+# a pip install.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+try:
+    os.mkdir(BASE_DIR / 'backups')
+except FileExistsError:
+    pass
+
+WARNINGS = 1
+
+# secrets
+secrets_form = {
+    "TOR_PASSWORD": "",
+    "AID_TOKEN": "",
+    "AID_USERNAME": "",
+    "AID_PASSWORD": ""
+}
+try:
+    with open(BASE_DIR / 'app/secrets.json') as file:
+        secrets = json.load(file)
+except FileNotFoundError:
+    if WARNINGS:
+        warnings.warn('File with credentials was not found.')
+except json.decoder.JSONDecodeError:
+    # First time, create the file
+    with open(BASE_DIR / 'app/secrets.json', 'w') as file:
+        file.write(json.dumps(secrets_form))
+
+def get_secret(setting):
+    try:
+        return secrets[setting]
+    except (NameError, KeyError) as exc:
+        raise ImproperlyConfigured(
+            f'Setting {setting} was not found in your secrets.json file.'
+        ) from exc
+
+# requests settings
+headers = {
+            'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) ' \
+                         'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                         'Chrome/90.0.4430.93 Safari/537.36',
+            'Referer': 'https://play.aidungeon.io/',
+            'Origin': 'https://play.aidungeon.io',
+            'Host': 'api.aidungeon.io',
+            'DNT': '1',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'content-type': 'application/json'
+}
---
+++ packages/aids/aids-0.1.5.5-py3-none-any.whl/aids/app/writelogs.py
@@ -0,0 +1,63 @@
+import sys
+import os
+from datetime import datetime
+
+from aids.app.settings import BASE_DIR, DEACTIVATE_LOG
+
+
+# default is linux console.
+ERROR_FILE =  BASE_DIR / 'app/client.error' # '/dev/stdout'
+LOG_FILE = '/dev/stdout' #BASE_DIR / 'app/client.log'
+
+
+init_str = '---------------INIT---------------\n'
+
+def make_log_message(level, extra_msg):
+    return f'{str(datetime.today())} [{level}] {extra_msg}'
+
+def log(lvl, msg):
+    """
+    Logs a message in whatever file is the current default.
+    """
+    if DEACTIVATE_LOG:
+        return
+
+    if lvl == 'init':
+        msg = init_str + msg
+
+    if sys.platform == 'linux':
+        try:
+            if os.stat(LOG_FILE).st_size > 5000000:
+                os.system(f'mv {LOG_FILE} {BASE_DIR / "old_logs"}')
+                with open(LOG_FILE, 'w'): pass
+        except TypeError:
+            # console
+            pass
+
+        try:
+            with open(LOG_FILE, 'a') as log_file:
+                log_file.write(make_log_message(lvl, msg) + '\n')
+        except OSError:
+            pass
+    else:
+        # On windows simply print the message
+        print(make_log_message(lvl, msg) + '\n')
+
+def log_error(lvl, msg):
+    """
+    Logs an error in whatever file is the current default.
+    """
+    if DEACTIVATE_LOG:
+        return
+    # bigger than 1 mb
+    if sys.platform == 'linux':
+        try:
+            if os.stat(ERROR_FILE).st_size > 5000000:
+                os.system(f'mv {ERROR_FILE} {BASE_DIR / "old_errors"}')
+        except TypeError:
+            # console
+            pass
+        with open(ERROR_FILE, 'a') as error_file:
+            error_file.write(make_log_message(lvl, msg) + '\n')
+    else:
+        print(make_log_message(lvl, msg) + '\n')
