31098a50547f6f559d10c2fd96366d96ac8b8697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:33 +0000

    Add package rgf-python/3.12.0/rgf_python-3.12.0-py3-none-win_amd64.whl

---
+++ packages/rgf-python/rgf_python-3.12.0-py3-none-win_amd64.whl/rgf/sklearn.py
@@ -0,0 +1,5 @@
+__all__ = ('RGFClassifier', 'RGFRegressor',
+           'FastRGFClassifier', 'FastRGFRegressor')
+
+from rgf.rgf_model import RGFRegressor, RGFClassifier
+from rgf.fastrgf_model import FastRGFRegressor, FastRGFClassifier
