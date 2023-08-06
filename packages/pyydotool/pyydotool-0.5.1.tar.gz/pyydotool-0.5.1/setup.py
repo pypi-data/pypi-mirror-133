10c58ebfe5a6288d13bae5713e01ea3b2ba9cd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:46 +0000

    Add package pyydotool/0.5.1/pyydotool-0.5.1.tar.gz

---
+++ packages/pyydotool/pyydotool-0.5.1.tar.gz/pyydotool-0.5.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ydotool']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyydotool',
+    'version': '0.5.1',
+    'description': 'Python bindings to ydtool',
+    'long_description': None,
+    'author': 'Jerzy Drozdz',
+    'author_email': 'jerzy.drozdz@jdsieci.pl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/jdsieci/pyydotool',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
