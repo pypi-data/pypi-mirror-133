a02b3eda421b7a3db92aab91c412d6b480f96889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:03 +0000

    Add package Table2JSON/2.0.1/Table2JSON-2.0.1-py310-none-any.whl

---
+++ packages/Table2JSON/Table2JSON-2.0.1-py310-none-any.whl/table2json/utils/getter.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from table2json import __name__, __version__
+
+
+def get_package_path():
+    """Get Package Path"""
+
+    package_path = Path(__file__).parent.parent
+    return package_path
+
+
+def get_name():
+    """Get Name"""
+
+    return __name__
+
+
+def get_version():
+    """Get Version"""
+
+    return __version__
+
+
+def get_named_version():
+    """Get Named Version"""
+
+    return get_name() + " " + get_version()
