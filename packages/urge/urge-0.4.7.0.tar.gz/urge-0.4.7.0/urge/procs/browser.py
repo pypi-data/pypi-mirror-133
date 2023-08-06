179ebb876005e65937f794adf7865c66bf278027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package urge/0.4.7.0/urge-0.4.7.0.tar.gz

---
+++ packages/urge/urge-0.4.7.0.tar.gz/urge-0.4.7.0/urge/procs/browser.py
@@ -0,0 +1,43 @@
+from urge.base import ProcManager, action
+from path import Path
+
+
+def web_screenshot(url: str, target: str = "./", use_mobile=True,**kwargs) -> str:
+
+    from playwright.sync_api import sync_playwright
+    from datetime import datetime
+
+    print("🤖 : Chromium import is OK...")
+
+    with sync_playwright() as p:
+        iphone_11 = p.devices["iPhone 11 Pro"]
+        browser_type = p.chromium
+        print("🤖 : Hey, I found the chromium binary...")
+        print(browser_type)
+        print(
+            '🤖 : Launching browser... Please wait for a while then you will see the'
+            ' result.'
+        )
+        browser = browser_type.launch(headless=True)
+        if not use_mobile:
+            iphone_11 = {}
+
+        context = browser.new_context(**iphone_11)
+        page = context.new_page()
+        if use_mobile:
+            page.set_viewport_size({"width": 375, "height": 635})
+
+        page.goto(url, wait_until="networkidle")
+        # page.screenshot(path=f'./example-{browser_type.name}haha.png')
+        target = Path(target) 
+        file_name = target/f'screenshot{datetime.now().strftime("%m-%d-%H-%M-%S")}.jpeg'
+        page.screenshot(path=file_name,type="jpeg",quality=20)
+        print(f"🤖 : DONE")
+        print(f'The file is at {file_name}')
+        browser.close()
+    return file_name
+
+
+def get_screenshot(url):
+    p = ProcManager(web_screenshot)
+    return p(url)
