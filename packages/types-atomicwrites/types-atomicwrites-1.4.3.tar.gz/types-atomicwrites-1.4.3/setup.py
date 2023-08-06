37ef8be93530b88cbb16f9a2dc0c292dc84e9ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:35 +0000

    Add package types-atomicwrites/1.4.3/types-atomicwrites-1.4.3.tar.gz

---
+++ packages/types-atomicwrites/types-atomicwrites-1.4.3.tar.gz/types-atomicwrites-1.4.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-atomicwrites"
+description = "Typing stubs for atomicwrites"
+long_description = '''
+## Typing stubs for atomicwrites
+
+This is a PEP 561 type stub package for the `atomicwrites` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `atomicwrites`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/atomicwrites. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `a40d79a4e63c4e750a8d3a8012305da942251eb4`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.4.3",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['atomicwrites-stubs'],
+      package_data={'atomicwrites-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Stubs Only",
+      ]
+)
