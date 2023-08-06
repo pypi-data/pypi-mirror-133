231fdbdfc7756b2994d60ad00e9f01195eea1b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:50 +0000

    Add package Pmdr/1.0.0/Pmdr-1.0.0.tar.gz

---
+++ packages/Pmdr/Pmdr-1.0.0.tar.gz/Pmdr-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name='Pmdr',
+    version='1.0.0',
+    author='Jeff Salisbury',
+    author_email='salisbury.jeffery@gmail.com',
+    license='MIT',
+    description='CLI Pomodoro Timer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JDSalisbury/doro',
+    py_modules=['timer', 'app'],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires='>=3.7',
+    classifiers=["Programming Language :: Python :: 3.8",
+                 "Operating System :: OS Independent"],
+    entry_points='''
+        [console_scripts]
+        pmdr=timer:cli
+    '''
+)
