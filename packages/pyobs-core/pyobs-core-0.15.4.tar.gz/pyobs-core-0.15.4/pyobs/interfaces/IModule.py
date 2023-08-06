7ed4df3aa5457b37b4e93b629c4f6fa160358335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:39 +0000

    Add package pyobs-core/0.15.4/pyobs-core-0.15.4.tar.gz

---
+++ packages/pyobs-core/pyobs-core-0.15.4.tar.gz/pyobs-core-0.15.4/pyobs/interfaces/IModule.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, Optional
+
+from .interface import Interface
+
+
+class IModule(Interface, metaclass=ABCMeta):
+    """The module is actually a module. Implemented by all modules."""
+
+    __module__ = "pyobs.interfaces"
+
+    @abstractmethod
+    async def get_label(self, **kwargs: Any) -> str:
+        """Returns label of module."""
+        ...
+
+    @abstractmethod
+    async def get_version(self, **kwargs: Any) -> str:
+        """Returns pyobs version of module."""
+        ...
+
+
+__all__ = ["IModule"]
