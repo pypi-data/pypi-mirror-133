b848d90d751f57773b4484ae4c59ec0691b515cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:39 +0000

    Add package AutoTransform/0.1.0/AutoTransform-0.1.0.tar.gz

---
+++ packages/AutoTransform/AutoTransform-0.1.0.tar.gz/AutoTransform-0.1.0/autotransform/repo/factory.py
@@ -0,0 +1,26 @@
+# AutoTransform
+# Large scale, component based code modification library
+#
+# Licensed under the MIT License <http://opensource.org/licenses/MIT
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022-present Nathan Rockenbach <http://github.com/nathro>
+
+from typing import Any, Callable, Dict, Mapping
+
+from autotransform.repo.base import Repo, RepoBundle
+from autotransform.repo.git import GitRepo
+from autotransform.repo.github import GithubRepo
+from autotransform.repo.type import RepoType
+
+
+class RepoFactory:
+    # pylint: disable=too-few-public-methods
+
+    _getters: Dict[RepoType, Callable[[Mapping[str, Any]], Repo]] = {
+        RepoType.GIT: GitRepo.from_data,
+        RepoType.GITHUB: GithubRepo.from_data,
+    }
+
+    @staticmethod
+    def get(repo: RepoBundle) -> Repo:
+        return RepoFactory._getters[repo["type"]](repo["params"])
