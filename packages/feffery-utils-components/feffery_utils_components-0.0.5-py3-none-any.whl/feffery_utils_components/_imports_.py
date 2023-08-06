b5196835992df7aadc225bdcc55f0fe13305541b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:26 +0000

    Add package feffery-utils-components/0.0.5/feffery_utils_components-0.0.5-py3-none-any.whl

---
+++ packages/feffery-utils-components/feffery_utils_components-0.0.5-py3-none-any.whl/feffery_utils_components/_imports_.py
@@ -0,0 +1,11 @@
+from .FefferyCaptcha import FefferyCaptcha
+from .FefferyPasteImage import FefferyPasteImage
+from .FefferySyntaxHighlighter import FefferySyntaxHighlighter
+from .FefferyWaterMark import FefferyWaterMark
+
+__all__ = [
+    "FefferyCaptcha",
+    "FefferyPasteImage",
+    "FefferySyntaxHighlighter",
+    "FefferyWaterMark"
+]
