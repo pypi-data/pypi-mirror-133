acbf082cd00cc660fadf1a2d1a2a3061619b1761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:42 +0000

    Add package monopy/1.0.3/monopy-1.0.3-py3-none-any.whl

---
+++ packages/monopy/monopy-1.0.3-py3-none-any.whl/mono/api/__init__.py
@@ -0,0 +1,5 @@
+from .misc import Misc
+from .mono_account import Account
+from .mono_user import UserMono
+from .mono_wallet import Wallet
+from .directpay import DirectPay
---
+++ packages/monopy/monopy-1.0.3-py3-none-any.whl/mono/api/base_api.py
@@ -0,0 +1,55 @@
+import os
+import json
+import requests
+
+
+from .errors import MonoAuthException, HttpMethodException
+
+
+class BaseAPI:
+    """
+    Base Mono class
+    """
+    _BASE_URL = 'https://api.withmono.com'
+
+    def __init__(self, mono_sec_key: str = None) -> None:
+        if mono_sec_key is None:
+            raise MonoAuthException('your mono secret key is required.')
+
+        self._mono_sec_key = mono_sec_key
+
+    def _headers(self):
+        return {
+            'Accept': 'application/json',
+            'mono-sec-key': self._mono_sec_key,
+            'Content-Type': 'application/json'
+        }
+
+
+    def _make_request(self, method, url, **kwargs):
+
+        HTTP_METHODS = {
+            'GET': requests.get,
+            'POST': requests.post,
+            'PUT': requests.put,
+            'DELETE': requests.delete
+        }
+
+        params = kwargs.get('params', None)
+        json = kwargs.get('json', None)
+
+        request = HTTP_METHODS.get(method, None)
+
+        if request is None:
+            raise HttpMethodException('unrecognized HTTP method; you may only use POST, GET, PUT or DELETE')
+
+        response = request(headers=self._headers(), url=url, params=params, json=json)
+
+
+        if response.status_code >= 400:
+            return response.status_code, response.json()
+        return response.status_code, response.json()
+
+
+    def __repr__(self):
+        return '%s <%r>' % (self.__class__.__name__, self._mono_sec_key[:5])
