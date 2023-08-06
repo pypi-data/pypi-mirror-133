91cb65071ce0661e39e6c6fd23cadd6c5b389da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:30 +0000

    Add package hello-robot-stretch-body-tools/0.2.1/hello_robot_stretch_body_tools-0.2.1-py2-none-any.whl

---
+++ packages/hello-robot-stretch-body-tools/hello_robot_stretch_body_tools-0.2.1-py2-none-any.whl/hello_robot_stretch_body_tools-0.2.1.data/scripts/stretch_robot_stow.py
@@ -0,0 +1,10 @@
+#!python
+from __future__ import print_function
+import stretch_body.robot as rb
+import stretch_body.hello_utils as hu
+hu.print_stretch_re_use()
+
+robot = rb.Robot()
+robot.startup()
+robot.stow()
+robot.stop()
