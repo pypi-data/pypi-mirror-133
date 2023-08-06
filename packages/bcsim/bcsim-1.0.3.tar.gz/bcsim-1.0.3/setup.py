b4e002cc6d909999a1ffa878faf2996b34fac0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package bcsim/1.0.3/bcsim-1.0.3.tar.gz

---
+++ packages/bcsim/bcsim-1.0.3.tar.gz/bcsim-1.0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Setup script"""
+
+from setuptools import setup
+
+setup(
+    project_urls={
+        "Source Code": "https://github.com/geozeke/bcsim",
+        "Bug Tracker": "https://github.com/geozeke/bcsim/issues",
+        "Documentation": "https://geozeke.github.io/bcsim"
+    }
+)
