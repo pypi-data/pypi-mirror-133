1a39bf7f985cbc7ba71f658049194f493915a318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:49 +0000

    Add package rudalle/0.5.0rc1/rudalle-0.5.0rc1-py3-none-any.whl

---
+++ packages/rudalle/rudalle-0.5.0rc1-py3-none-any.whl/rudalle/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from .vae import get_vae
+from .dalle import get_rudalle_model
+from .tokenizer import get_tokenizer
+from .realesrgan import get_realesrgan
+from .ruclip import get_ruclip
+from .emojich_unet import get_emojich_unet
+from . import vae, dalle, tokenizer, realesrgan, pipelines, ruclip, image_prompts
+
+
+__all__ = [
+    'get_vae',
+    'get_rudalle_model',
+    'get_tokenizer',
+    'get_realesrgan',
+    'get_ruclip',
+    'get_emojich_unet',
+    'vae',
+    'dalle',
+    'ruclip',
+    'tokenizer',
+    'realesrgan',
+    'pipelines',
+    'image_prompts',
+]
+
+__version__ = '0.5.0-rc1'
