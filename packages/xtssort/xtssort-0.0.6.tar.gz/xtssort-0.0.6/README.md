3a248e176361621e4822b06f07bb6013929f0f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:18 +0000

    Add package xtssort/0.0.6/xtssort-0.0.6.tar.gz

---
+++ packages/xtssort/xtssort-0.0.6.tar.gz/xtssort-0.0.6/README.md
@@ -0,0 +1,32 @@
+# xtssort
+##### This is the ten method of list sorting.
+
+>install
+>>```sheel
+>>pip install xtssort
+>>```
+
+```python
+from xtssort import *
+
+# Bubble_sorting
+Bubble_sorting(lt=list,Positive_or_reverse_order=2 or 1)
+# selectionSort
+selectionSort(arr=list)
+# insertionSort
+insertionSort(arr=list)
+# shellSort
+shellSort(arr=list)
+# mergeSort
+mergeSort(arr=list)
+# quickSort
+quickSort(arr=list,left=,right=)
+# heapSort
+heapSort(arr=list)
+# countingSort
+countingSort(arr=list,maxValue=)
+# bucket_sort
+bucket_sort(arr=list,max_num=)
+# radix_sort
+radix_sort(data=list)
+```
---
+++ packages/xtssort/xtssort-0.0.6.tar.gz/xtssort-0.0.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+setup(
+    name='xtssort',
+    version='0.0.6',
+    description='sort',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='xiongtianshuo',
+    author_email='seoul1k@163.com',
+    maintainer='xiontianshuo',
+    maintainer_email='seoul1k@163.com',
+    license='BSD License',
+    packages=['xtssort/'],
+    platforms=["all"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
