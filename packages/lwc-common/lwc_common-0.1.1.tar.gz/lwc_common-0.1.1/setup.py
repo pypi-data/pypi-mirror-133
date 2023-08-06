b00ade00c5809cfbc7d3cae2a49708ff4b7f0915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:55 +0000

    Add package lwc-common/0.1.1/lwc_common-0.1.1.tar.gz

---
+++ packages/lwc-common/lwc_common-0.1.1.tar.gz/lwc_common-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lwc_common']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'lwc-common',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'AminuIsrael',
+    'author_email': 'israel.aminu@data2bots.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
