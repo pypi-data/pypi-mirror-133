f580346bb07f55df733ed3670a181816f42c3a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:47 +0000

    Add package drinkingbird/0.1.4/drinkingbird-0.1.4-py2.py3-none-any.whl

---
+++ packages/drinkingbird/drinkingbird-0.1.4-py2.py3-none-any.whl/drinkingbird/cli.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+from .base import activity, get_target_time
+from .art import Printer
+
+
+@click.command()
+@click.option('--t', default=10, help="seconds to wait before repeating the action.")
+@click.option('--stoptime', help="A time in iso format (e.g., 08:30) to stop running.")
+@click.option('--anim/--no-anim', default=True, help="Show an animation in the console")
+def main(t, stoptime, anim):
+    if stoptime is not None:
+        stoptime = get_target_time(stoptime)
+
+    activity(sleep_time=t, stop_time=stoptime, monitor=Printer() if anim else None)
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
