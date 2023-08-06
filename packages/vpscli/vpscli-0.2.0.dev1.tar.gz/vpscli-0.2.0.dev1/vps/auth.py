fead74078bef39ce04a311310dd69df98f6c8215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:53 +0000

    Add package vpscli/0.2.0.dev1/vpscli-0.2.0.dev1.tar.gz

---
+++ packages/vpscli/vpscli-0.2.0.dev1.tar.gz/vpscli-0.2.0.dev1/vps/auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import random
+import re
+import os
+import subprocess
+import sys
+from codefast.utils import shell
+import joblib
+from collections import defaultdict
+from functools import reduce
+import codefast as cf
+from getpass import getpass
+from cryptography.fernet import Fernet
+import base64
+import json
+from typing import List, Dict, Tuple, Union, Optional
+
+
+class AccountLoader:
+    __db = os.path.join(cf.io.dirname(), 'memory.joblib')
+    __keys = ['REDIS_HOST', 'REDIS_PORT', 'REDIS_PASSWORD']
+
+    @classmethod
+    def decode_config_file(cls) -> dict:
+        cf.info('decode config file and dump to memory')
+        loc = os.path.join(cf.io.dirname(), 'data/redis.txt')
+        text = cf.io.reads(loc).encode()
+        passwd = getpass('Enter password: ').rstrip()
+        passwd = base64.urlsafe_b64encode(passwd.encode() * 10)
+        key = Fernet.generate_key()
+        key = passwd.decode('utf-8')[:43] + key.decode('utf-8')[43:]
+        f = Fernet(key.encode())
+        return json.loads(f.decrypt(text).decode('utf-8'))
+
+    @classmethod
+    def query_secrets(cls) -> Tuple[str]:
+        try:
+            auth_path = os.path.join(cf.io.dirname(), 'data/cauth')
+            cmd = auth_path + ' -a'
+            xstr = subprocess.check_output(
+                cmd, shell=True).decode('utf-8').strip()
+            js = json.loads(xstr)
+            return js['redis_host'], js['redis_port'], js['redis_pass']
+        except json.decoder.JSONDecodeError:
+            cf.error('failed to decode json {}'.format(xstr))
+            return None, None, None
+        except Exception as e:
+            cf.error('failed to query secrets: {}'.format(e))
+            return None, None, None
+
+    @classmethod
+    def set_secrets(cls, secrets: Dict[str, str]) -> None:
+        values = [secrets[k] for k in cls.__keys]
+        joblib.dump(values, cls.__db)
