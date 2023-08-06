1f733d005ac8c6408625c06f95b5b5f12f518d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:40 +0000

    Add package eod/0.1.1/eod-0.1.1.tar.gz

---
+++ packages/eod/eod-0.1.1.tar.gz/eod-0.1.1/eod/fundamental_economic_data/fundamental_economic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 10:00:38 2021
+
+@author: lauta
+"""
+
+from eod.fundamental_economic_data.fundamental_api import StockEtfFundsIndexFundamentalData
+from eod.fundamental_economic_data.calendar_earnings_trends_ipos_splits_api import CalendarEarningsTrendsIposSplits
+from eod.fundamental_economic_data.macroeconomic_api import MacroEconomicIndicators
+from eod.fundamental_economic_data.insider_transactions_api import InsiderTransactions
+
+class FundamentalEconomicData(StockEtfFundsIndexFundamentalData, CalendarEarningsTrendsIposSplits,
+                              MacroEconomicIndicators, InsiderTransactions):
+    def __init__(self, api_key:str, timeout:int):
+        # inhereting the API classes
+        StockEtfFundsIndexFundamentalData.__init__(self, api_key, timeout)
+        CalendarEarningsTrendsIposSplits.__init__(self, api_key, timeout)
+        MacroEconomicIndicators.__init__(self, api_key, timeout)
+        InsiderTransactions.__init__(self, api_key, timeout)
