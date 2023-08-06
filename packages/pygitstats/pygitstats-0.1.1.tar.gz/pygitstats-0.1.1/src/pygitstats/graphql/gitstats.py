2962d7ffcae3d61090bbdc60a3477ff1606afc9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:29 +0000

    Add package pygitstats/0.1.1/pygitstats-0.1.1.tar.gz

---
+++ packages/pygitstats/pygitstats-0.1.1.tar.gz/pygitstats-0.1.1/src/pygitstats/graphql/gitstats.py
@@ -0,0 +1,95 @@
+import requests
+import json
+
+URL = 'https://api.github.com/graphql'
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class QueryFailError(Exception):
+    pass
+
+
+class GitStats:
+    """Use GitHub's GraphQL API to get information about repositories, users and more"""
+
+    def __init__(self, git_token: str):
+        self.token = git_token
+        self.headers = {"Authorization": "Bearer " + self.token}
+
+    def get_query(self, query):
+
+        request = requests.post(
+            URL, json={'query': query}, headers=self.headers)
+        if request.status_code == 200:
+            return json.dumps(request.json(), indent=4, sort_keys=True)
+        else:
+            raise QueryFailError("Query failed to run by returning code of {}. {}".format(
+                request.status_code, query))
+
+    def custom_query(self, query):
+        """Allows usage of custom GraphQL queries for the GitHub GrapQL API"""
+        return self.get_query(query)
+
+    def user_info(self, user: str):
+        """Returns basic information about given user"""
+        query = '''
+query user {
+  user(login: "''' + user + '''") { 
+    url
+    login
+    name
+    twitterUsername
+    websiteUrl
+    email
+    company
+    location
+    status {
+      message
+      indicatesLimitedAvailability
+    }
+    avatarUrl
+    bio
+    isDeveloperProgramMember
+    isCampusExpert
+    starredRepositories {
+      totalCount
+    }
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    issues {
+      totalCount
+    }
+    pullRequests {
+      totalCount
+    }
+    itemShowcase {
+      hasPinnedItems
+      items(first: 10) {
+        nodes {
+          ... on Repository {
+            name
+          }
+        }
+      }
+    }
+    organizations(first: 10) {
+      totalCount
+      nodes {
+        name
+      }
+    }
+    repositories {
+      totalCount
+    }
+  }
+}
+        '''
+        response = self.get_query(query)
+        return response
