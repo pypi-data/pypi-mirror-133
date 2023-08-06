7af8b9482430b066929fc968532f6c44b21d7263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:36 +0000

    Add package ndd-forked-keycloak/3.1.1/ndd_forked_keycloak-3.1.1-py2.py3-none-any.whl

---
+++ packages/ndd-forked-keycloak/ndd_forked_keycloak-3.1.1-py2.py3-none-any.whl/keycloak/welcome.py
@@ -0,0 +1,15 @@
+
+
+# -*- coding: utf-8 -*-
+
+
+def welcome():
+    print("""
+Hello, I am NhanDD
+Because I found no way to disable ssl verify in origin source.
+I forked from https://github.com/keycloak-client/keycloak-client
+    """)
+
+
+if __name__ == '__main__':
+    welcome()
