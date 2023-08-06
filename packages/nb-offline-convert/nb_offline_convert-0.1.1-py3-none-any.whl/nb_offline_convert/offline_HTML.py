8c637f560aabe6276d858b7bb129aa2038d13459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:27 +0000

    Add package nb-offline-convert/0.1.1/nb_offline_convert-0.1.1-py3-none-any.whl

---
+++ packages/nb-offline-convert/nb_offline_convert-0.1.1-py3-none-any.whl/nb_offline_convert/offline_HTML.py
@@ -0,0 +1,19 @@
+#############################################################
+# Created on Thu Nov 04 2021                                #
+# Copyright (c) 2021 Duc Trung Le (leductrungxf@gmail.com)  #
+# Distributed under the terms of the MIT License            #
+#############################################################
+
+
+from nbconvert import HTMLExporter
+from .tools import OfflineMixing
+
+
+class OfflineHTMLExporter(OfflineMixing, HTMLExporter):
+
+    template_name = "lab_offline"
+
+    def from_notebook_node(self, nb, resources=None, **kw):
+        self.mathjax_url = self.mathjax_url + f"?config={self.mathjax_option}"
+        resources["offline_module_path"] = self.offline_module_path
+        return super().from_notebook_node(nb, resources, **kw)
---
+++ packages/nb-offline-convert/nb_offline_convert-0.1.1-py3-none-any.whl/nb_offline_convert/offline_WebPDF.py
@@ -0,0 +1,18 @@
+#############################################################
+# Created on Thu Nov 04 2021                                #
+# Copyright (c) 2021 Duc Trung Le (leductrungxf@gmail.com)  #
+# Distributed under the terms of the MIT License            #
+#############################################################
+
+from nbconvert import WebPDFExporter
+from .tools import OfflineMixing
+
+
+class OfflineWebPDFExporter(OfflineMixing, WebPDFExporter):
+
+    template_name = "webpdf_offline"
+
+    def from_notebook_node(self, nb, resources=None, **kw):
+        self.mathjax_url = self.mathjax_url + f"?config={self.mathjax_option}"
+        resources["offline_module_path"] = self.offline_module_path
+        return super().from_notebook_node(nb, resources, **kw)
