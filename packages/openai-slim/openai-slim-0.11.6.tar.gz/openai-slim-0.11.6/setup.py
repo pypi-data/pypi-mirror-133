63952a436b7e6f84b109d3f15b1410c8beb4c18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:05 +0000

    Add package openai-slim/0.11.6/openai-slim-0.11.6.tar.gz

---
+++ packages/openai-slim/openai-slim-0.11.6.tar.gz/openai-slim-0.11.6/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import find_packages, setup
+
+version_contents = {}
+version_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "openai/version.py"
+)
+with open(version_path, "rt") as f:
+    exec(f.read(), version_contents)
+
+setup(
+    name="openai-slim",
+    description="Python client library for the OpenAI API",
+    version=version_contents["VERSION"],
+    install_requires=[
+        "requests>=2.20",  # to get the patch for CVE-2018-18074
+    ],
+    extras_require={"dev": ["black~=21.6b0", "pytest==6.*"]},
+    python_requires=">=3.7.1",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={
+        "openai-slim": [
+            "data/ca-certificates.crt",
+            "py.typed",
+        ]
+    },
+    author="OpenAI",
+    author_email="mitranopeter@gmail.com",
+    url="https://github.com/PeterMitrano/openai-python-slim",
+)
