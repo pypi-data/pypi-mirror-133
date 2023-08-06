ab12a0afccd06d3e1d52c080ebad6e8157bdf48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package docstats/0.1.2/docstats-0.1.2-py3-none-any.whl

---
+++ packages/docstats/docstats-0.1.2-py3-none-any.whl/docstats/__init__.py
@@ -0,0 +1,40 @@
+"""Sphinx extension initialization."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from .directives.call_graph import *
+
+
+def setup(app):
+    """Setup sphinx extension."""
+
+    from docutils import nodes
+    from sphinx.ext.graphviz import (
+        render_dot_html,
+        render_dot_latex,
+        render_dot_texinfo,
+    )
+
+    from .directives.call_graph import CallGraph, call_graph
+
+    def skip(self: Any, node: Any) -> None:
+        raise nodes.SkipNode
+
+    app.setup_extension("sphinx.ext.graphviz")
+
+    app.add_node(
+        call_graph,
+        latex=(render_dot_latex, None),
+        html=(render_dot_html, None),
+        text=(skip, None),
+        man=(skip, None),
+        texinfo=(render_dot_texinfo, None),
+    )
+    app.add_directive("callgraph", CallGraph)
+
+    return {
+        "version": "0.1",
+        "parallel_read_safe": True,
+    }
