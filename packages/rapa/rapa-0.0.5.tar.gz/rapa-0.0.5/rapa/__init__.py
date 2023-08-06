ab13eaaedacf7450d365ed1aa7bbd8bbf6c18002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:00 +0000

    Add package rapa/0.0.5/rapa-0.0.5.tar.gz

---
+++ packages/rapa/rapa-0.0.5.tar.gz/rapa-0.0.5/rapa/__init__.py
@@ -0,0 +1,7 @@
+from .version import __version__
+
+# Warnings from dependencies are suppressed everywhere, propagating into all created loggers
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from . import rapa, base, utils
---
+++ packages/rapa/rapa-0.0.5.tar.gz/rapa-0.0.5/rapa/rapa.py
@@ -0,0 +1,25 @@
+from .base import RAPABase 
+from . import utils
+
+import datarobot as dr
+
+class RAPAClassif(RAPABase):
+    """
+        RAPA class meant for classification problems.
+    """
+
+    def __init__(self, project: dr.Project = None):
+        self.project = project
+        self._classification = True 
+        self._regression = False
+
+
+class RAPARegress(RAPABase):
+    """
+        RAPA class meant for regression problems.
+    """
+    
+    def __init__(self, project: dr.Project = None):
+        self.project = project
+        self._classification = False 
+        self._regression = True
