da84b1947832f3f842872d97ccdeda0bec96de85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:02 +0000

    Add package BioSequences/1.0.9/BioSequences-1.0.9.tar.gz

---
+++ packages/BioSequences/BioSequences-1.0.9.tar.gz/BioSequences-1.0.9/bioseq/utils.py
@@ -0,0 +1,97 @@
+from typing import List, Tuple
+from bioseq.config import SYMBOL
+
+
+def read_fasta(filename: str) -> Tuple[List[str], List[str]]:
+    """
+    Read fasta file
+    Args:
+        filename: the fasta file's name
+    Returns:
+        seq_list: the list of sequences
+        seq_ids: the list of sequence's ids
+    """
+    seq = ""
+    seq_list = []
+    seq_ids = []
+
+    with open(filename) as f:
+        for line in f.readlines():
+            if line.startswith(">"):
+                seq_ids.append(line[1:].strip())
+
+                if seq:
+                    seq_list.append(seq)
+                    seq = ""
+                continue
+            seq += line.strip()
+        seq_list.append(seq)
+
+    return seq_list, seq_ids
+
+
+def printAlign(
+        sequence1: str,
+        sequence2: str,
+        spacing: int = 10,
+        line_width: int = 30,
+        show_seq: bool = True) -> None:
+    """
+    Print two sequence by a pretty format
+    Args:
+        sequence1:  Sequence1
+        sequence2:  Sequence2
+        spacing:    A space each $spacing char
+        line_width: the width of each line
+        show_seq:   if False, only print the alignment result
+    """
+    symbol_line = ""
+    format_seq1 = ""
+    format_seq2 = ""
+    count = 0
+    length = len(sequence1) if len(sequence1) < len(
+        sequence2) else len(sequence2)
+    match_symbol, mismatch_symbol, gap_symbol = SYMBOL["printAlign"]
+
+    for i in range(length):
+        base1 = sequence1[i]
+        base2 = sequence2[i]
+        if base1 == base2:
+            symbol_line += match_symbol
+        elif base1 == "-" or base2 == "-":
+            symbol_line += gap_symbol
+        else:
+            symbol_line += mismatch_symbol
+
+        format_seq1 += base1
+        format_seq2 += base2
+        count += 1
+
+        if count % spacing == 0:
+            format_seq1 += " "
+            format_seq2 += " "
+            symbol_line += " "
+        if count % line_width == 0:
+            count = 0
+
+    space_num = 0
+    space_each_line = line_width // spacing
+
+    for i in range(0, length, line_width):
+        start = i + space_num
+        end = start + line_width + space_each_line
+        space_num += space_each_line
+        if show_seq:
+            print(f"{i + 1:>5}", end=" ")
+            print(format_seq1[start: end])
+
+            print(f" " * 5, end=" ")
+            print(symbol_line[start: end])
+
+            print(f"{i + 1:>5}", end=" ")
+            print(format_seq2[start: end])
+        else:
+            print(f"{i + 1:>5}", end=" ")
+            print(symbol_line[start: end])
+
+        print()
