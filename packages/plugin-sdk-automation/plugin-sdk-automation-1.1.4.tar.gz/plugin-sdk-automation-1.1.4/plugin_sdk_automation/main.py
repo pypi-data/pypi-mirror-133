db6e24bd5ab11b4945482ebd80b965366edc2ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:07 +0000

    Add package plugin-sdk-automation/1.1.4/plugin-sdk-automation-1.1.4.tar.gz

---
+++ packages/plugin-sdk-automation/plugin-sdk-automation-1.1.4.tar.gz/plugin-sdk-automation-1.1.4/plugin_sdk_automation/main.py
@@ -0,0 +1,53 @@
+import logging
+
+import click
+from rich.logging import RichHandler
+from rich.traceback import install
+
+from .handlers import DockerHandler
+from .handlers.props_handler import PropsHandler
+
+
+install()
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+st = RichHandler()
+fmt = logging.Formatter("%(filename)s:%(lineno)d - %(message)s")
+st.setFormatter(fmt)
+log.addHandler(st)
+
+
+@click.group(invoke_without_command=True)
+@click.option("--debug", default=False, is_flag=True)
+@click.pass_context
+def cli(ctx, debug):
+    log.info(f"Debug is: {debug}")
+    if debug:
+        log.setLevel(logging.DEBUG)
+    if ctx.invoked_subcommand is None:
+        log.info("No subcommand provided, executing build")
+        ctx.invoke(build)
+
+
+@cli.command()
+@click.option("--directory", "-d")
+@click.option("--python-version", "-p", type=click.Choice(["py36", "py38"]))
+def build(directory: str, python_version: str):
+    d = DockerHandler(log=log, directory=directory, python_version=python_version)
+    d.build()
+
+
+@cli.command()
+def gen_props():
+    p = PropsHandler(log=log)
+    p.run()
+
+
+def main():
+    cli()
+
+
+if __name__ == "__main__":
+    main()
