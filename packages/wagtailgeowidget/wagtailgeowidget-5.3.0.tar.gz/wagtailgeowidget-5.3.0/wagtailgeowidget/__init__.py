a7083ca644d381c69fb2f758b6a2084861b1b37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:54 +0000

    Add package wagtailgeowidget/5.3.0/wagtailgeowidget-5.3.0.tar.gz

---
+++ packages/wagtailgeowidget/wagtailgeowidget-5.3.0.tar.gz/wagtailgeowidget-5.3.0/wagtailgeowidget/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+wagtailgeowidget
+----------
+Wagtail-Geo-Widget is the complete map solution for your Wagtail site.
+"""
+
+__title__ = "wagtailgeowidget"
+__version__ = "5.3.0"
+__build__ = 530
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2021 Fröjd Interactive"
