64f0ad578bb74b7e720b325551e924ac62adf9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:48 +0000

    Add package types-appdirs/1.4.2/types-appdirs-1.4.2.tar.gz

---
+++ packages/types-appdirs/types-appdirs-1.4.2.tar.gz/types-appdirs-1.4.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-appdirs"
+description = "Typing stubs for appdirs"
+long_description = '''
+## Typing stubs for appdirs
+
+This is a PEP 561 type stub package for the `appdirs` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `appdirs`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/appdirs. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `032e6ee90cbb938259a2aa2183966502de19108e`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.4.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['appdirs-stubs'],
+      package_data={'appdirs-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Stubs Only",
+      ]
+)
