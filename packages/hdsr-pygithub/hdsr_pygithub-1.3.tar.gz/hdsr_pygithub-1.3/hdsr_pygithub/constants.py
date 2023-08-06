5ce3565c63cc5d2963cdd8b30a12d2fe0c21615e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:22 +0000

    Add package hdsr-pygithub/1.3/hdsr_pygithub-1.3.tar.gz

---
+++ packages/hdsr-pygithub/hdsr_pygithub-1.3.tar.gz/hdsr_pygithub-1.3/hdsr_pygithub/constants.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+
+# read-only token for github account https://github.com/HdsrMidReadOnly
+GITHUB_HDSR_READ_ONLY_ACCOUNT_ACCESS_TOKEN = "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"
+DEFAULT_GITHUB_ORGANISATION = "hdsr-mid"
+
+# authentication and authorization can be set online (you need admin rights for https://github.com/hdr-mid) at:
+# https://github.com/orgs/hdsr-mid/people/HdsrMidReadOnly
+
+# how to create a access token?
+# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+
+# how to change access token for account HdsrMidReadOnly?
+# login github.com with account HdsrMidReadOnly
+# email=hdsrmidgithub@gmail.com
+# password=please contact renier.kramer@hdsr.nl
+
+# BASE_DIR avoid 'Path.cwd()'
+BASE_DIR = Path(__file__).parent.parent
+
+
+def check_constants():
+    if not (BASE_DIR.name == "hdsr_pygithub" and BASE_DIR.parent != "hdsr_pygithub"):
+        raise AssertionError(f"BASE_DIR {BASE_DIR} is wrong, should be the name of the project (root dir)")
+
+
+check_constants()
