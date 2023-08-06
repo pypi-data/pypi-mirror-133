a4df4479e0910ca0b497d5e6aa98872d77d06816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:27 +0000

    Add package pdf2up/0.2.0/pdf2up-0.2.0-py3-none-any.whl

---
+++ packages/pdf2up/pdf2up-0.2.0-py3-none-any.whl/pdf2up/cli.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from argparse import ArgumentParser
+
+import argcomplete
+
+
+class Pdf2upParser(ArgumentParser):
+    argument_signatures: list[tuple[str], dict] = [
+        (["input_file"], {"metavar": "input"}),
+        (["-b", "--box"], {"type": int, "nargs": "+"}),
+        (["--all"], {"dest": "all_pages", "action": "store_true"}),
+        (["-s", "--skip"], {"type": int}),
+        (["-n", "--n-up"], {"type": int}),
+    ]
+    kwarg_names: list[str] = "input_file box all_pages skip".split()
+
+    def __init__(self):
+        super().__init__()
+        for args, kwargs in self.argument_signatures:
+            self.add_argument(*args, **kwargs)
+        argcomplete.autocomplete(self)
