bc76a5b668ffb58bf811fe6fa5122e2399cf311d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:49 +0000

    Add package dominodatalab-data/0.1.9.dev2/dominodatalab-data-0.1.9.dev2.tar.gz

---
+++ packages/dominodatalab-data/dominodatalab-data-0.1.9.dev2.tar.gz/dominodatalab-data-0.1.9.dev2/datasource_api_client/models/datasource_dto_data_source_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class DatasourceDtoDataSourceType(str, Enum):
+    GCSCONFIG = "GCSConfig"
+    MYSQLCONFIG = "MySQLConfig"
+    POSTGRESQLCONFIG = "PostgreSQLConfig"
+    REDSHIFTCONFIG = "RedshiftConfig"
+    S3CONFIG = "S3Config"
+    SNOWFLAKECONFIG = "SnowflakeConfig"
+    SQLSERVERCONFIG = "SQLServerConfig"
+
+    def __str__(self) -> str:
+        return str(self.value)
