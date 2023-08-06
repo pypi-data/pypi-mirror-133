0475d823559641e7f146c75032c404da3c7a8df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:26 +0000

    Add package web3-wrapped-contract/0.0.16/web3_wrapped_contract-0.0.16-py3-none-any.whl

---
+++ packages/web3-wrapped-contract/web3_wrapped_contract-0.0.16-py3-none-any.whl/web3_wrapped_contract/__init__.py
@@ -0,0 +1,4 @@
+from .wrapped_contract import WrappedContract
+from .no_account_exception import NoAccountException
+
+from .contract_utils import method_signature
---
+++ packages/web3-wrapped-contract/web3_wrapped_contract-0.0.16-py3-none-any.whl/web3_wrapped_contract/contract_utils.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------ Imports ----------------------------------------------------------- #
+
+# Pip
+from web3._utils.abi import normalize_event_input_types, abi_to_signature
+from web3.contract import ContractFunction
+
+# -------------------------------------------------------------------------------------------------------------------------------- #
+
+
+
+# -------------------------------------------------------- Public functions ------------------------------------------------------ #
+
+def method_signature(
+    method: ContractFunction
+) -> str:
+    return "{fn_name}({fn_input_types})({fn_output_types})".format(
+        fn_name=method.abi['name'],
+        fn_input_types=','.join([
+            arg['type'] for arg in normalize_event_input_types(method.abi.get('inputs', []))
+        ]),
+        fn_output_types=','.join([
+            arg['type'] for arg in normalize_event_input_types(method.abi.get('outputs', []))
+        ])
+    )
+
+
+# -------------------------------------------------------------------------------------------------------------------------------- #
