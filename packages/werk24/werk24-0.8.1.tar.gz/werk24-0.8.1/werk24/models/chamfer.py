25f09a3128eaa1e7c73c68ea902ff2f8cb5d1f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:58 +0000

    Add package werk24/0.8.1/werk24-0.8.1.tar.gz

---
+++ packages/werk24/werk24-0.8.1.tar.gz/werk24-0.8.1/werk24/models/chamfer.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+
+from pydantic import BaseModel
+
+from .unit import W24UnitAngle
+
+
+class W24Chamfer(BaseModel):
+    """ Chamfer in degree.
+
+    Attributes:
+        blurb: String representation for human consumption
+
+        angle: Chamfer angle in degrees
+
+        unit: Angle Unit. Currently only degrees are
+            supported.
+
+    !!! note
+        The chamfers can be tolerated.
+        Future implementations will take this into account.
+
+    !!! note
+        If you are dealing with GON, let us know.
+        Happy to do exend the API for the pleasure of
+        dealing with the concept.
+    """
+
+    blurb: str
+
+    angle: Decimal
+
+    unit: W24UnitAngle = W24UnitAngle.DEGREE
