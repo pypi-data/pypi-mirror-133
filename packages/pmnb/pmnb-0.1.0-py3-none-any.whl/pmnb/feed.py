c5e1070054a67cfcfb3ee084cc3de7baf677a599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:05 +0000

    Add package pmnb/0.1.0/pmnb-0.1.0-py3-none-any.whl

---
+++ packages/pmnb/pmnb-0.1.0-py3-none-any.whl/pmnb/feed.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+#比较两个句子的相似度
+import sys
+
+def xsd(text1,text2):
+	#集合必须满足互斥性，所以去重
+	text1=set(text1)
+	text2=set(text2)
+	sml=(len(text1&text2))/(len(text1|text2))
+	print("两个句子的相似度为:{0}".format(sml))
+
