1b10815deacd01344503909a7f919d8b106a7b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:45 +0000

    Add package mosfpdf/1.0/mosfpdf-1.0.tar.gz

---
+++ packages/mosfpdf/mosfpdf-1.0.tar.gz/mosfpdf-1.0/setup.py
@@ -0,0 +1,10 @@
+from typing import Text
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="mosfpdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["test", "data"])
+)
