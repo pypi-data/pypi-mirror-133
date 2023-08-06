12328505b70032cfa7b92f057770b2485838caa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:35 +0000

    Add package simpleSQLiteDB/1.0.1/simpleSQLiteDB-1.0.1.tar.gz

---
+++ packages/simpleSQLiteDB/simpleSQLiteDB-1.0.1.tar.gz/simpleSQLiteDB-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from simpleDB import __version__
+
+setup(
+    name='simpleSQLiteDB',
+    version=__version__,
+
+    url='https://github.com/KingHacker9000/simpleDB',
+    author='Ashish Ajin Thomas',
+    author_email='Coolioboss7@gmail.com',
+
+    py_modules=['simpleDB'],
+)
