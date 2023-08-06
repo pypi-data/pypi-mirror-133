56241077371035f47cccc72b0873bda7cfa5cf7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:23 +0000

    Add package googlescrape/0.0.7/googlescrape-0.0.7.tar.gz

---
+++ packages/googlescrape/googlescrape-0.0.7.tar.gz/googlescrape-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='googlescrape',
+  version='0.0.7',
+  description='This is a python api to scrape search results from a url.',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type="text/markdown",
+  url='https://github.com/JavaProgswing/googlescrape',
+  author='JavaProgswing',
+  author_email='thejavaprofessional@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='googlescrape',
+  packages=find_packages(),
+  install_requires=['selenium >= 4.1.0','beautifulsoup4 >= 4.10.0','webdriver-manager == 3.5.2','validators >= 0.18.2']
+)
