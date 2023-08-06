9b5de2609eb4172af827e8d8254000f5aa151cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:25 +0000

    Add package distributions-lex/0.1/distributions_lex-0.1.tar.gz

---
+++ packages/distributions-lex/distributions_lex-0.1.tar.gz/distributions_lex-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_lex',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_lex'],
+      author='Alex Macharia',
+      author_email='alex.machariam@gmail.com',
+      zip_safe=False)
