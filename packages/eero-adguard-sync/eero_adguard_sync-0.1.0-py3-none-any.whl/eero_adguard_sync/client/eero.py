ebd80675324df443920e9d4c6e442061a8e5646e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:57 +0000

    Add package eero-adguard-sync/0.1.0/eero_adguard_sync-0.1.0-py3-none-any.whl

---
+++ packages/eero-adguard-sync/eero_adguard_sync-0.1.0-py3-none-any.whl/eero_adguard_sync/client/eero.py
@@ -0,0 +1,45 @@
+import os
+
+import eero
+
+from eero_adguard_sync.utils import app_paths
+from eero_adguard_sync.models import EeroClientDevice
+
+
+class CookieStore(eero.SessionStorage):
+    # See: https://github.com/343max/eero-client/blob/master/sample.py
+    def __init__(self, cookie_file):
+        self.cookie_file = os.path.abspath(cookie_file)
+
+        try:
+            with open(self.cookie_file, "r") as f:
+                self.__cookie = f.read()
+        except IOError:
+            self.__cookie = None
+
+    @property
+    def cookie(self):
+        return self.__cookie
+
+    @cookie.setter
+    def cookie(self, cookie):
+        self.__cookie = cookie
+        with open(self.cookie_file, "w+") as f:
+            f.write(self.__cookie)
+
+
+class EeroClient(eero.Eero):
+    model_fields = {"ips", "mac", "nickname", "device_type", "wireless"}
+
+    def __init__(self):
+        session = CookieStore(os.path.join(app_paths.app_data_path, "session.cookie"))
+        super().__init__(session)
+
+    def get_clients(self, network: str) -> list[EeroClientDevice]:
+        devices: list[EeroClientDevice] = []
+        for device in self.devices(network):
+            new_device = {}
+            for key in self.model_fields:
+                new_device[key] = device[key]
+            devices.append(EeroClientDevice(**new_device))
+        return devices
---
+++ packages/eero-adguard-sync/eero_adguard_sync-0.1.0-py3-none-any.whl/eero_adguard_sync/models/eero/client_device.py
@@ -0,0 +1,33 @@
+import macaddress
+from dataclasses import dataclass
+
+from eero_adguard_sync.utils import CLIENT_TAG_MAP
+from eero_adguard_sync.models.adguard.client_device import AdGuardClientDevice
+
+
+@dataclass
+class EeroClientDevice:
+    ips: list[str]
+    mac: str
+    nickname: str
+    device_type: str
+    wireless: bool = False
+
+    @property
+    def adguard_device_type(self) -> str:
+        return CLIENT_TAG_MAP.get(self.device_type, "device_other")
+
+    @property
+    def identifiers(self) -> list[str]:
+        return [self.mac, *self.ips]
+
+    @property
+    def normalized_mac(self) -> str:
+        return str(macaddress.MAC(self.mac))
+
+    def as_adguard_device(self) -> AdGuardClientDevice:
+        return AdGuardClientDevice(
+            ids=self.identifiers,
+            name=self.nickname or self.mac,
+            tags=[self.adguard_device_type],
+        )
