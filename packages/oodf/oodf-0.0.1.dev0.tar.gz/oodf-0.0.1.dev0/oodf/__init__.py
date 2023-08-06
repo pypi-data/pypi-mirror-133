f8932467c2b9ec7727f68d882925d19555186147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:04 +0000

    Add package oodf/0.0.1.dev0/oodf-0.0.1.dev0.tar.gz

---
+++ packages/oodf/oodf-0.0.1.dev0.tar.gz/oodf-0.0.1.dev0/oodf/__init__.py
@@ -0,0 +1,8 @@
+# Project is under an MIT-style license that can be found in the LICENSE file at the monorepo
+# Monorepo: https://github.com/Arthurdw/oodf/libraries/python
+"""
+A library for interacting with oodf in python.
+Usage/documentation when library is in a more stable/finished state.
+"""
+
+from .__lib__ import *
