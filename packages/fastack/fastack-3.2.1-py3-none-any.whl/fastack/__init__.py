6b00c36f65ed6e69f9a398af0c8e9782e9ae3922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:43 +0000

    Add package fastack/3.2.1/fastack-3.2.1-py3-none-any.whl

---
+++ packages/fastack/fastack-3.2.1-py3-none-any.whl/fastack/__init__.py
@@ -0,0 +1,13 @@
+from .app import Fastack, create_app  # noqa
+from .constants import *  # noqa
+from .controller import (  # noqa
+    Controller,
+    CreateController,
+    CreateUpdateController,
+    DestroyController,
+    ListController,
+    ModelController,
+    ReadOnlyController,
+    RetrieveController,
+    UpdateController,
+)
