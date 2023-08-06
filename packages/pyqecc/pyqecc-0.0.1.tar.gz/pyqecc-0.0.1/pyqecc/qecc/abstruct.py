fd143932d59a489b545fb780da44eeaefbd71f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:34 +0000

    Add package pyqecc/0.0.1/pyqecc-0.0.1.tar.gz

---
+++ packages/pyqecc/pyqecc-0.0.1.tar.gz/pyqecc-0.0.1/pyqecc/qecc/abstruct.py
@@ -0,0 +1,45 @@
+from abc import ABCMeta, abstractmethod
+import numpy as np
+
+class CODE(metaclass=ABCMeta):
+    _name = ""
+    def __init__(self,n,k):
+        self._n = n
+        self._k = k
+        self._nk = n - k
+        self._R = self.k/self.n
+
+    '''
+    #未実装
+    @property
+    def enc_circ(self):
+        return self._enc_circuit
+    '''
+
+    @property
+    def n(self):
+        return self._n
+
+    @property
+    def k(self):
+        return self._k
+    
+    @property
+    def nk(self):
+        return self._nk
+
+    @property
+    def R(self):
+        return self._R
+
+    @property
+    def name(self):
+        return self._name
+
+    def __str__(self):
+        output = ""
+        output+="codename        :"+str(self.name)+"\n"
+        output+="n               : "+str(self.n)+"\n"
+        output+="k               : "+str(self.k)+"\n"
+        output+="R               : "+str(self.R)+"\n"
+        return output
