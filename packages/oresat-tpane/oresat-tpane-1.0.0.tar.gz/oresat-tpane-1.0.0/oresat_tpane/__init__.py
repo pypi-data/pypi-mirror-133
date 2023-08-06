970798f8fbe7a70c05660eda5000da58de3fb3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:53 +0000

    Add package oresat-tpane/1.0.0/oresat-tpane-1.0.0.tar.gz

---
+++ packages/oresat-tpane/oresat-tpane-1.0.0.tar.gz/oresat-tpane-1.0.0/oresat_tpane/__init__.py
@@ -0,0 +1,23 @@
+"""This module is responsible for providing a high-level interface for elements
+of Curses UI and general user interaction with the app,
+"""
+
+MAJOR = 1
+MINOR = 0
+PATCH = 0
+
+LIB_NAME = 'oresat-tpane'
+LIB_DESCRIPTION = 'This module is responsible for providing a high-level' \
+    ' interface for elements of Curses UI and general user interaction with' \
+    ' the app'
+LIB_VERSION = f'{MAJOR}.{MINOR}.{PATCH}'
+LIB_AUTHOR = 'Dmitri McGuckin'
+LIB_EMAIL = 'dmitri3@pdx.edu'
+LIB_URL = 'https://github.com/oresat/oresat-tpane'
+LIB_LICENSE = 'GPL-3.0'
+LIB_DOCS = 'https://canopen-monitor.readthedocs.io'
+LIB_ISSUES = 'https://github.com/oresat/oresat-tpane/labels/bug'
+
+
+MAINTAINER_NAME = 'Portland State Aerospace Society'
+MAINTAINER_EMAIL = 'oresat@pdx.edu'
