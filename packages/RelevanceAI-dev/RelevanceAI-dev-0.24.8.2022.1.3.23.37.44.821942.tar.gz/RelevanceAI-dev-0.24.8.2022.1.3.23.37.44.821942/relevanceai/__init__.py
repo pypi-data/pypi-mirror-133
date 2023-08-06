be3f7bbbf418a625932e7f4c9bc62de26f579e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package RelevanceAI-dev/0.24.8.2022.1.3.23.37.44.821942/RelevanceAI-dev-0.24.8.2022.1.3.23.37.44.821942.tar.gz

---
+++ packages/RelevanceAI-dev/RelevanceAI-dev-0.24.8.2022.1.3.23.37.44.821942.tar.gz/RelevanceAI-dev-0.24.8.2022.1.3.23.37.44.821942/relevanceai/__init__.py
@@ -0,0 +1,10 @@
+from relevanceai import vector_tools
+from relevanceai.http_client import Client
+
+# Import useful utility if possible as well
+try:
+    from jsonshower import show_json
+except ModuleNotFoundError:
+    pass
+
+__version__ = "0.24.8"
