cf7696b3474fe833e918a2c2cb1d81bb2ea3f335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:12 +0000

    Add package types-httplib2/0.20.3/types-httplib2-0.20.3.tar.gz

---
+++ packages/types-httplib2/types-httplib2-0.20.3.tar.gz/types-httplib2-0.20.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-httplib2"
+description = "Typing stubs for httplib2"
+long_description = '''
+## Typing stubs for httplib2
+
+This is a PEP 561 type stub package for the `httplib2` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `httplib2`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/httplib2. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `032e6ee90cbb938259a2aa2183966502de19108e`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.20.3",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['httplib2-stubs'],
+      package_data={'httplib2-stubs': ['__init__.pyi', 'auth.pyi', 'certs.pyi', 'error.pyi', 'iri2uri.pyi', 'socks.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Stubs Only",
+      ]
+)
