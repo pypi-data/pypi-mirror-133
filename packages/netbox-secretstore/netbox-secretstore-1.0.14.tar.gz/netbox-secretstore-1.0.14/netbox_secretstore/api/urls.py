1b92ab8e25ae741809f35e0b6e24570c0a9eb1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package netbox-secretstore/1.0.14/netbox-secretstore-1.0.14.tar.gz

---
+++ packages/netbox-secretstore/netbox-secretstore-1.0.14.tar.gz/netbox-secretstore-1.0.14/netbox_secretstore/api/urls.py
@@ -0,0 +1,16 @@
+from netbox.api import OrderedDefaultRouter
+from . import views
+
+
+router = OrderedDefaultRouter()
+router.APIRootView = views.SecretsRootView
+
+# Secrets
+router.register('secret-roles', views.SecretRoleViewSet)
+router.register('secrets', views.SecretViewSet)
+
+# Miscellaneous
+router.register('get-session-key', views.GetSessionKeyViewSet, basename='get-session-key')
+router.register('generate-rsa-key-pair', views.GenerateRSAKeyPairViewSet, basename='generate-rsa-key-pair')
+
+urlpatterns = router.urls
