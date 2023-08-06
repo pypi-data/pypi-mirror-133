d55c55cdccd23ff1c2f966fe55cdc9c784b745ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:49 +0000

    Add package linora/1.1.0/linora-1.1.0-py3-none-any.whl

---
+++ packages/linora/linora-1.1.0-py3-none-any.whl/linora/__init__.py
@@ -0,0 +1,26 @@
+from linora import chart
+from linora import metrics
+from linora import parallel
+from linora import param_search
+from linora import feature_column
+from linora import feature_selection
+from linora import sample
+from linora import sample_splits
+from linora import text
+from linora import image
+from linora import utils
+
+__version__ = '1.1.0'
+__author__ = 'JinQing Lee'
+
+def _hello():
+    print(f"""
+------------------------------------------------------------------------------------
+      Linora
+--------------------
+      Version      : --  {__version__}  --
+      Author       : --  {__author__}  --
+      License      : Apache-2.0
+      Homepage     : https://github.com/Hourout/linora
+      Description  : Simple and efficient tools for data mining and data analysis.
+------------------------------------------------------------------------------------""")
