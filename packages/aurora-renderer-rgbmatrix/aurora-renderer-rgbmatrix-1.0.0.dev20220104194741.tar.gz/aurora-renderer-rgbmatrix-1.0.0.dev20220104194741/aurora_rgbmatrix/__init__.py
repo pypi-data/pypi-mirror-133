69c4264f58e336c32d62d0a60e7ebe7a57a9c66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:00 +0000

    Add package aurora-renderer-rgbmatrix/1.0.0.dev20220104194741/aurora-renderer-rgbmatrix-1.0.0.dev20220104194741.tar.gz

---
+++ packages/aurora-renderer-rgbmatrix/aurora-renderer-rgbmatrix-1.0.0.dev20220104194741.tar.gz/aurora-renderer-rgbmatrix-1.0.0.dev20220104194741/aurora_rgbmatrix/__init__.py
@@ -0,0 +1,65 @@
+from typing import Optional
+
+from PIL import Image
+from aurora.base.render import AuroraRenderTarget
+from aurora.types import TwoDimSize
+
+from rgbmatrix.core import RGBMatrix, RGBMatrixOptions
+
+
+__version__ = '1.0.0-dev20220104194741'
+__license__ = 'MIT'
+__title__ = 'aurora-renderer-rgbmatrix'
+
+
+class RgbMatrixRenderTarget(AuroraRenderTarget):
+    """
+    RgbMatrixRenderTarget is a render target for rpi-rgb-led-matrix
+    allowing you to display images on a RGB LED Matrix Display
+    """
+    def __init__(self,
+                 size: TwoDimSize,
+                 rgb_options: Optional[RGBMatrixOptions] = None,
+                 rgb_brightness: int = 100,
+                 rgb_gpio_slowdown: int = 3,
+                 rgb_hardware_mapping: str = 'adafruit-hat'):
+        """
+        Initializes a new RgbMatrixRenderTarget with the provided options
+
+        Note: For all options labelled rgb_, refer to rpi-rgb-led-matrix documentation found here:
+        https://github.com/hzeller/rpi-rgb-led-matrix/tree/master/bindings/python
+
+        :param size: Dimensions of the device being rendered on (<px> x <px>)
+        :param rgb_options: (optional) RGBMatrixOptions settings, will override any other parameters
+        :param rgb_brightness: (optional) Brightness of the display
+        :param rgb_gpio_slowdown: (optional) Slowdown of GPIO communication to stabilize image
+        :param rgb_hardware_mapping: (optional) Mapping for hardware being used
+        """
+        super().__init__()
+        self._screen_size = size
+
+        self._matrix: Optional[RGBMatrix] = None
+        self._canvas: Optional[RGBMatrix] = None
+
+        if rgb_options:
+            self._options: RGBMatrixOptions = rgb_options
+        else:
+            self._options: RGBMatrixOptions = RGBMatrixOptions()
+            o = self._options
+            o.gpio_slowdown = rgb_gpio_slowdown
+            o.brightness = rgb_brightness
+            o.hardware_mapping = rgb_hardware_mapping
+
+    def init(self):
+        super().init()
+        self._matrix = RGBMatrix(options=self._options)
+        self._canvas = self._matrix.CreateFrameCanvas()
+
+    def render_image(self, image: Image, render_time: float):
+        c: RGBMatrix = self._canvas
+        c.SetImage(image)
+        self._matrix.SwapOnVSync()
+
+    @property
+    def screen_size(self) -> TwoDimSize:
+        return self._screen_size
---
+++ packages/aurora-renderer-rgbmatrix/aurora-renderer-rgbmatrix-1.0.0.dev20220104194741.tar.gz/aurora-renderer-rgbmatrix-1.0.0.dev20220104194741/setup.py
@@ -0,0 +1,51 @@
+from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+
+
+HERE = abspath(dirname(__file__))
+
+
+def _read_file(path: str) -> str:
+    """ Reads any file and returns its content as a string """
+    with open(path, 'r') as f:
+        return f.read()
+
+
+setup(
+    name='aurora-renderer-rgbmatrix',
+    version='1.0.0-dev20220104194741',
+    description='Renderer implementation for RGB Matrix Displays',
+    long_description=_read_file(join(HERE, 'README.md')),
+    long_description_content_type='text/markdown',
+    author='Raphael "rGunti" Guntersweiler',
+    author_email='raphael+pip@gunti.cloud',
+    license='MIT',
+    url='https://gitlab.com/aurora-display-lib/aurora-renderer-rgbmatrix',
+    packages=find_packages(include=['aurora_rgbmatrix', 'aurora_rgbmatrix.*']),
+    python_requires='>=3.5',
+    install_requires=[
+        'aurora-display-lib>=1.0.0.dev20220104173716'
+    ],
+    extras_require={
+        'deploy': [
+            'bump2version'
+        ],
+        'test': [
+            'testfixtures>=1.2.0',
+            'pytest>=3.4.0',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    include_package_data=True,
+    include_dirs=[
+    ],
+    entry_points={},
+    zip_safe=False)
