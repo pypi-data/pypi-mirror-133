7d2644f5da5980513a47c0c21dd5b2f961a26db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:16 +0000

    Add package cognite-tracing/0.2.0/cognite_tracing-0.2.0-py3-none-any.whl

---
+++ packages/cognite-tracing/cognite_tracing-0.2.0-py3-none-any.whl/cognite/tracing/traced.py
@@ -0,0 +1,38 @@
+import inspect
+from functools import wraps
+from inspect import getattr_static
+from typing import Any, Callable, Dict, List, Type
+
+from cognite.tracing import span
+
+
+def traced(f: Callable) -> Callable:
+    """Decorator for enabling tracing of this function"""
+    if inspect.iscoroutinefunction(f):
+
+        @wraps(f)
+        async def decorated(*args: List, **kwargs: Dict) -> Any:
+            with span(name=f.__qualname__):
+                result = await f(*args, **kwargs)
+                return result
+
+    else:
+
+        @wraps(f)
+        def decorated(*args: List, **kwargs: Dict) -> Any:
+            with span(name=f.__qualname__):
+                result = f(*args, **kwargs)
+                return result
+
+    return decorated
+
+
+def traced_methods(cls: Type) -> Type:
+    """Decorator for enabling tracing of all public instance methods within the class"""
+    for attr in cls.__dict__:
+        attr_is_public_method = not attr.startswith("_") and callable(getattr(cls, attr))
+        attr_is_staticmethod = isinstance(getattr_static(cls, attr), staticmethod)
+        attr_is_classmethod = isinstance(getattr_static(cls, attr), classmethod)
+        if attr_is_public_method and not attr_is_staticmethod and not attr_is_classmethod:
+            setattr(cls, attr, traced(getattr(cls, attr)))
+    return cls
