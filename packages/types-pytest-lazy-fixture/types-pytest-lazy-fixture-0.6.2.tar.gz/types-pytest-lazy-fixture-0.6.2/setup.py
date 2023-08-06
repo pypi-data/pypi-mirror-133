c4986a0d8b84ec987646697a7abe290fc1e03191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:26 +0000

    Add package types-pytest-lazy-fixture/0.6.2/types-pytest-lazy-fixture-0.6.2.tar.gz

---
+++ packages/types-pytest-lazy-fixture/types-pytest-lazy-fixture-0.6.2.tar.gz/types-pytest-lazy-fixture-0.6.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-pytest-lazy-fixture"
+description = "Typing stubs for pytest-lazy-fixture"
+long_description = '''
+## Typing stubs for pytest-lazy-fixture
+
+This is a PEP 561 type stub package for the `pytest-lazy-fixture` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `pytest-lazy-fixture`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/pytest-lazy-fixture. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `032e6ee90cbb938259a2aa2183966502de19108e`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.6.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['pytest_lazyfixture-stubs'],
+      package_data={'pytest_lazyfixture-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Stubs Only",
+      ]
+)
