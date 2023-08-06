966e2d45aa0e7469d5eb31daa59b9356794e8f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:24 +0000

    Add package cksplinter/0.17.1/cksplinter-0.17.1.tar.gz

---
+++ packages/cksplinter/cksplinter-0.17.1.tar.gz/cksplinter-0.17.1/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 splinter authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+from setuptools import setup, find_packages
+import codecs
+
+
+README = codecs.open("README.rst", encoding="utf-8").read()
+
+setup(
+    name="cksplinter",
+    version="0.17.1",
+    url="https://github.com/cobrateam/splinter",
+    description="browser abstraction for web acceptance testing",
+    long_description=README,
+    author="CobraTeam",
+    author_email="andrewsmedina@gmail.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+    ]
+    + [("Programming Language :: Python :: %s" % x) for x in "3.6 3.7 3.8 3.9".split()],
+    project_urls={
+        "Documentation": "https://splinter.readthedocs.io/",
+        "Changelog": "https://github.com/cobrateam/splinter/tree/master/docs/news",
+        "Source": "https://github.com/cobrateam/splinter/",
+        "Tracker": "https://github.com/cobrateam/splinter/issues",
+    },
+    packages=find_packages(exclude=["docs", "tests", "samples"]),
+    include_package_data=True,
+    install_requires=[],
+    extras_require={
+        "zope.testbrowser": ["zope.testbrowser>=5.5.1", "lxml>=4.2.4", "cssselect"],
+        "django": ["Django>=2.0.6", "lxml>=4.2.4", "cssselect"],
+        "flask": ["Flask>=2.0.2", "lxml>=4.2.4", "cssselect"],
+        "edge": ["msedge-selenium-tools"],
+        "selenium3": ["selenium>=3.141.0,<4.0"],
+        "selenium4": ["selenium>=4.1.0,<5.0"],
+    },
+    tests_require=["coverage", "flask"],
+)
