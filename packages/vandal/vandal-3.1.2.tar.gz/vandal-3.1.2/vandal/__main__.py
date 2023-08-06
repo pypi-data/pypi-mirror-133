c810ceeec82ca94da2bd04b3fbeca2a6e0937136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package vandal/3.1.2/vandal-3.1.2.tar.gz

---
+++ packages/vandal/vandal-3.1.2.tar.gz/vandal-3.1.2/vandal/__main__.py
@@ -0,0 +1,6 @@
+# imports all CLI clients.
+from vandal.cli import App
+
+# runs the main CLI client.
+if __name__ == '__main__':
+    App
---
+++ packages/vandal/vandal-3.1.2.tar.gz/vandal-3.1.2/vandal/cli/__init__.py
@@ -0,0 +1,9 @@
+# import all relevant contents from the associated module.
+from vandal.cli import (
+    App,
+)
+
+# all relevant contents.
+__all__ = [
+    App,
+]
