2fb7e1a166567a92934132371ab95a7aa7d87447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:31 +0000

    Add package kfsdutils/0.0.6/kfsdutils-0.0.6.tar.gz

---
+++ packages/kfsdutils/kfsdutils-0.0.6.tar.gz/kfsdutils-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='kfsdutils',
+      version='0.0.6',
+      description='Sample Pkg',
+      long_description='Sample Pkg',
+      long_description_content_type="text/markdown",
+      author='nathangokul',
+      author_email="nathangokul111@gmail.com",
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[]
+)
