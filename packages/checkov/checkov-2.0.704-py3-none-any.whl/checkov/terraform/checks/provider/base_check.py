d9d6bf3740444ad8f0cff0e78623e3a4e0c491a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:26 +0000

    Add package checkov/2.0.704/checkov-2.0.704-py3-none-any.whl

---
+++ packages/checkov/checkov-2.0.704-py3-none-any.whl/checkov/terraform/checks/provider/base_check.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod
+from typing import List, Dict, Any
+
+from checkov.common.checks.base_check import BaseCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.provider.registry import provider_registry
+
+
+class BaseProviderCheck(BaseCheck):
+    def __init__(self, name: str, id: str, categories: List[CheckCategories],
+                 supported_provider: List[str], guideline=None) -> None:
+        super().__init__(
+            name=name, id=id, categories=categories, supported_entities=supported_provider,
+            block_type="provider", guideline=guideline
+        )
+        self.supported_provider = supported_provider
+        provider_registry.register(self)
+
+    def scan_entity_conf(self, conf: Dict[str, List[Any]], entity_type: str) -> CheckResult:
+        return self.scan_provider_conf(conf)
+
+    @abstractmethod
+    def scan_provider_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
+        raise NotImplementedError()
