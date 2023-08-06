a5b4f1d116e0fb66fce83665433098ad0ddd3b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:47 +0000

    Add package simplelayouttest/0.0.1/simplelayouttest-0.0.1.tar.gz

---
+++ packages/simplelayouttest/simplelayouttest-0.0.1.tar.gz/simplelayouttest-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simplelayouttest",
+    version="0.0.1",
+    author="piglet94",
+    author_email="715654107@qq.com",
+    description="A small study package",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(include=['src', 'src.*']),
+    python_requires=">=3.0",
+    install_requires=['numpy','argparse','matplotlib','scipy'],
+    entry_points={'console_scripts': ['simplelayout=simplelayout.__main__:main']}
+)
