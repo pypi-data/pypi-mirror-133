b8447ea434bb749a81d9e4a8cbd3b3a1cc77373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:57 +0000

    Add package indico_toolkit/1.2/indico_toolkit-1.2.tar.gz

---
+++ packages/indico_toolkit/indico_toolkit-1.2.tar.gz/indico_toolkit-1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="indico-toolkit",
+    version="1.2",
+    packages=find_packages(exclude=["tests"]),
+    description="""Tools to assist with Indico IPA development""",
+    license="MIT License (See LICENSE)",
+    author="indico",
+    author_email="engineering@indico.io",
+    tests_require=["pytest>=5.2.1", "requests-mock>=1.7.0-7", "pytest-dependency==0.5.1", "coverage>=5.5"],
+    install_requires=[
+        "indico-client>=4.9.0",
+        "numpy>=1.16.0",
+        "Pillow>=6.2.0",
+        "requests>=2.22.0",
+        "setuptools>=41.4.0",
+        "pandas>=1.0.3",
+        "PyMuPDF==1.18.13",
+        "plotly==5.2.1",
+        "tqdm=4.50.0"
+    ],
+)
