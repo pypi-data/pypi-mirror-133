741972cb22208b2ab8c3342e8289041181748a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:00 +0000

    Add package ddtrace/0.51.3/ddtrace-0.51.3.tar.gz

---
+++ packages/ddtrace/ddtrace-0.51.3.tar.gz/ddtrace-0.51.3/tests/contrib/django/test_django_snapshots.py
@@ -0,0 +1,100 @@
+import django
+import pytest
+
+from tests.utils import snapshot
+
+
+@pytest.mark.skipif(django.VERSION < (2, 0), reason="")
+@snapshot(variants={"21x": (2, 0) <= django.VERSION < (2, 2), "": django.VERSION >= (2, 2)})
+def test_urlpatterns_include(client):
+    """
+    When a view is specified using `django.urls.include`
+        The view is traced
+    """
+    assert client.get("/include/test/").status_code == 200
+
+
+@snapshot(
+    variants={
+        "18x": django.VERSION < (1, 9),
+        "111x": (1, 9) <= django.VERSION < (1, 12),
+        "21x": (1, 12) < django.VERSION < (2, 2),
+        "": django.VERSION >= (2, 2),
+    }
+)
+def test_middleware_trace_callable_view(client):
+    # ensures that the internals are properly traced when using callable views
+    assert client.get("/feed-view/").status_code == 200
+
+
+@snapshot(
+    variants={
+        "18x": django.VERSION < (1, 9),
+        "111x": (1, 9) <= django.VERSION < (1, 12),
+        "21x": (1, 12) < django.VERSION < (2, 2),
+        "": django.VERSION >= (2, 2),
+    }
+)
+def test_middleware_trace_partial_based_view(client):
+    # ensures that the internals are properly traced when using a function views
+    assert client.get("/partial-view/").status_code == 200
+
+
+@pytest.mark.django_db
+@snapshot(
+    variants={
+        "18x": django.VERSION < (1, 9),
+        "111x": (1, 9) <= django.VERSION < (1, 12),
+        "21x": (1, 12) < django.VERSION < (2, 2),
+        "": django.VERSION >= (2, 2),
+    }
+)
+def test_safe_string_encoding(client):
+    assert client.get("/safe-template/").status_code == 200
+
+
+@snapshot(
+    variants={
+        "18x": django.VERSION < (1, 9),
+        "111x": (1, 9) <= django.VERSION < (1, 12),
+        "21x": (1, 12) < django.VERSION < (2, 2),
+        "": django.VERSION >= (2, 2),
+    }
+)
+def test_404_exceptions(client):
+    assert client.get("/404-view/").status_code == 404
+
+
+@pytest.fixture()
+def psycopg2_patched(transactional_db):
+    from django.db import connections
+
+    from ddtrace.contrib.psycopg.patch import patch
+    from ddtrace.contrib.psycopg.patch import unpatch
+
+    patch()
+
+    # # force recreate connection to ensure psycopg2 patching has occurred
+    del connections["postgres"]
+    connections["postgres"].close()
+    connections["postgres"].connect()
+
+    yield
+
+    unpatch()
+
+
+@snapshot(ignores=["meta.out.host"])
+@pytest.mark.django_db
+def test_psycopg_query_default(client, psycopg2_patched):
+    """Execute a psycopg2 query on a Django database wrapper"""
+    from django.db import connections
+    from psycopg2.sql import SQL
+
+    query = SQL("""select 'one' as x""")
+    conn = connections["postgres"]
+    with conn.cursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
+        assert len(rows) == 1, rows
+        assert rows[0][0] == "one"
