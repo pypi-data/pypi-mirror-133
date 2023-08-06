8cb354088242cf216179b88c6733f7f60d305a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package strawberry-graphql/0.93.15/strawberry-graphql-0.93.15.tar.gz

---
+++ packages/strawberry-graphql/strawberry-graphql-0.93.15.tar.gz/strawberry-graphql-0.93.15/strawberry/experimental/pydantic/exceptions.py
@@ -0,0 +1,37 @@
+from typing import Any, Type
+
+from pydantic import BaseModel
+from pydantic.typing import NoArgAnyCallable
+
+
+class MissingFieldsListError(Exception):
+    def __init__(self, type: Type[BaseModel]):
+        message = (
+            f"List of fields to copy from {type} is empty. Add fields with the "
+            f"`auto` type annotation"
+        )
+
+        super().__init__(message)
+
+
+class UnsupportedTypeError(Exception):
+    pass
+
+
+class UnregisteredTypeException(Exception):
+    def __init__(self, type: BaseModel):
+        message = (
+            f"Cannot find a Strawberry Type for {type} did you forget to register it?"
+        )
+
+        super().__init__(message)
+
+
+class BothDefaultAndDefaultFactoryDefinedError(Exception):
+    def __init__(self, default: Any, default_factory: NoArgAnyCallable):
+        message = (
+            f"Not allowed to specify both default and default_factory. "
+            f"default:{default} default_factory:{default_factory}"
+        )
+
+        super().__init__(message)
