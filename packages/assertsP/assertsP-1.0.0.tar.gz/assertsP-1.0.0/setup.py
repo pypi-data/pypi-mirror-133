4c9d242ce4401c809dbd0fcfe21337ffe8f4131c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:59 +0000

    Add package assertsP/1.0.0/assertsP-1.0.0.tar.gz

---
+++ packages/assertsP/assertsP-1.0.0.tar.gz/assertsP-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='assertsP',
+    version='1.0.0',
+    py_modules=['assertsP'],
+    author='qa.yw',
+    author_email='byteflow@163.com',
+    url='https://gitee.com/Testworm/assert-p.git',
+    description='数据库数据(字典)断言工具, compare tool for db data'
+)
