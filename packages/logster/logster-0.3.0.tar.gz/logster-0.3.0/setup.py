9b1d2e16afaf4a19ffc732708a4e53671f35a22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:44 +0000

    Add package logster/0.3.0/logster-0.3.0.tar.gz

---
+++ packages/logster/logster-0.3.0.tar.gz/logster-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="logster",
+    version="0.3.0",
+    description="Easy python console logging with colourful formatting",
+    url="https://github.com/aljabadi/logster",
+    download_url="https://github.com/aljabadi/logster/archive/refs/tags/v0.3.0.tar.gz",
+    author="Al J Abadi",
+    author_email="al.jal.abadi@gmail.com",
+    license="MIT",
+    packages=["logster"],
+    python_requires=">=3.5",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    zip_safe=False,
+)
