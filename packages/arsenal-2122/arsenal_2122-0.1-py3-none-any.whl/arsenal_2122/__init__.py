5066039eb8bdeaf03cc8c6036285e802bed696c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:23 +0000

    Add package arsenal-2122/0.1/arsenal_2122-0.1-py3-none-any.whl

---
+++ packages/arsenal-2122/arsenal_2122-0.1-py3-none-any.whl/arsenal_2122/__init__.py
@@ -0,0 +1,1 @@
+from arsenal.arsenal import *
---
+++ packages/arsenal-2122/arsenal_2122-0.1-py3-none-any.whl/arsenal_2122/arsenal.py
@@ -0,0 +1,20 @@
+import webbrowser
+#from importlib import resources
+
+def arsenal():
+    #im = Image.open('./image/tomiyasu.jpeg')
+    webbrowser.open("https://www.arsenal.com/", new=0, 
+    autoraise=True)
+
+def tomiyasu():
+    #im = Image.open('./image/tomiyasu.jpeg')
+    webbrowser.open("https://en.wikipedia.org/wiki/Takehiro_Tomiyasu", 
+    new=0, 
+    autoraise=True)
+
+
+def white():
+    #im = Image.open('./image/tomiyasu.jpeg')
+    webbrowser.open("https://en.wikipedia.org/wiki/Ben_White_(footballer)", 
+    new=0, 
+    autoraise=True)
