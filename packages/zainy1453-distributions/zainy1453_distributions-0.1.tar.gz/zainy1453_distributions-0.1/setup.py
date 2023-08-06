1873dd2eacc377bac956b1573bc946b1c62160bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:28 +0000

    Add package zainy1453-distributions/0.1/zainy1453_distributions-0.1.tar.gz

---
+++ packages/zainy1453-distributions/zainy1453_distributions-0.1.tar.gz/zainy1453_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zainy1453_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['zainy1453_distributions'],
+      author = 'Mohamad AlZainy',
+      author_email = 'zainy1453@gmail.com', 
+      zip_safe=False)
