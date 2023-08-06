ca9e7a0debceafe66e9e1ea533306a10a4488e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:56 +0000

    Add package vapl-interpreter/0.1.0/vapl_interpreter-0.1.0.tar.gz

---
+++ packages/vapl-interpreter/vapl_interpreter-0.1.0.tar.gz/vapl_interpreter-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vapl_interpreter',
+      version='0.1.0',
+      description='An interpreter for VAPL',
+      url='https://replit.com/@WWWtheW/VAPL-very-annoying-programming-language',
+      author='WWWtheW',
+      author_email='dontemailme@heheehheeeeeeeeeeeeeheehehehehe.com',
+      license='MIT',
+      packages=['vapl_interpreter'],
+      zip_safe=False)
