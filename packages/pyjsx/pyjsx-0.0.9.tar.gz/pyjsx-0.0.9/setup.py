572ada5a6ac46f823bd6ccbe69a5caaa9442f85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:06 +0000

    Add package pyjsx/0.0.9/pyjsx-0.0.9.tar.gz

---
+++ packages/pyjsx/pyjsx-0.0.9.tar.gz/pyjsx-0.0.9/setup.py
@@ -0,0 +1,8 @@
+import json
+from setuptools import setup
+
+with open('README.md', 'r') as R, open('SETUP.json', 'r') as S:
+    cfg = json.loads(S.read())
+    cfg["long_description"] = R.read()
+
+setup(**cfg)
