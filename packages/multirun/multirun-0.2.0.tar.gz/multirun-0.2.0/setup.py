8180ecf113dec48a94da0f860ec03aa758b11ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:32 +0000

    Add package multirun/0.2.0/multirun-0.2.0.tar.gz

---
+++ packages/multirun/multirun-0.2.0.tar.gz/multirun-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['multirun']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['toml>=0.10.2,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['multirun = multirun:main']}
+
+setup_kwargs = {
+    'name': 'multirun',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Rob Sayers',
+    'author_email': 'rsayers@robsayers.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
