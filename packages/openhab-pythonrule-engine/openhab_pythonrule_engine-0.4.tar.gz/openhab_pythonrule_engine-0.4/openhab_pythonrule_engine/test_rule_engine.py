f5215fa26b102bb2ee86c280501b2a176416c3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:57 +0000

    Add package openhab-pythonrule-engine/0.4/openhab_pythonrule_engine-0.4.tar.gz

---
+++ packages/openhab-pythonrule-engine/openhab_pythonrule_engine-0.4.tar.gz/openhab_pythonrule_engine-0.4/openhab_pythonrule_engine/test_rule_engine.py
@@ -0,0 +1,22 @@
+import logging
+from time import sleep
+from openhab_pythonrule_engine.rule_engine import RuleEngine
+
+
+
+logging.basicConfig(format='%(asctime)s %(name)-20s: %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+logging.getLogger('sseclient').disabled = True
+logging.getLogger('urllib3.connectionpool').disabled = True
+
+
+rule_engine = RuleEngine.new_singleton("http://192.168.1.27:8080/", "C:\\workspace\\\\openhab_rules", "grro", "Stabilo33!")
+rule_engine.start()
+
+'''
+for i in range(0, 10000):
+    sleep(5)
+    print("")
+    for rule in rule_engine.rules:
+        print(str(rule))
+'''
+sleep(1222222)
