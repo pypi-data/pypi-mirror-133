9e1a87c36473dcd8eed69069780a183d3e94584a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:10 +0000

    Add package bungieapi/0.0.2/bungieapi-0.0.2.tar.gz

---
+++ packages/bungieapi/bungieapi-0.0.2.tar.gz/bungieapi-0.0.2/bungieapi/client.py
@@ -0,0 +1,40 @@
+import dataclasses as dt
+import typing as t
+
+import aiohttp
+
+from bungieapi.generated.clients.root import Client as RootClient
+
+
+BASE_URL = "https://www.bungie.net"
+BASE_PATH = "/Platform"
+
+
+@dt.dataclass
+class Credentials:
+    api_key: str
+    token: t.Optional[str] = None
+
+
+class Client:
+    _session = t.Optional[aiohttp.ClientSession]
+
+    def __init__(self, credentials: Credentials, base_url: str = BASE_URL) -> None:
+        self._base_url = base_url
+        self._session = None
+        self._credentials = credentials
+
+    async def __aenter__(self) -> RootClient:
+        headers = {"X-Api-Key": self._credentials.api_key}
+        if self._credentials.token:
+            headers["Authorization"] = f"Bearer {self._credentials.token}"
+        self._session = aiohttp.ClientSession(base_url=self._base_url, headers=headers)
+        return RootClient(self._session, BASE_PATH)
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        assert self._session  # type: ignore
+        await self._session.close()  # type: ignore
+
+    @classmethod
+    def from_credentials(cls, api_key: str, token: t.Optional[str] = None) -> "Client":
+        return cls(Credentials(api_key=api_key, token=token))
