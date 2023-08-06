1a2e845e83b245f69187067072dc1567c623a185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:11 +0000

    Add package DiffPriv/2.0.0b2/DiffPriv-2.0.0b2.tar.gz

---
+++ packages/DiffPriv/DiffPriv-2.0.0b2.tar.gz/DiffPriv-2.0.0b2/DiffPriv/mech/__init__.py
@@ -0,0 +1,9 @@
+"""The mech module contains the DiffPriv Differential Privacy Mechanisms. See the `mech.main` module for more."""
+
+from .. import math
+from .. import random
+
+from .main import *
+from . import validate
+from . import pre
+from . import utils
