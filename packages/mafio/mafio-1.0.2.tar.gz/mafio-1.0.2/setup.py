87df8c741a0de9dd7b4ddbd884d417e654f72986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:22 +0000

    Add package mafio/1.0.2/mafio-1.0.2.tar.gz

---
+++ packages/mafio/mafio-1.0.2.tar.gz/mafio-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='mafio',
+      version='1.0.2',
+      description='A package for maf files io',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Zeyad Khaled',
+      url='https://github.com/zeyad-kay/mafio',
+      project_urls={
+          "Bug Tracker": "https://github.com/zeyad-kay/mafio/issues",
+      },
+      packages=['mafio'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      )
