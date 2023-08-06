40d76ad91e82e87fd1aeb198f615687eb86b4f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:28 +0000

    Add package WhatSender/0.1.5/WhatSender-0.1.5.tar.gz

---
+++ packages/WhatSender/WhatSender-0.1.5.tar.gz/WhatSender-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.5'
+DESCRIPTION = 'Sending Whatsapp messages.'
+LONG_DESCRIPTION = 'A package that allows to send Whatsapp messages, by given number, message, and time. SourceCode : https://github.com/idobarel/whatsender'
+
+# Setting up
+setup(
+    name="WhatSender",
+    version=VERSION,
+    author="GodZilo (Ido Barel)",
+    author_email="<vikbarel5@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pyautogui', 'termcolor', 'pyperclip'],
+    keywords=['python', 'Whatsapp', 'message', 'Whatsapp sender', 'message sender'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
