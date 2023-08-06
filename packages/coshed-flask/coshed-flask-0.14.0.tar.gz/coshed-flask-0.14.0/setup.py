d1659504f3aafb23dec782eedd30c5566b82601e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:02 +0000

    Add package coshed-flask/0.14.0/coshed-flask-0.14.0.tar.gz

---
+++ packages/coshed-flask/coshed-flask-0.14.0.tar.gz/coshed-flask-0.14.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['coshed_flask']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask-Compress>=1.10.1,<2.0.0',
+ 'Flask-Cors>=3.0.10,<4.0.0',
+ 'Flask>=2.0.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'coshed-flask',
+    'version': '0.14.0',
+    'description': 'flask helper for lazy developer(s)',
+    'long_description': None,
+    'author': 'doubleO8',
+    'author_email': 'wb008@hdm-stuttgart.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
