51d153a98c643b3de95848113d387a294633a3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:37 +0000

    Add package voice100/1.0/voice100-1.0.tar.gz

---
+++ packages/voice100/voice100-1.0.tar.gz/voice100-1.0/voice100/train_align.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2021 Katsuya Iida. All rights reserved.
+
+from argparse import ArgumentParser
+import pytorch_lightning as pl
+
+from .datasets import AudioTextDataModule
+from .models.align import AudioAlignCTC
+
+
+def cli_main():
+    pl.seed_everything(1234)
+
+    parser = ArgumentParser()
+    parser.add_argument('--task', type=str, help='Task', default='asr')
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = AudioTextDataModule.add_data_specific_args(parser)
+    parser = AudioAlignCTC.add_model_specific_args(parser)
+    parser.set_defaults(task='asr')
+    args = parser.parse_args()
+
+    data = AudioTextDataModule.from_argparse_args(args)
+    model = AudioAlignCTC.from_argparse_args(args)
+    trainer = pl.Trainer.from_argparse_args(args)
+    trainer.fit(model, data)
+
+
+if __name__ == '__main__':
+    cli_main()
---
+++ packages/voice100/voice100-1.0.tar.gz/voice100-1.0/voice100/train_asr.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2021 Katsuya Iida. All rights reserved.
+
+from argparse import ArgumentParser
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from .datasets import AudioTextDataModule
+from .models.asr import AudioToCharCTC
+
+
+def cli_main():
+    pl.seed_everything(1234)
+
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument('--task', type=str, help='Task', default='asr')
+    args, _ = parser.parse_known_args()
+
+    parser = ArgumentParser(parents=[parser])
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = AudioTextDataModule.add_data_specific_args(parser)
+    parser = AudioToCharCTC.add_model_specific_args(parser)
+    args = parser.parse_args(namespace=args)
+
+    data = AudioTextDataModule.from_argparse_args(args)
+    model = AudioToCharCTC.from_argparse_args(args)
+    checkpoint_callback = ModelCheckpoint(monitor='val_loss', save_last=True)
+    trainer = pl.Trainer.from_argparse_args(
+        args,
+        callbacks=[checkpoint_callback])
+    trainer.fit(model, data)
+
+
+if __name__ == '__main__':
+    cli_main()
