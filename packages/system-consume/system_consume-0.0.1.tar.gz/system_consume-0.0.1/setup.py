aaf31fa041ee98b2923024d3f51de5456d63a1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:19 +0000

    Add package system-consume/0.0.1/system_consume-0.0.1.tar.gz

---
+++ packages/system-consume/system_consume-0.0.1.tar.gz/system_consume-0.0.1/setup.py
@@ -0,0 +1,36 @@
+# import setuptools
+#
+# setuptools.setup(
+#     name="system_consume",
+#     version="0.0.1",
+#     author="cm_ll",
+#     author_email="2810140991@qq.com",
+#     description="check the consumption of running",
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/",
+#     packages=setuptools.find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+# )
+import setuptools
+
+setuptools.setup(
+    name="system_consume",
+    version="0.0.1",
+    author="cm_ll",
+    author_email="2810140991@qq.com",
+    description="check the consumption of running",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
