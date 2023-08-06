29b7c1db9941c2b8b47a9383795cf6a0ae7e6ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:58 +0000

    Add package spider-auto-parse/1.0.1/spider_auto_parse-1.0.1.tar.gz

---
+++ packages/spider-auto-parse/spider_auto_parse-1.0.1.tar.gz/spider_auto_parse-1.0.1/auto_parse/__init__.py
@@ -0,0 +1,2 @@
+from abstract import extraction
+from gae import show_list, show_datetime, show_title
---
+++ packages/spider-auto-parse/spider_auto_parse-1.0.1.tar.gz/spider_auto_parse-1.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='spider_auto_parse',
+    version='1.0.1',
+    author='xueqiuyu',
+    author_email='xueqy.boot@foxmail.com',
+    url='https://blog.csdn.net/m0_50889678/article/details/122344474',
+    description=u'自动解析详情、时间、列表',
+    packages=['auto_parse'],
+    install_requires=['cchardet', 'dateparser', 'Distance', 'environs', 'joblib', 'loguru', 'lxml', 'matplotlib',
+                      'numpy', 'python_Levenshtein', 'readability_lxml', 'requests', 'scikit_learn', 'setuptools',
+                      'w3lib'
+                      ],
+    entry_points={
+    }
+)
