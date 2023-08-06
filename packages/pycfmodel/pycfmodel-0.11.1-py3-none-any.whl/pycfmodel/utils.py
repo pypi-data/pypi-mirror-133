6fb7b6a0e9620a031705ceec64b64191fb1be035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:22 +0000

    Add package pycfmodel/0.11.1/pycfmodel-0.11.1-py3-none-any.whl

---
+++ packages/pycfmodel/pycfmodel-0.11.1-py3-none-any.whl/pycfmodel/utils.py
@@ -0,0 +1,24 @@
+import re
+from typing import Any, List, Pattern, Union
+
+from pycfmodel.constants import IMPLEMENTED_FUNCTIONS
+
+
+def is_resolvable_dict(value: Any) -> bool:
+    return isinstance(value, dict) and len(value) == 1 and next(iter(value)) in IMPLEMENTED_FUNCTIONS
+
+
+def convert_to_list(item: Union[Any, List[Any]]) -> List[Any]:
+    if isinstance(item, list):
+        return item
+    return [item]
+
+
+def regex_from_cf_string(action: str) -> Pattern:
+    # Replace *
+    action = action.replace("*", ".*")
+
+    # Replace ?
+    action = action.replace("?", ".{1}")
+
+    return re.compile(f"^{action}$", re.IGNORECASE)
