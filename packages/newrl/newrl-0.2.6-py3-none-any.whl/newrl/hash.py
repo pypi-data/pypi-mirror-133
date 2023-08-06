f20c1b0d6280208acfe768e1a633fc83db5a5f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:43 +0000

    Add package newrl/0.2.6/newrl-0.2.6-py3-none-any.whl

---
+++ packages/newrl/newrl-0.2.6-py3-none-any.whl/newrl/hash.py
@@ -0,0 +1,13 @@
+import hashlib
+
+def get_file_hash(file_path):
+    h = hashlib.sha256()
+
+    with open(file_path, 'rb') as file:
+        while True:
+            # Reading is buffered, so we can read smaller chunks.
+            chunk = file.read(h.block_size)
+            if not chunk:
+                break
+            h.update(chunk)
+    return h.hexdigest()
