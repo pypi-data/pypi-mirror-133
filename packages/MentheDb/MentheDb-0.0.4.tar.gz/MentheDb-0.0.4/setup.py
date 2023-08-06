f85f2abff6c1fd28117f0cbf7034ccfda136c93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:31 +0000

    Add package MentheDb/0.0.4/MentheDb-0.0.4.tar.gz

---
+++ packages/MentheDb/MentheDb-0.0.4.tar.gz/MentheDb-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MentheDb",
+    version="0.0.4",
+    author="Menthe Authors",
+    author_email="nikolay.gyuneliev@gmail.com",
+    description="A package with dynamodb functionalities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ProjectIbX/Menthedb",
+    project_urls={
+        "Bug Tracker": "https://github.com/ProjectIbX/Menthedb/src",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
