802904c6b7dab2cc060151f812fb29d2b0c8b97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:34 +0000

    Add package math-package-08/0.1/math_package_08-0.1.tar.gz

---
+++ packages/math-package-08/math_package_08-0.1.tar.gz/math_package_08-0.1/math_package_08/__init__.py
@@ -0,0 +1,10 @@
+from .trig import m1,m2
+from .other import m3,m4
+
+'''sine(3.14)
+cosine(3.14)
+asine(3.14)
+acosine(3.14)
+lcm(11,55)
+gcd(55,84)
+'''
---
+++ packages/math-package-08/math_package_08-0.1.tar.gz/math_package_08-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(name='math_package_08',
+      version='0.01',
+      description='Mathematical functions',
+      packages=['math_package_08'],
+      author='Naheen Bijapure',
+      author_email='naheenbijapure094@gmail.com',
+      zip_safe=False)
