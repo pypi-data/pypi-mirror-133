e1d0adc63f013ecb9787d66e4dd5a78477c20de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:05 +0000

    Add package xrd-simulator/0.0.3/xrd_simulator-0.0.3-py3-none-any.whl

---
+++ packages/xrd-simulator/xrd_simulator-0.0.3-py3-none-any.whl/tests/run_all.py
@@ -0,0 +1,26 @@
+"""Run all unittests in the tests folder"""
+from unittest import TestLoader, TestResult
+from pathlib import Path
+
+def run_tests():
+    test_loader = TestLoader()
+    test_result = TestResult()
+
+    test_directory = str(Path(__file__).resolve().parent)
+
+    test_suite = test_loader.discover(test_directory, pattern='test_*.py')
+
+    test_suite.run(result=test_result)
+
+    print("\n")
+    if test_result.wasSuccessful():
+        print(" All unittests ran successfully !")
+        exit(0)
+    else:
+        print(" Unittests Failed !")
+        print(test_result.failures)
+        print(test_result.errors)
+        exit(-1)
+
+if __name__ == '__main__':
+    run_tests() # Runs all unittests
