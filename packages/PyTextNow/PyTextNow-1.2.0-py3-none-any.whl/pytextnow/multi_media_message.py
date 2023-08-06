483cfd751479d639e63ff68c1b5f7deb3392d8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:20 +0000

    Add package PyTextNow/1.2.0/PyTextNow-1.2.0-py3-none-any.whl

---
+++ packages/PyTextNow/PyTextNow-1.2.0-py3-none-any.whl/pytextnow/multi_media_message.py
@@ -0,0 +1,22 @@
+from pytextnow.message import Message
+import cloudscraper
+
+MULTIMEDIA_MESSAGE_TYPE = 1
+
+scraper = cloudscraper.create_scraper()
+
+class MultiMediaMessage(Message):
+    def __init__(self, msg_obj, outer_self):
+        super().__init__(msg_obj, outer_self)
+        file_req = scraper.get(self.content)
+        self.raw_data = file_req.content
+        self.content_type = file_req.headers["Content-Type"]
+        self.extension = self.content_type.split("/")[1]
+        self.type = MULTIMEDIA_MESSAGE_TYPE
+        self.self = outer_self
+
+    def mv(self, file_path=None):
+        if not file_path:
+            file_path = f"./file.{self.extension}"
+        with open(file_path, mode="wb") as f:
+            f.write(self.raw_data)
