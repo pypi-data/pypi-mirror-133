b32b394f4cfa3e56650fa013010efa9b27f3f5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:05 +0000

    Add package glide-cli/0.1.1/glide_cli-0.1.1-py3-none-any.whl

---
+++ packages/glide-cli/glide_cli-0.1.1-py3-none-any.whl/src/deployment.py
@@ -0,0 +1,16 @@
+from src.api import Api
+
+class Deployment:
+    def __init__(self, deploymentName, directoryToDeploy):
+        self.deploymentName = deploymentName
+        self.directoryToDeploy = directoryToDeploy
+        self.api = Api(self.deploymentName, directoryToDeploy)
+    
+    def deployToNetlify(self):
+        self.api.deployToNetlify(self.directoryToDeploy)
+    
+    def deployToVercel(self):
+        self.api.deployToVercel(self.directoryToDeploy)
+    
+    def deployToAWS(self):
+        self.api.deployToAWS(self.directoryToDeploy)
