ff4e5490532864f4f6b14b79870840c8d72dc36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:55 +0000

    Add package siebel-crash-report/0.1.0/siebel-crash-report-0.1.0.tar.gz

---
+++ packages/siebel-crash-report/siebel-crash-report-0.1.0.tar.gz/siebel-crash-report-0.1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+requirements = ['PyYAML>=5.4', 'simplejson>=3.17.2']
+
+setup(
+    name="siebel-crash-report",
+    version='0.1.0',
+    author="Alceu Rodrigues de Freitas Junior",
+    author_email="arfreitas@cpan.org",
+    url="https://github.com/glasswalk3r/siebel-crash-report",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'simplejson>=3.17.2',
+        'pyyaml>=5.4'
+    ],
+    description='Searchs and aggregates Siebel component crashes information \
+into a nice report.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    scripts=['bin/crash_reporter'],
+    keywords="Siebel crash core dump component",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
