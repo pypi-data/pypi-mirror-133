7f462307e84ba3316731543a1706c15d76f44ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:22 +0000

    Add package rubika/5.1/rubika-5.1.tar.gz

---
+++ packages/rubika/rubika-5.1.tar.gz/rubika-5.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'rubika',
+  packages = ['rubika'],
+  version = '5.1',
+  license='MIT', 
+  description = 'rubika library is a unofficial library for making bot in the rubika. this library works with rubika’s API',
+  author = 'Bahman Ahmadi',
+  author_email = 'bahmanahmadi.mail@gmail.com',
+  url = 'https://github.com/Bahman-Ahmadi/rubika',
+  download_url = 'https://github.com/Bahman-Ahmadi/rubika/archive/refs/tags/v_51.tar.gz',
+  keywords = ["rubika","bot","robot","library"],
+  install_requires=[
+          'requests',
+          'pycryptodome',
+          'urllib3',
+          'tqdm'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
