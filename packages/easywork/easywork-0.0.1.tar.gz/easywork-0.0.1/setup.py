16fb42adf6bcc99a59d4fb160689d814efac8327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:44 +0000

    Add package easywork/0.0.1/easywork-0.0.1.tar.gz

---
+++ packages/easywork/easywork-0.0.1.tar.gz/easywork-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easywork',
+    version='0.0.1',
+    author='paomianplus',
+    author_email='paomianplus@foxmail.com',
+    packages=find_packages()
+)
