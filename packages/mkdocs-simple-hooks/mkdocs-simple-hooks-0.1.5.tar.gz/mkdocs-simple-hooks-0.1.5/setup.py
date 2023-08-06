23947b4a2193ba137f50f5e153fef8852012386d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:06 +0000

    Add package mkdocs-simple-hooks/0.1.5/mkdocs-simple-hooks-0.1.5.tar.gz

---
+++ packages/mkdocs-simple-hooks/mkdocs-simple-hooks-0.1.5.tar.gz/mkdocs-simple-hooks-0.1.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mkdocs-simple-hooks",
+    version="0.1.5",
+    author="Andrzej Klajnert",
+    author_email="python@aklajnert.pl",
+    description="Define your own hooks for mkdocs, without having to create a new package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aklajnert/mkdocs-simple-hooks",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["mkdocs>=1.2"],
+    extras_require={"test": ["pytest>=4.0", "pytest-cov"]},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "mkdocs.plugins": [
+            "mkdocs-simple-hooks = mkdocs_simple_hooks:SimpleHooksPlugin"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Documentation",
+    ],
+)
