515399a6b8e8cbee6bf291ea0b9a659c1c6dece4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:45 +0000

    Add package amadeuslib/0.3/amadeuslib-0.3.tar.gz

---
+++ packages/amadeuslib/amadeuslib-0.3.tar.gz/amadeuslib-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='amadeuslib',
+    version='0.3',
+    license='MIT',
+    author="Justin",
+    author_email='hello@justin681.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/justinl681',
+    keywords='amadeus flight travel api',
+    install_requires=[
+          'requests'
+      ],
+)
