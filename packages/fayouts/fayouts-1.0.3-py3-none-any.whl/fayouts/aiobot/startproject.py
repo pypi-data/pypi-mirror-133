2dd68184689bcd3f17897f00f7fab2262e58f6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:49 +0000

    Add package fayouts/1.0.3/fayouts-1.0.3-py3-none-any.whl

---
+++ packages/fayouts/fayouts-1.0.3-py3-none-any.whl/fayouts/aiobot/startproject.py
@@ -0,0 +1,36 @@
+bot = open('bot.py', 'w')
+bot.write(
+    'import logging\n'
+    'from aiogram import Bot, Dispatcher, executor, types\n'
+    'import configs\n\n'
+    
+    '# Configure logging\n'
+    'logging.basicConfig(level=logging.INFO)\n\n'
+
+    '# Initialize bot and dispatcher\n'
+    'bot = Bot(token=configs.BOT_TOKEN)\n'
+    'dp = Dispatcher(bot)\n\n'
+
+    '@dp.message_handler()\n'
+    'async def echo(message: types.Message):\n'
+    '   await message.answer(message.text)\n'
+    '   if \'some\' in message.text:\n'
+    '       await message.answer(message.text)\n\n'
+
+    'if __name__ == \'__main__\':\n'
+    '   executor.start_polling(dp, skip_updates=True)'
+) 
+bot.close()
+
+filters = open('filters.py', 'w')
+filters.write(
+    '# Custom filters\n'
+)
+filters.close()
+
+configs = open('configs.py', 'w')
+configs.write(    
+    'BOT_TOKEN = 00000000\n'
+    'BOT_OWNER = 00000000'
+)
+configs.close()
