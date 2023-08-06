93a9415216579a312a4b4b1bc62432b40b5626e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:23 +0000

    Add package StoaCalculator/1.0.4/StoaCalculator-1.0.4.tar.gz

---
+++ packages/StoaCalculator/StoaCalculator-1.0.4.tar.gz/StoaCalculator-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='StoaCalculator',
+    version='1.0.4',
+    description='Stoa Calculator is a powerful calculator that have everything you need to do any kind of calculations!',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    url='https://jorgeeldis.me',
+    author='Jorge Eldis Gonzalez',
+    author_email='jorgeeldisg30@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['']
+)
