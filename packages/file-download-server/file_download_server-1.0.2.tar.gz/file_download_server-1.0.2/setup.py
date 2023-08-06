40e3eacc519c27953a775018d74d144414dd3a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:09 +0000

    Add package file-download-server/1.0.2/file_download_server-1.0.2.tar.gz

---
+++ packages/file-download-server/file_download_server-1.0.2.tar.gz/file_download_server-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="file_download_server",
+    version="1.0.2",
+    py_modules=["file_server"],
+    author="Ricardo",
+    author_email="GeekRicardozzZ@gmail.com",
+    url="https://github.com/GeekRicardo/file-download-server",
+    description="A simple file server",
+)
