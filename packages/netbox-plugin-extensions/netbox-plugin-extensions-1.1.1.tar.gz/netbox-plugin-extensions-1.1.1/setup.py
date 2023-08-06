e6a8298b99c4ed5cc017ad6f9d1e20823a6fcef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package netbox-plugin-extensions/1.1.1/netbox-plugin-extensions-1.1.1.tar.gz

---
+++ packages/netbox-plugin-extensions/netbox-plugin-extensions-1.1.1.tar.gz/netbox-plugin-extensions-1.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-plugin-extensions',
+    version='1.1.1',
+    description='NetBox Plugin Extensions',
+    long_description='Wrappers for Netbox Generic Objects',
+    url='https://github.com/dansheps/netbox-plugin-extensions/',
+    download_url='https://pypi.org/project/netbox-plugin-extensions/',
+    author='Daniel Sheppard',
+    author_email='dans@dansheps.com',
+    maintainer='Daniel Sheppard',
+    maintainer_email='dans@dansheps.com',
+    license='All rights reserved',
+    platform=[],
+    keywords=['netbox', 'netbox-plugin'],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'importlib',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
