135045c7ca05c4238cf4a9d948f32c2e84ddd2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:53 +0000

    Add package ngsfragments/1.0.8/ngsfragments-1.0.8.tar.gz

---
+++ packages/ngsfragments/ngsfragments-1.0.8.tar.gz/ngsfragments-1.0.8/ngsfragments/__init__.py
@@ -0,0 +1,10 @@
+"""Init for mylibrary."""
+from __future__ import absolute_import
+from .fragments import fragments
+from . import utilities
+from .segment import correction, cnv
+from .plot import plot_plt
+from . import data
+
+# This is extracted automatically by the top-level setup.py.
+__version__ = '1.0.8'
