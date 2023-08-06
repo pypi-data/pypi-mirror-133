4a08581eb97d3fecb96a8dea37a562e8e65875a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:41 +0000

    Add package xarray-dataclasses/0.9.0/xarray_dataclasses-0.9.0-py3-none-any.whl

---
+++ packages/xarray-dataclasses/xarray_dataclasses-0.9.0-py3-none-any.whl/xarray_dataclasses/dataset.py
@@ -0,0 +1,384 @@
+__all__ = ["AsDataset", "asdataset"]
+
+
+# standard library
+from dataclasses import Field
+from functools import partial, wraps
+from types import MethodType
+from typing import Any, Callable, Dict, Optional, Type, TypeVar, overload
+
+
+# dependencies
+import numpy as np
+import xarray as xr
+from morecopy import copy
+from typing_extensions import ParamSpec, Protocol
+
+
+# submodules
+from .datamodel import DataModel
+from .dataoptions import DataOptions
+from .typing import DataType, Order, Shape, Sizes
+
+
+# constants
+DEFAULT_OPTIONS = DataOptions(xr.Dataset)
+
+
+# type hints
+P = ParamSpec("P")
+TDataset = TypeVar("TDataset", bound=xr.Dataset)
+
+
+class DataClass(Protocol[P]):
+    """Type hint for a dataclass object."""
+
+    def __init__(self, *args: P.args, **kwargs: P.kwargs) -> None:
+        ...
+
+    __dataclass_fields__: Dict[str, Field[Any]]
+
+
+class DatasetClass(Protocol[P, TDataset]):
+    """Type hint for a dataclass object with a Dataset factory."""
+
+    def __init__(self, *args: P.args, **kwargs: P.kwargs) -> None:
+        ...
+
+    __dataclass_fields__: Dict[str, Field[Any]]
+    __dataoptions__: DataOptions[TDataset]
+
+
+# runtime functions and classes
+@overload
+def asdataset(
+    dataclass: DatasetClass[Any, TDataset],
+    reference: Optional[DataType] = None,
+    dataoptions: DataOptions[Any] = DEFAULT_OPTIONS,
+) -> TDataset:
+    ...
+
+
+@overload
+def asdataset(
+    dataclass: DataClass[Any],
+    reference: Optional[DataType] = None,
+    dataoptions: DataOptions[TDataset] = DEFAULT_OPTIONS,
+) -> TDataset:
+    ...
+
+
+def asdataset(
+    dataclass: Any,
+    reference: Optional[DataType] = None,
+    dataoptions: DataOptions[Any] = DEFAULT_OPTIONS,
+) -> Any:
+    """Create a Dataset object from a dataclass object.
+
+    Args:
+        dataclass: Dataclass object that defines typed Dataset.
+        reference: DataArray or Dataset object as a reference of shape.
+        dataoptions: Options for Dataset creation.
+
+    Returns:
+        Dataset object created from the dataclass object.
+
+    """
+    try:
+        # for backward compatibility (deprecated in v1.0.0)
+        dataoptions = DataOptions(dataclass.__dataset_factory__)
+    except AttributeError:
+        pass
+
+    try:
+        dataoptions = dataclass.__dataoptions__
+    except AttributeError:
+        pass
+
+    model = DataModel.from_dataclass(dataclass)
+    dataset = dataoptions.factory()
+
+    for item in model.data.values():
+        dataset[item.name] = item(reference)
+
+    for item in model.coord.values():
+        if item.name in dataset.dims:
+            dataset.coords[item.name] = item(dataset)
+
+    for item in model.coord.values():
+        if item.name not in dataset.dims:
+            dataset.coords[item.name] = item(dataset)
+
+    for item in model.attr.values():
+        dataset.attrs[item.name] = item()
+
+    return dataset
+
+
+# runtime classes
+class classproperty:
+    """Class property only for AsDataset.new().
+
+    As a classmethod and a property can be chained together since Python 3.9,
+    this class will be removed when the support for Python 3.7 and 3.8 ends.
+
+    """
+
+    def __init__(self, func: Callable[..., Any]) -> None:
+        self.__func__ = func
+
+    @overload
+    def __get__(
+        self,
+        obj: Any,
+        cls: Type[DatasetClass[P, TDataset]],
+    ) -> Callable[P, TDataset]:
+        ...
+
+    @overload
+    def __get__(
+        self,
+        obj: Any,
+        cls: Type[DataClass[P]],
+    ) -> Callable[P, xr.Dataset]:
+        ...
+
+    def __get__(self, obj: Any, cls: Any) -> Any:
+        return self.__func__(cls)
+
+
+class AsDataset:
+    """Mix-in class that provides shorthand methods."""
+
+    @classproperty
+    def new(cls: Any) -> Any:
+        """Create a Dataset object from dataclass parameters."""
+
+        init = copy(cls.__init__)
+        init.__doc__ = cls.__init__.__doc__
+        init.__annotations__["return"] = TDataset
+
+        @wraps(init)
+        def new(cls: Any, *args: Any, **kwargs: Any) -> Any:
+            return asdataset(cls(*args, **kwargs))
+
+        return MethodType(new, cls)
+
+    @overload
+    @classmethod
+    def shaped(
+        cls: Type[DatasetClass[P, TDataset]],
+        func: Callable[[Shape], np.ndarray],
+        sizes: Sizes,
+        **kwargs: Any,
+    ) -> TDataset:
+        ...
+
+    @overload
+    @classmethod
+    def shaped(
+        cls: Type[DataClass[P]],
+        func: Callable[[Shape], np.ndarray],
+        sizes: Sizes,
+        **kwargs: Any,
+    ) -> xr.Dataset:
+        ...
+
+    @classmethod
+    def shaped(
+        cls: Any,
+        func: Callable[[Shape], np.ndarray],
+        sizes: Sizes,
+        **kwargs: Any,
+    ) -> Any:
+        """Create a Dataset object from a shaped function.
+
+        Args:
+            func: Function to create an array with given shape.
+            sizes: Sizes of the new Dataset object.
+            kwargs: Args of the Dataset class except for data vars.
+
+        Returns:
+            Dataset object created from the shaped function.
+
+        """
+        model = DataModel.from_dataclass(cls)
+        data_vars: Dict[str, Any] = {}
+
+        for name, item in model.data.items():
+            shape = tuple(sizes[dim] for dim in item.type["dims"])
+            data_vars[name] = func(shape)
+
+        return asdataset(cls(**data_vars, **kwargs))
+
+    @overload
+    @classmethod
+    def empty(
+        cls: Type[DatasetClass[P, TDataset]],
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> TDataset:
+        ...
+
+    @overload
+    @classmethod
+    def empty(
+        cls: Type[DataClass[P]],
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> xr.Dataset:
+        ...
+
+    @classmethod
+    def empty(
+        cls: Any,
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> Any:
+        """Create a Dataset object without initializing data vars.
+
+        Args:
+            sizes: Sizes of the new Dataset object.
+            order: Whether to store data in row-major (C-style)
+                or column-major (Fortran-style) order in memory.
+            kwargs: Args of the Dataset class except for data vars.
+
+        Returns:
+            Dataset object without initializing data vars.
+
+        """
+        func = partial(np.empty, order=order)
+        return cls.shaped(func, sizes, **kwargs)
+
+    @overload
+    @classmethod
+    def zeros(
+        cls: Type[DatasetClass[P, TDataset]],
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> TDataset:
+        ...
+
+    @overload
+    @classmethod
+    def zeros(
+        cls: Type[DataClass[P]],
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> xr.Dataset:
+        ...
+
+    @classmethod
+    def zeros(
+        cls: Any,
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> Any:
+        """Create a Dataset object whose data vars are filled with zeros.
+
+        Args:
+            sizes: Sizes of the new Dataset object.
+            order: Whether to store data in row-major (C-style)
+                or column-major (Fortran-style) order in memory.
+            kwargs: Args of the Dataset class except for data vars.
+
+        Returns:
+            Dataset object whose data vars are filled with zeros.
+
+        """
+        func = partial(np.zeros, order=order)
+        return cls.shaped(func, sizes, **kwargs)
+
+    @overload
+    @classmethod
+    def ones(
+        cls: Type[DatasetClass[P, TDataset]],
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> TDataset:
+        ...
+
+    @overload
+    @classmethod
+    def ones(
+        cls: Type[DataClass[P]],
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> xr.Dataset:
+        ...
+
+    @classmethod
+    def ones(
+        cls: Any,
+        sizes: Sizes,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> Any:
+        """Create a Dataset object whose data vars are filled with ones.
+
+        Args:
+            sizes: Sizes of the new Dataset object.
+            order: Whether to store data in row-major (C-style)
+                or column-major (Fortran-style) order in memory.
+            kwargs: Args of the Dataset class except for data vars.
+
+        Returns:
+            Dataset object whose data vars are filled with ones.
+
+        """
+        func = partial(np.ones, order=order)
+        return cls.shaped(func, sizes, **kwargs)
+
+    @overload
+    @classmethod
+    def full(
+        cls: Type[DatasetClass[P, TDataset]],
+        sizes: Sizes,
+        fill_value: Any,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> TDataset:
+        ...
+
+    @overload
+    @classmethod
+    def full(
+        cls: Type[DataClass[P]],
+        sizes: Sizes,
+        fill_value: Any,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> xr.Dataset:
+        ...
+
+    @classmethod
+    def full(
+        cls: Any,
+        sizes: Sizes,
+        fill_value: Any,
+        order: Order = "C",
+        **kwargs: Any,
+    ) -> Any:
+        """Create a Dataset object whose data vars are filled with given value.
+
+        Args:
+            sizes: Sizes of the new Dataset object.
+            fill_value: Value for data vars of the new Dataset object.
+            order: Whether to store data in row-major (C-style)
+                or column-major (Fortran-style) order in memory.
+            kwargs: Args of the Dataset class except for data vars.
+
+        Returns:
+            Dataset object whose data vars are filled with given value.
+
+        """
+        func = partial(np.full, fill_value=fill_value, order=order)
+        return cls.shaped(func, sizes, **kwargs)
---
+++ packages/xarray-dataclasses/xarray_dataclasses-0.9.0-py3-none-any.whl/xarray_dataclasses/typing.py
@@ -0,0 +1,321 @@
+"""Submodule for type hints to define fields of dataclasses.
+
+Note:
+    The following code is supposed in the examples below::
+
+        from dataclasses import dataclass
+        from typing import Literal
+        from xarray_dataclasses import AsDataArray, AsDataset
+        from xarray_dataclasses import Attr, Coord, Data, Name
+        from xarray_dataclasses import Coordof, Dataof
+
+
+        X = Literal["x"]
+        Y = Literal["y"]
+
+"""
+__all__ = ["Attr", "Coord", "Coordof", "Data", "Dataof", "Name"]
+
+
+# standard library
+from dataclasses import Field
+from enum import auto, Enum
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Hashable,
+    Optional,
+    Sequence,
+    Tuple,
+    TypeVar,
+    Union,
+)
+
+
+# dependencies
+import xarray as xr
+from typing_extensions import (
+    Annotated,
+    Literal,
+    Protocol,
+    get_args,
+    get_origin,
+    get_type_hints,
+    runtime_checkable,
+)
+
+
+# constants
+class FieldType(Enum):
+    """Annotation of xarray-related field hints."""
+
+    ATTR = auto()
+    """Annotation of attribute field hints."""
+
+    COORD = auto()
+    """Annotation of coordinate field hints."""
+
+    COORDOF = auto()
+    """Annotation of coordinate field hints."""
+
+    DATA = auto()
+    """Annotation of data (variable) field hints."""
+
+    DATAOF = auto()
+    """Annotation of data (variable) field hints."""
+
+    NAME = auto()
+    """Annotation of name field hints."""
+
+    def annotates(self, hint: Any) -> bool:
+        """Check if a field hint is annotated."""
+        return self in get_args(hint)[1:]
+
+
+# type hints
+DataType = Union[xr.DataArray, xr.Dataset]
+Dims = Tuple[str, ...]
+Dtype = Optional[str]
+Order = Literal["C", "F"]
+Shape = Union[Sequence[int], int]
+Sizes = Dict[str, int]
+T = TypeVar("T")
+TDims = TypeVar("TDims", covariant=True)
+TDtype = TypeVar("TDtype", covariant=True)
+TName = TypeVar("TName", bound=Hashable)
+
+
+@runtime_checkable
+class ArrayLike(Protocol[TDims, TDtype]):
+    """Type hint of array-like objects."""
+
+    def astype(self: T, dtype: Any) -> T:
+        """Method to convert data type of the object."""
+        ...
+
+    @property
+    def ndim(self) -> int:
+        """Number of dimensions of the object."""
+        ...
+
+    @property
+    def shape(self) -> Tuple[int, ...]:
+        """Shape of the object."""
+        ...
+
+
+class DataClass(Protocol):
+    """Type hint of dataclasses or their objects."""
+
+    __init__: Callable[..., None]
+    __dataclass_fields__: Dict[str, Field[Any]]
+
+
+TDataClass = TypeVar("TDataClass", bound=DataClass)
+
+
+Attr = Annotated[T, FieldType.ATTR]
+"""Type hint to define attribute fields (``Attr[T]``).
+
+Example:
+    ::
+
+        @dataclass
+        class Image(AsDataArray):
+            data: Data[tuple[X, Y], float]
+            long_name: Attr[str] = "luminance"
+            units: Attr[str] = "cd / m^2"
+
+Hint:
+    The following field names are specially treated when plotting.
+
+    - ``long_name`` or ``standard_name``: Coordinate name.
+    - ``units``: Coordinate units.
+
+Reference:
+    https://xarray.pydata.org/en/stable/user-guide/plotting.html
+
+"""
+
+Coord = Annotated[Union[ArrayLike[TDims, TDtype], TDtype], FieldType.COORD]
+"""Type hint to define coordinate fields (``Coord[TDims, TDtype]``).
+
+Example:
+    ::
+
+        @dataclass
+        class Image(AsDataArray):
+            data: Data[tuple[X, Y], float]
+            mask: Coord[tuple[X, Y], bool]
+            x: Coord[X, int] = 0
+            y: Coord[Y, int] = 0
+
+Hint:
+    A coordinate field whose name is the same as ``TDims``
+    (e.g. ``x: Coord[X, int]``) can define a dimension.
+
+"""
+
+Coordof = Annotated[Union[TDataClass, Any], FieldType.COORDOF]
+"""Type hint to define coordinate fields (``Coordof[TDataClass]``).
+
+Unlike ``Coord``, it specifies a dataclass that defines a DataArray class.
+This is useful when users want to add metadata to dimensions for plotting.
+
+Example:
+    ::
+
+        @dataclass
+        class XAxis:
+            data: Data[X, int]
+            long_name: Attr[str] = "x axis"
+
+
+        @dataclass
+        class YAxis:
+            data: Data[Y, int]
+            long_name: Attr[str] = "y axis"
+
+
+        @dataclass
+        class Image(AsDataArray):
+            data: Data[tuple[X, Y], float]
+            x: Coordof[XAxis] = 0
+            y: Coordof[YAxis] = 0
+
+Hint:
+    A class used in ``Coordof`` does not need to inherit ``AsDataArray``.
+
+"""
+
+Data = Annotated[Union[ArrayLike[TDims, TDtype], TDtype], FieldType.DATA]
+"""Type hint to define data fields (``Coordof[TDims, TDtype]``).
+
+Examples:
+    Exactly one data field is allowed in a DataArray class
+    (the second and subsequent data fields are just ignored)::
+
+        @dataclass
+        class Image(AsDataArray):
+            data: Data[tuple[X, Y], float]
+
+    Multiple data fields are allowed in a Dataset class::
+
+        @dataclass
+        class ColorImage(AsDataset):
+            red: Data[tuple[X, Y], float]
+            green: Data[tuple[X, Y], float]
+            blue: Data[tuple[X, Y], float]
+
+"""
+
+Dataof = Annotated[Union[TDataClass, Any], FieldType.DATAOF]
+"""Type hint to define data fields (``Coordof[TDataClass]``).
+
+Unlike ``Data``, it specifies a dataclass that defines a DataArray class.
+This is useful when users want to reuse a dataclass in a Dataset class.
+
+Example:
+    ::
+
+        @dataclass
+        class Image:
+            data: Data[tuple[X, Y], float]
+            x: Coord[X, int] = 0
+            y: Coord[Y, int] = 0
+
+
+        @dataclass
+        class ColorImage(AsDataset):
+            red: Dataof[Image]
+            green: Dataof[Image]
+            blue: Dataof[Image]
+
+Hint:
+    A class used in ``Dataof`` does not need to inherit ``AsDataArray``.
+
+"""
+
+Name = Annotated[TName, FieldType.NAME]
+"""Type hint to define name fields (``Name[TName]``).
+
+Example:
+    ::
+
+        @dataclass
+        class Image(AsDataArray):
+            data: Data[tuple[X, Y], float]
+            name: Name[str] = "image"
+
+"""
+
+
+# runtime functions
+def get_dims(hint: Any) -> Dims:
+    """Return dims parsed from a type hint."""
+    t_dims = get_inner(hint, 0, 0)
+
+    if is_str_literal(t_dims):
+        return (get_inner(t_dims, 0),)
+
+    args: Any = get_args(t_dims)
+
+    if args == () or args == ((),):
+        return ()
+
+    if all(map(is_str_literal, args)):
+        return tuple(map(get_inner, args, [0] * len(args)))
+
+    raise ValueError(f"Could not parse dims from {hint!r}.")
+
+
+def get_dtype(hint: Any) -> Dtype:
+    """Return dtype parsed from a type hint."""
+    t_dtype = get_inner(hint, 0, 1)
+
+    if t_dtype is Any:
+        return None
+
+    if t_dtype is type(None):
+        return None
+
+    if isinstance(t_dtype, type):
+        return t_dtype.__name__
+
+    if is_str_literal(t_dtype):
+        return get_inner(t_dtype, 0)
+
+    raise ValueError(f"Could not parse dtype from {hint!r}.")
+
+
+def get_inner(hint: Any, *indexes: int) -> Any:
+    """Return an inner type hint by indexes."""
+    if not indexes:
+        return hint
+
+    index, indexes = indexes[0], indexes[1:]
+    return get_inner(get_args(hint)[index], *indexes)
+
+
+def is_str_literal(hint: Any) -> bool:
+    """Check if a type hint is Literal[str]."""
+    args: Any = get_args(hint)
+    origin = get_origin(hint)
+
+    if origin is not Literal:
+        return False
+
+    if not len(args) == 1:
+        return False
+
+    return isinstance(args[0], str)
+
+
+def unannotate(hint: Any) -> Any:
+    """Recursively remove Annotated type hints."""
+
+    class Temp:
+        __annotations__ = dict(hint=hint)
+
+    return get_type_hints(Temp)["hint"]
