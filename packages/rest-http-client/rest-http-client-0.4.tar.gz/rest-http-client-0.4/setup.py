71ec28141bb699a34055a6332cb7d417e1c6629b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:43 +0000

    Add package rest-http-client/0.4/rest-http-client-0.4.tar.gz

---
+++ packages/rest-http-client/rest-http-client-0.4.tar.gz/rest-http-client-0.4/setup.py
@@ -0,0 +1,19 @@
+import os.path
+import sys
+
+import setuptools
+
+
+def get_requirements(filename):
+    try:
+        with open(filename, 'r') as fd:
+            packages = fd.read().split('\n')
+            packages = list(filter(lambda x: x or not x.startswith('#') or not x.startswith(' '), packages))
+            return packages
+
+    except Exception as ex:
+        sys.exit('Could not find requirements.txt \nException: {}'.format(ex))
+
+
+requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+setuptools.setup(install_requires=get_requirements(requirements_path))
