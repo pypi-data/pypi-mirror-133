322b87f698789fb57297cb2d1668e22eae5570c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:14 +0000

    Add package deep-fine/0.1.0/deep_fine-0.1.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/deep-fine/deep_fine-0.1.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/fine/activations/Linear.py
@@ -0,0 +1,12 @@
+class Linear:
+
+    def forward(self, inputs, training):
+        self.inputs = inputs
+        self.output = inputs
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+
+    @staticmethod
+    def predictions(output):
+        return output
