e12dc654c083d3ee6ab0b3f059572ba608d10768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:14 +0000

    Add package samino/1.6.9/samino-1.6.9.tar.gz

---
+++ packages/samino/samino-1.6.9.tar.gz/samino-1.6.9/samino/lib/headers.py
@@ -0,0 +1,48 @@
+import json
+from .util import c, s, uu, ua
+
+uid = None
+sid = None
+deviceId = None
+lang = None
+
+
+class Headers:
+    def __init__(self, data=None):
+        if deviceId: self.deviceId = deviceId
+        else: self.deviceId = c()
+
+        self.headers = {
+            "NDCDEVICEID": self.deviceId,
+            "User-Agent": ua(),
+            "AUID": uu(),
+            "SMDEVICEID": uu(),
+            "Content-Type": "application/json; charset=utf-8",
+            "Host": "service.narvii.com",
+            "Accept-Encoding": "gzip",
+            "Connection": "Upgrade"
+        }
+        self.web_headers = {
+            "accept": "*/*",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "ar,en-US;q=0.9,en;q=0.8",
+            "content-type": "application/json",
+            "sec-ch-ua": '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+            "user-agent": ua(),
+            "x-requested-with": "xmlhttprequest"
+        }
+
+        if sid:
+            self.headers["NDCAUTH"] = sid
+            self.web_headers["cookie"] = sid
+
+        if uid:
+            self.uid = uid
+
+        if data:
+            self.headers["Content-Length"] = str(len(data))
+            if type(data) is not str: data = json.dumps(data)
+            self.headers["NDC-MSG-SIG"] = s(data)
+
+        if lang:
+            self.headers.update({"NDCLANG": lang[:lang.index("-")], "Accept-Language": lang})
