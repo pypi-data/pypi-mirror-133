3c4bce3e7a7180e1be661b8f6927d65712816d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:07 +0000

    Add package django-arg-path/0.1/django-arg-path-0.1.tar.gz

---
+++ packages/django-arg-path/django-arg-path-0.1.tar.gz/django-arg-path-0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'django-arg-path',         # How you named your package folder (MyLib)
+  packages = ['django-arg-path'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'We like writing short codes but we don"t want to think about logic. Here with django-arg-path package you can write short and clean code easily',   # Give a short description about your library
+  author = 'Nuru Hasanov',                   # Type in your name
+  author_email = 'nurhesen@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['django', 'path', 'static', 'urls'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
