ceb297eeb0547ff604aac686d3f4ba6202699f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:18 +0000

    Add package UsefulHelper/1.0.2/UsefulHelper-1.0.2.tar.gz

---
+++ packages/UsefulHelper/UsefulHelper-1.0.2.tar.gz/UsefulHelper-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='UsefulHelper',
+    version='1.0.2',
+    author='CleaverCreator',
+    author_email='liuhanbo333@icloud.com',
+    packages=find_packages(),
+    zip_safe=False,
+    platforms=['Linux'],
+    install_requires=['UNKnownDB'],
+    description='Some useful python tools',
+    long_description="""
+                    Some python tools
+                    -Pack tools
+                                    """,
+    license='GNU',
+    url='https://github.com/CleverCreater/UsefulTools',
+    classifiers=[]
+)
