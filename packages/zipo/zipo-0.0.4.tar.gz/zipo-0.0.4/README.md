936683f864a44813904c46d74ceda66b8fcb8d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:15 +0000

    Add package zipo/0.0.4/zipo-0.0.4.tar.gz

---
+++ packages/zipo/zipo-0.0.4.tar.gz/zipo-0.0.4/README.md
@@ -0,0 +1,21 @@
+# zipp
+
+##### this is a .zip file aggregate
+
+## Installation
+
+```shell
+pip install zipp
+```
+
+
+
+```python
+import zipp
+
+# backup2zip
+zipp.backup2zip(floder=, backupfilename=)
+
+# decompression
+zipp.decompression(name=)
+```
---
+++ packages/zipo/zipo-0.0.4.tar.gz/zipo-0.0.4/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('README.md', 'r')as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='zipo',
+    version='0.0.4',
+    author='xiongtianshuo',
+    author_email='Mr_Xiongts@163.com',
+    description='functions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    maintainer='xiongtianshuo',
+    maintainer_email='seoul1k@163.com',
+    url='https://github.com/seoul2k/zipp',
+    license='BSD License',
+    packages=['zipo/'],
+    platforms=["all"],
+    project_urls={
+        "Bug Tracker": "https://github.com/seoul2k/zipp/issues",
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
