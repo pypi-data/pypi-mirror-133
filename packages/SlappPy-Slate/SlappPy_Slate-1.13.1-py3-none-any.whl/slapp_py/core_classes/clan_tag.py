4ba583e3c39558bd70d9932df7557cefea6a59a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:58 +0000

    Add package SlappPy-Slate/1.13.1/SlappPy_Slate-1.13.1-py3-none-any.whl

---
+++ packages/SlappPy-Slate/SlappPy_Slate-1.13.1-py3-none-any.whl/slapp_py/core_classes/clan_tag.py
@@ -0,0 +1,37 @@
+from typing import Optional, List
+from uuid import UUID
+
+from slapp_py.core_classes.name import Name
+from slapp_py.core_classes.tag_option import TagOption
+
+
+class ClanTag(Name):
+    layout_option: Optional[TagOption]
+
+    def __init__(self, value: Optional[str],
+                 sources: Optional[List[UUID]] = None,
+                 layout_option: Optional[TagOption] = TagOption.Unknown) -> None:
+        if sources is None:
+            sources = []
+
+        super().__init__(value, sources)
+
+        self.layout_option = layout_option
+
+    @staticmethod
+    def from_dict(obj: dict) -> 'ClanTag':
+        assert isinstance(obj, dict)
+        name = Name.from_dict(obj)
+
+        layout_option = obj.get("LayoutOption", TagOption.Unknown)
+        if isinstance(layout_option, TagOption):
+            pass
+        elif isinstance(layout_option, str):
+            layout_option = TagOption[layout_option]
+        assert isinstance(layout_option, TagOption)
+        return ClanTag(name.value, name.sources, layout_option)
+
+    def to_dict(self) -> dict:
+        result = Name.to_dict(self)
+        result["LayoutOption"] = self.layout_option.name
+        return result
