59303c5944a5a0984d256fcc92168bb719130532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:35 +0000

    Add package ocr-cv/1.0.5/ocr_cv-1.0.5.tar.gz

---
+++ packages/ocr-cv/ocr_cv-1.0.5.tar.gz/ocr_cv-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ocr_cv',
+    version='1.0.5',
+    description='Generate keywords from CVs',
+    author='Gal Ben Haim',
+    author_email='galbhmusic@gmail.com',
+    license='MIT',
+    packages=find_packages(include=['ocr', 'ocr.*']),
+    url='https://gitlab.com/galbh/ocr',
+    zip_safe=False
+)
