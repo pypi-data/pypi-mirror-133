4bd52154ffb2e46377483c27585ca40d21304295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:34 +0000

    Add package aligo/2.0.0/aligo-2.0.0-py3-none-any.whl

---
+++ packages/aligo/aligo-2.0.0-py3-none-any.whl/aligo/types/ShareLinkBaseFile.py
@@ -0,0 +1,12 @@
+"""..."""
+from dataclasses import dataclass
+
+from .BaseFile import BaseFile
+
+
+@dataclass
+class ShareLinkBaseFile(BaseFile):
+    """..."""
+    mime_type: str = None
+    mime_extension: str = None
+    revision_id: str = None
