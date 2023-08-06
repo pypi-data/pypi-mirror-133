fe331c5ab1ec87275c3139fa58f273afc37f7140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:21 +0000

    Add package ctwinecalculator/1.14/ctwinecalculator-1.14.tar.gz

---
+++ packages/ctwinecalculator/ctwinecalculator-1.14.tar.gz/ctwinecalculator-1.14/README.md
@@ -0,0 +1,11 @@
+# Chakri3 Twine Execution
+
+A Python package to get basic maths by using calculator.
+
+## Usage
+
+Following query on terminal will provide you the addition, multiplication, divison and subtraction.
+
+```
+Add_number(223,143189) Sub_num(3,9) 
+```
---
+++ packages/ctwinecalculator/ctwinecalculator-1.14.tar.gz/ctwinecalculator-1.14/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="ctwinecalculator",
+    version="1.14",
+    description="A Python package to get basic calculator ",
+    author="Chakradhar Nallajarla",
+    author_email="chakradharnallajarla@gmail.com",
+    url="https://github.com/Chakradhar-Nallajarla/Twineexe_cal.git",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    )
+       
