ce3607a9a0bbfef1b728e49f49ff7e345537cb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:44 +0000

    Add package openhgnn/0.1.1/openhgnn-0.1.1.tar.gz

---
+++ packages/openhgnn/openhgnn-0.1.1.tar.gz/openhgnn-0.1.1/openhgnn/dataset/__init__.py
@@ -0,0 +1,77 @@
+import importlib
+from .base_dataset import BaseDataset
+from .utils import load_acm, load_acm_raw
+from .academic_graph import AcademicDataset
+from .hgb_dataset import HGBDataset
+
+DATASET_REGISTRY = {}
+
+
+def register_dataset(name):
+    """
+    New dataset types can be added to cogdl with the :func:`register_dataset`
+    function decorator.
+
+    For example::
+
+        @register_dataset('my_dataset')
+        class MyDataset():
+            (...)
+
+    Args:
+        name (str): the name of the dataset
+    """
+
+    def register_dataset_cls(cls):
+        if name in DATASET_REGISTRY:
+            raise ValueError("Cannot register duplicate dataset ({})".format(name))
+        if not issubclass(cls, BaseDataset):
+            raise ValueError("Dataset ({}: {}) must extend cogdl.data.Dataset".format(name, cls.__name__))
+        DATASET_REGISTRY[name] = cls
+        return cls
+
+    return register_dataset_cls
+
+
+def try_import_task_dataset(task):
+    if task not in DATASET_REGISTRY:
+        if task in SUPPORTED_DATASETS:
+            importlib.import_module(SUPPORTED_DATASETS[task])
+        else:
+            print(f"Failed to import {task} dataset.")
+            return False
+    return True
+
+
+def build_dataset(dataset, task):
+    if not try_import_task_dataset(task):
+        exit(1)
+    if dataset in ['aifb', 'mutag', 'bgs', 'am']:
+        _dataset = 'rdf_' + task
+    elif dataset in ['acm4NSHE', 'acm4GTN', 'academic4HetGNN', 'acm_han', 'acm_han_raw', 'acm4HeCo', 'dblp', 'dblp4MAGNN',
+                     'imdb4MAGNN', 'imdb4GTN', 'acm4NARS', 'demo_graph', 'yelp4HeGAN', 'DoubanMovie', 'Book-Crossing',
+                     'amazon4SLICE', 'MTWM', 'HNE-PubMed', 'HGBl-ACM', 'HGBl-DBLP', 'HGBl-IMDB']:
+        _dataset = 'hin_' + task
+    elif dataset in ['ogbn-mag']:
+        _dataset = 'ogbn_' + task
+    elif dataset in ['HGBn-ACM', 'HGBn-DBLP', 'HGBn-Freebase', 'HGBn-IMDB']:
+        _dataset = 'HGBn_node_classification'
+    elif dataset in ['HGBl-amazon', 'HGBl-LastFM', 'HGBl-PubMed']:
+        _dataset = 'HGBl_link_prediction'
+    elif dataset in ['wn18', 'FB15k', 'FB15k-237']:
+        assert task == 'link_prediction'
+        _dataset = 'kg_link_prediction'
+    elif dataset in ['LastFM4KGCN']:
+        _dataset = 'kgcn_recommendation'
+    elif dataset in ['yelp4rec']:
+        _dataset = 'hin_' + task
+    elif dataset == 'demo':
+        _dataset = 'demo_' + task
+    return DATASET_REGISTRY[_dataset](dataset)
+
+
+SUPPORTED_DATASETS = {
+    "node_classification": "openhgnn.dataset.NodeClassificationDataset",
+    "link_prediction": "openhgnn.dataset.LinkPredictionDataset",
+    "recommendation": "openhgnn.dataset.RecommendationDataset"
+}
---
+++ packages/openhgnn/openhgnn-0.1.1.tar.gz/openhgnn-0.1.1/openhgnn/utils/best_config.py
@@ -0,0 +1,246 @@
+BEST_CONFIGS = {
+    "node_classification": {
+        'RGCN': {
+            'general': {
+                'seed': 0, 'patience': 40, 'validation': True, 'evaluation_metric': 'acc'
+            },
+            'aifb': {
+                'lr': 0.005, 'weight_decay': 0, 'max_epoch': 100,
+                'hidden_dim': 32, 'n_bases': -1, 'n_layers': 2, 'batch_size': 126, 'dropout': 0,
+                'mini_batch_flag': False, 'validation': True
+            },
+            'mutag': {
+                'lr': 0.005, 'weight_decay': 0.0005, 'max_epoch': 50,
+                'hidden_dim': 32, 'n_bases': 30, 'n_layers': 2, 'batch_size': 50, 'fanout': 4, 'dropout': 0.5,
+                'mini_batch_flag': False, 'validation': True
+            },
+            'bgs': {
+                'lr': 0.005, 'weight_decay': 0.0005, 'max_epoch': 50,
+                'hidden_dim': 16, 'n_bases': 40, 'n_layers': 3, 'batch_size': 126, 'fanout': 4, 'dropout': 0.1,
+                'mini_batch_flag': True, 'validation': True
+            },
+            'am': {
+                'lr': 0.01, 'weight_decay': 0.0005, 'max_epoch': 50,
+                'hidden_dim': 16, 'n_bases': 40, 'n_layers': 2, 'batch_size': 126, 'fanout': 4, 'dropout': 0,
+                'mini_batch_flag': True, 'validation': True
+            },
+            'ogbn-mag': {
+                'lr': 0.01, 'weight_decay': 0.0005, 'max_epoch': 10,
+                'hidden_dim': 16, 'n_bases': -1, 'n_layers': 2, 'batch_size': 126, 'fanout': 4, 'dropout': 0,
+                'mini_batch_flag': True, 'validation': True
+            },
+            'HGBn-ACM': {
+                'evaluation_metric': 'f1'
+            },
+            'HGBn-DBLP': {
+                'n_layers': 2, 'hidden_dim': 16, 'dropout': 0.5
+            },
+            'HGBn-Freebase': {
+                'n_layers': 5, 'hidden_dim': 16, 'dropout': 0.5
+            },
+            'HGBn-IMDB': {
+            }
+        },
+        'CompGCN': {
+            'general': {
+                'seed': 0, 'patience': 50, 'validation': True, 'evaluation_metric': 'acc'
+            },
+            'aifb': {
+                'lr': 0.01, 'weight_decay': 0.0001, 'max_epoch': 100,
+                'hidden_dim': 32, 'n_layers': 2, 'dropout': 0.2, 'comp_fn': 'sub', 'batch_size': 128,
+                'mini_batch_flag': False, 'validation': True
+                     },
+            'mutag': {
+                'lr': 0.01, 'weight_decay': 0.0001, 'max_epoch': 100,
+                'hidden_dim': 32, 'n_layers': 3, 'dropout': 0.5, 'comp_fn': 'sub',
+                'mini_batch_flag': False, 'validation': True
+            },
+            'bgs': {
+
+            },
+            'am': {
+                'lr': 0.001, 'weight_decay': 0.0001, 'max_epoch': 100, 'fanout': 4,
+                'hidden_dim': 32, 'n_layers': 2, 'dropout': 0.2, 'comp_fn': 'sub', 'batch_size': 128,
+                'mini_batch_flag': True, 'validation': True
+            },
+        },
+        'GTN': {
+            'general': {'lr': 0.005, 'weight_decay': 0.001, 'hidden_dim': 64, 'max_epoch': 100, 'patience': 20,
+                        'norm_emd_flag': True, 'mini_batch_flag': False},
+            'acm4GTN': {
+                'num_layers': 2, 'num_channels': 2, 'adaptive_lr_flag': True,
+            },
+            'imdb4GTN': {
+                'num_layers': 2, 'num_channels': 2, 'adaptive_lr_flag': True,
+            },
+            'dblp4GTN': {
+                'num_layers': 3, 'num_channels': 2, 'adaptive_lr_flag': False,
+            },
+        },
+        'fastGTN': {
+            'general': {'lr': 0.005, 'weight_decay': 0.001, 'hidden_dim': 64, 'max_epoch': 100, 'patience': 20,
+                        'norm_emd_flag': True, 'mini_batch_flag': False},
+            'acm4GTN': {
+                'num_layers': 2, 'num_channels': 8, 'adaptive_lr_flag': False,
+            },
+            'imdb4GTN': {
+                'num_layers': 2, 'num_channels': 4, 'adaptive_lr_flag': False,
+            },
+            'dblp4GTN': {
+                'num_layers': 3, 'num_channels': 2, 'adaptive_lr_flag': False,
+            },
+        },
+        'MHNF': {
+            'general': {'lr': 0.005, 'weight_decay': 0.001, 'hidden_dim': 64, 'max_epoch': 50, 'patience': 10,
+                        'norm_emd_flag': True, 'mini_batch_flag': False},
+            'acm4GTN': {
+                'num_layers': 2, 'num_channels': 2, 'adaptive_lr_flag': True,
+            },
+            'imdb4GTN': {
+                'num_layers': 2, 'num_channels': 2, 'adaptive_lr_flag': True,
+            },
+            'dblp4GTN': {
+                'num_layers': 3, 'num_channels': 2, 'adaptive_lr_flag': False,
+            },
+        },
+        'HAN': {
+            'general': {},
+            'acm_han_raw': {
+                'lr': 0.005, 'num_heads': [8], 'hidden_dim': 128, 'dropout': 0.6, 'weight_decay': 0.001,
+                'num_epochs': 200, 'patience': 100
+            },
+            'acm4GTN': {
+                'lr': 0.001, 'weight_decay': 0.00001, 'num_heads': [8, 8],
+            },
+            'imdb4GTN': {
+                'lr': 0.001, 'weight_decay': 0.00001, 'num_heads': [2, 2, 2]
+            },
+            'HGBn-ACM': {
+            },
+            'HGBn-DBLP': {
+            }
+
+        },
+        'NARS': {
+            'general': {},
+            'HGBn-ACM': {
+                'lr': 0.001, 'num_heads': [8], 'num_hops': 3, 'hidden_dim': 64, 'dropout': 0.5, 'weight_decay': 0.001,
+                'num_epochs': 200, 'patience': 100, 'metric': 'acc', 'ff_layer': 2,
+            },
+            'HGBn-DBLP': {
+                'lr': 0.001, 'num_heads': [8], 'num_hops': 3, 'hidden_dim': 512, 'dropout': 0.5, 'weight_decay': 0.001,
+                'num_epochs': 200, 'patience': 100, 'metric': 'acc', 'ff_layer': 2,
+            },
+            'HGBn-Freebase': {
+                'lr': 0.001, 'num_heads': [8], 'num_hops': 2, 'hidden_dim': 64, 'dropout': 0.5, 'weight_decay': 0.001,
+                'num_epochs': 200, 'patience': 100, 'metric': 'acc', 'ff_layer': 2,
+            },
+            'HGBn-IMDB': {
+                'lr': 0.001, 'num_heads': [8], 'num_hops': 2, 'hidden_dim': 64, 'dropout': 0.5, 'weight_decay': 0.001,
+                'num_epochs': 200, 'patience': 100, 'metric': 'acc', 'ff_layer': 2,
+            },
+
+
+        },
+        'HetGNN': {
+            'general': {'max_epoch': 500, 'patience': 20, 'mini_batch_flag': True},
+            'academic4HetGNN': {
+                'lr': 0.001, 'weight_decay': 0.00001, 'dim': 128, 'batch_size': 64, 'window_size': 5,
+                'batches_per_epoch': 50, 'rw_length': 50, 'rw_walks': 10, 'rwr_prob': 0.5,
+            }
+        },
+        'MAGNN': {
+            'general': {'mini_batch_flag': False, 'max_epoch': 500, 'patience': 30},
+            'imdb': {
+                'lr': 0.005, 'weight_decay': 0.001, 'dropout': 0.5, 'hidden_dim': 64, 'encoder_type': 'RotateE',
+                'inter_attn_feats': 128, 'num_heads': 8, 'num_layers': 2,
+            },
+        },
+        'NSHE': {
+            'general': {},
+            'acm4NSHE': {'weight_decay': 0.001, 'num_e_neg': 1, 'num_ns_neg': 4,
+                         'max_epoch': 500, 'patience': 10,
+                         }
+        },
+        'HeCo': {
+            'general': {
+                'hidden_dim': 64, 'max_epoch' : 10000, 'mini_batch_flag' : False
+            },
+            'acm4HeCo': {'seed': 84,  'eva_lr': 0.05, 'eva_wd': 0, 'patience': 5, 'learning_rate': 0.0008,
+                         'weight_decay': 0, 'tau': 0.8, 'feat_drop': 0.3, 'attn_drop': 0.5,
+                         'sample_rate': {'author': 7, "subject": 3}, 'lam': 0.5
+            }
+        },
+        'DMGI': {
+            'general': {
+
+                'seed': 0,  'sc': 3, 'learning_rate' : 0.0005
+            },
+            'acm_han_raw': {'l2_coef': 0.0001, 'dropout': 0.5,'reg_coef': 0.001,
+                            'patience': 20,'sup_coef': 0.1, 'hid_unit': 64,
+                            'isSemi': False,'isBias': False,'isAttn': False
+            },
+            'imdb4GTN': {'l2_coef': 0.0001, 'dropout': 0.5,'reg_coef': 0.1,
+                         'patience': 50,'sup_coef': 0.1, 'hid_unit': 64,
+                         'isSemi': False,'isBias': False,'isAttn': False
+            }
+        },
+        'RSHN': {
+            'general': {},
+            'aifb': {
+                'in_dim': 16, 'hidden_dim': 16, 'num_node_layer': 2,
+                'num_edge_layer': 2, 'rw_len': 4, 'batch_size': 5000,
+            },
+            'mutag': {
+                'hidden_dim': 16, 'num_node_layer': 2, 'num_edge_layer': 2, 'rw_len': 4, 'batch_size': 5000,
+                'dropout': 0.2,
+            },
+            'bgs': {
+                'hidden_dim': 16, 'num_node_layer': 2, 'num_edge_layer': 2, 'rw_len': 4, 'batch_size': 5000
+            },
+            'am': {'hidden_dim': 16, 'num_node_layer': 2, 'num_edge_layer': 2, 'rw_len': 4, 'batch_size': 5000
+                   },
+        },
+        'HGSL': {
+            'general': {},
+            'acm4GTN': {
+                'undirected_relations': 'author-paper,paper-subject', 'gnn_dropout': 0, 'fs_eps': 0.8,
+                'fp_eps': 0.2, 'mp_eps': 0.6,
+            }
+        }
+
+    },
+    
+    "link_prediction": {
+        'NARS': {
+            'general': {'num_hops': 3},
+        },
+        'HetGNN': {
+            'general': {'max_epoch': 500, 'patience': 10, 'mini_batch_flag': True},
+            'academic4HetGNN': {
+                'lr': 0.01, 'weight_decay': 0.0001, 'dim': 128, 'batch_size': 64, 'window_size': 5,
+                'batches_per_epoch': 50, 'rw_length': 50, 'rw_walks': 10, 'rwr_prob': 0.5,
+            }
+        },
+        'RGCN': {
+            'general': {
+
+            },
+            'FB15k-237': {
+                'lr': 0.01, 'weight_decay': 0.0005, 'max_epoch': 100,
+                'hidden_dim': 16, 'n_bases': 40, 'n_layers': 2, 'batch_size': 126, 'fanout': 4, 'dropout': 0,
+                'validation': True
+            },
+        },
+
+    },
+    "recommendation": {
+        'KGCN': {
+            "general": {},
+            'LastFM4KGCN': {
+                'in_dim': 16, 'hidden_dim': 16, 'n_relation': 60,
+                'batch_size': 128, 'lr' : 0.002
+            },
+        }
+    },
+}
