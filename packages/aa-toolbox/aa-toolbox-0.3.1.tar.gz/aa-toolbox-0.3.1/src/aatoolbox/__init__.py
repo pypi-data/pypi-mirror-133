d9b12887f4c6864d4834858a62af06afc4597a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:41 +0000

    Add package aa-toolbox/0.3.1/aa-toolbox-0.3.1.tar.gz

---
+++ packages/aa-toolbox/aa-toolbox-0.3.1.tar.gz/aa-toolbox-0.3.1/src/aatoolbox/__init__.py
@@ -0,0 +1,7 @@
+"""aa-toolbox: Toolbox for Anticipatory Action.
+
+The ``aatoolbox`` library contains modules designed to assist with
+downloading and processing the data required for disaster-related
+anticipatory action.
+"""
+from ._version import version as __version__  # noqa: F401
