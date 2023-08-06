f807796b54c35877323d6326dd2e0b4c50197ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:44 +0000

    Add package aws-lambda-typing/2.9.3/aws-lambda-typing-2.9.3.tar.gz

---
+++ packages/aws-lambda-typing/aws-lambda-typing-2.9.3.tar.gz/aws-lambda-typing-2.9.3/src/aws_lambda_typing/responses/s3_batch.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import List, Literal, TypedDict
+else:
+    from typing import List
+
+    from typing_extensions import Literal, TypedDict
+
+
+class S3BatchResponseResult(TypedDict):
+    """
+    S3BatchRequestTask
+
+    Attributes:
+    ----------
+    taskId: str
+
+    resultCode: str
+
+    resultString: str
+    """
+
+    taskId: str
+    resultCode: str
+    resultString: str
+
+
+class S3BatchResponse(TypedDict):
+    """
+    S3BatchResponse
+    https://docs.aws.amazon.com/lambda/latest/dg/services-s3-batch.html
+
+    Attributes:
+    ----------
+    invocationSchemaVersion: str
+
+    treatMissingKeysAs: Literal['Succeeded', 'TemporaryFailure',
+    'PermanentFailure']
+
+    invocationId: str
+
+    results: List[:py:class:`S3BatchResponseResult`]
+    """
+
+    invocationSchemaVersion: str
+    treatMissingKeysAs: Literal[
+        "Succeeded",
+        "TemporaryFailure",
+        "PermanentFailure",
+    ]
+    invocationId: str
+    results: List[S3BatchResponseResult]
