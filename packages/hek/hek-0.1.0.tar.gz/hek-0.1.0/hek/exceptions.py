33eede9cf53161dc9c9a6b0ea1320bfeb82fc7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:01 +0000

    Add package hek/0.1.0/hek-0.1.0.tar.gz

---
+++ packages/hek/hek-0.1.0.tar.gz/hek-0.1.0/hek/exceptions.py
@@ -0,0 +1,13 @@
+class SiteHostNameRequestError(LookupError):
+    """Request host name error"""
+
+class LengthError(ValueError):
+    """ length error exception """
+class ProxyFailed(ConnectionRefusedError):
+    """ Proxy failed to connect """
+class FileNotFound(FileNotFoundError):
+    """ if file not found """
+class ExifExtractError(InterruptedError):
+    """ exif error """
+class SocketConnectionError(ConnectionError):
+    """ socket connection error """
