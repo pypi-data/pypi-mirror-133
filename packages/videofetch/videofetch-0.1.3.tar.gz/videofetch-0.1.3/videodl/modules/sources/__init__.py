e4258b36f7ae8a20c9bead2e421794885aa8a0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:26 +0000

    Add package videofetch/0.1.3/videofetch-0.1.3.tar.gz

---
+++ packages/videofetch/videofetch-0.1.3.tar.gz/videofetch-0.1.3/videodl/modules/sources/__init__.py
@@ -0,0 +1,11 @@
+'''import all'''
+from .cntv import CNTV
+from .mgtv import MGTV
+from .migu import Migu
+from .acfun import AcFun
+from .zhihu import Zhihu
+from .xigua import Xigua
+from .iqiyi import Iqiyi
+from .douyin import Douyin
+from .haokan import Haokan
+from .bilibili import Bilibili
