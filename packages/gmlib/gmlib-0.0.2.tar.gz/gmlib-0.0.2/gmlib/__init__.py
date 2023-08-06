41c8cc38258ce98e7dc1e0636f938c17ea520e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:23 +0000

    Add package gmlib/0.0.2/gmlib-0.0.2.tar.gz

---
+++ packages/gmlib/gmlib-0.0.2.tar.gz/gmlib-0.0.2/gmlib/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: Vladislav117
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2021 Vladislav117
+"""
+
+from .geometry import *
+
+__author__ = "Vladislav117"
+__version__ = "0.0.1"
+__email__ = "siluetkysta@gmail.com"
