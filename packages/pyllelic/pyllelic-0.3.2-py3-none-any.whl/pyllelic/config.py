2a7ef522a975795ee49639519266bb756a792519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:29 +0000

    Add package pyllelic/0.3.2/pyllelic-0.3.2-py3-none-any.whl

---
+++ packages/pyllelic/pyllelic-0.3.2-py3-none-any.whl/pyllelic/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Configuration options for pyllelic."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Config:
+    base_directory: Path = Path("/")
+    promoter_file: Path = base_directory / "promoter.txt"
+    results_directory: Path = base_directory / "results"
+    bam_directory: Path = base_directory / "bam_output"
+    analysis_directory: Path = base_directory / "test"
+    promoter_start: str = "1293200"
+    promoter_end: str = "1296000"
+    chromosome: str = "5"
+    offset: int = 1298163
+    viz_backend: str = "plotly"
