239c8cf50a164b160572e5b6f8e1efdb554e23a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:55 +0000

    Add package pyfbad/0.1.2/pyfbad-0.1.2-py3-none-any.whl

---
+++ packages/pyfbad/pyfbad-0.1.2-py3-none-any.whl/pyfbad/features/create_feature.py
@@ -0,0 +1,44 @@
+import logging
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+class Features:
+
+    def getting_model_data(self, df, column_ds, column_y, country=''):
+        """
+        :param df: dataframe
+        :param country: string
+        :param column_ds: string
+        :param column_y: string
+        :return: dataframe
+        """
+        print("Preparing expected column names and getting model data...")
+        columns = [column_ds, column_y]
+        df_ = df[df.country == country].reset_index(drop=True) if country != '' else df
+        return df_[columns] \
+            .rename(columns={column_ds: 'ds', column_y: 'y'}) \
+            .sort_values('ds', ascending=True) \
+            .reset_index(drop=True)
+
+    def get_model_data(self, df, time_column_name, value_column_name, filter=None):
+        """ Returns a two column dataframe ready to use train model.
+        Args:
+            df (Dataframe): A data frame contains a bunch of column
+            time_column_name (str): The column name will be defined time axis
+            value_column_name (str): The column name will be used as main data to train
+            filter (list): To takes only this "value" from the "column_name" --> ['country','TR'] 
+        Returns:
+            df_ (Dataframe): Two column dataframe ready to use train model
+        """
+        print("Preparing expected column names and getting model data...")
+        try:
+            columns = [time_column_name, value_column_name]
+            df_ = df[df[filter[0]] == filter[1]].reset_index(drop=True) if filter != None else df
+
+            return df_[columns] \
+                .rename(columns={time_column_name: 'ds', value_column_name: 'y'}) \
+                .sort_values('ds', ascending=True) \
+                .reset_index(drop=True)
+        except:
+            print("Something went wrong when get the dataframe as ready to train.")
