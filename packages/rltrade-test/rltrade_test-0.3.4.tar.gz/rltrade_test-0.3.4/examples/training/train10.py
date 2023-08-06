b603461f0c0dc611ab53c92d193fee7c52f450dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:59 +0000

    Add package rltrade-test/0.3.4/rltrade_test-0.3.4.tar.gz

---
+++ packages/rltrade-test/rltrade_test-0.3.4.tar.gz/rltrade_test-0.3.4/examples/training/train10.py
@@ -0,0 +1,126 @@
+import asyncio
+
+import pandas as pd
+from rltrade import config
+from metaapi_cloud_sdk import MetaApi
+from rltrade.backtests import get_metrics
+from rltrade.data import OandaDownloader
+from rltrade.models import SmartDailyTradeForexAgent
+
+
+"""
+time_frame - last date available
+1d - (2018-01-07)
+"""
+
+time_frame = "1d" #keep is 1d in this file or else it won't work
+train_period = ('2012-08-04','2021-01-01') #for training the model
+test_period = ('2021-01-02','2021-12-20') #for testing the model
+start_time = "17:00:00"
+end_time = "23:55:00" #does not matter in daily trading.
+path = 'models/dailytrade/forex-train10'
+
+token = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIzYTI0YzMwYzFkNDRmZGFmZGI3NmVmYTUxZmQ2MDJmMCIsInBlcm1pc3Npb25zIjpbXSwidG9rZW5JZCI6IjIwMjEwMjEzIiwiaWF0IjoxNjM5NDkwMzEzLCJyZWFsVXNlcklkIjoiM2EyNGMzMGMxZDQ0ZmRhZmRiNzZlZmE1MWZkNjAyZjAifQ.DhA6Q-AlWNssi8ScOaWy2Bxo4Hebgw94BDE6PiT9J-TvsuF7OqvdYBQ1IWYEMtsYsg3Ij8p8Wpvn8ZdZeHRkR3vLcQnMH-GZj3DyYkeqovQKk6U3uOobV-GS3meJPZYfw2zItuTDBWxuHDZsVW1ZvF4sItBDmsWIe2svF0NmKE1nu-ephcYVzYo9grr93de_h-QwlP-yeZFeGEqrz3-q5gYWcARJsIR1BX63zePuDHkUK9k5W9Rm28WdB87MHEyMSWhcAZDf8si5MwsPYC3wpzNtzGqORF3UY-w5EmolCtSPMBqM7AI0LKc1n8GPS3ZhnvHkfGhWEdb5gKlCWwshk30tICN24C1bZG06zfs450oLm8ih9ls5oyshcg_xwawNvsA305D7Siz0Pzqr1xnUA8zMz8cVUFZtjdBWCfot05_ziVO0x_mApVyAVC2OA-Sh61RtkwNNpg4bTCzK30OpdiS9GO0HLgnepnuwWOO0T9DTzTAJUxyJcXOzcWcXGdMTWaGAp5ranytU97k8GxDHa5jOS_WvphL24C8QA6of0pYZwHM3Ul5Aw351H1SbJLIqs2AChDoUnpJb9OZb-27ESLZgM1mhU6rwzt8lRRbxUHaXSv5QpM29nPm3k5KrFSv-UTXiX6oU9c1nNh3qLb6FKV_B1CQarcvyx66iUg-1DcU'
+domain = 'agiliumtrade.agiliumtrade.ai'
+account_id = 'ddc8fb93-e0f5-4ce8-b5d5-8290d23fc143'
+
+ticker_list = ['AUDUSD','EURUSD','NZDUSD','USDCAD','USDCHF','GBPUSD']
+# ticker_list = ['USDCAD','USDCHF']
+sec_types = ['-'] * len(ticker_list)
+exchanges = ['-'] * len(ticker_list)
+
+#CADCHF - 3 epochs
+#EURGBP - 2 epochs
+#AUDNZD - 2 epochs
+
+# tech_indicators = config.STOCK_INDICATORS_LIST # indicators from stockstats
+# additional_indicators = config.ADDITIONAL_STOCK_INDICATORS
+
+tech_indicators = [
+    "open_2_sma",
+    "close_2_tema",
+    "tema",
+]
+
+additional_indicators = [
+    "max_value_price_5",
+    "max_value_price_22",
+    "max_value_price_66",
+    "max_value_price_1year",
+    "max_value_price_3year",
+    "max_value_price_5year",
+]
+
+env_kwargs = {
+    "initial_amount": 17500, #this does not matter as we are making decision for lots and not money.
+    "ticker_col_name":"tic",
+    "stop_loss":0.015,
+    "take_profit":0.015,
+    "filter_threshold":1, #between 0.1 to 1, select percentage of top stocks 0.3 means 30% of top stocks
+    "target_metrics":['asset'], #asset, cagr, sortino, calamar, skew and kurtosis are available options.
+    "transaction_cost":0, #transaction cost per order
+    "tech_indicator_list":tech_indicators + additional_indicators, 
+    "reward_scaling": 1}
+
+PPO_PARAMS = {'ent_coef':0.005,
+            'learning_rate':0.01,
+            'batch_size':252}
+
+async def train_model():
+    api = MetaApi(token,{'domain':domain})
+
+    try:
+        account = await api.metatrader_account_api.get_account(account_id)
+
+        if account.state != 'DEPLOYED':
+            print("Deploying account")
+            await account.deploy()
+        else:
+            print('Account already deployed')
+        if account.connection_status != 'CONNECTED':
+            print('Waiting for API server to connect to broker (may take couple of minutes)')
+            await account.wait_connected()
+        else:
+            print("Account already connected")
+    
+        oa = OandaDownloader(
+            account = account,
+            time_frame=time_frame,
+            start_date=train_period[0],
+            end_date=test_period[1],
+            start_time=start_time,
+            end_time=end_time,
+            ticker_list=ticker_list)
+
+        df = await oa.fetch_daily_data()
+
+        agent = SmartDailyTradeForexAgent("ppo",
+                            df=df,
+                            account=account,
+                            time_frame=time_frame,
+                            ticker_list=ticker_list,
+                            sec_types = sec_types,
+                            exchanges=exchanges,
+                            ticker_col_name="tic",
+                            tech_indicators=tech_indicators,
+                            additional_indicators=additional_indicators,
+                            train_period=train_period,
+                            test_period=test_period,
+                            start_time=start_time,
+                            end_time=end_time,
+                            env_kwargs=env_kwargs,
+                            model_kwargs=PPO_PARAMS,
+                            tb_log_name='ppo',
+                            epochs=4)
+
+        agent.train_model()
+        agent.save_model(path) #save the model for trading
+
+        df_daily_return,df_actions = agent.make_prediction() #testing model on testing period
+        get_metrics(path)
+
+    except Exception as err:
+        print(api.format_error(err))
+
+
+asyncio.run(train_model())
