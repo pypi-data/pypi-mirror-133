f409b8ac8b422a7ca3465d1029881ba12f37d4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:30 +0000

    Add package gitrics/0.1.0/gitrics-0.1.0.tar.gz

---
+++ packages/gitrics/gitrics-0.1.0.tar.gz/gitrics-0.1.0/gitrics/project/events.py
@@ -0,0 +1,38 @@
+import time
+
+from gitrics import configuration
+
+from glapi.project.project import GitlabProject
+
+class gitricsProjectEvents(GitlabProject):
+    """
+    gitricsProjectEvents is a collection of Gitlab Project Events modified and enriched for gitrics ecosystem.
+    """
+
+    def __init__(self, project_id: str = None, project: dict = None, date_start: str = configuration.DATE_START, date_end: str = configuration.DATE_END, token: str = None, version: str = configuration.GITLAB_API_VERSION):
+        """
+        Args:
+            date_end (string): iso 8601 date value
+            date_start (string): iso 8601 date value
+            project (dictionary): key/value pair representing a GitLab Project
+            project_id (string): GitLab Project id
+            token (string): GitLab personal access or deploy token
+            version (string): GitLab API version as base url
+        """
+
+        # initialize inheritance
+        super(gitricsProjectEvents, self).__init__(
+            id=project_id,
+            project=project,
+            token=token,
+            version=version
+        )
+
+        # superficial delay for api constraints
+        #time.sleep(2)
+
+        # get events
+        self.events = self.extract_events(
+            date_end=date_end,
+            date_start=date_start
+        )
