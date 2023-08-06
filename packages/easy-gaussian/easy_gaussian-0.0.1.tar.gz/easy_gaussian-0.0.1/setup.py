b61a4bc576f518275aeeba0dc6e9ddf74312825f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:07 +0000

    Add package easy-gaussian/0.0.1/easy_gaussian-0.0.1.tar.gz

---
+++ packages/easy-gaussian/easy_gaussian-0.0.1.tar.gz/easy_gaussian-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='easy_gaussian',
+  version='0.0.1',
+  description='',
+  long_description='',
+  url='https://www.gaussian.com',
+  author='Jacob Nite',
+  author_email='jnite@gaussian.com',
+  packages=[],
+  classifiers=['Development Status :: 1 - Planning'],
+)
+
