6a1ce3599d0ea0cd379c468b55fc14dc27354bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:55 +0000

    Add package verysimple24/0.24/verysimple24-0.24.tar.gz

---
+++ packages/verysimple24/verysimple24-0.24.tar.gz/verysimple24-0.24/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.24'
+DESCRIPTION = 'My first Python package1'
+LONG_DESCRIPTION = 'My first Python package1 with a slightly longer description'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimple24'
+    name="verysimple24",
+    version=VERSION,
+    author="Pooja Singla",
+    author_email="pooja_singla@persistent.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
