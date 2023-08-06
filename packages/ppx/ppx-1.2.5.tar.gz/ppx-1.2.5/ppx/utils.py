7c957b1959018842fe95cd6e133ebf7f10d1e64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:25 +0000

    Add package ppx/1.2.5/ppx-1.2.5.tar.gz

---
+++ packages/ppx/ppx-1.2.5.tar.gz/ppx-1.2.5/ppx/utils.py
@@ -0,0 +1,12 @@
+"""Utility Functions"""
+
+
+def listify(obj):
+    """Turn an object into a list, but don't split strings"""
+    try:
+        assert not isinstance(obj, str)
+        iter(obj)
+    except (AssertionError, TypeError):
+        obj = [obj]
+
+    return list(obj)
