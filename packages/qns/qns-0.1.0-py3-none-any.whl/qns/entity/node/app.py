cafd6cce7632d7c8af662b03cbc541ec8408c05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:19 +0000

    Add package qns/0.1.0/qns-0.1.0-py3-none-any.whl

---
+++ packages/qns/qns-0.1.0-py3-none-any.whl/qns/entity/node/app.py
@@ -0,0 +1,31 @@
+from qns.simulator.simulator import Simulator
+from qns.simulator import Event
+
+
+class Application(object):
+    """
+    Application can be deployed on the quantum nodes.
+    """
+    def __init__(self):
+        pass
+
+    def install(self, node, simulator: Simulator):
+        """
+        install initial events for this QNode
+
+        Args:
+            node (QNode): the node that will handle this event
+            simulator (Simulator): the simulator
+        """
+        self._simulator = simulator
+        self._node = node
+
+    def handle(self, node, event: Event):
+        """
+        process the event on the node.
+
+        Args:
+            node (QNode): the node that will handle this event
+            event (Event): the event
+        """
+        pass
