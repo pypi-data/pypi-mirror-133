0da8cf69fa53aded9a00a457ed268dd6d394c6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:23 +0000

    Add package hilandBasicLibrary/0.4.6/hilandBasicLibrary-0.4.6.tar.gz

---
+++ packages/hilandBasicLibrary/hilandBasicLibrary-0.4.6.tar.gz/hilandBasicLibrary-0.4.6/hilandBasicLibrary/data/dictHelper.py
@@ -0,0 +1,29 @@
+from builtins import *
+
+
+class DictHelper:
+    @staticmethod
+    def is_contains_key(whole_dict, key_name):
+        if key_name in whole_dict:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_value(cls, whole_dict, key_name, default_value=None):
+        if cls.is_contains_key(whole_dict, key_name):
+            return whole_dict[key_name]
+        else:
+            return default_value
+
+    @classmethod
+    def is_contains_value(cls, whole_dict, value):
+        for key in whole_dict:
+            if whole_dict[key] == value:
+                return True
+
+        return False
+
+    @classmethod
+    def get_summary(cls, dict_data):
+        pass
