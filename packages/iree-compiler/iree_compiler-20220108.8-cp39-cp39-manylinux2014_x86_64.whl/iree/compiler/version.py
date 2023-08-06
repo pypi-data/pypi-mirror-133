2586243fa0735c9d09a4b3a51e9f7e1f3af30158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:38 +0000

    Add package iree-compiler/20220108.8/iree_compiler-20220108.8-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/iree-compiler/iree_compiler-20220108.8-cp39-cp39-manylinux2014_x86_64.whl/iree/compiler/version.py
@@ -0,0 +1,4 @@
+# Auto-generated version info.
+PACKAGE_SUFFIX = ""
+VERSION = "20220108.8"
+REVISIONS = {"IREE": "8bec8ebbcbf7a5c31fbeff893d22afc36ee7c981", "LLVM_PROJECT": "f96ab6cc1b2016374a6e8500774f8cb2c3725b04", "TENSORFLOW": "baa6d31a0af6adb7058e229001931a1fdd6f81bb", "MLIR_HLO": "2e2f5f5ac8fd84555bddfe45a4c4f6037cbb63d3"}
