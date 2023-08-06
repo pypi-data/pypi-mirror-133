3bc73be748034c2b6de2f37c5171b3c9cef6c56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package docstats/0.1.2/docstats-0.1.2.tar.gz

---
+++ packages/docstats/docstats-0.1.2.tar.gz/docstats-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['docstats', 'docstats.directives']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Sphinx>=4.3.2,<5.0.0']
+
+setup_kwargs = {
+    'name': 'docstats',
+    'version': '0.1.2',
+    'description': 'Statistics and other numbers for your sphinx pages',
+    'long_description': None,
+    'author': 'GameDungeon',
+    'author_email': 'gamedungeon@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
