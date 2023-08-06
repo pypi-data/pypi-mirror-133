5157228ef16bac2c9094e7c2373d90d8e3202e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:10 +0000

    Add package pyqubo/1.1.2/pyqubo-1.1.2.tar.gz

---
+++ packages/pyqubo/pyqubo-1.1.2.tar.gz/pyqubo-1.1.2/setup.py
@@ -0,0 +1,173 @@
+import os
+import subprocess
+import sys
+import platform
+import re
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
+
+class PackageInfo(object):
+    def __init__(self, info_file):
+        with open(info_file) as f:
+            exec(f.read(), self.__dict__)
+        self.__dict__.pop('__builtins__', None)
+
+    def __getattribute__(self, name):
+        return super(PackageInfo, self).__getattribute__(name)
+
+
+package_info = PackageInfo(os.path.join('pyqubo', 'package_info.py'))
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cfg = "Debug" if self.debug else "Release"
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
+            "-DPYQUBOC_VERSION_INFO={}".format(self.distribution.get_version()),
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
+        ]
+        build_args = []
+
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            if not cmake_generator:
+                cmake_args += ["-GNinja"]
+
+        else:
+
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+                ]
+                build_args += ["--config", cfg]
+        
+        if platform.system() == 'Darwin':
+
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += ["-j{}".format(self.parallel)]
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+
+
+packages = ['pyqubo', 'pyqubo.integer', 'pyqubo.utils']
+
+install_requires = [
+    'numpy>=1.17.3',
+    'dimod>=0.9.14, <0.11',
+    'dwave-neal>=0.5.7',
+    'Deprecated>=1.2.12',
+    #'jij-cimod>=1.0.1'
+    'six>=1.15.0'
+]
+
+
+setup_requires = []
+
+tests_require = [
+    'coverage>=4.5.1',
+    'codecov>=2.1.9'
+]
+
+python_requires = '>=3.5, <3.10'
+
+setup(
+    name=package_info.__package_name__,
+    version=package_info.__version__,
+    description=package_info.__description__,
+    long_description=open('README.rst').read(),
+    author=package_info.__contact_names__,
+    author_email=package_info.__contact_emails__,
+    maintainer=package_info.__contact_names__,
+    maintainer_email=package_info.__contact_emails__,
+    url=package_info.__repository_url__,
+    download_url=package_info.__download_url__,
+    license=package_info.__license__,
+    ext_modules=[CMakeExtension('cpp_pyqubo')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    packages=packages,
+    keywords=package_info.__keywords__,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    python_requires=python_requires,
+    tests_require=tests_require,
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: Apache Software License',
+    ]
+)
