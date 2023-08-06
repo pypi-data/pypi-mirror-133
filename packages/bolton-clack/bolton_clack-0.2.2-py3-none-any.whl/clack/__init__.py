2bee57f171cdb0dd1d920ff268962c84f98f997d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:37 +0000

    Add package bolton-clack/0.2.2/bolton_clack-0.2.2-py3-none-any.whl

---
+++ packages/bolton-clack/bolton_clack-0.2.2-py3-none-any.whl/clack/__init__.py
@@ -0,0 +1,33 @@
+"""(C)ommand (L)ine (A)pplication (C)onfig (K)it
+
+A wrapper around the `argparse` library that aims to handle _all_ application
+configuration.
+"""
+
+import logging as _logging
+
+from . import xdg
+from ._config import AbstractConfig, Config
+from ._helpers import NewCommand, comma_list_or_file, new_command_factory
+from ._main import MainType, main_factory
+from ._parser import Parser, filter_cli_args
+
+
+__all__ = [
+    "AbstractConfig",
+    "Config",
+    "MainType",
+    "NewCommand",
+    "Parser",
+    "comma_list_or_file",
+    "filter_cli_args",
+    "main_factory",
+    "new_command_factory",
+    "xdg",
+]
+
+__author__ = "Bryan M Bugyi"
+__email__ = "bryanbugyi34@gmail.com"
+__version__ = "0.2.2"
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
