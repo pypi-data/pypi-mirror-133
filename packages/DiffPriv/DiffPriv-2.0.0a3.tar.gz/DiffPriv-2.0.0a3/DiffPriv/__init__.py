0da453913b2be46dd451faab49abb1fe0a2d2a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:00 +0000

    Add package DiffPriv/2.0.0a3/DiffPriv-2.0.0a3.tar.gz

---
+++ packages/DiffPriv/DiffPriv-2.0.0a3.tar.gz/DiffPriv-2.0.0a3/DiffPriv/__init__.py
@@ -0,0 +1,68 @@
+"""
+DiffPriv is a package focused on privacy, with differential privacy and encryption schemes.
+
+.. include:: ../README.md
+"""
+__docformat__ = "restructuredtext"
+
+# Sanity Check Imports
+import warnings
+import json
+from urllib import request
+
+# Dependecy Imports
+import sys
+import csv
+import webbrowser
+import math
+import random
+import numpy as np
+
+# Local
+from . import mech
+from . import enc
+from . import cli
+
+__version__ = 'v1.0.3'
+__stable__ = True
+"""If package is stable or not."""
+__source__ = 'https://github.com/Quantalabs/DiffPriv'
+"""Source Repo"""
+__docs__ = 'https://diffpriv.readthedocs.io'
+"""Documentation URL"""
+
+def _sanity_check():
+    # Import Requirements
+    try:
+        import numpy as np
+    except ImportError: # pragma: no cover
+        raise ImportError(' \
+            Please make sure you have numpy installed. If you have cloned the package from the source, then use: \
+                pip install -r requirements.txt \
+        ')
+    
+    url = f'https://pypi.python.org/pypi/DiffPriv/json'
+
+    if url.lower().startswith('http'):
+        releases = json.loads(request.urlopen(url).read()) # skipcq
+    else: # pragma: no cover
+        raise ValueError from None
+
+    latest_version = releases['info']['version']
+    
+
+    try: # pragma: no cover
+        if __version__ != 'v'+latest_version:
+            raise AssertionError
+    except AssertionError:  # pragma: no cover
+        # We ignore code coverage for this because there is no way to test this through pytest, but it has been tested manually
+        warnings.warn(
+            '\u001b[33m \n'
+            'You have DiffPriv '+__version__+', however, newer versions are avaliable. Use \n'
+            '   pip install --upgrade DiffPriv \n'
+            'to upgrade your package. \u001b[0m'    
+        )
+
+_sanity_check()
+
+del _sanity_check
---
+++ packages/DiffPriv/DiffPriv-2.0.0a3.tar.gz/DiffPriv-2.0.0a3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DiffPriv",
+    version="v2.0.0a3",
+    author="Quantalabs",
+    description="A Differential Privacy Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Quantalabs/DiffPriv",
+    packages=setuptools.find_packages(),
+    entry_points={"console_scripts": ["diffpriv=DiffPriv.cli:run"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=' >= 3.8.1',
+)
