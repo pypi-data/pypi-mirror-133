552a2c78dd281e6b0455302ccde725b3db9140e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:07 +0000

    Add package streamlit-gov-uk-components/0.0.1/streamlit-gov-uk-components-0.0.1.tar.gz

---
+++ packages/streamlit-gov-uk-components/streamlit-gov-uk-components-0.0.1.tar.gz/streamlit-gov-uk-components-0.0.1/streamlit_gov_uk_components_checkbox/frontend/build/precache-manifest.c73d355b0e1400fb9337c2f430a926d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cab09e98d17dd59727b69bafecd8d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7898b3d1faa95a25b90a",
+    "url": "./static/js/2.5a41f5fd.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.5a41f5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9563aa59e38b47c90d80",
+    "url": "./static/js/main.34995fac.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
