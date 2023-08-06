4cc706a8c28534daa6f1729c6254adeb886747fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:39 +0000

    Add package selenium-essentials/1.0.1/selenium-essentials-1.0.1.tar.gz

---
+++ packages/selenium-essentials/selenium-essentials-1.0.1.tar.gz/selenium-essentials-1.0.1/selenium-essentials/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="selenium-essentials",
+    version="1.0.0",
+    description="Speed up selenium development",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bloemetjesgordijn/selenium-essentials",
+    author="Bloemetjesgordijn",
+    author_email="casbertrams@live.nl",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["selenium-essentials"],
+    include_package_data=True,
+    install_requires=["random", "time", "selenium"]
+)
