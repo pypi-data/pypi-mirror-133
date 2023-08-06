4795903f4fd0d32806f0b5fe06165b97ff3f4573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:59 +0000

    Add package docstats/0.1.1/docstats-0.1.1-py3-none-any.whl

---
+++ packages/docstats/docstats-0.1.1-py3-none-any.whl/docstats/directives/call_graph.py
@@ -0,0 +1,63 @@
+"""Generate Callgraphs for documentation."""
+
+import os
+import subprocess
+from typing import List
+
+from docutils.nodes import Node
+from docutils.parsers.rst import Directive
+from sphinx.ext.graphviz import graphviz
+from sphinx.util.typing import OptionSpec
+
+callgraph_count = 0
+
+
+class CallGraphException(Exception):
+    """Exception for callgraph class."""
+
+
+class call_graph(graphviz):
+    """A docutils node to use as a placeholder for the call graph."""
+
+
+class CallGraph(Directive):
+    """Generate a callgraph."""
+
+    has_content = False
+    required_arguments = 0
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec: OptionSpec = {}
+
+    def run(self) -> List[Node]:
+        """Run the directive."""
+        global callgraph_count
+
+        if not os.path.exists("source/graphs"):
+            os.makedirs("source/graphs")
+
+        subprocess.run(
+            [
+                "code2flow",
+                "../",
+                "-o",
+                f"source/graphs/callgraph_{callgraph_count}.dot",
+                "--language",
+                "py",
+                "--quite",
+            ]
+        )
+
+        with open(
+            f"source/graphs/callgraph_{callgraph_count}.dot", encoding="utf-8"
+        ) as fp:
+            dotcode = fp.read()
+
+        callgraph_count += 1
+
+        node = graphviz()
+
+        node["code"] = dotcode
+        node["options"] = {}
+
+        return [node]
