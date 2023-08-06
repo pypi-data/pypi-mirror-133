544a05c2c978c9c82c4716c2ec1dd4812a5da1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:08 +0000

    Add package superset-pdf-report/0.6.7.1/superset_pdf_report-0.6.7.1-py3-none-any.whl

---
+++ packages/superset-pdf-report/superset_pdf_report-0.6.7.1-py3-none-any.whl/supersetpdfreport/task.py
@@ -0,0 +1,25 @@
+from superset.extensions import celery_app
+from celery.utils.log import get_task_logger
+from supersetpdfreport.pdf_report import PDF_report
+from typing import Any
+import logging
+import glob
+
+logger = get_task_logger(__name__)
+logger.setLevel(logging.INFO)
+
+
+@celery_app.task(name="reports-pdf-scheduler")
+def reports_pdf_scheduler() -> None:
+
+    logger.info("superset_pdf_report started")
+    joblist = glob.glob('*.json')
+
+    for job_name in joblist:
+        pdf_report_task = PDF_report()
+        pdf_report_task.execute(job_name)
+    
+    
+    logger.info("superset_pdf_report finished")
+
+reports_pdf_scheduler()
