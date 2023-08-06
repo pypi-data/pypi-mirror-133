7252cb1d9d5857206e837b8cccc8d3747901a04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:31 +0000

    Add package telegram-bell/0.7.0/telegram-bell-0.7.0.tar.gz

---
+++ packages/telegram-bell/telegram-bell-0.7.0.tar.gz/telegram-bell-0.7.0/README.md
@@ -0,0 +1,59 @@
+# telegram-bell
+
+![PyPI](https://img.shields.io/pypi/v/telegram-bell)
+
+Notify you when something is mentioned in a telegram channel.
+
+## Install
+
+    pip install telegram-bell
+
+## Usage
+
+### CLI
+
+#### Run
+
+    tbell run
+
+Before you can use, it will ask you for:
+
+- your Telegram API ID
+- your Telegram API hash
+- channels and keywords which you want to get notified
+- Telegram token (2FA)
+
+The app will then forward the matching messages to your "Saved Messages" channel in Telegram over time.
+
+#### Config
+
+    tbell config
+
+You can reconfigure the application at any time with this command.
+
+#### Show susbscribed channels
+
+    tbell show
+
+### Systemd user service
+
+Clone the repo and:
+
+    cd telegram-bell/scripts
+    sh install_service.sh
+    sh start_service.sh # it will ask you for config
+
+Check the service is running:
+
+    sh check_service.sh
+
+You can check the services logs too:
+
+    sh show_service_logs.sh
+
+If the service fails or the machine is restarted, the service will run transparently again.
+
+If you want to change your config in some moment:
+
+    tbell config
+    sh restart_service.sh
---
+++ packages/telegram-bell/telegram-bell-0.7.0.tar.gz/telegram-bell-0.7.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['telegram_bell']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['12factor-configclasses>=1.0.0,<2.0.0',
+ 'anyio>=3.3.4,<4.0.0',
+ 'asyncclick>=8.0.1,<9.0.0',
+ 'python-dotenv>=0.19.1,<0.20.0',
+ 'rich>=10.12.0,<11.0.0',
+ 'telethon>=1.23,<2.0']
+
+entry_points = \
+{'console_scripts': ['tbell = telegram_bell.bell:cli']}
+
+setup_kwargs = {
+    'name': 'telegram-bell',
+    'version': '0.7.0',
+    'description': 'Notify you when something is mentioned in a telegram channel',
+    'long_description': '# telegram-bell\n\n![PyPI](https://img.shields.io/pypi/v/telegram-bell)\n\nNotify you when something is mentioned in a telegram channel.\n\n## Install\n\n    pip install telegram-bell\n\n## Usage\n\n### CLI\n\n#### Run\n\n    tbell run\n\nBefore you can use, it will ask you for:\n\n- your Telegram API ID\n- your Telegram API hash\n- channels and keywords which you want to get notified\n- Telegram token (2FA)\n\nThe app will then forward the matching messages to your "Saved Messages" channel in Telegram over time.\n\n#### Config\n\n    tbell config\n\nYou can reconfigure the application at any time with this command.\n\n#### Show susbscribed channels\n\n    tbell show\n\n### Systemd user service\n\nClone the repo and:\n\n    cd telegram-bell/scripts\n    sh install_service.sh\n    sh start_service.sh # it will ask you for config\n\nCheck the service is running:\n\n    sh check_service.sh\n\nYou can check the services logs too:\n\n    sh show_service_logs.sh\n\nIf the service fails or the machine is restarted, the service will run transparently again.\n\nIf you want to change your config in some moment:\n\n    tbell config\n    sh restart_service.sh',
+    'author': 'Pablo Cabezas',
+    'author_email': 'headsrooms@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
