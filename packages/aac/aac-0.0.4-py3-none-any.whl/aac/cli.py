74a66a6753c1f728d3476391ec6c79f9a1a96907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package aac/0.0.4/aac-0.0.4-py3-none-any.whl

---
+++ packages/aac/aac-0.0.4-py3-none-any.whl/aac/cli.py
@@ -0,0 +1,96 @@
+"""The command line processor for aac."""
+import argparse
+import inspect
+import itertools
+import sys
+from typing import Callable
+
+from pluggy import PluginManager
+
+from aac import parser, plugins
+from aac.AacCommand import AacCommand, AacCommandArgument
+from aac.spec.core import get_aac_spec_as_yaml
+from aac.validator import validation
+
+
+def run_cli():
+    """
+    The main entry point for aac.
+
+    This method parses the command line and performs the
+    requested user command...or outputs usage.
+    """
+
+    plugin_manager = plugins.get_plugin_manager()
+
+    arg_parser, aac_plugin_commands = _setup_arg_parser(plugin_manager)
+
+    args = arg_parser.parse_args()
+
+    for command in aac_plugin_commands:
+        if args.command == command.name:
+
+            keyword_args = {}
+            args_dict = vars(args)
+
+            # Leverage inspect here to make sure that the arg names we're trying to access are sourced from the target function itself
+            parameters = inspect.signature(command.callback).parameters.keys()
+            for argument in parameters:
+                keyword_args[argument] = args_dict[argument]
+
+            command.callback(**keyword_args)
+
+
+def _validate_cmd(model_file: str):
+    """The built-in validate command."""
+    with validation(parser.parse_file, model_file) as model:
+        if not model:
+            sys.exit(1)
+
+        print(f"{model_file} is valid.")
+
+
+def _core_spec_cmd():
+    """The built-in aac-core-spec command."""
+    print(get_aac_spec_as_yaml())
+
+
+def _setup_arg_parser(
+    plugin_manager: PluginManager,
+) -> tuple[argparse.ArgumentParser, list[Callable]]:
+    arg_parser = argparse.ArgumentParser()
+    command_parser = arg_parser.add_subparsers(dest="command")
+
+    # Built-in commands
+    aac_commands = [
+        # Temporarily disabled.
+        # AacCommand(
+        #     "ui",
+        #     "Run the AaC Graphical User Interface",
+        #     ui.run_ui,
+        # ),
+        AacCommand(
+            "validate",
+            "Ensures the AaC yaml is valid per the AaC core spec",
+            _validate_cmd,
+            [AacCommandArgument("model_file", "The path to the AaC model yaml file to validate")],
+        ),
+        AacCommand(
+            "aac-core-spec",
+            "Prints the AaC model describing core AaC data types and enumerations",
+            _core_spec_cmd,
+        ),
+    ]
+
+    results = plugin_manager.hook.get_commands()
+    aac_and_plugin_commands = aac_commands + list(itertools.chain(*results))
+
+    for command in aac_and_plugin_commands:
+        command_subparser = command_parser.add_parser(command.name, help=command.description)
+
+        for argument in command.arguments:
+            command_subparser.add_argument(
+                argument.name, help=argument.description, nargs=argument.number_of_arguments
+            )
+
+    return arg_parser, aac_and_plugin_commands
---
+++ packages/aac/aac-0.0.4-py3-none-any.whl/aac/parser.py
@@ -0,0 +1,96 @@
+"""Parse Architecture-as-Code YAML files.
+
+The AaC parser reads a yaml file, performs validation (if not suppressed) and provides
+the caller with a dictionary of the content keyed by the named type.  This allows you
+to find a certain type in a model by just looking for that key.
+"""
+import os
+
+import yaml
+
+
+def parse_file(arch_file: str) -> dict[str, dict]:
+    """Parse an Architecture-as-Code YAML file.
+
+    Args:
+        arch_file (str): The Architecture-as-Code YAML file to be parsed.
+
+    Returns:
+        The parse method returns a dict of each root type defined in the Arch-as-Code spec. If
+        validation of the provided YAML file fails, an error message is displayed and None is
+        returned.
+    """
+    parsed_models: dict[str, dict] = {}
+
+    files = _get_files_to_process(arch_file)
+    for file in files:
+        contents = _read_file_content(file)
+        parsed_models = parsed_models | parse_str(arch_file, contents)
+    return parsed_models
+
+
+def parse_str(source: str, model_content: str) -> dict[str, dict]:
+    """Parse a string containing one or more yaml model definitions.
+
+    Args:
+        source:  The file the content came from (to help with better logging)
+        model_content:  The yaml to parse
+
+    Returns:
+        A dictionary of the parsed model(s).  The key is the type name from the model and the
+        value is the parsed model root.
+    """
+    parsed_models = {}
+
+    roots = yaml.load_all(model_content, Loader=yaml.FullLoader)
+    for root in roots:
+        if "import" in root:
+            del root["import"]
+        root_name = list(root.keys())[0]
+        parsed_models[root[root_name]["name"]] = root
+    return parsed_models
+
+
+def _read_file_content(arch_file: str) -> str:
+    """
+    The read file content method extracts text content from the specified file.
+
+    Args:
+        arch_file: The file to read.
+
+    Returns:
+        The contents of the file as a string.
+    """
+
+    arch_file_path = arch_file
+    content = ""
+    with open(arch_file_path, "r") as file:
+        content = file.read()
+    return content
+
+
+def _get_files_to_process(arch_file_path: str) -> list[str]:
+    """Return a list of all files referenced in the model.
+
+    Traverse the import path starting from the specified Arch-as-Code file and returns a list of
+    all files referenced by the model.
+    """
+
+    ret_val = [arch_file_path]
+    content = _read_file_content(arch_file_path)
+    roots = yaml.load_all(content, Loader=yaml.FullLoader)
+    for root in roots:
+        if root and "import" in root.keys():
+            for imp in root["import"]:
+                # parse the imported files
+                parse_path = ""
+                if imp.startswith("."):
+                    # handle relative path
+                    arch_file_dir = os.path.dirname(os.path.realpath(arch_file_path))
+                    parse_path = os.path.join(arch_file_dir, imp)
+                else:
+                    parse_path = imp
+                for append_me in _get_files_to_process(parse_path):
+                    ret_val.append(append_me)
+
+    return ret_val
