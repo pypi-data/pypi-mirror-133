928fd4e4f2b355f1db1770f382ebade1f1641b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:17 +0000

    Add package integrityguard/0.4.2/integrityguard-0.4.2.tar.gz

---
+++ packages/integrityguard/integrityguard-0.4.2.tar.gz/integrityguard-0.4.2/HISTORY.rst
@@ -0,0 +1,61 @@
+=======
+History
+=======
+
+0.4.2 (2022-01-05)
+------------------
+
+* Cleaned up variable checks #21
+
+0.4.1 (2022-01-05)
+------------------
+
+* Fixed CLI issues with Click requirement and preload of config #20
+
+0.4.0 (2022-01-02)
+------------------
+
+* Added copy config option to CLI #13
+* Adjusted hash report JSON indentation #14
+* Added CLI feature to define the conf file path #15
+* Added --save-to CLI option #16
+* Updated README and HISTORY files #17
+
+0.3.2 (2022-01-01)
+------------------
+
+* Fixed issue of hash report not being created #12
+
+0.3.1 (2022-01-01)
+------------------
+
+* Fixed copy config to user issue #11
+
+0.3.0 (2022-01-01)
+------------------
+
+* Replaced setuptools data_files feature with copy_config() method #7
+* Added new feature to CLI to show paths for the system #8
+* Adjusted versioning to 0.3.0 #10
+
+0.2.1 (2021-12-29)
+------------------
+
+* Fix version and release history
+
+0.2.0 (2021-12-29)
+------------------
+
+* Added support for conf file and multiplataform paths #3
+* Adjusted project basic directory structure #4
+* Added CLI support #5
+
+0.1.1 (2021-12-28)
+------------------
+
+* Adjusted README file.
+
+0.1.0 (2021-12-28)
+------------------
+
+* Alpha release on PyPI.
---
+++ packages/integrityguard/integrityguard-0.4.2.tar.gz/integrityguard-0.4.2/integrityguard/cli.py
@@ -0,0 +1,59 @@
+import sys
+import click
+from integrityguard.helpers.loadconfig import load_config
+from integrityguard.helpers.showpaths import show_paths
+from integrityguard.helpers.copyconfig import copy_config
+from integrityguard.hashreport import hash_report
+from integrityguard.monitor import monitor
+
+@click.command()
+
+# Required options
+@click.option('--task', default="monitor", help='Tasks available: monitor, generate_hashes')
+
+# Optional or conditional options
+@click.option('--target', help='Target path to monitor')
+@click.option('--config', help='Full path to .conf file')
+@click.option('--hash', help='Hash algorithm type (MD5, SHA1, SHA224, SHA256, SHA384, and SHA512).')
+
+# Options to use with copy_config
+@click.option('--destination', help='Used for copy_config task. Full destination path of the .conf file.')
+
+# Options to use with generate_hashes
+@click.option('--save-to', help='Destination folder of the hashes.json report.')
+
+def main(task,target,config,hash,destination,save_to):
+
+    """Console script for IntegrityGuard."""
+
+    if task == "generate_hashes":
+
+        if save_to and save_to.endswith('.json') == False:
+            print("Error: --save-to must be a full path including the file name. (e.g. hashes.json)")
+            exit()
+
+        hash_report(config,target,save_to)
+
+    elif task == "monitor":
+        monitor(config,target)
+
+    elif task == "copy_config":
+        # Handle possible errors
+        if not destination:
+            print("Error: --destination is required when using copy_config task.")
+            exit()
+
+        if destination and destination.endswith('.conf') == False:
+            print("Error: --destination must be a full path including the file name. (e.g. <any-name>.conf)")
+            exit()
+
+        # Copy config file
+        copy_config(destination)
+
+    elif task == "show_paths":
+        show_paths(config)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
