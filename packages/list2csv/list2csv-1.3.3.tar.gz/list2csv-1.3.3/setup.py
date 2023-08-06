f5f3f6fb2902a685e59e2e70a083d3e4edaa110a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package list2csv/1.3.3/list2csv-1.3.3.tar.gz

---
+++ packages/list2csv/list2csv-1.3.3.tar.gz/list2csv-1.3.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='list2csv',
+    version='1.3.3',
+    author='James Finnie-Ansley',
+    description='A simple package intended to help write iterables '
+                'of objects to CSV files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/James-Ansley/list2csv',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(
+        where='src',
+    ),
+    package_data={'': ['*.pyi']},
+    python_requires='>=3.6',
+    install_requires=[],
+)
---
+++ packages/list2csv/list2csv-1.3.3.tar.gz/list2csv-1.3.3/src/list2csv/__init__.py
@@ -0,0 +1,1 @@
+from ._list2csv import Writer
