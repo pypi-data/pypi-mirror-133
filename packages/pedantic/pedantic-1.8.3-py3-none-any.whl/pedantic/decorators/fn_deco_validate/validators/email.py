dc6e8528aa5cda6104990f8ed527d5ae71bc80a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:39 +0000

    Add package pedantic/1.8.3/pedantic-1.8.3-py3-none-any.whl

---
+++ packages/pedantic/pedantic-1.8.3-py3-none-any.whl/pedantic/decorators/fn_deco_validate/validators/email.py
@@ -0,0 +1,18 @@
+import re
+
+from pedantic import overrides
+from pedantic.decorators.fn_deco_validate.validators import Validator
+
+REGEX_EMAIL = r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$"
+
+
+class Email(Validator):
+    def __init__(self, email_pattern: str = REGEX_EMAIL) -> None:
+        self._pattern = email_pattern
+
+    @overrides(Validator)
+    def validate(self, value: str) -> str:
+        if not re.fullmatch(pattern=self._pattern, string=value):
+            self.raise_exception(msg=f'invalid email address: {value}', value=value)
+
+        return value
