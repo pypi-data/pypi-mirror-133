f056af4d4c394ae95703632d0c4e0cc22019334a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:08 +0000

    Add package serialhub/0.0.35/serialhub-0.0.35.tar.gz

---
+++ packages/serialhub/serialhub-0.0.35.tar.gz/serialhub-0.0.35/serialhub/_version.py
@@ -0,0 +1,25 @@
+"""Compute module version by parsing package.json settings."""
+
+import json
+from pathlib import Path
+
+__all__ = ["__version__"]
+
+def _fetch_version():
+    here = Path(__file__).parent.resolve()
+
+    for settings in here.rglob("package.json"):
+        try:
+            with settings.open() as f_settings:
+                version = json.load(f_settings)["version"]
+                return (
+                    version.replace("-alpha.", "a")
+                    .replace("-beta.", "b")
+                    .replace("-rc.", "rc")
+                )
+        except FileNotFoundError:
+            pass
+
+    raise FileNotFoundError(f"Could not find package.json under dir {here!s}")
+
+__version__ = _fetch_version()
