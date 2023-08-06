096d0cec8866799a2138378a2d5e05d80664cdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package DiscordUtilsv2/1.3.5/DiscordUtilsv2-1.3.5.tar.gz

---
+++ packages/DiscordUtilsv2/DiscordUtilsv2-1.3.5.tar.gz/DiscordUtilsv2-1.3.5/DiscordUtilsv2/__init__.py
@@ -0,0 +1,8 @@
+from DiscordUtils import Pagination
+from DiscordUtils.InviteTracker import InviteTracker
+from DiscordUtils.Music import Music
+
+__title__ = "DiscordUtilsv2"
+__version__ = "1.3.5"
+__author__ = "toxicrecker & EpikHost"
+__license__ = "MIT"
