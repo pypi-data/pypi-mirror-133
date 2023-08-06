4d5ace9715d403f7db8a5270907fc7dacab71a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:12 +0000

    Add package python-netlink/0.0.2/python-netlink-0.0.2.tar.gz

---
+++ packages/python-netlink/python-netlink-0.0.2.tar.gz/python-netlink-0.0.2/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+description = \
+	""
+
+long_description = description
+
+setuptools.setup(
+	name = "python-netlink",
+	version = "0.0.2",
+	description = description,
+	long_description = long_description,
+	author = "Yannik Marchand",
+	author_email = "ymarchand@me.com",
+	url = "https://github.com/kinnay/python-netlink",
+	license = "GPLv3",
+	platforms = ["Linux"],
+	#packages = ["netlink"]
+)
