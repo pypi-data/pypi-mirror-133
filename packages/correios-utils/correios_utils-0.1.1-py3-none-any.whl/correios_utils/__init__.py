789b8b856024baab43fd8c0139983e531dc31b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:25 +0000

    Add package correios-utils/0.1.1/correios_utils-0.1.1-py3-none-any.whl

---
+++ packages/correios-utils/correios_utils-0.1.1-py3-none-any.whl/correios_utils/__init__.py
@@ -0,0 +1,17 @@
+from .correios import (
+    Cotacao,
+    FormatoEncomenda,
+    SimNao,
+    Servico,
+    realizar_cotacao,
+)
+
+__all__ = [
+    "Cotacao",
+    "Servico",
+    "FormatoEncomenda",
+    "SimNao",
+    "realizar_cotacao",
+]
+
+__version__ = "0.1.1"
