06771aa5724d20851fb1acc6493e7617998f0b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:58 +0000

    Add package oep-client/0.11.0/oep-client-0.11.0.tar.gz

---
+++ packages/oep-client/oep-client-0.11.0.tar.gz/oep-client-0.11.0/oep_client/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+__version__ = "0.11.0"
+
+
+from .oep_client import OepClient
+from .advanced_api import AdvancedApiSession
+from .oep_client import (
+    DEFAULT_HOST,
+    DEFAULT_PROTOCOL,
+    DEFAULT_API_VERSION,
+    DEFAULT_SCHEMA,
+    DEFAULT_INSERT_RETRIES,
+    DEFAULT_BATCH_SIZE,
+)
+from .exceptions import (
+    OepApiException,
+    OepServerSideException,
+    OepClientSideException,
+    OepAuthenticationException,
+    OepTableNotFoundException,
+    OepTableAlreadyExistsException,
+)
+from .dialect import get_sqlalchemy_table
