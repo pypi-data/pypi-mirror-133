1deb9b1dbd3cbea5d6b9063dc86ae42b1f9af54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:05 +0000

    Add package vip-hci/1.0.3/vip_hci-1.0.3-py2.py3-none-any.whl

---
+++ packages/vip-hci/vip_hci-1.0.3-py2.py3-none-any.whl/vip_hci/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "1.0.3"
+
+from . import andromeda
+from . import preproc
+from . import conf
+from . import fits
+from . import frdiff
+from . import leastsq
+from . import llsg
+from . import medsub
+from . import negfc
+from . import nmf
+from . import pca
+from . import metrics
+from . import specfit
+from . import stats
+from . import var
+from .hci_dataset import *
+from .hci_postproc import *
+from .vip_ds9 import *
