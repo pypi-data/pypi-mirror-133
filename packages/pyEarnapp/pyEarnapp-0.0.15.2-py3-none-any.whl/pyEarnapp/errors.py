4b829e9bb73b28ed1d9fccd0b5d34a0763dc8906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package pyEarnapp/0.0.15.2/pyEarnapp-0.0.15.2-py3-none-any.whl

---
+++ packages/pyEarnapp/pyEarnapp-0.0.15.2-py3-none-any.whl/pyEarnapp/errors.py
@@ -0,0 +1,47 @@
+class AuthenticationError(Exception):
+    def __init__(self) -> None:
+        message = "Error authenticating. Enter a proper oauth-refresh-token"
+        super().__init__(message)
+
+class DeviceAddError(Exception):
+    pass
+
+class DeviceNotFoundError(DeviceAddError):
+    def __init__(self, error_message) -> None:
+        super().__init__(error_message)
+
+class DeviceAlreadyAddedError(DeviceAddError):
+    def __init__(self, error_message) -> None:
+        super().__init__(error_message)
+
+class UnKnownDeviceAddError(DeviceAddError):
+    def __init__(self, error_message) -> None:
+        super().__init__(error_message)
+
+class TooManyRequestsError(DeviceAddError):
+    def __init__(self) -> None:
+        super().__init__("EarnApp doesn't allow you to add many devices at time. Try adding delay in sending requests")
+
+class IPCheckError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class UnKnownIPCheckError(IPCheckError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class InValidIPAddressError(IPCheckError):
+    def __init__(self, *args: object) -> None:
+        super().__init__("The IP Address is not valid.", *args)
+
+class RedeemError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class UnKnownRedeemError(RedeemError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class MinimumRedeemBalanceError(RedeemError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
