a1d3eda68ad0403aa5fc615761b0b0005167f903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:12 +0000

    Add package pop/21.0.0/pop-21.0.0-py3-none-any.whl

---
+++ packages/pop/pop-21.0.0-py3-none-any.whl/pop/mods/pop/conf.py
@@ -0,0 +1,30 @@
+"""
+Convenience wrappers to make using the conf system as easy and seamless as possible
+"""
+import warnings
+from typing import Any
+from typing import Dict
+from typing import List
+
+
+def __virtual__(hub):
+    hub.pop.sub.add("pop_conf.conf")
+    return hasattr(hub.conf, "integrate"), "pop-conf is not installed"
+
+
+def integrate(
+    hub: "pop.hub.Hub",
+    imports: List[str] or str,
+    override: Dict[str, Any] = None,
+    cli: str = None,
+    roots: bool = None,
+    loader: str = "json",
+    logs: bool = True,
+):
+    """
+    Load the conf sub and run the integrate sequence.
+    """
+    warnings.warn("Use `hub.pop.config.load()` instead", DeprecationWarning)
+    hub.conf.integrate.load(
+        imports, override, cli=cli, roots=roots, loader=loader, logs=logs
+    )
