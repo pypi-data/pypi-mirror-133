df0e57dd83e002709ea22e54b2f3bb630585a3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:55 +0000

    Add package nembis/0.0.0/nembis-0.0.0-cp38-cp38-manylinux_2_24_x86_64.whl

---
+++ packages/nembis/nembis-0.0.0-cp38-cp38-manylinux_2_24_x86_64.whl/nembis/cmd/nembis.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2022 DeqiTang
+# Distributed under the terms of the MIT license
+# @author: DeqiTang
+# email: deqitang@gmail.com 
+
+
+import os
+import sys
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="driver", title="subcommands", description="choose one and only one subcommands")
+    # --------------------------------------------------------------------------
+    #  Test
+    # --------------------------------------------------------------------------
+    subparser = subparsers.add_parser("test", help="running test")
+
+    #subparser.add_argument("")
+
+
+
+    # ==========================================================
+    # transfer parameters from the praser
+    # ==========================================================
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        # display help message when no args provided
+        parser.print_help()
+        sys.exit(1)        
+    
+
+    if args.driver == "test":
+        from nembis.cpp.utils import version
+        print("testing...")
+        print("the version is:", version())
+
+
+if __name__ == "__main__":
+    main()
+
+
