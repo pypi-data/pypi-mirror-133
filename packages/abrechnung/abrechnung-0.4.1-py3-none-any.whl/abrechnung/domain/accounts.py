801060d72af440e2cf8562252636203b5a98a93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:05 +0000

    Add package abrechnung/0.4.1/abrechnung-0.4.1-py3-none-any.whl

---
+++ packages/abrechnung/abrechnung-0.4.1-py3-none-any.whl/abrechnung/domain/accounts.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class AccountType(Enum):
+    personal = "personal"
+
+
+@dataclass
+class Account:
+    id: int
+    group_id: int
+    type: str
+    name: str
+    description: str
+    priority: int
+    # created_by: int
+    deleted: bool
