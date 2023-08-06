cd2a35a931d430d4d5d9c1cb39ae028075b1e693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:54 +0000

    Add package ingradient-lib-temp/0.6.260/ingradient_lib_temp-0.6.260.tar.gz

---
+++ packages/ingradient-lib-temp/ingradient_lib_temp-0.6.260.tar.gz/ingradient_lib_temp-0.6.260/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name                = 'ingradient_lib_temp',
+    version             = '0.6.260',
+    description         = 'Medical Deep Learning Framework.',
+    author              = 'seungyeob.seon',
+    author_email        = 'liamseon@gmail.com',
+    url                 = 'https://github.com/InGradient/InGradient_AI_Library',
+    license             = 'MIT',
+    install_requires    =  ['SimpleITK', 'revlib', 'connected-components-3d', 'nibabel'],
+    py_modules          = ['data_augmentation', 'dataloads', 'deep_supervision_loss', 'get_imbalance_weight'
+                          ,'inference', 'patch_transform', 'preprocessing', 'model', 'visualization', 'sampling'
+                          ,'preprocessing', 'trainer', 'transform', 'data_organizer', 'get_nnunet_setting', 'optimizer', 'active_contour_loss'
+                          ,'medical_decathlon_organizer', 'nnunet_3D_run', 'maic', 'unet', 'lr_scheduler', 'loss'],
+    keywords            = ['pypi deploy'],
+    packages            = ['ingradient_library'],
+    zip_safe            = False
+)
