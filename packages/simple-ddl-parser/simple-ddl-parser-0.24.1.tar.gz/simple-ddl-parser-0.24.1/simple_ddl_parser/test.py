faa7072693d072b9a6f076a63e4ee93a13a29a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:46 +0000

    Add package simple-ddl-parser/0.24.1/simple-ddl-parser-0.24.1.tar.gz

---
+++ packages/simple-ddl-parser/simple-ddl-parser-0.24.1.tar.gz/simple-ddl-parser-0.24.1/simple_ddl_parser/test.py
@@ -0,0 +1,35 @@
+from simple_ddl_parser import DDLParser
+
+ddl =  """
+    CREATE EXTERNAL TABLE IF NOT EXISTS database.table_name
+    (
+        day_long_nm     string,
+        calendar_dt     date,
+        source_batch_id string,
+        field_qty       decimal(10, 0),
+        field_bool      boolean,
+        field_float     float,
+        create_tmst     timestamp,
+        field_double    double,
+        field_long      bigint
+    ) PARTITIONED BY (batch_id int);
+
+    CREATE TABLE IF NOT EXISTS database.table_name2
+    (
+        day_long_nm     string,
+        calendar_dt     date,
+        source_batch_id string,
+        field_qty       decimal(10, 0),
+        field_bool      boolean,
+        field_float     float,
+        create_tmst     timestamp,
+        field_double    double,
+        field_long      bigint
+    ) PARTITIONED BY (batch_id int)
+
+    """
+result = DDLParser(ddl).run(group_by_type=True, output_mode="hql")
+
+import pprint
+
+pprint.pprint(result)
