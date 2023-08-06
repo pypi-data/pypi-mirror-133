d0c7b5c998188898763050e20b65536221890375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:35 +0000

    Add package aliby-agora/0.1.1/aliby-agora-0.1.1.tar.gz

---
+++ packages/aliby-agora/aliby-agora-0.1.1.tar.gz/aliby-agora-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['agora']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0']
+
+setup_kwargs = {
+    'name': 'aliby-agora',
+    'version': '0.1.1',
+    'description': 'A gathering of shared utilities for the Swain Lab image processing pipeline.',
+    'long_description': None,
+    'author': 'Alán Muñoz',
+    'author_email': 'amuoz@ed.ac.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
