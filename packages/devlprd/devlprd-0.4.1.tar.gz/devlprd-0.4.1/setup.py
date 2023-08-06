e959619ef328b0e40a7122394748df9dade5b0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:36 +0000

    Add package devlprd/0.4.1/devlprd-0.4.1.tar.gz

---
+++ packages/devlprd/devlprd-0.4.1.tar.gz/devlprd-0.4.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['devlprd']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mypy>=0.910,<0.911',
+ 'numpy>=1.21.2,<2.0.0',
+ 'pydevlpr-protocol>=0.1.0,<0.2.0',
+ 'pyserial>=3.5,<4.0']
+
+setup_kwargs = {
+    'name': 'devlprd',
+    'version': '0.4.1',
+    'description': 'Daemon for managing integrations with the FATNM DEVLPR',
+    'long_description': "# devlprd\n[![Python application](https://github.com/FANTM/devlprd/actions/workflows/python-app.yml/badge.svg)](https://github.com/FANTM/devlprd/actions/workflows/python-app.yml)\n\n## Overview\nThe FANTM DEVLPR daemon for managing and publishing data streams. It serves as a middleware between the hardware itself, and whatever integration you want to make. \nIt exists to both abstract away data processing from the end application, and to allow for language agnostic integrations. Right now there is only a [python endpoint](https://github.com/FANTM/pydevlpr), but there are plans for JS and more in the near future. \nIn effect, first an Arduino is flashed with the provided firmware, then the daemon is launched, and then the application that you're using the data with. \n\n## Getting Started\nThe first thing that needs to be done is formatting the Arduino. In the `arduino_firmware` folder there are scripts that format the data in such a way that the daemon can package, process, and send it out easily. \nSo [flash an arduino](https://www.arduino.cc/en/Guide) with one of those scripts and make sure it is connected to the computer you will be running the daemon from.\n\nThis project uses [Poetry](https://python-poetry.org/) for dependency management, and while it's not required to get started it's a useful tool. All of the dependencies are in `pyproject.toml`,\nso just run `poetry install`. Then just run `poetry run python src/main.py` and the daemon should start.\n\n## Development\n### Adding a data transformer\nTo add support for a new data transformer, edit the `self.callbacks: Dict[str, Callable[[int], Union[int, bool]]]` dictionary in the `__init__` function of DaemonState (in `DaemonState.py`).\nYou will need to add a new entry with a data topic, using one of the supported topics defined in `protocol.py` and a function. \nThe function is a callback that takes a single int as an arguement representing the pin the data is coming from on the Arduino.\nThe function should be inside of DaemonState, but it can be wrapper for a function living in another file (`filtering.py` is usually a good choice). \n\nNow whenever a client subscribes to your topic, the callback will be called and you'll see your data stream.\n\n### Adding a new data topic\nAdd a new line to the DataTopic class in `protocol.py` with an ALL_CAPS_NAME and a unique two letter identifier. \nThen go to the [`protocol.py`](https://github.com/FANTM/pydevlpr/blob/main/src/pydevlpr/protocol.py) in the `pydevlpr` repository and add it there as well so others can use the topic. \n\n### Adding support for different types of messages\nAdd a new line in the PacketType class in `protocol.py` with an ALL_CAPS_NAME and a unique one letter identifier. \nThen go to `daemon.py` and add an `elif command == PacketType.ALL_CAPS_NAME` with the handler for your custom packet type to the `receive(...)` function.\n",
+    'author': 'Ezra Boley',
+    'author_email': 'eboley@wisc.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/FANTM/devlprd',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<3.11',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/devlprd/devlprd-0.4.1.tar.gz/devlprd-0.4.1/src/devlprd/daemon.py
@@ -0,0 +1,124 @@
+import asyncio
+import multiprocessing as mp
+import logging
+from typing import Optional
+from pydevlpr_protocol import DataFormatException, unwrap_packet, PacketType, DaemonSocket
+
+from .serif import DevlprSerif
+from .DaemonState import DaemonState
+from .config import BOARDS, Board, ADDRESS
+
+server = None
+state: Optional[DaemonState] = None  # Make sure to pass this to any other threads that need access to shared state
+devlpr_serial: Optional[DevlprSerif] = None
+
+logging.basicConfig(level=logging.INFO, format=f'%(levelname)s:{__name__}:%(message)s')
+
+class DaemonController:
+    def __init__(self, board_id: str) -> None:
+        self.board_id = board_id
+
+    async def start(self, block=False):
+        self.p = mp.Process(target=main, args=(self.board_id,))
+        self.p.start()
+        if block:
+            try:
+                self.p.join()
+            except KeyboardInterrupt:
+                self.p.kill()
+
+    def stop(self):
+        if self.p is not None and self.p.is_alive():
+            self.p.terminate()
+
+def main(board_id: str):
+    try:
+        board = BOARDS[board_id]
+    except KeyError:
+        logging.warning("Invalid board ID, try get_board_ids() for options")
+        logging.info('Assuming DEVLPR')
+        board = BOARDS['DEVLPR']
+
+    asyncio.run(startup(board))
+
+async def client_accept(sock: DaemonSocket) -> None:
+    """Delegate and process incoming messages from a websocket connection."""
+
+    message = ' '
+    while len(message) > 0:
+        message = await sock.recv()
+        if len(message) > 0:
+            try:
+                command, data = unwrap_packet(message)
+            except DataFormatException:
+                continue  # Handle an unexpected issue with the packet
+            if command == PacketType.SUBSCRIBE:
+                logging.info("Subscribing to {}".format(data))
+                try:
+                    state.subscribe(sock, data)
+                except AttributeError:
+                    logging.error("Failed to subscribe, Daemon State is None")
+
+async def client_handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    """Main function for socket connections. Holds the connection until the other side disconnects."""
+    
+    logging.debug("Incoming Connection")
+    dsock = DaemonSocket(reader, writer)
+    logging.info("Connected to {0}".format(dsock.get_remote_address()))
+    try:
+        await client_accept(dsock)
+    except (BrokenPipeError, ConnectionError):
+        pass  # Happens if we disconnect non-gracefully, but it's generally fine I think
+    finally:
+        logging.info("Disconnected from {0}".format(dsock.get_remote_address()))
+        try:
+            state.unsubscribe_all(dsock)
+        except AttributeError:
+            logging.error("Failed to unsubscribe_all, Daemon State is None")
+
+async def startup(board: Board) -> None:
+    """Initiallizes both the serial connection and the socket server. It then just hangs until everything is done internally before cleaning up."""
+
+    global server
+    global state
+    global devlpr_serial
+    # we'll want the asyncio event loop for subscriptions, some processing, and publishing
+    event_loop = asyncio.get_running_loop()
+    # the DaemonState requests publishes upon state change, so needs to know the event loop
+    state = DaemonState(event_loop, board)
+    # we initialize our serial connection, which is managed on a separate thread
+    devlpr_serial = DevlprSerif(board)
+    devlpr_serial.init_serial(state)
+    # start a server, waiting for incoming subscribers to data (pydevlpr and other libraries)
+    server = await asyncio.start_server(client_handler, ADDRESS[0], ADDRESS[1])
+    try:
+        logging.debug("Started Listening")
+        await server.serve_forever()
+    except (asyncio.exceptions.CancelledError):
+        logging.debug("Closing Server")
+        await server.wait_closed()
+        
+    devlpr_serial.deinit_serial()
+
+def shutdown() -> None:
+    """Manually closes out the server. Most of the time you don't need to do this because it should close when you exit the program."""
+    
+    global server
+    global devlpr_serial
+    try:
+        if server is not None and server.is_serving():
+            server.close()
+            # asyncio.run_coroutine_threadsafe(server.wait_closed(), asyncio.get_event_loop())
+    except:
+        pass 
+    try:
+        devlpr_serial.deinit_serial()
+    except AttributeError:
+        logging.warning("Serial couldn't close because devlpr_serial is already None")
+    except:
+        pass # not even sure this is necessary
+
+# TODO this should all probably be part of an object rather than global state
+def _get_state():
+    global state
+    return state
