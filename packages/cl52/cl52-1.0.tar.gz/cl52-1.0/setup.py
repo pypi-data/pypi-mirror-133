2b4661278b65b63dd238f4b39faf781900b0216d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:16 +0000

    Add package cl52/1.0/cl52-1.0.tar.gz

---
+++ packages/cl52/cl52-1.0.tar.gz/cl52-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="cl52",
+      version="1.0",
+      description="This is made for educational purpose",
+      Long_description="There is email_bomber and sms_bomber for bd",
+      author="Al Jabir {Telegram: t.me/jabir52}",
+      author_email="smaliahsanaljabir@gmail.com",
+      packages=['cl52'])
