6be34fe957b797630f1afa40e74de5b9558b756f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:52 +0000

    Add package souswift-core/0.2.1/souswift_core-0.2.1-py3-none-any.whl

---
+++ packages/souswift-core/souswift_core-0.2.1-py3-none-any.whl/souswift_core/providers/database/context.py
@@ -0,0 +1,15 @@
+from context_handler import AsyncContext, context_factory
+from context_handler._datastructures import AbstractAsyncContextFactory  # noqa
+from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
+
+from .connection import ConnectionProvider
+from .session import SessionProvider
+
+session_factory: AbstractAsyncContextFactory[AsyncSession] = context_factory( # noqa
+    SessionProvider, AsyncContext
+)
+connection_factory: AbstractAsyncContextFactory[
+    AsyncConnection
+] = context_factory( # noqa
+    ConnectionProvider, AsyncContext
+)
