b6aede041ac5a532f8e30f273c4872c5813a2f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:50 +0000

    Add package aio-request/0.1.20/aio_request-0.1.20-py3-none-any.whl

---
+++ packages/aio-request/aio_request-0.1.20-py3-none-any.whl/aio_request/response_classifier.py
@@ -0,0 +1,35 @@
+import abc
+import enum
+
+from .base import Header, Response
+
+
+class ResponseVerdict(enum.Enum):
+    ACCEPT = 1
+    REJECT = 2
+
+
+class ResponseClassifier(abc.ABC):
+    __slots__ = ()
+
+    @abc.abstractmethod
+    def classify(self, response: Response) -> ResponseVerdict:
+        ...
+
+
+class DefaultResponseClassifier(ResponseClassifier):
+    __slots__ = ("_network_errors_code",)
+
+    def __init__(self, network_errors_code: int = 489):
+        self._network_errors_code = network_errors_code
+
+    def classify(self, response: Response) -> ResponseVerdict:
+        if Header.X_DO_NOT_RETRY in response.headers:
+            return ResponseVerdict.ACCEPT
+        if response.is_server_error():
+            return ResponseVerdict.REJECT
+        if response.status == self._network_errors_code:
+            return ResponseVerdict.REJECT
+        if response.status == 408:
+            return ResponseVerdict.REJECT
+        return ResponseVerdict.ACCEPT
