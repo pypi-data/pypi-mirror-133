f8ec7df04252a31b32e4a46c1866484b3b2bebc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:39 +0000

    Add package pyghthouse/0.3.0.dev0/pyghthouse-0.3.0.dev0.tar.gz

---
+++ packages/pyghthouse/pyghthouse-0.3.0.dev0.tar.gz/pyghthouse-0.3.0.dev0/README.md
@@ -0,0 +1,13 @@
+# Pyghthouse 
+
+A Python Lighthouse Adapter
+
+(README WIP; please consult pyghthouse/ph.py for usage information.)
+
+Example scripts can be found [here](examples)
+
+## Troubeshooting
+
+If you are experiencing errors regarding SSL certificates,
+instantiate Pyghthouse using the `ignore_ssl_cert=True`
+keyword argument.
---
+++ packages/pyghthouse/pyghthouse-0.3.0.dev0.tar.gz/pyghthouse-0.3.0.dev0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyghthouse',
+    version='0.3.0.dev0',
+    packages=find_packages(where='.'),
+    url='https://github.com/Musicted/pyghthouse',
+    license='MIT',
+    author='Gavin Lüdemann',
+    author_email='gavin.luedemann@gmail.com',
+    description='Python Lighthouse adapter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy~=1.21.2', 'websocket-client~=1.2.1', 'msgpack~=1.0.2'],
+    package_dir={"": ".", "utils": "./utils"},
+    python_requires=">=3.8"
+)
