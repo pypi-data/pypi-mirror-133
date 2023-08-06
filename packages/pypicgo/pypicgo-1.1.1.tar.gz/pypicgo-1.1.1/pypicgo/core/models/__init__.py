f221a01641b35d4a83f303914530fe37447904f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:39 +0000

    Add package pypicgo/1.1.1/pypicgo-1.1.1.tar.gz

---
+++ packages/pypicgo/pypicgo-1.1.1.tar.gz/pypicgo-1.1.1/pypicgo/core/models/__init__.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Dict, Optional, List
+
+
+class PluginModel(BaseModel):
+    module: str
+    config: Optional[Dict] = None
+
+
+class UploaderModel(BaseModel):
+    module: str
+    config: Optional[Dict] = None
+
+
+class ConfigModel(BaseModel):
+    uploader: UploaderModel
+    plugins: List[PluginModel]
