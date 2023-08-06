aba538620939ecf80f96ef268ec84d15265a4c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:27 +0000

    Add package types-DateTimeRange/1.2.2/types-DateTimeRange-1.2.2.tar.gz

---
+++ packages/types-DateTimeRange/types-DateTimeRange-1.2.2.tar.gz/types-DateTimeRange-1.2.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-DateTimeRange"
+description = "Typing stubs for DateTimeRange"
+long_description = '''
+## Typing stubs for DateTimeRange
+
+This is a PEP 561 type stub package for the `DateTimeRange` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `DateTimeRange`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/DateTimeRange. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `032e6ee90cbb938259a2aa2183966502de19108e`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.2.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=['types-python-dateutil'],
+      packages=['datetimerange-stubs'],
+      package_data={'datetimerange-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Stubs Only",
+      ]
+)
