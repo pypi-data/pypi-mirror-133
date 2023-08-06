60f6195927ce66b09144b4e448a02ec9b07dd5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:44 +0000

    Add package LogicCircuit/0.0.1/LogicCircuit-0.0.1.tar.gz

---
+++ packages/LogicCircuit/LogicCircuit-0.0.1.tar.gz/LogicCircuit-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='LogicCircuit',
+    version='0.0.1',
+    description='Simple circuit builder only using logic gates',
+    long_description='',
+    url='https://github.com/TINYT1ME/LogicCircuit',
+    author='TINYT1ME',
+)
+
