7589d2a2220ff5ecf8c6f3077384ff0b8bdc2806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package hhs-vertretungsplan-parser/0.0.1/hhs_vertretungsplan_parser-0.0.1-py3-none-any.whl

---
+++ packages/hhs-vertretungsplan-parser/hhs_vertretungsplan_parser-0.0.1-py3-none-any.whl/hhs_vertretungsplan_parser/const.py
@@ -0,0 +1,5 @@
+"""Constants for the Heinrich-Hertz-Schule Vertretungsplan API."""
+
+BASE_URL = "https://www.heinrich-hertz-schule-hamburg.de/"
+LOGIN_URL = BASE_URL + "OnlineVertretungsplan.php"
+LOGIN_TEXT = "angemeldet als"
---
+++ packages/hhs-vertretungsplan-parser/hhs_vertretungsplan_parser-0.0.1-py3-none-any.whl/hhs_vertretungsplan_parser/model/vertretung.py
@@ -0,0 +1,35 @@
+"""Vertretung representation with all properties."""
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass
+class Vertretung:
+    datum: datetime.date = None
+    klasse: str = None
+    stunde: str = None
+    vertreter: str = None
+    fach: str = None
+    raum: str = None
+    text: str = None
+    nach: str = None
+    
+
+    def __lt__(self, other):
+        """Make Vertretung sortable."""
+        if self.klasse.zfill(3) < other.klasse.zfill(3):
+            return True
+        if self.klasse.zfill(3) == other.klasse.zfill(3):
+            if self.datum < other.datum:
+                return True
+            if self.datum == other.datum:
+                if self.stunde < other.stunde:
+                    return True
+        return False
+
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}"
+            f"(Datum={self.datum}, Klasse={self.klasse}, Stunde={self.stunde}, Vertreter={self.vertreter}, Fach={self.fach}, Raum={self.raum})"
+        )
