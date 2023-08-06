8e9800da24b52283a03994b947b72fdc13594afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:30 +0000

    Add package crocodile/5.41/crocodile-5.41.tar.gz

---
+++ packages/crocodile/crocodile-5.41.tar.gz/crocodile-5.41/myresources/crocodile/__init__.py
@@ -0,0 +1,7 @@
+
+__version__ = "5.41"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+release_notes = """
+Added croco ascii art to the setup.
+"""
