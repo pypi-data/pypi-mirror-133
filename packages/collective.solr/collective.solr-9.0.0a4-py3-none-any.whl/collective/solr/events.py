20b3573205d214f9a9ebc850976045239bab897a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:14 +0000

    Add package collective.solr/9.0.0a4/collective.solr-9.0.0a4-py3-none-any.whl

---
+++ packages/collective.solr/collective.solr-9.0.0a4-py3-none-any.whl/collective/solr/events.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from Products.CMFDefault.permissions import ModifyPortalContent
+
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware as CatalogMultiplex
+
+
+def reorderedEvent(event):
+    parent = event.object
+    mtool = getToolByName(parent, "portal_membership")
+    if mtool.checkPermission(ModifyPortalContent, parent):
+        for child in parent.objectValues():
+            if isinstance(child, CatalogMultiplex) or isinstance(
+                child, CMFCatalogAware
+            ):
+                child.reindexObject(["getObjPositionInParent"])
