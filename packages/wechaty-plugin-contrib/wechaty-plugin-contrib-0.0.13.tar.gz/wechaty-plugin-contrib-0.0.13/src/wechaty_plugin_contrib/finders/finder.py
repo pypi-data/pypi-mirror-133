3f40a7490823c10a7e1d82cb712dc29a2efaae92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:52 +0000

    Add package wechaty-plugin-contrib/0.0.13/wechaty-plugin-contrib-0.0.13.tar.gz

---
+++ packages/wechaty-plugin-contrib/wechaty-plugin-contrib-0.0.13.tar.gz/wechaty-plugin-contrib-0.0.13/src/wechaty_plugin_contrib/finders/finder.py
@@ -0,0 +1,33 @@
+"""Base finder for finding Room/Message"""
+from re import Pattern
+from typing import (
+    Any,
+    Callable,
+    Union,
+    List, Coroutine,
+)
+from wechaty_plugin_contrib.config import (
+    Room,
+    Contact,
+    Message
+)
+
+from wechaty import Wechaty
+
+FinderOption = Union[
+    str, Pattern, bool,
+    Callable[[Union[Contact, Room, Message]], List[Union[Room, Contact]]],
+    Callable[[Any], Coroutine[Any, Any, List[Any]]]
+]
+FinderOptions = Union[FinderOption, List[FinderOption]]
+
+
+class Finder:
+    def __init__(self, option: FinderOptions):
+        if isinstance(option, list):
+            self.options = option
+        else:
+            self.options = [option]
+
+    async def match(self, bot: Wechaty) -> List[Any]:
+        raise NotImplementedError
