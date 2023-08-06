b2b4d4802f676e3b74798fb6c48ed8e5dd716d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package better-interactions/2.0.2/better-interactions-2.0.2.tar.gz

---
+++ packages/better-interactions/better-interactions-2.0.2.tar.gz/better-interactions-2.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="better-interactions",
+    version="2.0.2",
+    description="Better interactions for discord-py-interactions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Toricane/better-interactions",
+    author="Toricane",
+    author_email="prjwl028@gmail.com",
+    license="MIT",
+    packages=["interactions.ext.better_interactions"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "discord-py-interactions",
+        "interactions-wait-for",
+    ],
+)
