9dc3a692a6afec31d77f63a9f34bb46b93075eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:21 +0000

    Add package streamlit-force-graph-simulator/0.0.2/streamlit_force_graph_simulator-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-force-graph-simulator/streamlit_force_graph_simulator-0.0.2-py3-none-any.whl/streamlit_force_graph_simulator/frontend/build/precache-manifest.1bd7eb049e380617636eb2a20a5235e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2010063b08a5f37ec6ffb8fea968c0b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd0f9a915d239a5446c3",
+    "url": "./static/js/2.ddfd28cd.chunk.js"
+  },
+  {
+    "revision": "164933573ecce9c5fa8212c1a75cc591",
+    "url": "./static/js/2.ddfd28cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed5d628d60552857663",
+    "url": "./static/js/main.a0c30abb.chunk.js"
+  },
+  {
+    "revision": "c6c613ae59ef7502cd97",
+    "url": "./static/js/runtime-main.3ca778a4.js"
+  },
+  {
+    "revision": "9cfe50b68736f61d061af526da6aaf14",
+    "url": "./static/media/bezier.9cfe50b6.cjs"
+  }
+]);
