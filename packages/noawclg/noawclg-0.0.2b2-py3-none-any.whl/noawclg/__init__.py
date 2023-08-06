853bc72a48ff237995a7c385699afada4aacc5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:24 +0000

    Add package noawclg/0.0.2b2/noawclg-0.0.2b2-py3-none-any.whl

---
+++ packages/noawclg/noawclg-0.0.2b2-py3-none-any.whl/noawclg/__init__.py
@@ -0,0 +1,13 @@
+from .main import get_noaa_data
+
+import json
+
+with open('noawclg/config.json','r') as conf:
+    config = json.loads(conf.read())
+
+__version__ = config['version']
+__author__ = config['author']
+
+
+
+all = [get_noaa_data,__version__,__author__]
