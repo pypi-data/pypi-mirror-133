d5f28a49c15d3ae0419cf4f9e3643ceb6146dc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:46 +0000

    Add package itkaven/1.0.1/itkaven-1.0.1.tar.gz

---
+++ packages/itkaven/itkaven-1.0.1.tar.gz/itkaven-1.0.1/main/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class kaven:
+    def __init__(self):
+        self.test= True
+
+    def print_hi(self, name):
+        print(f'Hi, {name}')
+
+
