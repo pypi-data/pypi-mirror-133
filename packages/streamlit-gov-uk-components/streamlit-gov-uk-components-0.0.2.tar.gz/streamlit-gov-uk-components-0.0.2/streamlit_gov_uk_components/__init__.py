4b685d3d2a8a7434d264a88cf447afcc0c5288fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:45 +0000

    Add package streamlit-gov-uk-components/0.0.2/streamlit-gov-uk-components-0.0.2.tar.gz

---
+++ packages/streamlit-gov-uk-components/streamlit-gov-uk-components-0.0.2.tar.gz/streamlit-gov-uk-components-0.0.2/streamlit_gov_uk_components/__init__.py
@@ -0,0 +1,14 @@
+import os
+import streamlit.components.v1 as components
+
+_DEV = os.environ.get('STREAMLIT_GOV_UK_COMPONENTS_DEV')
+_component_options = {
+    "url": "http://localhost:3001",
+} if _DEV else {
+    "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
+}
+
+_gov_uk_checkbox= components.declare_component("gov_uk_checkbox", **_component_options)
+
+def gov_uk_checkbox(label, id, key=None):
+    return _gov_uk_checkbox(label=label, id=id, key=key, default=0)
