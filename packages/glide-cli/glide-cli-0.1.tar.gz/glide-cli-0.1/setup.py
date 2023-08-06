feff62159003290e1b5e0b6545a4730a0b912812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:59 +0000

    Add package glide-cli/0.1/glide-cli-0.1.tar.gz

---
+++ packages/glide-cli/glide-cli-0.1.tar.gz/glide-cli-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+        name="glide-cli",
+    version='0.1',
+    py_modules=['src.cli'],
+    install_requires=[
+        'Click',
+        'tabulate'
+    ],
+    entry_points='''
+        [console_scripts]
+        glide=src.cli:cli
+    ''',
+)
