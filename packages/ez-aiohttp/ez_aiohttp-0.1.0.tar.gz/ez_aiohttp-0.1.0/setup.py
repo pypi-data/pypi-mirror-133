0586ce8427b3e10ef3241cb41b0cd82e81880bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:10 +0000

    Add package ez-aiohttp/0.1.0/ez_aiohttp-0.1.0.tar.gz

---
+++ packages/ez-aiohttp/ez_aiohttp-0.1.0.tar.gz/ez_aiohttp-0.1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="ez_aiohttp",
+    version="0.1.0",
+    author="Rukchad Wongprayoon",
+    author_email="contact@biomooping.tk",
+    description="Make aiohttp easier",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://ez-aiohttp.rukchadisa.live/en/latest/",
+    packages=["ez_rq"],
+    package_dir={"ez_rq": "src/ez_rq"},
+    install_requires=open("requirements.txt").readlines(),
+    classifiers=[],
+    
+)
