2cfe5a29f64a8adb7dc0b4ec2aeef7672b7bca80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:15 +0000

    Add package pyRVtest/0.1.2/pyRVtest-0.1.2.tar.gz

---
+++ packages/pyRVtest/pyRVtest-0.1.2.tar.gz/pyRVtest-0.1.2/pyRVtest/results/results.py
@@ -0,0 +1,38 @@
+"""Economy-level structuring of abstract BLP problem results."""
+
+import abc
+from typing import Any, Callable, Mapping, Optional, Sequence, Tuple, TYPE_CHECKING
+
+import numpy as np
+
+from ..utilities.basics import Array, StringRepresentation, output
+
+
+# only import objects that create import cycles when checking types
+if TYPE_CHECKING:
+    from ..economies.problem import ProblemEconomy  # noqa
+
+
+class Results(abc.ABC, StringRepresentation):
+    """Abstract results of a solved BLP problem."""
+
+    problem: 'ProblemEconomy'
+
+    def __init__(
+            self, problem: 'ProblemEconomy'
+            ) -> None:
+        """Store the underlying problem and parameter information."""
+        self.problem = problem
+
+
+    def _select_market_ids(self, market_id: Optional[Any] = None) -> Array:
+        """Select either a single market ID or all unique IDs."""
+        if market_id is None:
+            return self.problem.unique_market_ids
+        if market_id in self.problem.unique_market_ids:
+            return np.array(market_id, np.object)
+        raise ValueError(f"market_id must be None or one of {list(sorted(self.problem.unique_market_ids))}.")
+
+    @abc.abstractmethod
+    def _coerce_matrices(self, matrices: Any, market_ids: Array) -> Array:
+        """Coerce array-like stacked arrays into a stacked array and validate it."""
---
+++ packages/pyRVtest/pyRVtest-0.1.2.tar.gz/pyRVtest-0.1.2/setup.py
@@ -0,0 +1,26 @@
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+read = lambda p: Path(Path(__file__).resolve().parent / p).read_text()
+
+
+
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='pyRVtest',
+    url='https://github.com/chrissullivanecon/pyRVtest',
+    author='Marco Duarte, Lorenzo Magnolfi, Mikkel Solvsten, and Christopher Sullivan',
+    author_email='chris.sullivan.econ@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=read('requirements.txt').splitlines(),
+    license='MIT',
+    description='Code to perform econometric test of firm conduct',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    version='0.1.2'
+)
