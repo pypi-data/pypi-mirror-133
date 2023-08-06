44bf967e396ca3796506641db21942780013c5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:30 +0000

    Add package captur-ml-sdk/0.1.29/captur-ml-sdk-0.1.29.tar.gz

---
+++ packages/captur-ml-sdk/captur-ml-sdk-0.1.29.tar.gz/captur-ml-sdk-0.1.29/captur_ml_sdk/dtypes/interfaces/MLPredict/OfflinePostRunEventData.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class OfflinePostRunEventData(BaseModel):
+    gcs_destination: str
+    model_id: str
+    request_id: str
+    start_time: Optional[float] = .0
+    end_time: Optional[float] = .0
+    duration: Optional[float] = .0
+    labels_source: Optional[str]
+    webhooks: str
