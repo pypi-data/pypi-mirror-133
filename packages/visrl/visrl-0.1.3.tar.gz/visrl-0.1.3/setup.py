82ca022b84e11fd13020dab8ee8e2c4a665712c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:53 +0000

    Add package visrl/0.1.3/visrl-0.1.3.tar.gz

---
+++ packages/visrl/visrl-0.1.3.tar.gz/visrl-0.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='visrl',
+    version='0.1.3',
+    description="A simple wrapper to analyse and visualise reinforcement learning agents' behaviour in the environment.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jetnew/rl-debugger',
+    author='Jet New',
+    author_email='notesjet@gmail.com',
+    license='MIT',
+    packages=['visrl'],
+    install_requires=['pysimplegui',
+                      'numpy',
+                      'opencv-python',
+                      'imageio',
+                      'gym',
+                      'stable-baselines3',
+                      'pyglet',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
