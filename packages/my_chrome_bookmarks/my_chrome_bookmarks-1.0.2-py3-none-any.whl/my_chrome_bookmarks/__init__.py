120fb1e140aecc776b470172e7b4bef674c899f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:02 +0000

    Add package my_chrome_bookmarks/1.0.2/my_chrome_bookmarks-1.0.2-py3-none-any.whl

---
+++ packages/my_chrome_bookmarks/my_chrome_bookmarks-1.0.2-py3-none-any.whl/my_chrome_bookmarks/__init__.py
@@ -0,0 +1,18 @@
+"""Chrome bookmarks utils."""
+
+from my_chrome_bookmarks.bookmarks import bookmarks
+from my_chrome_bookmarks.bookmarks import bookmark_bar
+from my_chrome_bookmarks.bookmarks import BookmarkUrl
+from my_chrome_bookmarks.bookmarks import BookmarkFolder
+from my_chrome_bookmarks.bookmarks import get_bookmarks_path
+
+__version__ = "1.0.2"
+
+__all__ = [
+    "__version__",
+    "bookmarks",
+    "bookmark_bar",
+    "BookmarkUrl",
+    "BookmarkFolder",
+    "get_bookmarks_path",
+]
