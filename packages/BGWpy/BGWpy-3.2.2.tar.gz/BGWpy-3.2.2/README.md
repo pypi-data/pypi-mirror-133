c6653ed40f8f804fb1111714a8fd75a381d63d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package BGWpy/3.2.2/BGWpy-3.2.2.tar.gz

---
+++ packages/BGWpy/BGWpy-3.2.2.tar.gz/BGWpy-3.2.2/README.md
@@ -0,0 +1,60 @@
+
+<a href="url"><img src="https://github.com/BerkeleyGW/BGWpy/blob/master/dev/logo/BGWpy.png" height="300" ></a><br clear="all" />
+
+A python module to operate BerkeleyGW
+
+
+Documentation
+-------------
+
+We recommend learning the basic usage from the examples provided
+with the source distribution in ~BGWpy/Examples/. The docstrings
+of the various objects also contain information and can be accessed
+from a python interpreter with
+
+    help(BGWpy.<object>)
+
+
+or from ipython with
+
+    In  [1]: BGWpy.<object>?
+
+
+Requirements
+------------
+
+The following software and modules are required to use BGWpy.
+
+  * python 3 and python 2 compatible
+  * numpy 1.6+      (http://www.scipy.org/)
+  * pymatgen 4.0+   (http://pymatgen.org/)
+  * BerkeleyGW 1.2+ (http://www.berkeleygw.org/)
+
+Note that the binary executables of BerkeleyGW must be found
+in your PATH environment variable.
+
+
+Installing
+----------
+
+Automatic installation:
+
+    python -m pip install BGWpy
+
+Manual installation: once you have satisfied the requirements, install the package with
+
+    python setup.py install
+
+Whether you install automatically of manually,
+you should run the script `BGWpy_make_config_file.py`
+to generate a runtime configuration file `~/.BGWpyrc`
+which you can then modify. Most options in this file
+represent default values, and can be overridden when using the module.
+
+
+License
+-------
+
+This software is free to use under the BSD license.
+See license.txt for more information.
+
---
+++ packages/BGWpy/BGWpy-3.2.2.tar.gz/BGWpy-3.2.2/setup.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+import sys
+import os
+from glob import glob
+import shutil
+
+try:
+    import setuptools
+except ImportError:
+    sys.stderr.write(
+    "Please install setuptools before running this script. Exiting.")
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+
+
+# --------------------------------------------------------------------------- #
+# Basic project information
+# --------------------------------------------------------------------------- #
+
+name = 'BGWpy'
+description = 'Interface BerkeleyGW flows in python.'
+license = 'BSD'
+url = 'https://github.com/BerkeleyGW/BGWpy'
+__version__ = '3.2.2'
+
+# author and author_email should be a single string, not a list, but we can put
+# multiple authors / emails by separating them by commas inside the string.
+# If you contributed to this package, add your name and email. Don't be shy!
+author = 'Gabriel Antonius'
+author_email = 'gabriel.antonius@gmail.com'
+
+
+# Requirements
+install_requires = [
+    'numpy >=1.6',
+    'pymatgen >=2021',
+    ]
+
+# --------------------------------------------------------------------------- #
+# Helper functions
+# --------------------------------------------------------------------------- #
+
+def find_package_data():
+    package_data={'BGWpy': ['data/structures/*', 'data/pseudos/*']}
+    return package_data
+
+def find_data_files():
+    return [('config', ['config/BGWpyrc'])]
+
+def find_scripts():
+    scripts = []
+    for fname in glob(os.path.join('BGWpy', 'scripts', "*.py")):
+        if os.path.basename(fname) != '__init__.py':
+            scripts.append(fname)
+    return scripts
+
+def cleanup():
+    print('Cleaning up')
+    shutil.rmtree('BGWpy.egg-info', ignore_errors=True)
+    shutil.rmtree('build', ignore_errors=True)
+    shutil.rmtree('__pycache__', ignore_errors=True)
+
+# --------------------------------------------------------------------------- #
+# Setup
+# --------------------------------------------------------------------------- #
+
+setup_args = dict(
+      name              = name,
+      version           = __version__,
+      description       = description,
+      author            = author,
+      author_email      = author_email,
+      license           = license,
+      url               = url,
+      install_requires  = install_requires,
+      packages          = find_packages(),
+      package_data      = find_package_data(),
+      data_files        = find_data_files(),
+      scripts           = find_scripts(), 
+      )
+
+if __name__ == "__main__":
+    setup(**setup_args)
+
