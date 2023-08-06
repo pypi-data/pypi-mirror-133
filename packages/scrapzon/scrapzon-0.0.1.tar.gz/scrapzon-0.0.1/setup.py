9ec627ebf21c0d4ead0c994617b9ff918cf8b49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:15 +0000

    Add package scrapzon/0.0.1/scrapzon-0.0.1.tar.gz

---
+++ packages/scrapzon/scrapzon-0.0.1.tar.gz/scrapzon-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="scrapzon",
+    version="0.0.1",
+    author="sachin Sankar",
+    author_email="sachinpannadi@gmail.com",
+    python_requires='>=3.6, <4',
+    install_requires=['requests','lxml'],
+)
