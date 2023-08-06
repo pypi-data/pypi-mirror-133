5f10ce30771bba56ff835e1b21287b0fd7782432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:24 +0000

    Add package python-dataschema/0.1.4/python-dataschema-0.1.4.tar.gz

---
+++ packages/python-dataschema/python-dataschema-0.1.4.tar.gz/python-dataschema-0.1.4/dataschema/__init__.py
@@ -0,0 +1,29 @@
+from .base import Spec
+from .specs import (
+    Type,
+    CType,
+    EnumSpec,
+    TypeSpec,
+    IterSpec,
+    SeqSpec,
+    DictSpec,
+    ConditionalDictSpec,
+    Post,
+    Update,
+    Test,
+)
+
+__all__ = (
+    'Spec',
+    'Type',
+    'CType',
+    'EnumSpec',
+    'TypeSpec',
+    'IterSpec',
+    'SeqSpec',
+    'DictSpec',
+    'ConditionalDictSpec',
+    'Post',
+    'Update',
+    'Test',
+)
