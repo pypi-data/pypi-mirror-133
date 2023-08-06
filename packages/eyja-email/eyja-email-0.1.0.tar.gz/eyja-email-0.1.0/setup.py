276b6cd3a6cd6c318c48cf9005a4891dc8852660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:22 +0000

    Add package eyja-email/0.1.0/eyja-email-0.1.0.tar.gz

---
+++ packages/eyja-email/eyja-email-0.1.0.tar.gz/eyja-email-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='eyja-email',
+    zip_safe=True,
+    version='0.1.0',
+    description='Email helper for Eyja',
+    url='https://gitlab.com/public.eyja.dev/eyja-email',
+    maintainer='Anton Berdnikov',
+    maintainer_email='agratoth@yandex.ru',
+    packages=find_packages(),
+    package_dir={'eyja_email': 'eyja_email'},
+    install_requires=[
+        'eyja-internal>==0.3.8',
+        'aiosmtplib>==1.1.6',
+        'jinja2>==3.0.3',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.8',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
