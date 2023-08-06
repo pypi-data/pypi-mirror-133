43d959de01e5ffa60e24abe86f00f7bb7cee1cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:01 +0000

    Add package nuwa-pytorch/0.0.5/nuwa-pytorch-0.0.5.tar.gz

---
+++ packages/nuwa-pytorch/nuwa-pytorch-0.0.5.tar.gz/nuwa-pytorch-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'nuwa-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.5',
+  license='MIT',
+  description = 'NÜWA - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/nuwa-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers'
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.6',
+    'torchvision',
+    'unfoldNd',
+    'vector-quantize-pytorch>=0.4.10'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
