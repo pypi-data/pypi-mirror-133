4fd26eded37623628cb639af99a1989031969cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package dawgz/0.1.2/dawgz-0.1.2.tar.gz

---
+++ packages/dawgz/dawgz-0.1.2.tar.gz/dawgz-0.1.2/README.md
@@ -0,0 +1,43 @@
+# Directed Acyclic Workflow Graph Scheduling
+
+```python
+import time
+
+from dawgz import job, after, waitfor, ensure, schedule
+
+@job(name='A')
+def a():
+    print('a')
+    time.sleep(3)
+    print('a')
+    raise Exception()
+
+@job(name='B')
+def b():
+    time.sleep(1)
+    print('b')
+    time.sleep(1)
+    print('b')
+
+finished = [True] * 100
+finished[42] = False
+
+@after(a, b)
+@waitfor('any')
+@ensure(lambda i: finished[i])
+@job(name='C', array=100)
+def c(i: int):
+    print(f'c{i}')
+    finished[i] = True
+
+@after(a, status='any')
+@after(b, c, status='success')
+@waitfor('all')
+@job(name='D')
+def d():
+    print('d')
+    time.sleep(1)
+    print('d')
+
+schedule(d, backend=None)  # prints a b b c420 a d d
+```
---
+++ packages/dawgz/dawgz-0.1.2.tar.gz/dawgz-0.1.2/dawgz/__init__.py
@@ -0,0 +1,41 @@
+r"""Directed Acyclic Workflow Graph Scheduling"""
+
+__version__ = '0.1.2'
+
+
+from functools import partial
+from typing import Callable, Union
+
+from .workflow import Job
+from .schedulers import schedule
+
+
+def job(f: Callable = None, /, **kwargs) -> Union[Callable, Job]:
+    if f is None:
+        return partial(job, **kwargs)
+    else:
+        return Job(f, **kwargs)
+
+
+def after(*deps, status: str = 'success') -> Callable:
+    def decorator(self: Job) -> Job:
+        self.after(*deps, status=status)
+        return self
+
+    return decorator
+
+
+def waitfor(mode: str) -> Callable:
+    def decorator(self: Job) -> Job:
+        self.waitfor = mode
+        return self
+
+    return decorator
+
+
+def ensure(condition: Callable) -> Callable:
+    def decorator(self: Job) -> Job:
+        self.ensure(condition)
+        return self
+
+    return decorator
