3be2f063d2155911c65f3bce6c44a875fa272ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:49 +0000

    Add package infima-client/0.283.0/infima_client-0.283.0.tar.gz

---
+++ packages/infima-client/infima_client-0.283.0.tar.gz/infima_client-0.283.0/src/infima_client/extras/predictions.py
@@ -0,0 +1,44 @@
+import datetime
+from typing import TYPE_CHECKING, List, Optional, Union, cast
+
+import pandas as pd
+
+from infima_client.core.types import Unset
+
+if TYPE_CHECKING:
+    from infima_client.client import InfimaClient
+
+from .utils import (
+    ResponseMapping,
+    frame_chunker,
+    handle_date,
+    response_mapping_to_frame,
+)
+
+
+@frame_chunker("symbols")
+def get_predictions(
+    *,
+    client: "InfimaClient",
+    symbols: List[str],
+    as_of: Optional[datetime.date] = None,
+    col: Optional[str] = "cpr",
+    wide: bool = True,
+) -> Optional[pd.DataFrame]:
+    resp = client.api.prediction_v1.get(
+        symbols=symbols,
+        as_of=handle_date(as_of) if as_of is not None else None,
+    )
+
+    mapping = cast(Union[ResponseMapping, Unset], resp.predictions)
+    if isinstance(mapping, Unset):
+        return None
+    else:
+        record_path = ["values"]
+        meta = ["asOf", "symbol"]
+        index_cols = ["as_of", "symbol", "factor_date"]
+        wide_on = "factor_date" if wide else None
+
+        return response_mapping_to_frame(
+            mapping, record_path, meta, index_cols, col=col, wide_on=wide_on
+        )
