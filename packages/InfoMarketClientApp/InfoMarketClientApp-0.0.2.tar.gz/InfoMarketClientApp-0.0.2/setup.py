597524bafb7c8f06fd8eb44fb71b59a6bd763ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:13 +0000

    Add package InfoMarketClientApp/0.0.2/InfoMarketClientApp-0.0.2.tar.gz

---
+++ packages/InfoMarketClientApp/InfoMarketClientApp-0.0.2.tar.gz/InfoMarketClientApp-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='InfoMarketClientApp',
+    version='0.0.2',
+    description='Information Market client application',
+    long_description='Information Market client application to request data from the Information Market servers',
+    url='',
+    author='Ricardo Martins',
+    author_email='ricas.martins767@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='InformationMarket',
+    packages=find_packages(),
+    py_modules=["IMClient"]
+)
