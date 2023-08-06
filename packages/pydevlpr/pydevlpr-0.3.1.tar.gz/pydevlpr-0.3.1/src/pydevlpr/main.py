0585689a8ba3c07e935be3c65c8a08c8a006aa41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:27 +0000

    Add package pydevlpr/0.3.1/pydevlpr-0.3.1.tar.gz

---
+++ packages/pydevlpr/pydevlpr-0.3.1.tar.gz/pydevlpr-0.3.1/src/pydevlpr/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from .DevlprServer import DevlprServer
+from .DevlprClient import DevlprClient
+from .typing import Callback
+
+devlpr_client: DevlprClient = DevlprClient()
+devlpr_server: DevlprServer = DevlprServer('DEVLPR')
+
+## API ##
+def start() -> None:
+    """Manually start the front and backends. This leads to more responsive performance when adding callbacks"""
+    
+    devlpr_server.start_if_needed()
+    devlpr_client.start_if_needed()
+
+def stop() -> None:
+    """Disconnects from the backend and end all communication"""
+
+    devlpr_client.stop()
+    devlpr_server.stop()
+
+def add_callback(topic: str, pin: int, fn: Callback, ws = None) -> None:
+    """Adds a callback for a data stream from a topic and pin, also connects to the backend if it is currently disconnected."""
+    
+    devlpr_server.start_if_needed()
+    devlpr_client.start_if_needed()
+    devlpr_client.add_callback(topic, pin, fn, ws)
+
+def remove_callback(topic: str, pin: int, fn: Callback) -> None:
+    """Removes the first instance of a function from the callback list for a topic and pin."""
+
+    devlpr_client.remove_callback(topic, pin, fn)
