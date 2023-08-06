5108e77b2a4d3bc47f31b3136011e847aca2897a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:16 +0000

    Add package artific/0.0.12/artific-0.0.12-py2.py3-none-any.whl

---
+++ packages/artific/artific-0.0.12-py2.py3-none-any.whl/artific/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for artific."""
+
+__author__ = """Nipun Waas"""
+__email__ = 'waasnipun@gmail.com'
+__version__ = '0.0.11'
+
+from artific.artific import BubbleSort as BubbleSort
+from artific.artific import InsertionSort as InsertionSort
+from artific.artific import HeapSort as HeapSort
+from artific.artific import BinarySearch as BinarySearch
+from artific.artific import LinearSearch as LinearSearch
