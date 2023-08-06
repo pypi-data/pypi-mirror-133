33428953100e1144774a677f35168bf204c18e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:32 +0000

    Add package pytest-helm-charts/0.7.0b1/pytest_helm_charts-0.7.0b1-py3-none-any.whl

---
+++ packages/pytest-helm-charts/pytest_helm_charts-0.7.0b1-py3-none-any.whl/pytest_helm_charts/plugin.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+from _pytest.config.argparsing import Parser
+
+from pytest_helm_charts.api.fixtures import namespace_factory, random_namespace  # noqa: F401
+from pytest_helm_charts.fixtures import _existing_cluster_factory  # noqa: F401
+from pytest_helm_charts.fixtures import chart_extra_info  # noqa: F401
+from pytest_helm_charts.fixtures import chart_path  # noqa: F401
+from pytest_helm_charts.fixtures import chart_version  # noqa: F401
+from pytest_helm_charts.fixtures import cluster_type  # noqa: F401
+from pytest_helm_charts.fixtures import kube_cluster  # noqa: F401
+from pytest_helm_charts.fixtures import kube_config  # noqa: F401
+from pytest_helm_charts.fixtures import values_file_path  # noqa: F401
+from pytest_helm_charts.giantswarm_app_platform.apps.http_testing import gatling_app_factory  # noqa: F401
+from pytest_helm_charts.giantswarm_app_platform.apps.http_testing import stormforger_load_app_factory  # noqa: F401
+from pytest_helm_charts.giantswarm_app_platform.fixtures import app_catalog_factory  # noqa: F401
+from pytest_helm_charts.giantswarm_app_platform.fixtures import catalog_factory  # noqa: F401
+from pytest_helm_charts.giantswarm_app_platform.fixtures import app_factory  # noqa: F401
+
+
+def pytest_addoption(parser: Parser) -> None:
+    group = parser.getgroup("helm-charts")
+    group.addoption("--cluster-type", action="store", help="Pass information about cluster type being used for tests.")
+    group.addoption(
+        "--kube-config",
+        action="store",
+        default=os.path.join(str(Path.home()), ".kube", "config"),
+        help="The path to 'kube.config' file. Used when '--cluster-type existing' is used as well.",
+    )
+    group.addoption("--values-file", action="store", help="Path to the values file used for testing the chart.")
+    group.addoption("--chart-path", action="store", help="The path to a helm chart under test.")
+    group.addoption("--chart-version", action="store", help="Override chart version for the chart under test.")
+    group.addoption(
+        "--chart-extra-info",
+        action="store",
+        default="",
+        help="Pass any additional info about the chart in the 'key1=val1,key2=val2' format",
+    )
+    group.addoption(
+        "--test-extra-info",
+        action="store",
+        default="",
+        help="Pass any additional info about the test in the 'key1=val1,key2=val2' format",
+    )
