e6c09e93956ec59d0729397895006c1aca3ba170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:48 +0000

    Add package vital/1.1.4/vital-1.1.4-py3-none-any.whl

---
+++ packages/vital/vital-1.1.4-py3-none-any.whl/vital/api/activity.py
@@ -0,0 +1,31 @@
+from typing import List, Mapping, Optional
+
+from vital.api.api import API
+
+
+class Activity(API):
+    """Endpoints for getting activity data."""
+
+    def get(
+        self,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        Get Activity API
+        :param str user_key: users key
+        :param str start_date: date in ISO format
+        :param str end_date: date in ISO format
+        :param Optional[str] provider: Provider of data strava etc.
+        """
+        return self.client.get(
+            f"/summary/activity/{user_key}",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+            api_version="v2",
+        )
