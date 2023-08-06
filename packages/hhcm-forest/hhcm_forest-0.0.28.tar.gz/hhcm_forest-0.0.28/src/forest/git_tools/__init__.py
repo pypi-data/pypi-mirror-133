a11a9478d1803dffba0b9b267d175fcd7bbe7ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:23 +0000

    Add package hhcm-forest/0.0.28/hhcm_forest-0.0.28.tar.gz

---
+++ packages/hhcm-forest/hhcm_forest-0.0.28.tar.gz/hhcm_forest-0.0.28/src/forest/git_tools/__init__.py
@@ -0,0 +1,44 @@
+from forest.common import proc_utils
+import shutil
+
+class GitTools:
+
+    def __init__(self, srcdir) -> None:
+        self.srcdir = srcdir
+
+    def clone(self, 
+            server: str, 
+            repository: str, 
+            proto='ssh', 
+            recursive=False):
+        if proto == 'ssh':
+            addr = f'git@{server}:{repository}'
+        elif proto == 'https':
+            addr = f'https://{server}/{repository}'
+        else:
+            # TODO more specific exception
+            raise ValueError(f'unsupported protocol "{proto}"')
+        
+        cmd = ['git', 'clone', addr, self.srcdir]
+        if recursive:
+            cmd.insert(2, '--recursive')
+
+        # clone, and delete the source folder on failure
+        # (either exception or git returns != 0) 
+        try:
+             clone_ok = proc_utils.call_process(args=cmd)
+             if not clone_ok:
+                 self.rm()
+        except BaseException as e:
+            # remove src and re-raise exception
+            self.rm()
+            raise e
+
+        return True
+
+    def checkout(self, tag):
+        return proc_utils.call_process(['git', 'checkout', tag], cwd=self.srcdir)
+
+    def rm(self):
+        shutil.rmtree(self.srcdir,  ignore_errors=True)
+
