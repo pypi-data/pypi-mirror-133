bdfdaa996cbcb4370220130b89e4ae3b8ec882e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:39 +0000

    Add package DSND-2022-Distributions/0.1/DSND%202022%20Distributions-0.1.tar.gz

---
+++ packages/DSND-2022-Distributions/DSND%202022%20Distributions-0.1.tar.gz/DSND 2022 Distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DSND 2022 Distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['distributions'],
+      author='Fahad Albgumi',
+      auther_email='albgumifahd@gmail.com',
+      zip_safe=False)
