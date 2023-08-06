e4ab19d84698eee5e6452e9236d94f1dd0f1f218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:47 +0000

    Add package calculator-lzh/3.0.0/calculator_lzh-3.0.0.tar.gz

---
+++ packages/calculator-lzh/calculator_lzh-3.0.0.tar.gz/calculator_lzh-3.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+    
+setup(
+    name = 'calculator_lzh',
+    # ext_modules = cythonize("calculator.py"),
+    packages=["calculator"],
+    python_requires=">=3",
+    version="3.0.0",
+    entry_points={
+        'console_scripts': ['calculator=calculator:run','ctor=calculator:run']
+    },
+)
