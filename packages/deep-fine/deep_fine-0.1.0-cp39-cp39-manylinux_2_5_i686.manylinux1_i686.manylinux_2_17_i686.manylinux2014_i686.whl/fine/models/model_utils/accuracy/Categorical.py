c2dd6872c506957a3713177151c712b029c38317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:15 +0000

    Add package deep-fine/0.1.0/deep_fine-0.1.0-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/deep-fine/deep_fine-0.1.0-cp39-cp39-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/fine/models/model_utils/accuracy/Categorical.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from fine.models.model_utils.accuracy.Accuracy import Accuracy
+
+
+class Categorical(Accuracy):
+    def __init__(self, *, binary=False):
+        self.binary = binary
+
+    def init(self, y):
+        pass
+
+    def compare(self, predictions, y):
+        if not self.binary and len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+        return predictions == y
