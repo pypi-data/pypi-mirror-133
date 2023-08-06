d4704645893aef04e3ddf7afd703cb56bd6ac23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:18 +0000

    Add package pytoil/0.18.0/pytoil-0.18.0.tar.gz

---
+++ packages/pytoil/pytoil-0.18.0.tar.gz/pytoil-0.18.0/pytoil/starters/rust.py
@@ -0,0 +1,61 @@
+"""
+The rust starter template.
+
+
+Author: Tom Fleet
+Created: 29/12/2021
+"""
+
+from __future__ import annotations
+
+import asyncio
+import shutil
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+
+import aiofiles
+import aiofiles.os
+
+from pytoil.exceptions import CargoNotInstalledError
+
+
+@dataclass
+class RustStarter:
+    path: Path
+    name: str
+    cargo: str | None = shutil.which("cargo")
+
+    def __post_init__(self) -> None:
+        self.root = self.path.joinpath(self.name).resolve()
+        self.files: list[Path] = [
+            self.root.joinpath(filename) for filename in ["README.md"]
+        ]
+
+    async def generate(self, username: str | None = None) -> None:
+        """
+        Generate a new rust/cargo starter template.
+        """
+        if not self.cargo:
+            raise CargoNotInstalledError
+
+        await aiofiles.os.mkdir(self.root)
+
+        # Call cargo init
+        proc = await asyncio.create_subprocess_exec(
+            self.cargo,
+            "init",
+            cwd=self.root,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
+
+        await proc.wait()
+
+        # Create the README
+        for file in self.files:
+            file.touch()
+
+        readme = self.root.joinpath("README.md")
+        async with aiofiles.open(readme, mode="w", encoding="utf-8") as f:
+            await f.write(f"# {self.name}\n")
