407f3fc4caa1ab7094b9d166a058db24a080b15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:52 +0000

    Add package fikkie/0.3.4/fikkie-0.3.4.tar.gz

---
+++ packages/fikkie/fikkie-0.3.4.tar.gz/fikkie-0.3.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="fikkie",
+    version="0.3.4",
+    description="Lightweight monitoring tool",
+    license="MIT",
+    long_description=long_description,
+    author="Joris Hartog (@nootr)",
+    long_description_content_type="text/markdown",
+    url="https://nootr.github.io/fikkie/",
+    packages=["fikkie", "fikkie.notifiers"],
+    scripts=["scripts/fikkie"],
+    install_requires=[
+        "celery==5.2.2",
+        "PyYAML==6.0",
+        "tinydb==4.5.2",
+        "typing_extensions==4.0.1",
+    ],
+)
